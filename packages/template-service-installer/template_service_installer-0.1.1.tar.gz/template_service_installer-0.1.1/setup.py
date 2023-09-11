9891832e033bd7ded49662ee2f25fc06c5192027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:33 +0000

    Add package template-service-installer/0.1.1/template_service_installer-0.1.1.tar.gz

---
+++ packages/template-service-installer/template_service_installer-0.1.1.tar.gz/template_service_installer-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("readme.md") as file:
+    read_me_description = file.read()
+
+setup(
+    name='template_service_installer',
+    version='0.1.1',
+    description='Library for automating the installation of systemd services',
+    long_description=read_me_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Fox-sys/service_installer",
+    packages=['service_installer'],
+    author_email='berestovborisasz@gmail.com',
+    zip_safe=False
+)
