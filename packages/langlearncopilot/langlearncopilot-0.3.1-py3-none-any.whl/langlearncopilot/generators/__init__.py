e72ba3abd897f13940e6d559bd7dbace9b3fa2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 15:05:08 +0000

    Add package langlearncopilot/0.3.1/langlearncopilot-0.3.1-py3-none-any.whl

---
+++ packages/langlearncopilot/langlearncopilot-0.3.1-py3-none-any.whl/langlearncopilot/generators/__init__.py
@@ -0,0 +1,11 @@
+from .main import (
+    generate_phrase_for_multiple_words,
+    generate_phrases,
+    generate_unique_words,
+)
+
+__all__ = [
+    "generate_phrases",
+    "generate_unique_words",
+    "generate_phrase_for_multiple_words",
+]
---
+++ packages/langlearncopilot/langlearncopilot-0.3.1-py3-none-any.whl/langlearncopilot/llm_calls/__init__.py
@@ -0,0 +1,4 @@
+from .openai_call import call_openai
+from .set_credentials import set_openai_key
+
+__all__ = ["call_openai", "set_openai_key"]
