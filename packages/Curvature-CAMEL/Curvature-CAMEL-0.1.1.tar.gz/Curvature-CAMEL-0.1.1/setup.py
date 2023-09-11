80d2bf5e3c572614b254510dfb0f1b3a8b952d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:42 +0000

    Add package Curvature-CAMEL/0.1.1/Curvature-CAMEL-0.1.1.tar.gz

---
+++ packages/Curvature-CAMEL/Curvature-CAMEL-0.1.1.tar.gz/Curvature-CAMEL-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the README file's content
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='Curvature-CAMEL',
+    version='0.1.1',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A brief description of CAMEL',
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    url='https://github.com/yourusername/camel',  
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'pynndescent',
+        'numba'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  
+)
+
