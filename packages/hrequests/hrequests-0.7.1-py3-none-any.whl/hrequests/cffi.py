5aa5f9bee820a9e536af5d52d313a8b4a1c7a834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:24 +0000

    Add package hrequests/0.7.1/hrequests-0.7.1-py3-none-any.whl

---
+++ packages/hrequests/hrequests-0.7.1-py3-none-any.whl/hrequests/cffi.py
@@ -0,0 +1,107 @@
+import ctypes
+import os
+from platform import machine
+from sys import platform
+from typing import Tuple
+
+import rich.progress
+from httpx import get, stream
+from orjson import loads
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+
+# map machine architecture to tls-client binary name
+arch_map = {
+    'amd64': 'amd64',
+    'x86_64': 'amd64',
+    'x86': '386',
+    'i686': '386',
+    'i386': '386',
+    'arm64': 'arm64',
+    'aarch64': 'arm64',
+    'armv5l': 'arm-5',
+    'armv6l': 'arm-6',
+    'armv7l': 'arm-7',
+    'ppc64le': 'ppc64le',
+    'riscv64': 'riscv64',
+    's390x': 's390x',
+}
+
+
+class LibraryManager:
+    def __init__(self):
+        self.parent_path = os.path.join(root_dir, 'bin')
+        self.file_cont, self.file_ext = self.get_name()
+        filename = self.check_library()
+        self.full_path = os.path.join(self.parent_path, filename)
+
+    @staticmethod
+    def get_name() -> Tuple[str, str]:
+        try:
+            arch = arch_map[machine().lower()]
+        except KeyError as e:
+            raise OSError('Your machine architecture is not supported.') from e
+        if platform == 'darwin':
+            return f'darwin-{arch}', '.dylib'
+        elif platform in ('win32', 'cygwin'):
+            return f'windows-{arch}', '.dll'
+        return f'linux-{arch}', '.so'
+
+    def check_library(self):
+        for file in os.listdir(self.parent_path):
+            if file.endswith(self.file_ext):
+                return file
+        self.download_library()
+        return self.check_library()
+
+    def download_library(self):
+        print('Downloading tls-client library from bogdanfinn/tls-client...')
+        # pull release assets from github bogdanfinn/tls-client v1.6.0
+        resp = get('https://api.github.com/repos/bogdanfinn/tls-client/releases/119748617/assets')
+        assets = loads(resp.content)
+        for asset in assets:
+            if self.file_cont in asset['name'] and asset['name'].endswith(self.file_ext):
+                url: str = asset['browser_download_url']
+                name: str = asset['name']
+                break
+        else:
+            raise IOError('Could not find a matching tls-client binary for your system.')
+        with open(os.path.join(self.parent_path, name), 'wb') as fstream:
+            self.download_file(fstream, url)
+
+    @staticmethod
+    def download_file(fstream, url):
+        # file downloader with progress bar
+        total: int
+        with stream('GET', url, follow_redirects=True) as resp:
+            total = int(resp.headers['Content-Length'])
+            with rich.progress.Progress(
+                "[progress.percentage]{task.percentage:>3.0f}%",
+                rich.progress.BarColumn(bar_width=40),
+                rich.progress.DownloadColumn(),
+                rich.progress.TransferSpeedColumn(),
+            ) as progress:
+                download_task = progress.add_task("Download", total=total)
+                for chunk in resp.iter_bytes():
+                    fstream.write(chunk)
+                    progress.update(download_task, completed=resp.num_bytes_downloaded)
+
+
+libman = LibraryManager()
+
+library = ctypes.cdll.LoadLibrary(libman.full_path)
+del libman
+
+# extract the exposed request function from the shared package
+request = library.request
+request.argtypes = [ctypes.c_char_p]
+request.restype = ctypes.c_char_p
+
+
+freeMemory = library.freeMemory
+freeMemory.argtypes = [ctypes.c_char_p]
+freeMemory.restype = ctypes.c_char_p
+
+
+destroyAll = library.destroyAll
+destroyAll.restype = ctypes.c_char_p
