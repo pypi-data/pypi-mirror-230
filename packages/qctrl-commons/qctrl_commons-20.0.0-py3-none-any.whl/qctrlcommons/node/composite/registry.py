e25a1db113b999891d9a92a347ba33cd752ac7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:34 +0000

    Add package qctrl-commons/20.0.0/qctrl_commons-20.0.0-py3-none-any.whl

---
+++ packages/qctrl-commons/qctrl_commons-20.0.0-py3-none-any.whl/qctrlcommons/node/composite/registry.py
@@ -0,0 +1,73 @@
+# Copyright 2023 Q-CTRL. All rights reserved.
+#
+# Licensed under the Q-CTRL Terms of service (the "License"). Unauthorized
+# copying or use of this file, via any medium, is strictly prohibited.
+# Proprietary and confidential. You may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#    https://q-ctrl.com/terms
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS. See the
+# License for the specific language.
+"""Module for NodeRegistry."""
+import inspect
+from functools import partial
+
+import qctrlcommons.node.composite.filter_and_resample
+import qctrlcommons.node.composite.optimizable_signals
+import qctrlcommons.node.composite.signals
+import qctrlcommons.node.composite.signals_pwc
+import qctrlcommons.node.composite.signals_stf
+from qctrlcommons.node.composite.namespace import (
+    COMPOSITE_ATTR,
+    NAMESPACE_ATTR,
+)
+
+
+def _register(modules):
+    """
+    Collect exposed composite nodes from modules.
+    """
+    registered = []
+    for module in modules:
+        for _, member in inspect.getmembers(
+            module, predicate=partial(_filter, module_name=module.__name__)
+        ):
+            registered.append(member)
+    return registered
+
+
+def _filter(member, module_name):
+    """
+    - Exposed member must be a function.
+    - Exposed member must be defined in the module, not imported.
+    - Exposed member must not be private.
+    """
+    return (
+        (inspect.isfunction(member) or inspect.isclass(member))
+        and member.__module__ == module_name
+        and not member.__name__.startswith("_")
+        and hasattr(member, COMPOSITE_ATTR)
+    )
+
+
+_composite_registry = _register(
+    [
+        qctrlcommons.node.composite.optimizable_signals,
+        qctrlcommons.node.composite.filter_and_resample,
+        qctrlcommons.node.composite.signals_pwc,
+        qctrlcommons.node.composite.signals_stf,
+        qctrlcommons.node.composite.signals,
+    ]
+)
+
+# Collect the composite node namespaces.
+COMPOSITE_NAMESPACE_REGISTRY = [
+    node for node in _composite_registry if hasattr(node, NAMESPACE_ATTR)
+]
+
+# Collect the composite nodes that are not in a namespace.
+COMPOSITE_NODE_REGISTRY = [
+    node for node in _composite_registry if not hasattr(node, NAMESPACE_ATTR)
+]
