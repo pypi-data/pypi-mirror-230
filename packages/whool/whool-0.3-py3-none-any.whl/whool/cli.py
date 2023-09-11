018495c956a5587716d263d3ff73c77904482dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:26 +0000

    Add package whool/0.3/whool-0.3-py3-none-any.whl

---
+++ packages/whool/whool-0.3-py3-none-any.whl/whool/cli.py
@@ -0,0 +1,46 @@
+import argparse
+import logging
+import sys
+from pathlib import Path
+
+from .init import init
+from .version import version
+
+
+def main() -> None:
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version="Whool " + version,
+    )
+    ap.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+        help="Verbosity level (default: warn, once: info, twice: debug).",
+    )
+    subparsers = ap.add_subparsers(title="subcommands", dest="subcmd")
+
+    subparsers.add_parser(
+        "init",
+        help="Initialize pyproject.toml in addon_dir with the whool build-system.",
+    )
+    # TODO init --git-commit, --git-add
+
+    args = ap.parse_args(sys.argv[1:])
+    if args.verbose >= 2:
+        log_level = logging.DEBUG
+    elif args.verbose >= 1:
+        log_level = logging.INFO
+    else:
+        log_level = logging.WARN
+    logging.basicConfig(level=log_level)
+
+    if args.subcmd == "init":
+        init(Path.cwd())
+    else:
+        ap.print_help()
+        sys.exit(1)
