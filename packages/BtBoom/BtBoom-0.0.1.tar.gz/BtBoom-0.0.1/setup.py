aa3fc7a6287f5bae66bf90314a8160eef0147944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 15:05:49 +0000

    Add package BtBoom/0.0.1/BtBoom-0.0.1.tar.gz

---
+++ packages/BtBoom/BtBoom-0.0.1.tar.gz/BtBoom-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BtBoom",
+    version="0.0.1",
+    author="WindFog",
+    author_email="330160114@qq.com",
+    description="This is BtBoom - Boom Boom Boom",
+    # 项目主页
+    url="https://gitee.com/wind619/btboom.git",
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages()
+)
