eb89f3548a89a9031172936f498f9a0209f875ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:04:48 +0000

    Add package embedchain/0.0.55/embedchain-0.0.55-py3-none-any.whl

---
+++ packages/embedchain/embedchain-0.0.55-py3-none-any.whl/embedchain/loaders/local_qna_pair.py
@@ -0,0 +1,19 @@
+from embedchain.helper.json_serializable import register_deserializable
+from embedchain.loaders.base_loader import BaseLoader
+
+
+@register_deserializable
+class LocalQnaPairLoader(BaseLoader):
+    def load_data(self, content):
+        """Load data from a local QnA pair."""
+        question, answer = content
+        content = f"Q: {question}\nA: {answer}"
+        meta_data = {
+            "url": "local",
+        }
+        return [
+            {
+                "content": content,
+                "meta_data": meta_data,
+            }
+        ]
