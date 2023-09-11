e1439dfccbc6327b6f6c41b394c1cf8857d96934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:04:49 +0000

    Add package zaptools/0.0.6b1/zaptools-0.0.6b1.tar.gz

---
+++ packages/zaptools/zaptools-0.0.6b1.tar.gz/zaptools-0.0.6b1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name="zaptools",
+    version="0.0.6b1",
+    url="https://github.com/NathanDraco22/zap-adapter-python",
+    license='MIT',
+
+    author="Nathan Mejia",
+    author_email="nathandraco22@gmail.com",
+
+    description="Python Implementation to ZapTools WebSockets",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=[
+        "zaptools"
+    ],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
