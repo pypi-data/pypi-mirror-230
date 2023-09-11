7e69e1b6f9b203145de8137ee70ee25f94037daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:08 +0000

    Add package mkdocs-exclude-tagged-files/1.0.4/mkdocs_exclude_tagged_files-1.0.4-py3-none-any.whl

---
+++ packages/mkdocs-exclude-tagged-files/mkdocs_exclude_tagged_files-1.0.4-py3-none-any.whl/mkdocs_exclude_tagged_files/plugin.py
@@ -0,0 +1,44 @@
+import typing
+from typing import Optional
+
+from charset_normalizer import from_path
+import frontmatter
+import mkdocs.config
+import mkdocs.config.config_options
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.plugins import BasePlugin
+from mkdocs.structure.files import Files
+from mkdocs.structure.pages import Page
+
+
+class ExcludeTaggedFilesPluginConfig(mkdocs.config.base.Config):
+    tags_to_exclude = mkdocs.config.config_options.Type(list)
+    strip_leading_hashtags = mkdocs.config.config_options.Type(bool, default=True)
+
+
+class ExcludeTaggedFilesPlugin(BasePlugin[ExcludeTaggedFilesPluginConfig]):
+    def is_page_excluded(self, meta: typing.Dict):
+        if 'tags' in meta:
+            for tag in meta['tags']:
+                if self.config.strip_leading_hashtags and str(tag).startswith('#'):
+                    tag = str(tag)[1:]
+
+                if tag in self.config.tags_to_exclude:
+                    return True
+
+            return False
+
+    def on_files(self, files: Files, *, config: MkDocsConfig) -> Optional[Files]:
+        for file in files.documentation_pages():
+            raw_file = str(from_path('./docs/' + file.src_uri).best())
+            metadata = frontmatter.loads(raw_file).metadata
+            if self.is_page_excluded(metadata):
+                files.remove(file)
+
+        return files
+
+    def on_post_page(self, output: str, *, page: Page, config: MkDocsConfig) -> Optional[str]:
+        if self.is_page_excluded(page.meta):
+            return ''
+
+        return output
