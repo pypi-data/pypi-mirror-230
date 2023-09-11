b3ee382cc8bde7669eb9c43faf84c6a4f935311e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:11 +0000

    Add package pgx/1.3.3/pgx-1.3.3.tar.gz

---
+++ packages/pgx/pgx-1.3.3.tar.gz/pgx-1.3.3/pgx/__init__.py
@@ -0,0 +1,28 @@
+from pgx._src.api_test import v1_api_test
+from pgx._src.baseline import BaselineModelId, make_baseline_model
+from pgx._src.visualizer import (
+    save_svg,
+    save_svg_animation,
+    set_visualization_config,
+)
+from pgx.v1 import Env, EnvId, State, available_envs, make
+
+__version__ = "1.3.3"
+
+__all__ = [
+    # v1 api components
+    "State",
+    "Env",
+    "EnvId",
+    "make",
+    "available_envs",
+    # visualization
+    "set_visualization_config",
+    "save_svg",
+    "save_svg_animation",
+    # baseline model
+    "BaselineModelId",
+    "make_baseline_model",
+    # api tests
+    "v1_api_test",
+]
