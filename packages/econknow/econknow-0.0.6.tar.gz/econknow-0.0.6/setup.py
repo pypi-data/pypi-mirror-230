314577b8cb5cf143f426ef5b0ecd55d550e2356b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:37 +0000

    Add package econknow/0.0.6/econknow-0.0.6.tar.gz

---
+++ packages/econknow/econknow-0.0.6.tar.gz/econknow-0.0.6/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 18 18:07:05 2022
+@author:
+Зайцева Дарья
+"""
+
+from setuptools import setup, find_packages
+long_description = '''Library for the 6 semester'''
+setup(name='econknow',
+      version='0.0.06',
+      url='https://github.com/dashkazaitseva',
+      packages=['econknow'],
+      license='MIT',
+      description='',
+      zip_safe=False,
+      package_data={'econknow': ['*.txt', '*.docx']},
+      include_package_data=True
+      )
