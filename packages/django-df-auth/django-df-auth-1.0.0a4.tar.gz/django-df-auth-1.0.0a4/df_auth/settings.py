1b78c66785a7d629f4b2141be47cc6843c2449e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 15:04:38 +0000

    Add package django-df-auth/1.0.0a4/django-df-auth-1.0.0a4.tar.gz

---
+++ packages/django-df-auth/django-df-auth-1.0.0a4.tar.gz/django-df-auth-1.0.0a4/df_auth/settings.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from django.conf import settings
+from rest_framework.settings import APISettings
+
+DEFAULTS = {
+    "USER_SIGNUP_REQUIRED_FIELDS": {
+        "email": "rest_framework.serializers.EmailField",
+    },
+    "USER_SIGNUP_OPTIONAL_FIELDS": {
+        "first_name": "rest_framework.serializers.CharField",
+        "last_name": "rest_framework.serializers.CharField",
+        "password": "rest_framework.serializers.CharField",
+        "phone_number": "phonenumber_field.serializerfields.PhoneNumberField",
+    },
+    "USER_SOCIAL_AUTH_FIELDS": {
+        "first_name": "rest_framework.serializers.CharField",
+        "last_name": "rest_framework.serializers.CharField",
+    },
+    "USER_IDENTITY_FIELDS": {
+        "username": "rest_framework.serializers.CharField",
+        "email": "rest_framework.serializers.CharField",
+        "phone_number": "phonenumber_field.serializerfields.PhoneNumberField",
+    },
+    "REQUIRED_AUTH_FIELDS": {},
+    "OPTIONAL_AUTH_FIELDS": {
+        "otp": "rest_framework.serializers.CharField",
+        "password": "rest_framework.serializers.CharField",
+    },
+    "TEST_USER_EMAIL": None,
+    "OTP_IDENTITY_UPDATE_FIELD": True,
+    "OTP_DEVICE_MODELS": {
+        "email": "django_otp.plugins.otp_email.models.EmailDevice",
+        "totp": "django_otp.plugins.otp_totp.models.TOTPDevice",
+        "sms": "otp_twilio.models.TwilioSMSDevice",
+    },
+    "OTP_AUTO_CREATE_ACCOUNT": True,
+    "SEND_OTP_UNAUTHORIZED_USER": True,
+    "SIGNUP_ALLOWED": True,
+}
+
+IMPORT_STRINGS: List[str] = []
+
+api_settings = APISettings(getattr(settings, "DF_AUTH", IMPORT_STRINGS), DEFAULTS)
