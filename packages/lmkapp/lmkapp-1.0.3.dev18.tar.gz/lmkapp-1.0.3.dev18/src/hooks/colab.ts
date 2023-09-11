3a19318afd41566c0f8a0b8f7bddc568cc94aa3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 15:05:26 +0000

    Add package lmkapp/1.0.3.dev18/lmkapp-1.0.3.dev18.tar.gz

---
+++ packages/lmkapp/lmkapp-1.0.3.dev18.tar.gz/lmkapp-1.0.3.dev18/src/hooks/colab.ts
@@ -0,0 +1,14 @@
+import { useWidgetModelEvent } from "../lib/widget-model";
+
+export function useColabSupport(): void {
+  useWidgetModelEvent("msg:custom", async (_, event) => {
+    // console.log("Handling custom event", event, typeof google);
+    if (event?.type === "colab-update" && typeof google !== "undefined") {
+      // console.log(Date.now(), "Invoking sync function");
+      await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      // console.log(Date.now(), "Re-invoking sync function");
+      await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
+    }
+  });
+}
