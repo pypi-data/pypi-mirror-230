62c684f259b542f7bc71f177acf04cae697bd773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:04:54 +0000

    Add package dbgear/0.7.0/dbgear-0.7.0.tar.gz

---
+++ packages/dbgear/dbgear-0.7.0.tar.gz/dbgear-0.7.0/dbgear/api/project.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from fastapi import Request
+
+from ..models.project import project
+from .dtos import Result
+from .dtos import ProjectInfo
+
+router = APIRouter(prefix='/project')
+
+
+@router.get('', response_model=Result)
+def get_project_info(request: Request) -> Result:
+    proj = project(request)
+
+    info = ProjectInfo(
+        project_name=proj.project_name,
+        description=proj.description,
+        bindings=proj.bindings,
+        rules=proj.rules,
+        instances=proj.instances
+    )
+    return Result(status='OK', data=info)
---
+++ packages/dbgear/dbgear-0.7.0.tar.gz/dbgear-0.7.0/dbgear/main.py
@@ -0,0 +1,39 @@
+import logging
+from argparse import ArgumentParser
+
+from .models.project import Project
+from . import backend
+from . import operations
+
+
+def execute():
+    parser = ArgumentParser(
+        prog='dbgear',
+        description='database management tools'
+    )
+    parser.add_argument(
+        'command',
+        choices=['serve', 'check', 'apply'],
+        help='please specify the command to execute.')
+    parser.add_argument(
+        '--project',
+        default='etc/database',
+        help='please specify the folder for the project.')
+    parser.add_argument(
+        '--env',
+        help='target environment for the command.')
+    parser.add_argument(
+        '--deploy',
+        help='target deployment for the command.')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+    logging.info(args)
+
+    project = Project(args.project)
+    project.read_definitions()
+
+    if args.command == 'serve':
+        backend.run(project)
+    if args.command == 'apply':
+        operations.apply(project, args.env, args.deploy)
