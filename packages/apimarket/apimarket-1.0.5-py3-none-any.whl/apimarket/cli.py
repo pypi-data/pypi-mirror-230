5feeb3bb2c0699a20f8c45838c7015f7e4ccee0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:04:13 +0000

    Add package apimarket/1.0.5/apimarket-1.0.5-py3-none-any.whl

---
+++ packages/apimarket/apimarket-1.0.5-py3-none-any.whl/apimarket/cli.py
@@ -0,0 +1,132 @@
+import argparse
+import logging
+import sys
+import json
+
+from apimarket import __version__, fetch_curp_details
+
+__author__ = "Carlos Eduardo Sanchez Torres (sanchezcarlosjr)"
+__copyright__ = "API MARKET"
+__license__ = "MIT"
+
+_logger = logging.getLogger(__name__)
+
+
+# ---- Python API ----
+# The functions defined in this section can be imported by users in their
+# Python scripts/interactive interpreter, e.g. via
+# `from apimarket.skeleton import fib`,
+# when using this Python module as a library.
+
+
+# ---- CLI ----
+# The functions defined in this section are wrappers around the main Python
+# API allowing them to be called directly from the terminal as a CLI
+# executable/script.
+
+
+class CURPDetailsAction(argparse.Action):
+
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("nargs not allowed")
+        super().__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, curp, option_string=None):
+        _logger.debug("Fetching apimarket...")
+        print(f"{json.dumps(fetch_curp_details(curp))}")
+        _logger.debug("Script ends here")
+        setattr(namespace, self.dest, curp)
+
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="API Market Python Open Source Development")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"apimarket {__version__}",
+    )
+    parser.add_argument(
+        "-c",
+        "--curp",
+        dest="curp", 
+        help="Write a valid CURP. For instance, LOOA531113HTCPBN07", 
+        type=str, 
+        metavar="CURP",
+        action=CURPDetailsAction
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def main(args):
+    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
+
+    Instead of returning the value from :func:`fib`, it prints the result to the
+    ``stdout`` in a nicely formatted message.
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--verbose", "42"]``).
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+
+
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    # ^  This is a guard statement that will prevent the following code from
+    #    being executed in the case someone imports this file instead of
+    #    executing it as a script.
+    #    https://docs.python.org/3/library/__main__.html
+
+    # After installing your project with pip, users can also run your Python
+    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
+    #
+    #     python -m apimarket.cli 42
+    #
+    run()
