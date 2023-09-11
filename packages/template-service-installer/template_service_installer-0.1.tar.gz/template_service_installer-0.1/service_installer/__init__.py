158e4e80034a6e177448261f715fe3e43e71abc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:28 +0000

    Add package template-service-installer/0.1/template_service_installer-0.1.tar.gz

---
+++ packages/template-service-installer/template_service_installer-0.1.tar.gz/template_service_installer-0.1/service_installer/__init__.py
@@ -0,0 +1,7 @@
+import sys
+
+
+if __name__ == '__main__':
+    from service_installer.service_installer import install_services
+    install_services(sys.argv[1], sys.argv[2])
+from .service_installer import install_services
---
+++ packages/template-service-installer/template_service_installer-0.1.tar.gz/template_service_installer-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='template_service_installer',
+    version='0.1',
+    description='Library for automating the installation of systemd services',
+    packages=['service_installer'],
+    author_email='berestovborisasz@gmail.com',
+    zip_safe=False
+)
