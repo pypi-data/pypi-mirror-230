04441877f3492379e8ecd86518abc490a57f7a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:06 +0000

    Add package cbum-password/0.1.0/cbum_password-0.1.0.tar.gz

---
+++ packages/cbum-password/cbum_password-0.1.0.tar.gz/cbum_password-0.1.0/cbum_password/__init__.py
@@ -0,0 +1,22 @@
+import secrets
+import string
+
+
+def gerar_senha(tamanho=6):
+    if tamanho < 6:
+        print("Tamanho da senha deve ser no mínimo 6.")
+        return None
+
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = [
+        secrets.choice(string.ascii_uppercase),
+        secrets.choice(string.ascii_lowercase),
+        secrets.choice(string.digits),
+        secrets.choice(string.punctuation)
+    ]
+
+    for i in range(tamanho - 4):
+        senha.append(secrets.choice(caracteres))
+
+    secrets.SystemRandom().shuffle(senha)
+    return ''.join(senha)
