940bec4818496670f63ab7df8bba44d1ffbc2d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 15:05:11 +0000

    Add package vcorelib/2.7.2/vcorelib-2.7.2.tar.gz

---
+++ packages/vcorelib/vcorelib-2.7.2.tar.gz/vcorelib-2.7.2/vcorelib/math/__init__.py
@@ -0,0 +1,49 @@
+"""
+Math utilities.
+"""
+
+# internal
+from vcorelib.math.analysis import (
+    DEFAULT_DEPTH,
+    FloatBuffer,
+    MovingAverage,
+    MovingSum,
+    WeightedAverage,
+    ns_to_s,
+)
+from vcorelib.math.analysis.rate import RateTracker
+from vcorelib.math.analysis.rate.limiter import RateLimiter
+from vcorelib.math.time import (
+    TIMER,
+    LoggerType,
+    Timer,
+    byte_count_str,
+    default_time_ns,
+    nano_str,
+    rate_str,
+    seconds_str,
+)
+from vcorelib.math.unit import KIBI_UNITS, SI_UNITS, UnitSystem, unit_traverse
+
+__all__ = [
+    "UnitSystem",
+    "SI_UNITS",
+    "KIBI_UNITS",
+    "unit_traverse",
+    "byte_count_str",
+    "default_time_ns",
+    "nano_str",
+    "seconds_str",
+    "rate_str",
+    "LoggerType",
+    "Timer",
+    "TIMER",
+    "DEFAULT_DEPTH",
+    "MovingAverage",
+    "RateLimiter",
+    "RateTracker",
+    "ns_to_s",
+    "FloatBuffer",
+    "MovingSum",
+    "WeightedAverage",
+]
