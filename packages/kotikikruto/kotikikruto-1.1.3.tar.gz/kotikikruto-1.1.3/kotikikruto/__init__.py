8e35968cc50e590404dedb739a0b98afaa43a350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:37 +0000

    Add package kotikikruto/1.1.3/kotikikruto-1.1.3.tar.gz

---
+++ packages/kotikikruto/kotikikruto-1.1.3.tar.gz/kotikikruto-1.1.3/kotikikruto/__init__.py
@@ -0,0 +1,9 @@
+def count_vowels(text):
+    vowels = 'aeiou'
+    count = 0
+
+    for char in text:
+        if char.lower() in vowels:
+            count += 1
+
+    return count
---
+++ packages/kotikikruto/kotikikruto-1.1.3.tar.gz/kotikikruto-1.1.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='kotikikruto',
+    version='1.1.3',
+    packages=['kotikikruto']
+)
