fd282de6515bc92f0d641a6b35cd1731d4fa7963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:07 +0000

    Add package torch-build-nn/0.1.4/torch_build_nn-0.1.4.tar.gz

---
+++ packages/torch-build-nn/torch_build_nn-0.1.4.tar.gz/torch_build_nn-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    "filelock",
+    "typing-extensions",
+    "sympy",
+    "networkx",
+    "jinja2",
+    "fsspec",
+]
+setup(
+    name="torch_build_nn",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=install_requires,
+)
