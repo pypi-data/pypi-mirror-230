9fdb2aa0bcdcbd2f625bffd8081afcb8a3a01e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:08 +0000

    Add package mkdocs-exclude-tagged-files/1.0.4/mkdocs-exclude-tagged-files-1.0.4.tar.gz

---
+++ packages/mkdocs-exclude-tagged-files/mkdocs-exclude-tagged-files-1.0.4.tar.gz/mkdocs-exclude-tagged-files-1.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+
+setup(
+    name='mkdocs-exclude-tagged-files',
+    version='1.0.4',
+    packages=['mkdocs_exclude_tagged_files'],
+    url='https://github.com/JonasDoesThings/mkdocs-exclude-tagged-files',
+    license='MIT',
+    author='Jonas Lorenz <jonas@jonasdoesthings.com>',
+    author_email='jonas@jonasdoesthings.com',
+    description='A mkdocs plugin that excludes files based on a list of frontmatter tags from being included in the final mkdocs output.',
+    long_description=(this_directory / "README.md").read_text(),
+    long_description_content_type='text/markdown',
+    keywords=['mkdocs', 'mkdocs-plugin'],
+    install_requires=['mkdocs', 'python-frontmatter', 'charset_normalizer'],
+
+    entry_points={
+        'mkdocs.plugins': [
+            'mkdocs_exclude_tagged_files = mkdocs_exclude_tagged_files.plugin:ExcludeTaggedFilesPlugin',
+        ]
+    },
+)
