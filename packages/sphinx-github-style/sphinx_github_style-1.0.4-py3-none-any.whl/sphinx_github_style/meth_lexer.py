f9b7c134d925dd9be3350415cb10f5af7b9e1f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 15:05:07 +0000

    Add package sphinx-github-style/1.0.4/sphinx_github_style-1.0.4-py3-none-any.whl

---
+++ packages/sphinx-github-style/sphinx_github_style-1.0.4-py3-none-any.whl/sphinx_github_style/meth_lexer.py
@@ -0,0 +1,47 @@
+import types
+from typing import Type
+from sphinx.application import Sphinx
+from pygments.lexers.python import NumPyLexer
+from inspect import getmembers, isfunction, ismethod, ismodule, isclass
+
+
+def get_pkg_funcs(pkg: types.ModuleType):
+    # Get funcs/meths defined in pkg.__init__
+    funcs_meths = get_funcs(pkg)
+    modules = getmembers(pkg, ismodule)
+    # Get funcs/meths of each module in pkg
+    for name, module in modules:
+        funcs_meths += get_funcs(module)
+        classes = getmembers(module, isclass)
+        for class_name, _class in classes:
+            funcs_meths += get_funcs(_class)
+    # Set of all funcs/meths contained in modules used by package
+    return set(funcs_meths)
+
+
+def get_funcs(of):
+    members = getmembers(of, isfunction or ismethod)
+    return list(dict(members))
+
+
+class TDKMethLexer(NumPyLexer):
+    """Adds syntax highlighting for methods and functions within a python Package
+
+    """
+    name = 'TDK'
+    url = 'https://github.com/TDKorn'
+    aliases = ['tdk']
+
+    EXTRA_KEYWORDS = {}
+
+    @classmethod
+    def get_pkg_lexer(cls, pkg_name: str) -> Type["TDKMethLexer"]:
+        pkg = __import__(pkg_name)
+        funcs = get_pkg_funcs(pkg)
+        cls.EXTRA_KEYWORDS = funcs
+        return cls
+
+
+def setup(app: Sphinx):
+    top_level = app.config._raw_config.get('top_level', getattr(app.config, 'top_level'))
+    app.add_lexer('python', TDKMethLexer.get_pkg_lexer(top_level))
