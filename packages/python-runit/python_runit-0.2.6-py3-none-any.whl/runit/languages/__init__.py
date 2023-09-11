f9c99cec0466d8cdac72b5a6aaf096d387617a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:03 +0000

    Add package python-runit/0.2.6/python_runit-0.2.6-py3-none-any.whl

---
+++ packages/python-runit/python_runit-0.2.6-py3-none-any.whl/runit/languages/__init__.py
@@ -0,0 +1,38 @@
+from typing import Type
+from .php import PHP
+from .multi import Multi
+from .python import Python
+from .javascript import Javascript
+
+import os
+from typing import Union
+
+class LanguageParser(object):
+    '''
+    Class for parsing script language.
+    Determines which language paackage
+    to use
+    '''
+
+    EXT_TO_LANG = {'.py': Python, '.php': PHP, '.js': Javascript,
+                   '.jsx': Javascript, '.ts': Javascript, '.tsx': Javascript}
+
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def detect_language(filename: str, runtime: str, is_file: bool = False, is_docker: bool = False, project_id: str = '')-> Union[Python, PHP, Javascript, Multi]:
+        if runtime == 'multi':
+            return Multi(filename, runtime, is_docker, is_file, project_id)
+        return LanguageParser.EXT_TO_LANG[os.path.splitext(filename)[1].lower()](
+            filename, 
+            runtime, 
+            is_docker=is_docker,
+            project_id=project_id
+        )
+
+    @staticmethod
+    def run_file(filename, runtime):
+        lang_parser = LanguageParser.detect_language(filename, runtime, True)
+        lang_parser.is_file = True
+        lang_parser.anon_function()
