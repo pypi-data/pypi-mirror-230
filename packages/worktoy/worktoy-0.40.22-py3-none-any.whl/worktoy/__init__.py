c41e99717afd406165e6d5e019ba9aa509ebf777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:11 +0000

    Add package worktoy/0.40.22/worktoy-0.40.22-py3-none-any.whl

---
+++ packages/worktoy/worktoy-0.40.22-py3-none-any.whl/worktoy/__init__.py
@@ -0,0 +1,13 @@
+"""WorkToy - MetaWork
+This module provides the metaclass and namespace classes for the remaining
+package."""
+#  MIT Licence
+#  Copyright (c) 2023 Asger Jon Vistisen
+from __future__ import annotations
+
+import worktoy.core as core
+import worktoy.worktoyclass as worktoyclass
+import worktoy.metaclass as metaclass
+import worktoy.descriptors as descriptors
+import worktoy.sym as sym
+import worktoy.waitaminute as waitaminute
---
+++ packages/worktoy/worktoy-0.40.22-py3-none-any.whl/worktoy/descriptors/__init__.py
@@ -0,0 +1,12 @@
+"""WorkToy - Descriptors
+Implementation of descriptors as descriptors."""
+#  MIT Licence
+#  Copyright (c) 2023 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._field import Field
+from ._attribute import AbstractAttribute
+from ._int_attribute import IntAttribute
+from ._float_attribute import FloatAttribute
+from ._str_attribute import StrAttribute
+from ._flag import Flag
