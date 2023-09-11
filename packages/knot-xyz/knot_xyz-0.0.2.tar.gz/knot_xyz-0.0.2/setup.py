6b3944ab9b676a0ce88620bac2e91eb847863a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:09 +0000

    Add package knot-xyz/0.0.2/knot_xyz-0.0.2.tar.gz

---
+++ packages/knot-xyz/knot_xyz-0.0.2.tar.gz/knot_xyz-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='knot_xyz',
+    version='0.0.2',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    author='YANG Jiansong',
+    author_email='jsyang-c@my.cityu.edu.hk',
+    description='A small example package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['numpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
