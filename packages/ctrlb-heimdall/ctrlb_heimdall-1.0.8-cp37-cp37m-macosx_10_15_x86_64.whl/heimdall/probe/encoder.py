f0b7deee9e7ba82367b217d5143ffa8163535f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 15:05:21 +0000

    Add package ctrlb-heimdall/1.0.8/ctrlb_heimdall-1.0.8-cp37-cp37m-macosx_10_15_x86_64.whl

---
+++ packages/ctrlb-heimdall/ctrlb_heimdall-1.0.8-cp37-cp37m-macosx_10_15_x86_64.whl/heimdall/probe/encoder.py
@@ -0,0 +1,18 @@
+import json
+from heimdall.utils import debug_logger
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, z):
+        try:
+            if "to_json" in dir(z):
+                return z.to_json()
+            elif isinstance(z, bytes):
+                return z.decode('utf-8', errors='ignore')
+            else:
+                return super(JSONEncoder, self).default(z)
+        except Exception as e:
+            debug_logger(e)
+
+
+def to_json(data, separators=None):
+    return json.dumps(data, separators=separators, cls=JSONEncoder)
