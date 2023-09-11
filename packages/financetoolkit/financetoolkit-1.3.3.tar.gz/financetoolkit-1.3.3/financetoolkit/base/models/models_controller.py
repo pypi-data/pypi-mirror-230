96c312d20905f8904e787d16f79db3c5198266d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:16 +0000

    Add package financetoolkit/1.3.3/financetoolkit-1.3.3.tar.gz

---
+++ packages/financetoolkit/financetoolkit-1.3.3.tar.gz/financetoolkit-1.3.3/financetoolkit/base/models/models_controller.py
@@ -0,0 +1,330 @@
+"""Models Module"""
+__docformat__ = "google"
+
+import pandas as pd
+
+from financetoolkit.base.helpers import calculate_growth, handle_errors
+from financetoolkit.models.dupont import (
+    get_dupont_analysis,
+    get_extended_dupont_analysis,
+)
+from financetoolkit.models.enterprise import get_enterprise_value_breakdown
+
+# pylint: disable=too-many-instance-attributes
+
+
+class Models:
+    """
+    Models Controller Class
+    """
+
+    def __init__(
+        self,
+        tickers: str | list[str],
+        balance: pd.DataFrame,
+        income: pd.DataFrame,
+        cash: pd.DataFrame,
+        quarterly_historical: pd.DataFrame = pd.DataFrame(),
+        yearly_historical: pd.DataFrame = pd.DataFrame(),
+        quarterly: bool = False,
+        rounding: int | None = 4,
+    ):
+        """
+        Initializes the Models Controller Class.
+        """
+        self._tickers = tickers
+        self._balance_sheet_statement: pd.DataFrame = balance
+        self._income_statement: pd.DataFrame = income
+        self._cash_flow_statement: pd.DataFrame = cash
+        self._quarterly = quarterly
+        self._rounding = rounding
+
+        # Initilaization of Historical Data
+        self._quarterly_historical_data: pd.DataFrame = quarterly_historical
+        self._yearly_historical_data: pd.DataFrame = yearly_historical
+
+        # Initialization of Model Variables
+        self._dupont_analysis: pd.DataFrame = pd.DataFrame()
+        self._dupont_analysis_growth: pd.DataFrame = pd.DataFrame()
+        self._extended_dupont_analysis: pd.DataFrame = pd.DataFrame()
+        self._extended_dupont_analysis_growth: pd.DataFrame = pd.DataFrame()
+        self._enterprise_value_breakdown: pd.DataFrame = pd.DataFrame()
+        self._enterprise_value_breakdown_growth: pd.DataFrame = pd.DataFrame()
+        self._weighted_average_cost_of_capital: pd.DataFrame = pd.DataFrame()
+
+    @handle_errors
+    def get_dupont_analysis(
+        self,
+        rounding: int | None = None,
+        growth: bool = False,
+        lag: int | list[int] = 1,
+    ) -> pd.DataFrame:
+        """
+        Perform a Dupont analysis to breakdown the return on equity (ROE) into its components.
+
+        The Dupont analysis is a method used to dissect and understand the factors that drive a company's
+        return on equity (ROE). It breaks down the ROE into three key components: Profit Margin, Asset
+        Turnover, and Financial Leverage.
+
+        The formula is as follows:
+
+            - Profit Margin = Net Income / Revenue
+            - Asset Turnover = Revenue / Average Total Assets
+            - Financial Leverage = Average Total Assets / Average Total Equity
+            - ROE = Profit Margin * Asset Turnover * Financial Leverage
+
+        Args:
+            rounding (int, optional): The number of decimals to round the results to. Defaults to 4.
+            growth (bool, optional): Whether to calculate the growth of the values. Defaults to False.
+            lag (int | str, optional): The lag to use for the growth calculation. Defaults to 1.
+
+        Returns:
+            pd.DataFrame: DataFrame containing Dupont analysis results, including Profit Margin, Asset
+                          Turnover, Financial Leverage, and the calculated ROE values.
+
+        Notes:
+            - The Profit Margin is the ratio of Net Income to Total Revenue, indicating the percentage of
+            revenue that translates into profit.
+            - Asset Turnover measures the efficiency of a company's use of its assets to generate sales
+            revenue.
+            - Financial Leverage represents the use of debt to finance a company's operations, which can
+            amplify returns as well as risks.
+
+        As an example:
+
+        ```python
+        from financetoolkit import Toolkit
+
+        toolkit = Toolkit(["AAPL", "TSLA"], api_key=FMP_KEY)
+
+        dupont_analysis = toolkit.models.get_dupont_analysis()
+        ```
+        """
+        self._dupont_analysis = get_dupont_analysis(
+            self._income_statement.loc[:, "Net Income", :],
+            self._income_statement.loc[:, "Revenue", :],
+            self._balance_sheet_statement.loc[:, "Total Assets", :].shift(axis=1),
+            self._balance_sheet_statement.loc[:, "Total Assets", :],
+            self._balance_sheet_statement.loc[:, "Total Equity", :].shift(axis=1),
+            self._balance_sheet_statement.loc[:, "Total Equity", :],
+        )
+
+        if growth:
+            self._dupont_analysis_growth = calculate_growth(
+                self._dupont_analysis,
+                lag=lag,
+                rounding=rounding if rounding else self._rounding,
+                axis="index",
+            )
+
+        self._dupont_analysis = self._dupont_analysis.round(
+            rounding if rounding else self._rounding
+        )
+
+        if len(self._tickers) == 1:
+            return (
+                self._dupont_analysis_growth.droplevel(level=0)
+                if growth
+                else self._dupont_analysis.droplevel(level=0)
+            )
+
+        return self._dupont_analysis_growth if growth else self._dupont_analysis
+
+    @handle_errors
+    def get_extended_dupont_analysis(
+        self,
+        rounding: int | None = None,
+        growth: bool = False,
+        lag: int | list[int] = 1,
+    ) -> pd.DataFrame:
+        """
+        Perform an Extended Dupont analysis to breakdown the return on equity (ROE) into its components,
+        while considering additional financial metrics.
+
+        The Extended Dupont analysis is an advanced method used to break down the return on equity (ROE)
+        into multiple components, providing a more detailed insight into the factors influencing a
+        company's profitability. It considers additional metrics such as Return on Assets (ROA),
+        Total Asset Turnover, Financial Leverage, and more.
+
+        The formula is as follows:
+
+            - Profit Margin = Net Income / Revenue
+            - Asset Turnover = Revenue / Average Total Assets
+            - Financial Leverage = Average Total Assets / Average Total Equity
+            - ROA = Net Income / Average Total Assets
+            - Total Asset Turnover = Revenue / Average Total Assets
+            - ROE = Profit Margin * Asset Turnover * Financial Leverage * ROA * Total Asset Turnover
+
+        Args:
+            rounding (int, optional): The number of decimals to round the results to. Defaults to 4.
+            growth (bool, optional): Whether to calculate the growth of the values. Defaults to False.
+            lag (int | str, optional): The lag to use for the growth calculation. Defaults to 1.
+
+        Returns:
+            pd.DataFrame: DataFrame containing Extended Dupont analysis results, including Profit Margin, Asset Turnover,
+                        Financial Leverage, ROA, Total Asset Turnover, and the calculated ROE values.
+
+        Notes:
+            - The Profit Margin is the ratio of Net Income to Total Revenue, indicating the percentage of
+            revenue that translates into profit.
+            - Asset Turnover measures the efficiency of a company's use of its assets to generate
+            sales revenue.
+            - Financial Leverage represents the use of debt to finance a company's operations, which can
+            amplify returns as well as risks.
+            - Return on Assets (ROA) measures the efficiency of a company's use of its assets to
+            generate profit.
+            - Total Asset Turnover considers all assets, including both equity and debt financing.
+
+        As an example:
+
+        ```python
+        from financetoolkit import Toolkit
+
+        toolkit = Toolkit(["AAPL", "TSLA"], api_key=FMP_KEY)
+
+        extended_dupont_analysis = toolkit.models.get_extended_dupont_analysis()
+        ```
+        """
+        self._extended_dupont_analysis = get_extended_dupont_analysis(
+            self._income_statement.loc[:, "Income Before Tax", :],
+            self._income_statement.loc[:, "Operating Income", :],
+            self._income_statement.loc[:, "Net Income", :],
+            self._income_statement.loc[:, "Revenue", :],
+            self._balance_sheet_statement.loc[:, "Total Assets", :].shift(axis=1),
+            self._balance_sheet_statement.loc[:, "Total Assets", :],
+            self._balance_sheet_statement.loc[:, "Total Equity", :].shift(axis=1),
+            self._balance_sheet_statement.loc[:, "Total Equity", :],
+        )
+
+        if growth:
+            self._extended_dupont_analysis_growth = calculate_growth(
+                self._extended_dupont_analysis,
+                lag=lag,
+                rounding=rounding if rounding else self._rounding,
+                axis="columns",
+            )
+
+        self._extended_dupont_analysis = self._extended_dupont_analysis.round(
+            rounding if rounding else self._rounding
+        )
+
+        if len(self._tickers) == 1:
+            return (
+                self._extended_dupont_analysis_growth.droplevel(level=0)
+                if growth
+                else self._extended_dupont_analysis.droplevel(level=0)
+            )
+
+        return (
+            self._extended_dupont_analysis_growth
+            if growth
+            else self._extended_dupont_analysis
+        )
+
+    @handle_errors
+    def get_enterprise_value_breakdown(
+        self,
+        diluted: bool = True,
+        rounding: int | None = None,
+        growth: bool = False,
+        lag: int | list[int] = 1,
+    ) -> pd.DataFrame:
+        """
+        Calculate the Enterprise Value (EV) breakdown, providing a detailed view of its components.
+
+        The Enterprise Value breakdown includes the following components for each quarter or year:
+
+            - Share Price: The market price per share of the company's stock.
+            - Market Capitalization (Market Cap): The total value of a company's outstanding common and preferred shares.
+            - Debt: The sum of long-term and short-term debt on the company's balance sheet.
+            - Preferred Equity: The value of preferred shares, if applicable.
+            - Minority Interest: The equity value of a subsidiary with less than 50% ownership.
+            - Cash and Cash Equivalents: The total amount of liquid assets including cash, marketable securities,
+            and short-term investments.
+
+        The Enterprise Value is calculated as the sum of Market Cap, Debt, Preferred Equity,
+        Minority Interest, minus Cash and Cash Equivalents.
+
+        This breakdown is displayed in a DataFrame for each company and includes the option to show growth values as well.
+
+        Args:
+            diluted (bool, optional): Whether to use diluted shares in the calculation. Defaults to True.
+            rounding (int, optional): The number of decimals to round the results to. Defaults to 4.
+            growth (bool, optional): Whether to calculate the growth of the values. Defaults to False.
+            lag (int | str, optional): The lag to use for the growth calculation. Defaults to 1.
+
+        Returns:
+            pd.DataFrame: DataFrame containing the Enterprise Value breakdown, including the calculated components.
+
+        Notes:
+        - All the inputs must be in the same currency and unit for accurate calculations.
+        - The Enterprise Value is an important metric used for valuation and investment analysis.
+        - A positive Enterprise Value indicates that the company is financed primarily by equity and has excess cash.
+        - A negative Enterprise Value may indicate financial distress or unusual financial situations.
+        - Understanding the Enterprise Value breakdown can provide insights into the sources of a
+        company's value and potential risks.
+
+        As an example:
+
+        ```python
+        from financetoolkit import Toolkit
+
+        toolkit = Toolkit(["AAPL", "TSLA"], api_key=FMP_KEY)
+
+        enterprise_value_breakdown = toolkit.models.get_enterprise_value_breakdown()
+        ```
+        """
+        average_shares = (
+            self._income_statement.loc[:, "Weighted Average Shares Diluted", :]
+            if diluted
+            else self._income_statement.loc[:, "Weighted Average Shares", :]
+        )
+
+        years = self._cash_flow_statement.columns
+        begin, end = str(years[0]), str(years[-1])
+
+        if self._quarterly:
+            share_prices = self._quarterly_historical_data.loc[begin:end, "Adj Close"][
+                self._tickers
+            ].T
+        else:
+            share_prices = self._yearly_historical_data.loc[begin:end, "Adj Close"][
+                self._tickers
+            ].T
+
+        self._enterprise_value_breakdown = get_enterprise_value_breakdown(
+            share_price=share_prices,
+            shares_outstanding=average_shares,
+            total_debt=self._balance_sheet_statement.loc[:, "Total Debt", :],
+            minority_interest=self._balance_sheet_statement.loc[
+                :, "Minority Interest", :
+            ],
+            preferred_equity=self._balance_sheet_statement.loc[:, "Preferred Stock", :],
+            cash_and_cash_equivalents=self._balance_sheet_statement.loc[
+                :, "Cash and Cash Equivalents", :
+            ],
+        )
+
+        if growth:
+            self._enterprise_value_breakdown_growth = calculate_growth(
+                self._enterprise_value_breakdown,
+                lag=lag,
+                rounding=rounding if rounding else self._rounding,
+            )
+
+        self._enterprise_value_breakdown = self._enterprise_value_breakdown.round(
+            rounding if rounding else self._rounding
+        )
+
+        if len(self._tickers) == 1:
+            return (
+                self._enterprise_value_breakdown_growth.droplevel(level=0)
+                if growth
+                else self._enterprise_value_breakdown.droplevel(level=0)
+            )
+
+        return (
+            self._enterprise_value_breakdown_growth
+            if growth
+            else self._enterprise_value_breakdown
+        )
