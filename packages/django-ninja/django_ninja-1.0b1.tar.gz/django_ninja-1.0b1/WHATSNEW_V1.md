9e0820c40c0a7dab0734933d819495ba3d656216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:12 +0000

    Add package django-ninja/1.0b1/django_ninja-1.0b1.tar.gz

---
+++ packages/django-ninja/django_ninja-1.0b1.tar.gz/django_ninja-1.0b1/WHATSNEW_V1.md
@@ -0,0 +1,16 @@
+ - Pydnatic2
+   - request in pydantic context
+ - CSRF changes
+ - Auth async support
+ - Schema.Meta
+ - pagination: request in paginate_queryset
+ - decorators
+ - openapi docs plugable
+ - add_router supports strings
+
+TODO:
+ - async pagination
+
+Backwards incompatible stuff
+ - resolve_xxx(self, ...)
+ - pydantic v1
---
+++ packages/django-ninja/django_ninja-1.0b1.tar.gz/django_ninja-1.0b1/ninja/__init__.py
@@ -0,0 +1,34 @@
+"""Django Ninja - Fast Django REST framework"""
+
+__version__ = "1.0b1"
+
+
+from pydantic import Field
+
+from ninja.files import UploadedFile
+from ninja.filter_schema import FilterSchema
+from ninja.main import NinjaAPI
+from ninja.openapi.docs import Redoc, Swagger
+from ninja.orm import ModelSchema
+from ninja.params_functions import Body, Cookie, File, Form, Header, Path, Query
+from ninja.router import Router
+from ninja.schema import Schema
+
+__all__ = [
+    "Field",
+    "UploadedFile",
+    "NinjaAPI",
+    "Body",
+    "Cookie",
+    "File",
+    "Form",
+    "Header",
+    "Path",
+    "Query",
+    "Router",
+    "Schema",
+    "ModelSchema",
+    "FilterSchema",
+    "Swagger",
+    "Redoc",
+]
