70fb0c50aaed3a38e3c7ba579a6e5aa8ce95f5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:58 +0000

    Add package apimarket/1.0.2/apimarket-1.0.2.tar.gz

---
+++ packages/apimarket/apimarket-1.0.2.tar.gz/apimarket-1.0.2/src/apimarket/__init__.py
@@ -0,0 +1,25 @@
+import sys
+import requests
+from decouple import config
+
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+else:
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+
+def fetch_curp_details(curp, api_key=False):
+    response = requests.post(f"https://apimarket.mx/api/renapo/grupo/valida-curp?curp={curp}", headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    })
+    return response.json()['data']
