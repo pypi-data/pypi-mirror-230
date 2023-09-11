69656f1c8b36e08812f910983a64d416fab26379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:05:33 +0000

    Add package svelte-web-components/0.0.22/svelte_web_components-0.0.22-py3-none-any.whl

---
+++ packages/svelte-web-components/svelte_web_components-0.0.22-py3-none-any.whl/svelte_web_components/svelte_app/vite.config.js
@@ -0,0 +1,22 @@
+import {defineConfig} from "vite";
+import {svelte} from "@sveltejs/vite-plugin-svelte";
+
+
+export default defineConfig({
+    mode: 'production', plugins: [svelte({
+        include: [`./components/**/*.svelte`], compilerOptions: {
+            customElement: true,
+        },
+    }),],
+    build: {
+        write: false,
+        rollupOptions: {
+            input: "./components.js", // Set the entry point to main.js
+            output: {
+                entryFileNames: `[name].js`,
+            }
+        },
+    }
+});
+
+
