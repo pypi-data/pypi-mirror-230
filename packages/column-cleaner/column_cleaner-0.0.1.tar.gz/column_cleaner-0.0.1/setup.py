204faec3115bf0c419349c75d3bed34e208bac27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 15:04:38 +0000

    Add package column-cleaner/0.0.1/column_cleaner-0.0.1.tar.gz

---
+++ packages/column-cleaner/column_cleaner-0.0.1.tar.gz/column_cleaner-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='column_cleaner',
+    version='0.0.1',
+    description='A column cleaner, which will clean the numerical values and spaces',
+    author= 'Abhishek Kumar',
+    # url = 'https://github.com/Spidy20/PyMusic_Player',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['column cleaner', 'number cleaner', 'space cleaner'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['column_cleaner'],
+    package_dir={'':'src'},
+    install_requires = [
+        'pandas',
+    ]
+)
