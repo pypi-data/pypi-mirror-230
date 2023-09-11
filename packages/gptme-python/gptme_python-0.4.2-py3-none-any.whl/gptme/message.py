03af5a410ab1899009ccdf7789af8aeac17e0f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:23 +0000

    Add package gptme-python/0.4.2/gptme_python-0.4.2-py3-none-any.whl

---
+++ packages/gptme-python/gptme_python-0.4.2-py3-none-any.whl/gptme/message.py
@@ -0,0 +1,117 @@
+import io
+import shutil
+import sys
+import textwrap
+from datetime import datetime
+from typing import Literal
+
+from rich import print
+from rich.console import Console
+from rich.syntax import Syntax
+
+from .constants import ROLE_COLOR
+
+
+class Message:
+    """A message sent to or from the AI."""
+
+    def __init__(
+        self,
+        role: Literal["system", "user", "assistant"],
+        content: str,
+        user: str | None = None,
+        pinned: bool = False,
+        hide: bool = False,
+    ):
+        assert role in ["system", "user", "assistant"]
+        self.role = role
+        self.content = content.strip()
+        self.timestamp = datetime.now()
+        if user:
+            self.user = user
+        else:
+            role_names = {"system": "System", "user": "User", "assistant": "Assistant"}
+            self.user = role_names[role]
+
+        # Wether this message should be pinned to the top of the chat, and never context-trimmed.
+        self.pinned = pinned
+        # Wether this message should be hidden from the chat output (but still be sent to the assistant)
+        self.hide = hide
+
+    def to_dict(self):
+        """Return a dict representation of the message, serializable to JSON."""
+        return {
+            "role": self.role,
+            "content": self.content,
+        }
+
+    def format(self, oneline: bool = False, highlight: bool = False) -> str:
+        return format_msgs([self], oneline=oneline, highlight=highlight)[0]
+
+    def __repr__(self):
+        return f"<Message role={self.role} content={self.content}>"
+
+
+def format_msgs(
+    msgs: list[Message],
+    oneline: bool = False,
+    highlight: bool = False,
+    indent: int = 0,
+) -> list[str]:
+    """Formats messages for printing to the console. Stores the result in msg.output"""
+    outputs = []
+    for msg in msgs:
+        color = ROLE_COLOR[msg.role]
+        userprefix = f"[bold {color}]{msg.user}[/bold {color}]"
+        # get terminal width
+        max_len = shutil.get_terminal_size().columns - len(userprefix)
+        output = ""
+        if oneline:
+            output += textwrap.shorten(
+                msg.content.replace("\n", "\\n"), width=max_len, placeholder="..."
+            )
+            if len(output) < 20:
+                output = msg.content.replace("\n", "\\n")[:max_len] + "..."
+        else:
+            multiline = len(msg.content.split("\n")) > 1
+            output += "\n" + indent * " " if multiline else ""
+            for i, block in enumerate(msg.content.split("```")):
+                if i % 2 == 0:
+                    output += textwrap.indent(block, prefix=indent * " ")
+                elif highlight:
+                    lang = block.split("\n")[0]
+                    console = Console(
+                        file=io.StringIO(), width=shutil.get_terminal_size().columns
+                    )
+                    console.print(Syntax(block.rstrip(), lang))
+                    block = console.file.getvalue()  # type: ignore
+                    output += f"```{block.rstrip()}\n```"
+                else:
+                    output += "```" + block.rstrip() + "\n```"
+        outputs.append(f"\n{userprefix}: {output.rstrip()}")
+    return outputs
+
+
+def print_msg(
+    msg: Message | list[Message],
+    oneline: bool = False,
+    highlight: bool = True,
+    show_hidden: bool = False,
+) -> None:
+    """Prints the log to the console."""
+    # if not tty, force highlight=False (for tests and such)
+    if not sys.stdout.isatty():
+        highlight = False
+
+    msgs = msg if isinstance(msg, list) else [msg]
+    msgstrs = format_msgs(msgs, highlight=highlight, oneline=oneline)
+    skipped_hidden = 0
+    for m, s in zip(msgs, msgstrs):
+        if m.hide and not show_hidden:
+            skipped_hidden += 1
+            continue
+        print(s)
+    if skipped_hidden:
+        print(
+            f"[grey30]Skipped {skipped_hidden} hidden system messages, show with --show-hidden[/]"
+        )
