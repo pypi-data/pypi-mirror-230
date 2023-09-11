815fa38921581e1b341bb3440a87f235416caf39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:21 +0000

    Add package shyft/5.3.0/shyft-5.3.0-cp311-cp311-manylinux1_x86_64.whl

---
+++ packages/shyft/shyft-5.3.0-cp311-cp311-manylinux1_x86_64.whl/shyft/hydrology/viz/data_extractors/__init__.py
@@ -0,0 +1,14 @@
+__all__=[
+    'hindcast_data',
+    'shyft_GeoTsVector_data',
+    'shyft_multi_regmod_data',
+    'shyft_regmod_data',
+    'shyft_TsRepo_data',
+    'shyft_TsVector_data'
+]
+from . import hindcast_data
+from . import shyft_GeoTsVector_data
+from . import shyft_multi_regmod_data
+from . import shyft_regmod_data
+from . import shyft_TsRepo_data
+from . import shyft_TsVector_data
