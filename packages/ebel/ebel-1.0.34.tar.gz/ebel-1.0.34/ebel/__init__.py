9252111df1e0dda296136423a47f158531205bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 15:05:35 +0000

    Add package ebel/1.0.34/ebel-1.0.34.tar.gz

---
+++ packages/ebel/ebel-1.0.34.tar.gz/ebel-1.0.34/ebel/__init__.py
@@ -0,0 +1,18 @@
+"""Root init for eBEL."""
+from . import cache, constants, errors, parser, transformers
+from .manager.orientdb.biodbs.bel import Bel
+
+__version__ = "1.0.34"
+
+__title__ = "e(BE:L)"
+__description__ = "Validation and extension of biomedical knowledge graphs"
+__url__ = "https://github.com/e-bel/ebel"
+
+__author__ = "Christian Ebeling"
+__email__ = "christian.ebeling@scai.fraunhofer.de"
+
+__license__ = "?"
+__copyright__ = """Copyright (c) 2021 Christian Ebeling, Fraunhofer Institute for Algorithms and Scientific
+Computing SCAI, Schloss Birlinghoven, 53754 Sankt Augustin, Germany"""
+
+project_name = __title__
