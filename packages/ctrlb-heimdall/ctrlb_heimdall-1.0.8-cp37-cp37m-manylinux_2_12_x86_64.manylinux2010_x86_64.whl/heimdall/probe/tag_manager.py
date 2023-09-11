cc17579101d6e856694b2bbb932adcbebbf15c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 15:04:46 +0000

    Add package ctrlb-heimdall/1.0.8/ctrlb_heimdall-1.0.8-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/ctrlb-heimdall/ctrlb_heimdall-1.0.8-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/heimdall/probe/tag_manager.py
@@ -0,0 +1,39 @@
+from heimdall.probe.breakpoints.tracepoint import TracePointManager
+from heimdall.probe.breakpoints.logpoint import LogPointManager
+
+from heimdall.utils import get_logger
+logger = get_logger(__name__)
+
+class TagManager(object):
+    __instance = None
+
+    def __init__(self):
+        self.trace_point_manager = TracePointManager.instance()
+        self.log_point_manager = LogPointManager.instance()
+        TagManager.__instance = self
+
+    @staticmethod
+    def instance(*args, **kwargs):
+        return TagManager(*args,**kwargs) if TagManager.__instance is None else TagManager.__instance
+
+    def enable_tag(self, tag, client):
+        self.trace_point_manager.enable_tag(tag, client)
+        self.log_point_manager.enable_tag(tag, client)
+        self._publish_status(client)
+
+    def disable_tag(self, tag, client):
+        self.trace_point_manager.disable_tag(tag, client)
+        self.log_point_manager.disable_tag(tag, client)
+        self._publish_status(client)
+
+    def remove_tag(self, tag, client):
+        self.trace_point_manager.remove_tag(tag, client)
+        self.log_point_manager.remove_tag(tag, client)
+        self._publish_status(client)
+
+    def _publish_status(self, client):
+        self.trace_point_manager.publish_application_status()
+        self.log_point_manager.publish_application_status()
+        if client:
+            self.trace_point_manager.publish_application_status(client)
+            self.log_point_manager.publish_application_status(client)
