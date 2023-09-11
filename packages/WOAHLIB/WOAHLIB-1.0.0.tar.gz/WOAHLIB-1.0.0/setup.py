07bca38f54d0dd5cc0b3a3acdb9ec8fa280c5b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 03:04:30 +0000

    Add package WOAHLIB/1.0.0/WOAHLIB-1.0.0.tar.gz

---
+++ packages/WOAHLIB/WOAHLIB-1.0.0.tar.gz/WOAHLIB-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+print("lemme install that real quick")
+setup(
+    name="WOAHLIB",
+    description="Woah!",
+    url="http://www.example.com/",
+    version="1.0.0",
+    author="Woah",
+    author_email="davidbeumers@gmail.com",
+    license="MIT",
+    packages=["woahlib"],
+    zip_safe = False
+)
+print("yoo letsgo installed!!")
