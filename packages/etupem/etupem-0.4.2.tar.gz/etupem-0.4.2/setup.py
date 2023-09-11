a1d0c7c20e5f5aa61c7e4470caea1ebaa85e9726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:04:27 +0000

    Add package etupem/0.4.2/etupem-0.4.2.tar.gz

---
+++ packages/etupem/etupem-0.4.2.tar.gz/etupem-0.4.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='etupem',
+    version='0.4.2',
+    license='MIT',
+    description='make Easy To Understand Python Error Message for those who are not good at English.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['etupem'],
+    install_requires=['colorama>=0.4.6'],
+    entry_points={'console_scripts': ['pythonja=etupem.ja:runner']},
+    url='https://github.com/nodai2hITC/etupem/',
+    author='nodai2hITC',
+    author_email='nodai2h.itc@gmail.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
