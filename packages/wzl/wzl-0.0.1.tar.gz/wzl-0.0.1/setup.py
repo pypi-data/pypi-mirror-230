f9edaeee5b6da3cfd4d917ef946c39e78dffb741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:04:31 +0000

    Add package wzl/0.0.1/wzl-0.0.1.tar.gz

---
+++ packages/wzl/wzl-0.0.1.tar.gz/wzl-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Anomaly detection for one-dimensional data.'
+
+setup(
+    name="wzl",
+    version=VERSION,
+    author="Zhilin Wang",
+    author_email="wangzhil@iu.edu",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md',encoding="UTF8").read(),
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['python', 'anomaly detection', 'one-dimentional data'],
+    license="MIT",
+    url="https://github.com/wzljerry/xiezhi",
+    #scripts=['cut_video/cut_video.py'],
+    classifiers= [
+        "Programming Language :: Python :: 3",
+    ]
+)
