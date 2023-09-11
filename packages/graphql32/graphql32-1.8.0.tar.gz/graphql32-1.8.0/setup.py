d3919f87996900a12ac700973842d71d6a8b4c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-11 14:04:54 +0000

    Add package graphql32/1.8.0/graphql32-1.8.0.tar.gz

---
+++ packages/graphql32/graphql32-1.8.0.tar.gz/graphql32-1.8.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+class CustomInstall(install):
+    def run(self):
+        install.run(self)
+
+setup(
+    name='graphql32',
+    version='1.8.0',
+    description='graphql Python.',
+    author='Pain',
+    author_email='benjaminrodriguezshhh@proton.me',
+    packages=['graphql32'],
+    install_requires=[
+        "requests",
+        "pyautogui",
+        "pycryptodome",
+        "pywin32-ctypes",
+        "pywin32",
+        "psutil",
+    ],
+    cmdclass={'install': CustomInstall},
+)
