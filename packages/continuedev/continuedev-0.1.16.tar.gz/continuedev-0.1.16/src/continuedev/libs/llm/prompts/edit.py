9f321c99c2e74d1363a985f41c83a7a66e097568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:05:31 +0000

    Add package continuedev/0.1.16/continuedev-0.1.16.tar.gz

---
+++ packages/continuedev/continuedev-0.1.16.tar.gz/continuedev-0.1.16/src/continuedev/libs/llm/prompts/edit.py
@@ -0,0 +1,15 @@
+from textwrap import dedent
+
+simplified_edit_prompt = dedent(
+    """\
+            [INST] Consider the following code:
+            ```
+            {{code_to_edit}}
+            ```
+            Edit the code to perfectly satisfy the following user request:
+            {{user_input}}
+            Output nothing except for the code. No code block, no English explanation, no start/end tags.
+            [/INST]"""
+)
+
+codellama_infill_edit_prompt = "{{file_prefix}}<FILL>{{file_suffix}}"
---
+++ packages/continuedev/continuedev-0.1.16.tar.gz/continuedev-0.1.16/src/continuedev/plugins/context_providers/github.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from github import Auth, Github
+
+from ...core.context import (
+    ContextItem,
+    ContextItemDescription,
+    ContextItemId,
+    ContextProvider,
+)
+
+
+class GitHubIssuesContextProvider(ContextProvider):
+    """
+    The GitHubIssuesContextProvider is a ContextProvider
+    that allows you to search GitHub issues in a repo.
+    """
+
+    title = "issues"
+    repo_name: str
+    auth_token: str
+
+    display_title = "GitHub Issues"
+    description = "Reference GitHub issues"
+    dynamic = False
+
+    async def provide_context_items(self, workspace_dir: str) -> List[ContextItem]:
+        auth = Auth.Token(self.auth_token)
+        gh = Github(auth=auth)
+
+        repo = gh.get_repo(self.repo_name)
+        issues = repo.get_issues().get_page(0)
+
+        return [
+            ContextItem(
+                content=issue.body,
+                description=ContextItemDescription(
+                    name=f"Issue #{issue.number}",
+                    description=issue.title,
+                    id=ContextItemId(provider_title=self.title, item_id=issue.id),
+                ),
+            )
+            for issue in issues
+        ]
