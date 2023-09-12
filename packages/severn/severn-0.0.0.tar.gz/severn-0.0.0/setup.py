52bfbca3d265c2086c689feeb4f26898bfbed925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:07:49 +0000

    Add package severn/0.0.0/severn-0.0.0.tar.gz

---
+++ packages/severn/severn-0.0.0.tar.gz/severn-0.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="severn",
+    version="0.0.0",
+    description="WIP!",
+    author="Ethan Henderson, Jonxslays",
+    classifiers=["Development Status :: 1 - Planning"],
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+)
