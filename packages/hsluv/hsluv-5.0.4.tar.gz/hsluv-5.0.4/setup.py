6a0dc0004ed0ff26166e90e7a3b5a0be3bd619ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:06:52 +0000

    Add package hsluv/5.0.4/hsluv-5.0.4.tar.gz

---
+++ packages/hsluv/hsluv-5.0.4.tar.gz/hsluv-5.0.4/setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+from setuptools import setup
+
+from hsluv import __version__
+
+setup(
+    name='hsluv',
+    version=__version__,
+    description='Human-friendly HSL',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    author_email="alexei@boronine.com",
+    url="https://www.hsluv.org",
+    keywords="color hsl cie cieluv colorwheel hsluv hpluv",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    python_requires='>=3.7',
+    setup_requires=[
+        'setuptools>=38.6.0',  # for long_description_content_type
+    ],
+    py_modules=["hsluv"],
+    test_suite="tests.test_hsluv",
+    project_urls={
+        "Bug Tracker": "https://github.com/hsluv/hsluv-python/issues",
+        "Source Code": "https://github.com/hsluv/hsluv-python",
+    }
+)
---
+++ packages/hsluv/hsluv-5.0.4.tar.gz/hsluv-5.0.4/tests/test_hsluv.py
@@ -0,0 +1,85 @@
+import unittest
+import json
+import os.path
+
+
+from hsluv import (hex_to_hpluv, hex_to_hsluv, hex_to_rgb, hpluv_to_hex,
+                   hpluv_to_lch, hpluv_to_rgb, hsluv_to_hex, hsluv_to_lch,
+                   hsluv_to_rgb, lch_to_hpluv, lch_to_hsluv, lch_to_luv,
+                   luv_to_lch, luv_to_xyz, rgb_to_hex, rgb_to_xyz,
+                   xyz_to_luv, xyz_to_rgb)
+from hsluv import (_hsluv_to_rgb, _hpluv_to_rgb)  # no normalized output
+
+rgb_range_tolerance = 1e-11
+# Note: we had 1e-11 precision before, but lowered it for FreeBSD: https://github.com/hsluv/hsluv/issues/87
+snapshot_tolerance = 1e-10
+
+
+class TestHsluv(unittest.TestCase):
+    def setUp(self):
+        # Load snapshot into memory
+        name = os.path.join(os.path.dirname(__file__), 'snapshot-rev4.json')
+        json_data = open(name)
+        self.snapshot = json.load(json_data)
+        json_data.close()
+
+    def test_within_rgb_range(self):
+        for h in range(0, 361, 5):
+            for s in range(0, 101, 5):
+                for l in range(0, 101, 5):
+                    for func in [_hsluv_to_rgb, _hpluv_to_rgb]:
+                        hsl = [h, s, l]
+                        rgb = func(hsl)
+                        for channel in rgb:
+                            in_range = -rgb_range_tolerance <= channel <= 1 + rgb_range_tolerance
+                            assert in_range, (hsl, rgb)
+                    for func in [hsluv_to_rgb, hpluv_to_rgb]:
+                        hsl = h, s, l
+                        rgb = func(hsl)
+                        for channel in rgb:
+                            self.assertLessEqual(channel, 1)
+                            self.assertLessEqual(0, channel)
+
+    def test_snapshot(self):
+        for hex_color, colors in self.snapshot.items():
+            # Test forward functions
+            test_rgb = hex_to_rgb(hex_color)
+            self.assert_tuples_close(test_rgb, colors['rgb'])
+            test_xyz = rgb_to_xyz(test_rgb)
+            self.assert_tuples_close(test_xyz, colors['xyz'])
+            test_luv = xyz_to_luv(test_xyz)
+            self.assert_tuples_close(test_luv, colors['luv'])
+            test_lch = luv_to_lch(test_luv)
+            self.assert_tuples_close(test_lch, colors['lch'])
+            test_hsluv = lch_to_hsluv(test_lch)
+            self.assert_tuples_close(test_hsluv, colors['hsluv'])
+            test_hpluv = lch_to_hpluv(test_lch)
+            self.assert_tuples_close(test_hpluv, colors['hpluv'])
+
+            # Test backward functions
+            test_lch = hsluv_to_lch(colors['hsluv'])
+            self.assert_tuples_close(test_lch, colors['lch'])
+            test_lch = hpluv_to_lch(colors['hpluv'])
+            self.assert_tuples_close(test_lch, colors['lch'])
+            test_luv = lch_to_luv(test_lch)
+            self.assert_tuples_close(test_luv, colors['luv'])
+            test_xyz = luv_to_xyz(test_luv)
+            self.assert_tuples_close(test_xyz, colors['xyz'])
+            test_rgb = xyz_to_rgb(test_xyz)
+            self.assert_tuples_close(test_rgb, colors['rgb'])
+            self.assertEqual(rgb_to_hex(test_rgb), hex_color)
+
+            # Full test
+            self.assertEqual(hsluv_to_hex(colors['hsluv']), hex_color)
+            self.assert_tuples_close(hex_to_hsluv(hex_color), colors['hsluv'])
+            self.assertEqual(hpluv_to_hex(colors['hpluv']), hex_color)
+            self.assert_tuples_close(hex_to_hpluv(hex_color), colors['hpluv'])
+
+    def assert_tuples_close(self, tup1, tup2):
+        for a, b in zip(tup1, tup2):
+            if abs(a - b) > snapshot_tolerance:
+                raise Exception(f"Mismatch: {a} {b}")
+
+
+if __name__ == '__main__':
+    unittest.main()
