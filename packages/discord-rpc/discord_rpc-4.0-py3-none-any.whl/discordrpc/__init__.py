995cd71558bccefee3facb86ea247c3c5db10b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 15:05:21 +0000

    Add package discord-rpc/4.0/discord_rpc-4.0-py3-none-any.whl

---
+++ packages/discord-rpc/discord_rpc-4.0-py3-none-any.whl/discordrpc/__init__.py
@@ -0,0 +1,9 @@
+from .presence import RPC
+from .button import button
+from .exceptions import *
+
+__title__ = "Discord RPC"
+__version__ = "4.0"
+__authors__ = "LyQuid"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2021-present LyQuid"
