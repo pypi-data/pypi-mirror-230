8454f00de15d592bc8239e13b4342932d3839ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:06:33 +0000

    Add package dz-dynamodb/1.4.4/dz-dynamodb-1.4.4.tar.gz

---
+++ packages/dz-dynamodb/dz-dynamodb-1.4.4.tar.gz/dz-dynamodb-1.4.4/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="dz-dynamodb",
+    version="1.4.4",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["dz_dynamodb"],
+    install_requires=[
+        'boto3==1.14.9',
+        "singer-python==5.9.0",
+        'terminaltables==3.1.0',
+        'backoff==1.8.0',
+    ],
+    extras_require={
+        'dev': [
+            'ipdb',
+            'pylint',
+            'nose'
+        ]
+    },
+    entry_points="""
+    [console_scripts]
+    dz-dynamodb=dz_dynamodb:main
+    """,
+    packages=["dz_dynamodb", 'dz_dynamodb.sync_strategies'],
+    package_data = {},
+    include_package_data=True,
+)
