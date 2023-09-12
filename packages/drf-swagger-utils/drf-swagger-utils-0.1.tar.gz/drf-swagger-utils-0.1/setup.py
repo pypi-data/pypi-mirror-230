35199ac092bf97ca194d1ebc25c35403964965de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:06:10 +0000

    Add package drf-swagger-utils/0.1/drf-swagger-utils-0.1.tar.gz

---
+++ packages/drf-swagger-utils/drf-swagger-utils-0.1.tar.gz/drf-swagger-utils-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drf-swagger-utils',
+    version='0.1',
+    author='Chinni Raja Ammela',
+    description='Custom Django REST framework generators',
+    packages=find_packages(),
+    install_requires=['Django>=1.11']
+)
