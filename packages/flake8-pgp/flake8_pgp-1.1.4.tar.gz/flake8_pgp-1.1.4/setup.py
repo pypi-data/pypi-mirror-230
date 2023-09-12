f8d95d93099a7400f0f4928f7ac40cd90c856636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:06:03 +0000

    Add package flake8-pgp/1.1.4/flake8_pgp-1.1.4.tar.gz

---
+++ packages/flake8-pgp/flake8_pgp-1.1.4.tar.gz/flake8_pgp-1.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    install_requires=["flake8 > 3.0.0"],
+    name="flake8_pgp",
+    license="MIT",
+    version="1.1.4",
+    description="flake8 plugin which checks for various bad python practises",
+    url="https://github.com/UP929312/pbp",
+    packages=find_packages(),
+    classifiers=[
+        "Framework :: Flake8",
+    ],
+    entry_points={
+        'flake8.extension': [
+            'PBP1 = src.flake8_pbp:ProduceBetterPythonPlugin',
+        ],
+    },
+)
