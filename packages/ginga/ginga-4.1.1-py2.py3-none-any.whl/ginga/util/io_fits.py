c71d309e2feab1d4097f782c47fbe789ffe4bfeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:07:30 +0000

    Add package ginga/4.1.1/ginga-4.1.1-py2.py3-none-any.whl

---
+++ packages/ginga/ginga-4.1.1-py2.py3-none-any.whl/ginga/util/io_fits.py
@@ -0,0 +1,6 @@
+import warnings
+warnings.warn("this module has moved; "
+              "change your import to ginga.util.io.io_fits",
+              DeprecationWarning)
+
+from ginga.util.io.io_fits import *   # noqa
---
+++ packages/ginga/ginga-4.1.1-py2.py3-none-any.whl/ginga/util/io_rgb.py
@@ -0,0 +1,6 @@
+import warnings
+warnings.warn("this module has moved; "
+              "change your import to ginga.util.io.io_rgb",
+              DeprecationWarning)
+
+from ginga.util.io.io_rgb import *   # noqa
