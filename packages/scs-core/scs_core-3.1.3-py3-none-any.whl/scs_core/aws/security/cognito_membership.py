0e16350d4dabc94143b9020f0df0ae602fa55204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:07:19 +0000

    Add package scs-core/3.1.3/scs_core-3.1.3-py3-none-any.whl

---
+++ packages/scs-core/scs_core-3.1.3-py3-none-any.whl/scs_core/aws/security/cognito_membership.py
@@ -0,0 +1,72 @@
+"""
+Created on 2 Feb 2023
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+example document:
+"""
+
+from collections import OrderedDict
+
+from scs_core.data.array_dict import ArrayDict
+from scs_core.data.json import JSONable
+from scs_core.data.str import Str
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class CognitoMembership(JSONable):
+    """
+    classdocs
+    """
+
+    @classmethod
+    def merge(cls, cognito_accounts, org_memberships):
+        org_dict = ArrayDict([(org_membership.username, org_membership) for org_membership in sorted(org_memberships)])
+
+        # Accounts...
+        return [cls(cognito_account, org_dict.get(cognito_account.username)) for cognito_account in cognito_accounts]
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, cognito_account, memberships):
+        """
+        Constructor
+        """
+        self.__cognito_account = cognito_account            # CognitoUserIdentity or CognitoDeviceIdentity
+        self.__memberships = memberships                    # array of OrganisationUser
+
+
+    def __lt__(self, other):
+        return self.cognito_account < other.cognito_account
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def as_json(self):
+        jdict = OrderedDict()
+
+        jdict['account'] = self.cognito_account
+        jdict['memberships'] = self.memberships
+
+        return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def cognito_account(self):
+        return self.__cognito_account
+
+
+    @property
+    def memberships(self):
+        return self.__memberships
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "CognitoMembership:{cognito_account:%s, memberships:%s}" % \
+               (self.cognito_account, Str.collection(self.memberships))
