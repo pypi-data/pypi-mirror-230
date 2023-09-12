67f3c4cd6ae45c1add4c58c8659441df3b5044d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 15:07:17 +0000

    Add package embedchain/0.0.56/embedchain-0.0.56-py3-none-any.whl

---
+++ packages/embedchain/embedchain-0.0.56-py3-none-any.whl/embedchain/loaders/pdf_file.py
@@ -0,0 +1,28 @@
+from langchain.document_loaders import PyPDFLoader
+
+from embedchain.helper.json_serializable import register_deserializable
+from embedchain.loaders.base_loader import BaseLoader
+from embedchain.utils import clean_string
+
+
+@register_deserializable
+class PdfFileLoader(BaseLoader):
+    def load_data(self, url):
+        """Load data from a PDF file."""
+        loader = PyPDFLoader(url)
+        output = []
+        pages = loader.load_and_split()
+        if not len(pages):
+            raise ValueError("No data found")
+        for page in pages:
+            content = page.page_content
+            content = clean_string(content)
+            meta_data = page.metadata
+            meta_data["url"] = url
+            output.append(
+                {
+                    "content": content,
+                    "meta_data": meta_data,
+                }
+            )
+        return output
