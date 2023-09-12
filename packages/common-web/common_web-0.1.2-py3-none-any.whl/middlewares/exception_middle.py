0f19875ceed030a89b9d16e2e2126fd23a65618a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 15:06:34 +0000

    Add package common-web/0.1.2/common_web-0.1.2-py3-none-any.whl

---
+++ packages/common-web/common_web-0.1.2-py3-none-any.whl/middlewares/exception_middle.py
@@ -0,0 +1,28 @@
+from django.utils.deprecation import MiddlewareMixin
+
+from dto.res_dto import ResObj
+from exception.auth_exception import AuthException
+from exception.business_exception import BusinessException
+
+SYSTEM_EXCEPTION_CODE = 999
+BUSINESS_EXCEPTION_CODE = 500
+AUTH_EXCEPTION_CODE = 400
+
+
+class ExceptionMiddle(MiddlewareMixin):
+    """
+    全局拦截异常信息 For Django
+    """
+
+    def process_exception(self, request, exception):
+        print('exception is:', exception)
+        if isinstance(exception, BusinessException):
+            code = BUSINESS_EXCEPTION_CODE
+            message = exception.value
+        elif isinstance(exception, AuthException):
+            code = AUTH_EXCEPTION_CODE
+            message = exception.value
+        else:
+            code = SYSTEM_EXCEPTION_CODE
+            message = "system error"
+        return ResObj(code=code, message=message).json()
