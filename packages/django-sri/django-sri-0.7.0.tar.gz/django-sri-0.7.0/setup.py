9f33e318eb9946c64240afd0f5bc511ddf880fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 16:05:10 +0000

    Add package django-sri/0.7.0/django-sri-0.7.0.tar.gz

---
+++ packages/django-sri/django-sri-0.7.0.tar.gz/django-sri-0.7.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="django-sri",
+    version="0.7.0",
+    url="https://github.com/RealOrangeOne/django-sri",
+    author="Jake Howard",
+    description="Subresource Integrity for Django",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="BSD",
+    packages=find_packages(include=["sri*"]),
+    package_data={"sri": ["py.typed"]},
+    install_requires=["Django>=3.2"],
+    python_requires=">=3.8",
+    keywords="django subresource integrity sri",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development",
+        "Typing :: Typed",
+    ],
+)
---
+++ packages/django-sri/django-sri-0.7.0.tar.gz/django-sri-0.7.0/sri/hashers.py
@@ -0,0 +1,42 @@
+import base64
+import hashlib
+from functools import lru_cache
+from pathlib import Path
+
+from sri.algorithm import Algorithm
+from sri.utils import get_cache
+
+HASHERS = {
+    Algorithm.SHA256: hashlib.sha256,
+    Algorithm.SHA384: hashlib.sha384,
+    Algorithm.SHA512: hashlib.sha512,
+}
+
+READ_BUFFER_SIZE = 2**18  # 256k - matches `hashlib.file_digest`
+
+
+def calculate_hash(path: Path, algorithm: Algorithm) -> str:
+    cache = get_cache()
+    cache_key = get_cache_key(path, algorithm)
+    file_hash = cache.get(cache_key)
+    if file_hash is None:
+        # Cache miss, do the calculation
+        with path.open("rb") as f:
+            if hasattr(hashlib, "file_digest"):
+                hasher = hashlib.file_digest(f, HASHERS[algorithm])
+            else:
+                hasher = HASHERS[algorithm]()
+                while True:
+                    data = f.read(READ_BUFFER_SIZE)
+                    if not data:
+                        break
+                    hasher.update(data)
+        file_hash = base64.b64encode(hasher.digest()).decode()
+        cache.set(cache_key, file_hash)
+    return file_hash
+
+
+@lru_cache(maxsize=None)
+def get_cache_key(path: Path, algorithm: Algorithm) -> str:
+    path_hash = hashlib.sha1(str(path).encode()).hexdigest()
+    return f"sri-{path_hash}-{algorithm.value}"
