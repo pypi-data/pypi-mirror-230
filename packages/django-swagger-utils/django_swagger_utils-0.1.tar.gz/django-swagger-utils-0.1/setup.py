fe9a4fb8b45a628c7a2e3900f9fb519dee5e9cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:06:10 +0000

    Add package django-swagger-utils/0.1/django_swagger_utils-0.1.tar.gz

---
+++ packages/django-swagger-utils/django_swagger_utils-0.1.tar.gz/django-swagger-utils-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-swagger-utils',
+    version='0.1',
+    author='Your Name',
+    description='Custom Django REST framework generators',
+    packages=find_packages(),
+    install_requires=['Django>=1.11']
+)
