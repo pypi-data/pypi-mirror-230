457e8be5c88ece826f8bdac8a411621a900c8c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:06:29 +0000

    Add package kot/0.40.3/kot-0.40.3.tar.gz

---
+++ packages/kot/kot-0.40.3.tar.gz/kot-0.40.3/kot/__init__.py
@@ -0,0 +1,15 @@
+from .kot import KOT
+from .kot import HASHES
+from .kot import KOT_Serial
+from .kot import KOT_Remote
+from .kot import KOT_Cloud
+from .kot import KOT_Cloud_Pro
+from .kot import KOT_Cloud_Dedicated
+from .kot import KOT_Cloud_Dedicated_Prepare
+
+from .helper import no_exception
+from .helper import requires
+
+__version__ = '0.40.3'
+
+
