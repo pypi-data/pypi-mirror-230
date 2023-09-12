fa0f4c9c075d7fced60c283469a5cfd0938ff964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 15:05:21 +0000

    Add package discord-rpc/4.0/discord-rpc-4.0.tar.gz

---
+++ packages/discord-rpc/discord-rpc-4.0.tar.gz/discord-rpc-4.0/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+import re
+
+with open('README.md') as f:
+    long_description = f.read()
+
+version = ''
+with open('discordrpc/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Version is not set')
+
+def read_requirements():
+    with open('requirements.txt', 'r') as req:
+        content = req.read()
+        requirements = content.split('\n')
+
+    return requirements
+
+
+
+setup(
+    name='discord-rpc',
+    version=version,
+    author='LyQuid',
+    author_email='lyquidpersonal@gmail.com',
+    description='An Python wrapper for Discord RPC',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    url='https://github.com/LyQuid12/discord-rpc',
+    project_urls={
+        "Discord": "https://discord.gg/qpT2AeYZRN",
+        "Issue tracker": "https://github.com/LyQuid12/discord-rpc/issues",
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    keywords=["Discord", "rpc", "discord rpc"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python'
+    ]
+)
