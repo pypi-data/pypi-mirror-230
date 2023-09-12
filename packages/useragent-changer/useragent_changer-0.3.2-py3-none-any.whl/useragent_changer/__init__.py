77ae0fefbd471e3040bbdf2fac8d4f8daee746ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 16:05:09 +0000

    Add package useragent-changer/0.3.2/useragent_changer-0.3.2-py3-none-any.whl

---
+++ packages/useragent-changer/useragent_changer-0.3.2-py3-none-any.whl/useragent_changer/__init__.py
@@ -0,0 +1,11 @@
+# Make the module name 'useragent_changer' optional.
+from .useragent_changer import *
+
+__copyright__    = 'Copyright (C) 2023 Yuki Moriya'
+__version__      = '0.3.2'
+__description__  = 'User-Agent changing library for Python.'
+__author__       = 'Yuki Moriya'
+__author_email__ = 'ym19820219@gmail.com'
+__keywords__     = 'useragent user_agent user-agent ua useragent_change useragent_changer useragent-changer useragent-change'
+__url__          = 'https://github.com/gitmori/useragent-changer'
+__license__      = 'MIT'
