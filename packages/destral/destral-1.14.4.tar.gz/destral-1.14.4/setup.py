ebd8e1be97e1f3a3861d968b4308ba3295111bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 16:04:36 +0000

    Add package destral/1.14.4/destral-1.14.4.tar.gz

---
+++ packages/destral/destral-1.14.4.tar.gz/destral-1.14.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='destral',
+    version='1.14.4',
+    packages=find_packages(),
+    url='https://github.com/gisce/destral',
+    install_requires=[
+        'requests',
+        'osconf',
+        'expects',
+        'six',
+        'click',
+        'mamba<0.11.0',
+        'coverage',
+        'lazy-object-proxy<1.7.0',
+        'pylint',
+        'python-dateutil',
+        'babel>=2.4.0',
+        'junit_xml',
+        'psycopg2',
+    ],
+    license='GNU GPLv3',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    entry_points={
+        'console_scripts': [
+            'destral = destral.cli:destral'
+        ]
+    },
+    description='OpenERP testing framework'
+)
