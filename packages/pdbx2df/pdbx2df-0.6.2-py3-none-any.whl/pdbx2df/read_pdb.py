3c10124b1a8da45fe69d43b6beddc255e7dd2796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:05:48 +0000

    Add package pdbx2df/0.6.2/pdbx2df-0.6.2-py3-none-any.whl

---
+++ packages/pdbx2df/pdbx2df-0.6.2-py3-none-any.whl/pdbx2df/read_pdb.py
@@ -0,0 +1,312 @@
+# pdbx2df
+# Author: Ruibin Liu <ruibinliuphd@gmail.com>
+# License: MIT
+# Code Repository: https://github.com/Ruibin-Liu/pdbx2df
+"""PDB format reading."""
+from __future__ import annotations
+
+import io
+import os
+import urllib.request
+import warnings
+from collections import defaultdict
+from pathlib import Path
+
+import numpy as np  # type: ignore
+import pandas as pd  # type: ignore
+
+from .constants import AMINO_ACIDS
+
+IMPLEMENTED_PDB_CATS = ["_atom_site", "_seq_res"]
+ATOM_SITE = ("ATOM", "HETATM", "TER")
+NMR_MDL = ("NUMMDL", "MODEL", "ENDMDL")
+AF2_MODEL = 4
+
+
+def read_pdb(
+    pdb_file: str | os.PathLike[str] | None = None,
+    pdb_id: str | None = None,
+    category_names: list | None = None,
+    save_pdb_file: bool = True,
+    pdb_file_dir: str | os.PathLike | None = None,
+    allow_chimera: bool = True,
+    need_ter_lines: bool = True,
+) -> dict[str, pd.DataFrame]:
+    """
+    Read a pdb file's categories into a dict of Pandas DataFrames.
+
+    Args:
+        pdb_id (str|None; defaults to None): PDB/Uniprot ID. Required if pdb_file is None.
+        pdb_file (str|os.PathLike[str]|None; defaults to None): file name for a PDB file. Used over pdb_id.
+        category_names (list|None; defaults to None): a list of names for the categories as to the mmCIF file format.
+            If None, "_atom_site" is used.
+            To be consistent with the PDBx file format, the following category names are used to refer
+            to block(s) in a PDB file and only they are supported:
+            1. _atom_site: 'ATOM', 'HETATM', and 'TER' lines
+            2. TBD
+        save_pdb_file(bool; defaults to True): whether to save the fetched PDB file to pdb_file_dir.
+        pdb_file_dir(str|os.PathLike|None; defaults to None): directory to save fetched PDB files.
+        allow_chimera (bool; defaults to True): whether to allow Chimera-formatted PDB files.
+        need_ter_lines (bool; defaults to True): whether to read the TER lines into the DataFrame.
+
+    Returns:
+        dict[str, pd.DataFrame]: A dict of {category_name: pd.DataFrame of the info belongs to the category}
+    """  # noqa
+    data: dict[str, pd.DataFrame] = {}
+    if pdb_id is None and pdb_file is None:
+        raise ValueError("At least one of pdb_id and pdb_file has to be given.")
+    elif pdb_file is None:
+        pdb_id = str(pdb_id).upper()
+        if len(pdb_id) == 4:
+            pdb_file_url = f"https://files.rcsb.org/view/{pdb_id.upper()}.pdb"
+        else:
+            pdb_file_url = f"https://alphafold.ebi.ac.uk/files/AF-{pdb_id.upper()}-F1-model_v{AF2_MODEL}.pdb"
+        try:
+            with urllib.request.urlopen(pdb_file_url) as response:
+                raw_data = response.read()
+            text = raw_data.decode("utf-8")
+            pdb_file_handle: io.TextIOWrapper | io.StringIO = io.StringIO(text)
+            if save_pdb_file:
+                if pdb_file_dir is None:
+                    pdb_file_dir = "./PDB_files"
+                pdb_file_dir = Path(pdb_file_dir)
+                if not pdb_file_dir.exists():
+                    pdb_file_dir.mkdir(parents=True, exist_ok=True)
+                file_path = Path(pdb_file_dir, f"{pdb_id}.pdb")
+                with open(file_path, "w", encoding="utf-8") as p_file:
+                    p_file.write(text)
+        except urllib.error.HTTPError as http_error:
+            raise ValueError(
+                f"Cannot download PDB file from url {pdb_file_url}."
+            ) from http_error
+    else:
+        pdb_file = Path(pdb_file)
+        if not pdb_file.exists():
+            raise FileNotFoundError(f"File {pdb_file} not found.")
+        pdb_file_handle = open(pdb_file, "r", encoding="utf-8")
+    if category_names is None:
+        category_names = ["_atom_site"]
+    for category_name in category_names:
+        if category_name not in IMPLEMENTED_PDB_CATS:
+            implemented = ", ".join(IMPLEMENTED_PDB_CATS)
+            raise NotImplementedError(
+                f"Only {implemented} are implemented for the PDB format."
+            )
+        data[category_name] = pd.DataFrame()
+
+    num_nmr_models = 0
+    n_model_lines = 0
+    nmr_model = -1  # -1 means there is no NMR model
+    n_endmdl_lines = 0
+    # atom_site_rows = []
+    column_name_types = [
+        ("record_name", "U6"),
+        ("atom_number", "i4"),
+        ("atom_name", "U4"),
+        ("alt_loc", "U1"),
+        ("residue_name", "U4"),
+        ("chain_id", "U1"),
+        ("residue_number", "i2"),
+        ("insertion", "U1"),
+        ("x_coord", "f4"),
+        ("y_coord", "f4"),
+        ("z_coord", "f4"),
+        ("occupancy", "f4"),
+        ("b_factor", "f4"),
+        ("segment_id", "U4"),
+        ("element_symbol", "U2"),
+        ("charge", "U2"),
+        ("nmr_model", "i2"),
+    ]
+
+    if not need_ter_lines:
+        all_atom_site_reads = ("ATOM", "HETATM") + NMR_MDL
+    else:
+        all_atom_site_reads = ATOM_SITE + NMR_MDL  # type: ignore
+
+    if pdb_file is not None:  # This check is just for mypy
+        file_stat = os.stat(pdb_file)
+        total_lines = int(file_stat.st_size / 81)
+    else:
+        total_lines = 100000
+    n_record = 0
+    n_lines_till_atom_lines = 0
+    with pdb_file_handle:
+        line = pdb_file_handle.readline()
+        for line in pdb_file_handle:
+            if line.startswith(all_atom_site_reads) and "_atom_site" in category_names:
+                if (not n_record) and line.startswith(("ATOM", "HETATM", "MODEL")):
+                    array = np.zeros(
+                        total_lines - n_lines_till_atom_lines, column_name_types
+                    )
+                if line.startswith(("ATOM", "HETATM")):
+                    array[n_record] = _split_atom_line(
+                        line,
+                        nmr_model=nmr_model,
+                        allow_chimera=allow_chimera,
+                        is_ter_line=False,
+                    )
+                    n_record += 1
+                elif need_ter_lines and line.startswith("TER"):
+                    array[n_record] = _split_atom_line(
+                        line,
+                        nmr_model=nmr_model,
+                        allow_chimera=allow_chimera,
+                        is_ter_line=True,
+                    )
+                    n_record += 1
+                elif line.startswith("MODEL"):
+                    n_model_lines += 1
+                    nmr_model = int(line[6:].strip())
+                elif line.startswith("ENDMDL"):
+                    n_endmdl_lines += 1
+                    if num_nmr_models and n_endmdl_lines == num_nmr_models:
+                        break
+                else:  # line.startswith("NUMMDL")
+                    num_nmr_models = int(line[6:].strip())
+            elif line.startswith("SEQRES") and "_seq_res" in category_names:
+                chain_residues: dict[str, list[str]] = defaultdict(list)
+                chain_lengths: dict[str, int] = defaultdict(int)
+                while line.startswith("SEQRES"):
+                    items: list[str] = line.strip().split()
+                    chain_id: str = items[2]
+                    chain_length: int = int(items[3])
+                    chain_lengths[chain_id] = chain_length
+                    chain_residues[chain_id].extend(items[4:])
+                    if not n_record and "_atom_site" in category_names:
+                        n_lines_till_atom_lines += 1
+                    line = pdb_file_handle.readline()
+
+            if not n_record and "_atom_site" in category_names:
+                n_lines_till_atom_lines += 1
+
+    if (
+        "_atom_site" in category_names
+        and num_nmr_models != n_model_lines
+        and num_nmr_models != 0
+    ):
+        warnings.warn(
+            f"The NUMMDL says {num_nmr_models} NMR models, but {n_model_lines} found.",
+            RuntimeWarning,
+            stacklevel=2,
+        )
+
+    if "_atom_site" in category_names:
+        df_atom_site = pd.DataFrame.from_records(array[:n_record])
+        data["_atom_site"] = df_atom_site
+        if not n_model_lines:
+            data["_atom_site"].drop(columns=["nmr_model"], inplace=True)
+
+    if "_seq_res" in category_names:
+        chain_sequences: dict[str, str] = {}
+        for chain_id, residues in chain_residues.items():
+            chain_sequences[chain_id] = "".join([AMINO_ACIDS[i] for i in residues])
+            if len(residues) != chain_lengths[chain_id]:
+                warnings.warn(
+                    f"Cols 14-17 value {chain_lengths[chain_id]} contrasts real length {len(residues)}.",
+                    RuntimeWarning,
+                    stacklevel=2,
+                )
+        chain_ids = chain_sequences.keys()
+        chain_num_residues = [len(chain_sequences[chain_id]) for chain_id in chain_ids]
+        chain_seqs = [chain_sequences[chain_id] for chain_id in chain_ids]
+        data["_seq_res"] = pd.DataFrame(
+            {
+                "chain_id": chain_ids,
+                "chain_sequence": chain_seqs,
+                "chain_length": chain_num_residues,
+            }
+        )
+
+    return data
+
+
+def _split_atom_line(
+    line: str,
+    nmr_model: int = -1,
+    allow_chimera: bool = True,
+    is_ter_line: bool = False,
+) -> tuple:
+    """Internal function to parse a single line belonging to 'ATOM', 'HETATM', or 'TER' lines
+
+    Args:
+        line (str): A 'ATOM', 'HETATM', or 'TER' line.
+        nmr_model (int; defaults to -1): the NMR model number for the line; default -1 means not an NMR model
+        allow_chimera (bool; defaults to True): try to parse as a Chimera-formatted PDB file.
+        is_ter_line (bool; defaults to False): whether the line starts with 'TER'.
+
+    Returns:
+        tuple: parsed values
+    """
+    if not (is_ter_line or allow_chimera):
+        return (
+            line[0:6],
+            line[6:11],
+            line[12:16],
+            line[16],
+            line[17:20],
+            line[21],
+            line[22:26],
+            line[26],
+            line[30:38],
+            line[38:46],
+            line[46:54],
+            line[54:60],
+            line[60:66],
+            line[72:76],
+            line[76:78],
+            line[78:80],
+            nmr_model,
+        )
+    elif is_ter_line:
+        if allow_chimera:
+            record_name = line[0:5] + " "
+            residue_name = line[17:21]
+        else:
+            record_name = line[0:6]
+            residue_name = line[17:20]
+        return (
+            record_name,
+            line[6:11],
+            line[12:16],
+            line[16],
+            residue_name,
+            line[21],
+            line[22:26],
+            line[26],
+            None,
+            None,
+            None,
+            None,
+            None,
+            line[72:76],
+            line[76:78],
+            line[78:80],
+            nmr_model,
+        )
+    else:
+        if line[0:5] == "HETAT":
+            record_name = "HETATM"
+            atom_number = line[6:11]
+        else:
+            record_name = line[0:5] + " "
+            atom_number = line[5:11]
+        return (
+            record_name,
+            atom_number,
+            line[12:16],
+            line[16],
+            line[17:21],
+            line[21],
+            line[22:26],
+            line[26],
+            line[30:38],
+            line[38:46],
+            line[46:54],
+            line[54:60],
+            line[60:66],
+            line[72:76],
+            line[76:78],
+            line[78:80],
+            nmr_model,
+        )
---
+++ packages/pdbx2df/pdbx2df-0.6.2-py3-none-any.whl/pdbx2df/read_pdbx.py
@@ -0,0 +1,198 @@
+# pdbx2df
+# Author: Ruibin Liu <ruibinliuphd@gmail.com>
+# License: MIT
+# Code Repository: https://github.com/Ruibin-Liu/pdbx2df
+"""PDBx/mmCIF format reading."""
+from __future__ import annotations
+
+import io
+import os
+import urllib.request
+from collections import defaultdict
+from pathlib import Path
+
+import pandas as pd  # type: ignore
+
+from .split_line import split_line
+
+AF2_MODEL = 4
+
+
+def read_pdbx(
+    pdbx_file: str | os.PathLike[str] | None = None,
+    pdb_id: str | None = None,
+    save_pdbx_file: bool = True,
+    pdbx_file_dir: str | os.PathLike | None = None,
+    category_names: list | None = None,
+) -> dict[str, pd.DataFrame]:
+    """
+    Read a pdbx file's categories into a dict of Pandas DataFrames.
+
+    Args:
+        pdb_id (str|None; defaults to None): PDB/Uniprot ID. Required if pdbx_file is None.
+        pdbx_file (str|os.PathLike[str]|None; defaults to None): file name for a PDBx/mmCIF file. Used over pdb_id.
+        category_names (list|None; defaults to None): a list of names for the categories in the mmCIF file format.
+            If None, "all" is used and all categories will be processed.
+        save_pdbx_file(bool; defaults to True): whether to save the fetched PDBx file to pdbx_file_dir.
+        pdbx_file_dir(str|os.PathLike|None; defaults to None): directory to save fetched PDBx files.
+
+    Returns:
+        dict[str, pd.DataFrame]: A dict of {category_name: pd.DataFrame of the info belongs to the category}
+    """  # noqa
+    data: dict[str, pd.DataFrame] = {}
+    if pdb_id is None and pdbx_file is None:
+        raise ValueError("At least one of pdb_id and pdbx_file has to be given.")
+    elif pdbx_file is None:
+        pdb_id = str(pdb_id).upper()
+        if len(pdb_id) == 4:
+            pdbx_file_url = f"https://files.rcsb.org/view/{pdb_id.upper()}.cif"
+        else:
+            pdbx_file_url = f"https://alphafold.ebi.ac.uk/files/AF-{pdb_id.upper()}-F1-model_v{AF2_MODEL}.cif"
+        try:
+            with urllib.request.urlopen(pdbx_file_url) as response:
+                raw_data = response.read()
+            text = raw_data.decode("utf-8")
+            pdbx_file_handle: io.TextIOWrapper | io.StringIO = io.StringIO(text)
+            if save_pdbx_file:
+                if pdbx_file_dir is None:
+                    pdbx_file_dir = "./PDBx_files"
+                pdbx_file_dir = Path(pdbx_file_dir)
+                if not pdbx_file_dir.exists():
+                    pdbx_file_dir.mkdir(parents=True, exist_ok=True)
+                file_path = Path(pdbx_file_dir, f"{pdb_id}.cif")
+                with open(file_path, "w", encoding="utf-8") as p_file:
+                    p_file.write(text)
+        except urllib.error.HTTPError as http_error:
+            raise ValueError(
+                f"Cannot download PDBx file from url {pdbx_file_url}."
+            ) from http_error
+    else:
+        pdbx_file = Path(pdbx_file)
+        if not pdbx_file.exists():
+            raise FileNotFoundError(f"File {pdbx_file} not found.")
+        pdbx_file_handle = open(pdbx_file, "r", encoding="utf-8")
+    if not category_names:
+        category_names = ["all"]
+    category_name = ""
+    with pdbx_file_handle:
+        line = pdbx_file_handle.readline()  # title line
+        _ = line.strip().split("_")[1]
+        processing_category = False
+        while line:
+            if line[0] == "#":
+                processing_category = True
+                loop_category = False
+                category_lines = ""
+                category_cols = []
+                category_dict = defaultdict(list)
+                line = pdbx_file_handle.readline()  # category first line
+                if not line:
+                    break
+                if line == "loop_\n":
+                    loop_category = True
+                    line = pdbx_file_handle.readline()
+                in_multiple_line = (
+                    False  # lines quoted by ';', they need to be specially treated
+                )
+                while line.strip() not in ["#", "#   #", "##"]:
+                    if line[0] == "_" and not in_multiple_line:
+                        category_name, category_nth_col = (
+                            line.strip().split(" ")[0].split(".")
+                        )
+                        category_cols.append(category_nth_col)
+                    if (
+                        category_name in category_names or "all" in category_names
+                    ):  # in a required category
+                        if line[0] == ";":
+                            in_multiple_line = True  # a ';' quoted record begins
+                            line = line.replace(
+                                '"', "'"
+                            )  # to avoid conflict with the next action
+                            line = (
+                                '"' + line[1:]
+                            )  # these ; symbols are not contents but a pair of container symbols for a single record
+                            # but other ';' are parts of a single record, so we use " to make it easier to parse
+                            if len(line.strip()) == 1:
+                                in_multiple_line = False  # a ';' quoted record ends
+                        elif in_multiple_line:
+                            line = line.replace(
+                                '"', "'"
+                            )  # to avoid conflict with the above action
+                        category_lines += line
+                    line = pdbx_file_handle.readline()
+                    if not line:
+                        raise ValueError(
+                            f"{pdbx_file} not normally ended. Download it again?"
+                        )
+                if "all" not in category_names and category_name not in category_names:
+                    continue
+                category_lines = category_lines.replace(
+                    "\n", " "
+                )  # cannot rely on newline to parse anyway.
+                records = split_line(category_lines)
+                if not loop_category:
+                    if len(records) != 2 * len(category_cols):
+                        raise ValueError(
+                            f"{pdbx_file} category {category_name} has irregular contents: {records}"
+                        )
+                    for i, rec in enumerate(records):
+                        if i % 2 == 0:
+                            col_name = rec.split(".")[1]
+                            if col_name in [
+                                "pdbx_seq_one_letter_code",
+                                "pdbx_seq_one_letter_code_can",
+                            ]:
+                                records[i + 1] = records[i + 1].replace(" ", "")
+                            category_dict[rec.split(".")[1]] = [records[i + 1]]
+                else:
+                    if (
+                        records[len(category_cols)][0] == "_"
+                        or records[len(category_cols) - 1][0] != "_"
+                        or not len(records) % len(category_cols) == 0
+                    ):
+                        raise ValueError(
+                            f"""{pdbx_file} category {category_name} has irregular contents:
+                            {len(records)} for {len(category_cols)}"""
+                        )
+                    record_data = records[len(category_cols) :]
+                    for i, i_data in enumerate(record_data):
+                        col = i % len(category_cols)
+                        if category_cols[col] in [
+                            "pdbx_seq_one_letter_code",
+                            "pdbx_seq_one_letter_code_can",
+                        ]:
+                            i_data = i_data.replace(
+                                " ", ""
+                            )  # delete the gaps in the one_letter_code
+                        category_dict[category_cols[col]].append(i_data)
+                data[category_name] = pd.DataFrame(category_dict)
+                for c_name in category_names:
+                    if c_name not in data:
+                        break  # at least one required_category is not processed, continue the main while loop
+                else:
+                    break  # all required_categories are processed, break the main while loop
+
+            if not processing_category:
+                line = pdbx_file_handle.readline()
+    if "_atom_site" in data:
+        col_dtypes = {
+            "id": "int",
+            "label_entity_id": "int",
+            "label_seq_id": "int",
+            "Cartn_x": "float",
+            "Cartn_y": "float",
+            "Cartn_z": "float",
+            "occupancy": "float",
+            "B_iso_or_equiv": "float",
+            "auth_seq_id": "int",
+            "pdbx_PDB_model_num": "int",
+            "pdbx_sifts_xref_db_num": "int",
+        }
+        col_dtypes = {
+            col_name: data_type
+            for col_name, data_type in col_dtypes.items()
+            if col_name in data["_atom_site"]
+        }
+
+        data["_atom_site"] = data["_atom_site"].astype(col_dtypes)
+    return data
