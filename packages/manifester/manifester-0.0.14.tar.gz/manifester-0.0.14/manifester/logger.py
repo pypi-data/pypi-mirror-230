b41a5ac0400e09efe1fa79f5a43d371ad9c38b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:06:38 +0000

    Add package manifester/0.0.14/manifester-0.0.14.tar.gz

---
+++ packages/manifester/manifester-0.0.14.tar.gz/manifester-0.0.14/manifester/logger.py
@@ -0,0 +1,32 @@
+import logging
+from pathlib import Path
+
+import logzero
+
+from manifester.settings import settings
+
+
+def setup_logzero(level="info", path="logs/manifester.log", silent=True):
+    Path(path).parent.mkdir(parents=True, exist_ok=True)
+    log_fmt = "%(color)s[%(levelname)s %(asctime)s]%(end_color)s %(message)s"
+    debug_fmt = (
+        "%(color)s[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d]"
+        "%(end_color)s %(message)s"
+    )
+    log_level = getattr(logging, level.upper(), logging.INFO)
+    # formatter for terminal
+    formatter = logzero.LogFormatter(
+        fmt=debug_fmt if log_level is logging.DEBUG else log_fmt
+    )
+    logzero.setup_default_logger(formatter=formatter, disableStderrLogger=silent)
+    logzero.loglevel(log_level)
+    # formatter for file
+    formatter = logzero.LogFormatter(
+        fmt=debug_fmt if log_level is logging.DEBUG else log_fmt, color=False
+    )
+    logzero.logfile(
+        path, loglevel=log_level, maxBytes=1e9, backupCount=3, formatter=formatter
+    )
+
+
+setup_logzero(level=settings.get("log_level", "info"))
