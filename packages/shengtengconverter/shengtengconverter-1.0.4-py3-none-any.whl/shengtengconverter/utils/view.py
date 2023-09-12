3e2df856887d471abc94ef91325b210605833b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 16:05:10 +0000

    Add package shengtengconverter/1.0.4/shengtengconverter-1.0.4-py3-none-any.whl

---
+++ packages/shengtengconverter/shengtengconverter-1.0.4-py3-none-any.whl/shengtengconverter/utils/view.py
@@ -0,0 +1,37 @@
+import os
+
+
+def list_onnx_models(path):
+    """
+    This function is used to list all onnx model in the path.
+
+    Parameters:
+        path(str): The directory path may contain onnx model.
+
+    Returns:
+        str: The list of onnx model path.
+    """
+    onnx_model = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".onnx"):
+                onnx_model.append(os.path.join(root, file))
+    return onnx_model
+
+
+def list_pytorch_models(path):
+    """
+    This function is used to list all pytorch model in the path.
+
+    Parameters:
+        path(str): The directory path may contain pytorch model.
+
+    Returns:
+        str: The list of pytorch model path.
+    """
+    pytorch_model = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".pth"):
+                pytorch_model.append(os.path.join(root, file))
+    return pytorch_model
