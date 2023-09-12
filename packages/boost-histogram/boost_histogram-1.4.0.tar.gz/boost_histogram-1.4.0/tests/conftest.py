4ed057335fc565d67bc465337adf0e0c9096d252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:08:11 +0000

    Add package boost-histogram/1.4.0/boost_histogram-1.4.0.tar.gz

---
+++ packages/boost-histogram/boost_histogram-1.4.0.tar.gz/boost_histogram-1.4.0/tests/conftest.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import pytest
+
+import boost_histogram as bh
+
+
+@pytest.fixture(params=(False, True), ids=("no_growth", "growth"))
+def growth(request):
+    return request.param
+
+
+@pytest.fixture(params=(False, True), ids=("no_overflow", "overflow"))
+def overflow(request):
+    return request.param
+
+
+@pytest.fixture(params=(False, True), ids=("no_underflow", "underflow"))
+def underflow(request):
+    return request.param
+
+
+@pytest.fixture(params=(False, True), ids=("no_flow", "flow"))
+def flow(request):
+    return request.param
+
+
+@pytest.fixture(
+    params=(None, "str", 1, {"a": 1}),
+    ids=("no_metadata", "str_metadata", "int_metadata", "dict_metadata"),
+)
+def metadata(request):
+    return request.param
+
+
+@pytest.fixture(
+    params=(
+        bh.storage.Double,
+        bh.storage.Int64,
+        bh.storage.AtomicInt64,
+        bh.storage.Weight,
+        bh.storage.Unlimited,
+    ),
+    ids=("Double", "Int64", "AtomicInt64", "Weight", "Unlimited"),
+)
+def count_storage(request):
+    return request.param
+
+
+@pytest.fixture(
+    params=(
+        bh.storage.Double,
+        bh.storage.Int64,
+        bh.storage.AtomicInt64,
+        bh.storage.Unlimited,
+    ),
+    ids=("Double", "Int64", "AtomicInt64", "Unlimited"),
+)
+def count_single_storage(request):
+    return request.param
