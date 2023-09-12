d2217ffb7d4a41c0ba5b2aced89ec0ebaadc21d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:06:34 +0000

    Add package temboard-agent/8.1.0/temboard_agent-8.1.0-py3-none-any.whl

---
+++ packages/temboard-agent/temboard_agent-8.1.0-py3-none-any.whl/temboardagent/web/core.py
@@ -0,0 +1,90 @@
+import logging
+
+from bottle import (
+    default_app, get, post, request, response, HTTPError, HTTPResponse
+)
+
+from ..notification import NotificationMgmt
+from ..toolkit.signing import canonicalize_request, verify_v1, InvalidSignature
+from ..version import __version__ as version
+
+
+logger = logging.getLogger(__name__)
+
+
+@get('/error/<error_code:int>', skip=['signature'])
+def get_error(error_code):
+    if error_code < 400:
+        raise HTTPResponse('HTTPResponse raised', error_code)
+    elif error_code < 500:
+        raise HTTPError(error_code, 'HTTPError raised')
+    else:
+        raise Exception("This is an Exception %s" % error_code)
+
+
+@get('/discover', skip=['signature'])
+def get_discover():
+    app = default_app().temboard
+    discover = app.discover.ensure_latest().copy()
+    discover['signature_status'] = 'enabled'
+
+    signature = request.headers.get('x-temboard-signature')
+    if signature:
+        crequest = canonicalize_request(
+            request.method,
+            request.path,
+            request.headers,
+        )
+        try:
+            if not signature.startswith('v1:'):
+                raise InvalidSignature("Unsupported signature version.")
+            signature = signature[3:]
+
+            verify_v1(
+                app.config.signing_key, signature, crequest)
+            discover['signature_status'] = 'valid'
+        except InvalidSignature:
+            discover['signature_status'] = 'invalid'
+
+    response.set_header('ETag', app.discover.etag)
+    return discover
+
+
+@post('/discover')
+def post_discover(pgconn):
+    app = default_app().temboard
+    data = app.discover.refresh(pgconn).copy()
+    # POST endpoint does not bypass signature verification.
+    data['signature_status'] = 'valid'
+    response.set_header('ETag', app.discover.etag)
+    return data
+
+
+@get
+def profile():
+    return {'username': request.username, 'signature': 'valid'}
+
+
+@get('/notifications')
+def notifications():
+    config = default_app().temboard.config
+    return list(NotificationMgmt.get_last_n(config, -1))
+
+
+@get('/status', skip=['signature'])
+def get_status():
+    app = default_app().temboard
+
+    try:
+        reload_datetime = app.reload_datetime.strftime("%Y-%m-%dT%H:%M:%S%Z")
+    except AttributeError:
+        reload_datetime = None
+
+    return dict(
+        pid=app.pid,
+        user=app.user,
+        start_datetime=app.start_datetime.strftime("%Y-%m-%dT%H:%M:%S%Z"),
+        reload_datetime=reload_datetime,
+        configfile=app.config.temboard.configfile,
+        version=version,
+    )
