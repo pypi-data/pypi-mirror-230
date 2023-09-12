18286a1063894827ad7a49428128a39ffcaeed1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:06:38 +0000

    Add package weather-warning-summary-xethhung12/0.0.1/weather_warning_summary_xethhung12-0.0.1.tar.gz

---
+++ packages/weather-warning-summary-xethhung12/weather_warning_summary_xethhung12-0.0.1.tar.gz/weather_warning_summary_xethhung12-0.0.1/README.md
@@ -0,0 +1,7 @@
+# build
+```shell
+pip install -r requirements.txt
+rm -rf dist
+python -m build
+python -m twine upload dist/* -u __token__ -p pypi-AgEIcHlwaS5vcmcCJDQyOGRkOWI4LTYxNDYtNGQ2Ni04ZmVlLThiMjZlNzg5MDZhMAACKlszLCIyZTZkMmM3Yy03YTNkLTQwMDktYTM3Ny1hZWJlZWY4ZDQzNTUiXQAABiC96D2pMWOWxyiZXyYQ4wVY7pPmYsr7e7JdKDFt5mU7_Q
+```
---
+++ packages/weather-warning-summary-xethhung12/weather_warning_summary_xethhung12-0.0.1.tar.gz/weather_warning_summary_xethhung12-0.0.1/src/weather_warning_summary_xethhung12/__main__.py
@@ -0,0 +1,12 @@
+from weather_warning_summary_xethhung12 import get_connection, load_map
+import datetime as dt
+
+if __name__ == '__main__':
+    conn = get_connection()
+    time = dt.datetime.now().isoformat()
+    m = load_map("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=warnsum&lang=en")
+    conn.execute("insert into weather (time, message) values (?, ?)", (time, m))
+    conn.commit()
+    print(time)
+    print(m)
+    print("Done")
