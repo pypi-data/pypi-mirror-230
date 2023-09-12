d25abcbe373dd4d03085916f0a37933c008f4c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:06:48 +0000

    Add package space-classy/0.7/space_classy-0.7.tar.gz

---
+++ packages/space-classy/space_classy-0.7.tar.gz/space_classy-0.7/classy/__init__.py
@@ -0,0 +1,11 @@
+"""Taxonomic classification of asteroid reflectance spectra."""
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+from .log import set_log_level
+from .core import Spectrum
+from .core import Spectra
+
+# Welcome to classy
+__version__ = "0.7"
