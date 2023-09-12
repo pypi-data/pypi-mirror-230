67013da802d0d8822614fa2830ae6b9108cd38ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 05:04:36 +0000

    Add package bieniciscraper/1.1.2/bieniciscraper-1.1.2.tar.gz

---
+++ packages/bieniciscraper/bieniciscraper-1.1.2.tar.gz/bieniciscraper-1.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='bieniciscraper',
+    version='1.1.2',
+    description='scrape housing listings on bien\'ici from any bien\'ici search url 💛',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/lobstrio/bieniciscraper',
+    author='sasha bouloudnine',
+    author_email='sasha.bouloudnine@lobstr.io',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'retry',
+    ],
+    entry_points={
+        'console_scripts': [
+            'bieniciscraper = bieniciscraper.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
