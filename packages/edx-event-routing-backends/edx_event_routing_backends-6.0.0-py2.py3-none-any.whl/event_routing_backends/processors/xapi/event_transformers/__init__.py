ad225e44d0bf5696f1cca3d45827c56a22b6d43e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:06:39 +0000

    Add package edx-event-routing-backends/6.0.0/edx_event_routing_backends-6.0.0-py2.py3-none-any.whl

---
+++ packages/edx-event-routing-backends/edx_event_routing_backends-6.0.0-py2.py3-none-any.whl/event_routing_backends/processors/xapi/event_transformers/__init__.py
@@ -0,0 +1,40 @@
+"""
+All xAPI transformers.
+"""
+
+from event_routing_backends.processors.xapi.event_transformers.enrollment_events import (
+    EnrollmentActivatedTransformer,
+    EnrollmentDeactivatedTransformer,
+)
+from event_routing_backends.processors.xapi.event_transformers.forum_events import (
+    ThreadCreatedTransformer,
+    ThreadDeletedTransformer,
+    ThreadEditedTransformer,
+    ThreadResponseCreatedTransformer,
+    ThreadResponseReportedTransformer,
+    ThreadResponseUnReportedTransformer,
+    ThreadViewedTransformer,
+    ThreadVotedTransformer,
+)
+from event_routing_backends.processors.xapi.event_transformers.grading_events import (
+    CourseGradedTransformer,
+    SubsectionGradedTransformer,
+)
+from event_routing_backends.processors.xapi.event_transformers.navigation_events import (
+    LinkClickedTransformer,
+    OutlineSelectedTransformer,
+    TabNavigationTransformer,
+)
+from event_routing_backends.processors.xapi.event_transformers.problem_interaction_events import (
+    ProblemCheckTransformer,
+    ProblemEventsTransformer,
+    ProblemSubmittedTransformer,
+)
+from event_routing_backends.processors.xapi.event_transformers.video_events import (
+    VideoCCTransformers,
+    VideoCompletedTransformer,
+    VideoInteractionTransformers,
+    VideoLoadedTransformer,
+    VideoPositionChangedTransformer,
+    VideoSpeedChangedTransformer,
+)
