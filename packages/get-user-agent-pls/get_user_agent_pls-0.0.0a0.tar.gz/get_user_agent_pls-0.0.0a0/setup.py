477c1986b5a0aac7d9c8d099bb9fe1e9397e3ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 15:06:35 +0000

    Add package get-user-agent-pls/0.0.0a0/get_user_agent_pls-0.0.0a0.tar.gz

---
+++ packages/get-user-agent-pls/get_user_agent_pls-0.0.0a0.tar.gz/get_user_agent_pls-0.0.0a0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='get_user_agent_pls',
+    version='0.0.0a',
+    author='David Hooton',
+    author_email='get_user_agent_pls@hooton.org',
+    description='A package to fetch get-user-agent strings',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/djh00t/get-user-agent-pls',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+    ],
+    entry_points={
+        'console_scripts': [
+            'get_user_agent_pls=get_user_agent_pls.get_user_agent_pls:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
