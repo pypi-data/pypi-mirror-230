2023027a0f962f8c6fa2a621f8ff229883a8c39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:07:21 +0000

    Add package masonite-dolphinido/0.0.7/masonite_dolphinido-0.0.7-py3-none-any.whl

---
+++ packages/masonite-dolphinido/masonite_dolphinido-0.0.7-py3-none-any.whl/masonitedolphinido/commands/fingerprint.py
@@ -0,0 +1,24 @@
+from masonite.commands import Command
+from masonitedolphinido.dolphinido import Dolphinido
+
+class Fingerprint(Command):
+    """
+    The command generates audio fingerprints of the given audio file. It accepts mp3 file only
+
+    dolphinido:fingerprint
+        {audiofile : The absolute file path of the audio file to be fingerprinted. }
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.dolphinido = Dolphinido()
+
+    def handle(self):
+        audio_file = self.argument("audiofile")
+        try:
+            result = self.dolphinido.create_audio(audio_file)
+            if result:
+                self.info("Fingerprint operation successful")		
+        except Exception as err:
+            self.error(str(err))
+        
