fc5d8d079cfdbc6c419acc1dc8defed7471fa767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 15:05:46 +0000

    Add package ace-elispot/0.1.0.6/ace-elispot-0.1.0.6.tar.gz

---
+++ packages/ace-elispot/ace-elispot-0.1.0.6.tar.gz/ace-elispot-0.1.0.6/README.md
@@ -0,0 +1,69 @@
+# ACE Configurator for ELISpot
+
+ACE facilitates (1) generation of ELISpot configurations (peptide-pool assignments) 
+using a deep learning approach to cluster similar peptides and (2) deconvolution 
+of pool spot counts for identification of immunogenic peptides.
+
+[![Build Status](https://app.travis-ci.com/pirl-unc/ace.svg?branch=main)](https://app.travis-ci.com/pirl-unc/ace)
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+## 01. Installation
+
+### 01-1. Standalone Graphical User Interface (Recommended)
+
+| Operating System | Link | Version           |
+|------------------| ---- |-------------------|
+| Mac              | [Download](https://github.com/pirl-unc/ace/releases/download/v0.1.0.4/ace-0.1.0.4.tar.gz) | v0.1.0.5 (latest) | 
+| Windows          | [Download](https://github.com/pirl-unc/ace/releases/download/v0.1.0.4/ace-0.1.0.4.tar.gz) | v0.1.0.5 (latest) |
+
+Previous versions of ACE are available [here](https://github.com/pirl-unc/ace/releases).
+
+### 01-2. Python Package 
+
+ACE is also available as a python package. Download the latest stable release version from [here](https://github.com/pirl-unc/ace/releases).<br/>
+Subsequently install the ACE package using pip:
+
+```
+pip install ace-<version>.tar.gz
+```
+
+#### Dependencies
+
+* python3 (>= 3.10)
+* pandas (>=1.5.2)
+* numpy (>=1.23.1)
+* ortools (9.3.10497)
+* torch
+* transformers (==4.30.2)
+* scikit-learn
+* openpyxl
+* golfy (>=2.5.0) 
+* levenshtein
+
+## 02. Usage
+
+ACE is available as a command-line interface after you install the python package:
+
+```
+usage: ace [-h] [--version] {generate,deconvolve,verify} ...
+
+ACE Configurator for ELISpot.
+
+positional arguments:
+  {generate,deconvolve,verify}
+                        ACE sub-commands.
+    generate            Generates an ELISpot experiment configuration.
+    deconvolve          Deconvolve hit peptide IDs given read-outs from an ELISpot experiment.
+    verify              Verifies whether an ELISpot assignment satisfies all ACE constraints.
+
+options:
+  -h, --help            show this help message and exit
+  --version, -v         show program's version number and exit
+```
+
+Read the full documentation on the python package at https://pirl-unc.github.io/ace/
+
+## 03. Citation
+
+If you use ACE in a publication, please cite our 
+[preprint](https://www.biorxiv.org/content/10.1101/2023.09.02.554864v1) describing ACE.
---
+++ packages/ace-elispot/ace-elispot-0.1.0.6.tar.gz/ace-elispot-0.1.0.6/test/test_deconvolve.py
@@ -0,0 +1,170 @@
+import pandas as pd
+from .data import get_data_path
+from acelib.block_assignment import BlockAssignment
+from acelib.main import run_ace_deconvolve
+from acelib.constants import DeconvolutionLabels, DeconvolveModes
+from acelib.plate_readout import PlateReadout
+
+
+def test_deconvolve_golfy_assignment_25pep5per3x_empirical(golfy_assignment_25pep5per3x):
+    ground_truth_hit_peptide_ids = ['peptide_1','peptide_10']
+    df_assignment = golfy_assignment_25pep5per3x.to_dataframe()
+    hit_pool_ids = list(df_assignment.loc[df_assignment['peptide_id'].isin(ground_truth_hit_peptide_ids), 'pool_id'].unique())
+    data = {
+        'pool_id': [],
+        'spot_count': []
+    }
+    for pool_id in df_assignment['pool_id'].unique():
+        data['pool_id'].append(pool_id)
+        if pool_id in hit_pool_ids:
+            data['spot_count'].append(300)
+        else:
+            data['spot_count'].append(0)
+    df_readout = pd.DataFrame(data)
+    deconvolution_result = run_ace_deconvolve(
+        df_readout=df_readout,
+        block_assignment=golfy_assignment_25pep5per3x,
+        statistical_deconvolution_method=DeconvolveModes.EM,
+        empirical_min_coverage=3,
+        empirical_min_spot_count=300,
+        statistical_min_peptide_activity=1.0
+    )
+    df_deconvolution = deconvolution_result.to_dataframe()
+    hit_peptide_ids = sorted(df_deconvolution.loc[df_deconvolution['deconvolution_result'] == DeconvolutionLabels.CONFIDENT_HIT, 'peptide_id'].values.tolist())
+    assert hit_peptide_ids == ground_truth_hit_peptide_ids, 'peptide_1 and peptide_10 are hits.'
+
+
+def test_deconvolve_golfy_assignment_25pep5per3x_empirical_nohits(golfy_assignment_25pep5per3x):
+    ground_truth_hit_peptide_ids = ['peptide_1','peptide_10']
+    df_assignment = golfy_assignment_25pep5per3x.to_dataframe()
+    hit_pool_ids = list(df_assignment.loc[df_assignment['peptide_id'].isin(ground_truth_hit_peptide_ids), 'pool_id'].unique())
+    data = {
+        'pool_id': [],
+        'spot_count': []
+    }
+    for pool_id in df_assignment['pool_id'].unique():
+        data['pool_id'].append(pool_id)
+        if pool_id in hit_pool_ids:
+            data['spot_count'].append(300)
+        else:
+            data['spot_count'].append(0)
+    df_readout = pd.DataFrame(data)
+    deconvolution_result = run_ace_deconvolve(
+        df_readout=df_readout,
+        block_assignment=golfy_assignment_25pep5per3x,
+        statistical_deconvolution_method=DeconvolveModes.EM,
+        empirical_min_coverage=3,
+        empirical_min_spot_count=1000,
+        statistical_min_peptide_activity=1.0
+    )
+    df_deconvolution = deconvolution_result.to_dataframe()
+    hit_peptide_ids = sorted(df_deconvolution.loc[df_deconvolution['deconvolution_result'] == DeconvolutionLabels.CONFIDENT_HIT, 'peptide_id'].values.tolist())
+    assert hit_peptide_ids == [], 'There should be no hits.'
+
+
+def test_deconvolve_golfy_assignment_25pep5per3x_em(golfy_assignment_25pep5per3x):
+    ground_truth_hit_peptide_ids = ['peptide_1','peptide_10']
+    df_assignment = golfy_assignment_25pep5per3x.to_dataframe()
+    hit_pool_ids = list(df_assignment.loc[df_assignment['peptide_id'].isin(ground_truth_hit_peptide_ids), 'pool_id'].unique())
+    data = {
+        'pool_id': [],
+        'spot_count': []
+    }
+    for pool_id in df_assignment['pool_id'].unique():
+        data['pool_id'].append(pool_id)
+        if pool_id in hit_pool_ids:
+            data['spot_count'].append(300)
+        else:
+            data['spot_count'].append(0)
+    df_readout = pd.DataFrame(data)
+    deconvolution_result = run_ace_deconvolve(
+        df_readout=df_readout,
+        block_assignment=golfy_assignment_25pep5per3x,
+        statistical_deconvolution_method=DeconvolveModes.EM,
+        empirical_min_coverage=3,
+        empirical_min_spot_count=300,
+        statistical_min_peptide_activity=1.0
+    )
+    df_deconvolution = deconvolution_result.to_dataframe()
+
+
+def test_deconvolve_golfy_assignment_25pep5per3x_lasso(golfy_assignment_25pep5per3x):
+    ground_truth_hit_peptide_ids = ['peptide_1','peptide_10']
+    df_assignment = golfy_assignment_25pep5per3x.to_dataframe()
+    hit_pool_ids = list(df_assignment.loc[df_assignment['peptide_id'].isin(ground_truth_hit_peptide_ids), 'pool_id'].unique())
+    data = {
+        'pool_id': [],
+        'spot_count': []
+    }
+    for pool_id in df_assignment['pool_id'].unique():
+        data['pool_id'].append(pool_id)
+        if pool_id in hit_pool_ids:
+            data['spot_count'].append(300)
+        else:
+            data['spot_count'].append(0)
+    df_readout = pd.DataFrame(data)
+    deconvolution_result = run_ace_deconvolve(
+        df_readout=df_readout,
+        block_assignment=golfy_assignment_25pep5per3x,
+        statistical_deconvolution_method=DeconvolveModes.LASSO,
+        empirical_min_coverage=3,
+        empirical_min_spot_count=300,
+        statistical_min_peptide_activity=1.0
+    )
+    df_deconvolution = deconvolution_result.to_dataframe()
+
+
+def test_deconvolve_aid_plate_reader_readout():
+    block_assignment = BlockAssignment.read_excel_file(
+        excel_file=get_data_path(name='25peptides_5perpool_3x_configuration.xlsx'),
+    )
+    plate_readout = PlateReadout.read_aid_plate_reader_file(
+        excel_file=get_data_path(name='25peptides_5perpool_3x_aid-plate-reader_readout.xlsx'),
+        plate_id=1
+    )
+    df_readout = pd.merge(block_assignment.to_dataframe(), plate_readout.to_dataframe(), on=['plate_id', 'well_id'])
+    deconvolution_result = run_ace_deconvolve(
+        df_readout=df_readout,
+        block_assignment=block_assignment,
+        statistical_deconvolution_method=DeconvolveModes.EM,
+        empirical_min_coverage=3,
+        empirical_min_spot_count=300,
+        statistical_min_peptide_activity=1.0
+    )
+    df_deconvolution = deconvolution_result.to_dataframe()
+    expected_peptide_ids = sorted(['peptide_1', 'peptide_10'])
+    hit_peptide_ids = sorted(df_deconvolution.loc[df_deconvolution['deconvolution_result'] == DeconvolutionLabels.CONFIDENT_HIT, 'peptide_id'].values.tolist())
+    assert hit_peptide_ids == expected_peptide_ids, 'peptide_1 and peptide_10 are hits.'
+
+
+def test_deconvolve_pool_id_readout():
+    block_assignment = BlockAssignment.read_excel_file(
+        excel_file=get_data_path(name='25peptides_5perpool_3x_configuration.xlsx')
+    )
+    df_assignment = block_assignment.to_dataframe()
+    df_readout = pd.read_excel(
+        get_data_path(name='25peptides_5perpool_3x_pool-id_readout.xlsx')
+    )
+    pool_ids = []
+    for index, row in df_readout.iterrows():
+        curr_plate_id = row['plate_id']
+        curr_well_id = row['well_id']
+        curr_pool_id = df_assignment.loc[
+            (df_assignment['plate_id'] == curr_plate_id) &
+            (df_assignment['well_id'] == curr_well_id),'pool_id'].values[0]
+        pool_ids.append(curr_pool_id)
+    df_readout['pool_id'] = pool_ids
+
+    deconvolution_result = run_ace_deconvolve(
+        df_readout=df_readout,
+        block_assignment=block_assignment,
+        statistical_deconvolution_method=DeconvolveModes.EM,
+        empirical_min_coverage=3,
+        empirical_min_spot_count=300,
+        statistical_min_peptide_activity=1.0
+    )
+    df_deconvolution = deconvolution_result.to_dataframe()
+    expected_peptide_ids = sorted(['peptide_1', 'peptide_10'])
+    hit_peptide_ids = sorted(df_deconvolution.loc[df_deconvolution['deconvolution_result'] == DeconvolutionLabels.CONFIDENT_HIT, 'peptide_id'].values.tolist())
+    assert hit_peptide_ids == expected_peptide_ids, 'peptide_1 and peptide_10 are hits.'
+
