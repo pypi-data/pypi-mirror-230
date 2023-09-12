7bc9300d437e9c338444d092ccbe330e05b92f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:07:19 +0000

    Add package SIRITVIS/1.0/SIRITVIS-1.0.tar.gz

---
+++ packages/SIRITVIS/SIRITVIS-1.0.tar.gz/SIRITVIS-1.0/setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup, find_packages
+
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    print("README.md file not found.")
+    long_description = ""
+except Exception as e:
+    print("An error occurred while reading README.md:", str(e))
+    long_description = ""
+
+setup(
+    name='SIRITVIS',
+    version='1.0',
+    author='Sagar Narwade, Gillian Kant, Benjamin Saefken, Benjamin Leiding',
+    description="SIRITVIS: Social Media Interaction & Reaction Insights Topic Visualisation",
+    maintainer="Sagar Narwade",
+    maintainer_email="sagarnarwade147@gmail.com",
+    python_requires='>=3.10, <3.11',
+    packages=find_packages(),
+    install_requires=[
+        'pip==23.2.1',
+        'praw==7.7.1',
+        'numpy',
+        'pandas==1.5.3',
+        'spacy',
+        'gensim',
+        'pyLDAvis==3.4.0',
+        'matplotlib',
+        'tweepy==3.9.0',
+        'urllib3',
+        'langdetect',
+        'octis',
+        'topic-wizard==0.3.1',
+        'tweepy==3.9.0',
+        'scikit-learn==1.2.2',
+        'urllib3',
+        'langdetect',
+        'folium==0.14.0',
+        'pickle4==0.0.1',
+        'plotly==5.15.0',
+        'nltk==3.6.2',
+        'notebook==6.5.5',
+        'ipywidgets',
+        'tensorflow',
+        'vaderSentiment',
+        'ipyleaflet',
+        'geopy',
+        'Flask==2.2.5',
+        'Flask-Caching==2.0.1',
+        'wordcloud==1.8.2.2',
+        'ipython==7.34.0',
+        'tweepy==3.9.0',
+        'textblob==0.15.3',
+        'apify-client==1.4.0',
+        'instagram_private_api==1.6.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+    ],
+    
+)
+
