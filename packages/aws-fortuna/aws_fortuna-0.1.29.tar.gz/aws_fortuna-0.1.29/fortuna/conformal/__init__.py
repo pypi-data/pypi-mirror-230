b1902ee2697d18528c965966679410c8e4f3666d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:06:47 +0000

    Add package aws-fortuna/0.1.29/aws_fortuna-0.1.29.tar.gz

---
+++ packages/aws-fortuna/aws_fortuna-0.1.29.tar.gz/aws_fortuna-0.1.29/fortuna/conformal/__init__.py
@@ -0,0 +1,34 @@
+# conformal classification
+from fortuna.conformal.classification.adaptive_conformal_classifier import (
+    AdaptiveConformalClassifier,
+)
+from fortuna.conformal.classification.adaptive_prediction import (
+    AdaptivePredictionConformalClassifier,
+)
+from fortuna.conformal.classification.batch_mvp import BatchMVPConformalClassifier
+from fortuna.conformal.classification.binary_multicalibrator import (
+    BinaryClassificationMulticalibrator,
+)
+from fortuna.conformal.classification.simple_prediction import (
+    SimplePredictionConformalClassifier,
+)
+from fortuna.conformal.classification.top_label_multicalibrator import (
+    TopLabelMulticalibrator,
+)
+from fortuna.conformal.multivalid.multicalibrator import Multicalibrator
+from fortuna.conformal.regression.adaptive_conformal_regressor import (
+    AdaptiveConformalRegressor,
+)
+from fortuna.conformal.regression.batch_mvp import BatchMVPConformalRegressor
+from fortuna.conformal.regression.cvplus import CVPlusConformalRegressor
+from fortuna.conformal.regression.enbpi import EnbPI
+from fortuna.conformal.regression.jackknife_minmax import (
+    JackknifeMinmaxConformalRegressor,
+)
+from fortuna.conformal.regression.jackknifeplus import JackknifePlusConformalRegressor
+
+# conformal regression
+from fortuna.conformal.regression.onedim_uncertainty import (
+    OneDimensionalUncertaintyConformalRegressor,
+)
+from fortuna.conformal.regression.quantile import QuantileConformalRegressor
