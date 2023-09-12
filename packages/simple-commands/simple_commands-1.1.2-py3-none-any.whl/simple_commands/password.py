9feb55093b7705e9b89331d0620156fa2ac9611c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:06:34 +0000

    Add package simple-commands/1.1.2/simple_commands-1.1.2-py3-none-any.whl

---
+++ packages/simple-commands/simple_commands-1.1.2-py3-none-any.whl/simple_commands/password.py
@@ -0,0 +1,23 @@
+import random
+import string
+
+def generate_random_password(length=12, use_uppercase=True, use_digits=True, use_symbols=True):
+    # ตัวแปรที่ใช้เก็บตัวอักษรที่ใช้สร้างรหัสผ่าน
+    characters = string.ascii_lowercase  # ใช้ตัวอักษรตัวเล็กเป็นค่าเริ่มต้น
+
+    # เพิ่มตัวอักษรตัวใหญ่ (ถ้าต้องการ)
+    if use_uppercase:
+        characters += string.ascii_uppercase
+
+    # เพิ่มตัวเลข (ถ้าต้องการ)
+    if use_digits:
+        characters += string.digits
+
+    # เพิ่มสัญลักษณ์ (ถ้าต้องการ)
+    if use_symbols:
+        characters += string.punctuation
+
+    # สุ่มเลือกตัวอักษรตามความยาวที่กำหนด
+    password = ''.join(random.choice(characters) for _ in range(length))
+
+    return password
