182e777240bb4e517527d93e90bdfcc1ac77470e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:08:33 +0000

    Add package py-db-migrate/0.3.1/py_db_migrate-0.3.1.tar.gz

---
+++ packages/py-db-migrate/py_db_migrate-0.3.1.tar.gz/py_db_migrate-0.3.1/py_db_migrate/service/migration_files.py
@@ -0,0 +1,82 @@
+"""Migration files service."""
+from datetime import datetime, timezone
+from pathlib import Path
+
+import aiofiles.os
+from aiofiles import open
+from pydantic import validate_call
+
+from py_db_migrate.service.service import Service
+
+from py_db_migrate.service.utils import check_existence_of_file
+
+
+class MigrationFiles(Service):
+    """MigrationFiles class."""
+
+    @validate_call
+    async def __call__(self, folder_path: Path, name: str) -> None:
+        """Run the main logic of the class.
+
+        After finding the formatted name, insert two sql files by adding
+        `-up.sql` and `-down.sql` names to the given folder.
+
+        Arguments:
+            folder_path: Folder to add new files.
+            name: Name of the migration files to create.
+
+        Returns:
+            None.
+        """
+        formatted_name: str = self.format_file_names(name=name)
+
+        if not (await check_existence_of_file(path=folder_path)):
+            await self.create_migration_folder(path=folder_path)
+            self.logger.info(f"{folder_path} folder is created.")
+
+        await self.add_migration_files(folder_path=folder_path, name=formatted_name)
+        self.logger.info("New migration files are added.")
+
+    @staticmethod
+    @validate_call
+    async def add_migration_files(folder_path: Path, name: str) -> None:
+        """Add migration files to the given folder.
+
+        Arguments:
+            folder_path: Folder to add new files.
+            name: Name of the migration files to create.
+
+        Returns:
+            None.
+        """
+        for new_file_name in (f"{name}-up.sql", f"{name}-down.sql"):
+            async with open(file=folder_path.joinpath(new_file_name), mode="w") as file:
+                await file.write("/* Insert your SQL commands here. */")
+
+    @staticmethod
+    @validate_call
+    def format_file_names(name: str) -> str:
+        """Format the given name to the project format.
+
+        Arguments:
+            name: Desired name.
+
+        Returns:
+            The formatted name. (YYYYMMDDmmss-{name})
+        """
+        now: datetime = datetime.now(tz=timezone.utc)
+        formatted_datetime = now.strftime("%Y%m%d%H%M%S")
+        return f"{formatted_datetime}-{name}"
+
+    @staticmethod
+    @validate_call
+    async def create_migration_folder(path: Path) -> None:
+        """Create a folder to store migration files.
+
+        Arguments:
+            path: Path of the new folder.
+
+        Returns:
+            None.
+        """
+        await aiofiles.os.mkdir(path=path, mode=0o755)
---
+++ packages/py-db-migrate/py_db_migrate-0.3.1.tar.gz/py_db_migrate-0.3.1/py_db_migrate/service/start.py
@@ -0,0 +1,59 @@
+"""Start Service."""
+from pathlib import Path
+
+import yaml
+from pydantic import validate_call
+
+from py_db_migrate.service.service import Service
+
+from py_db_migrate.service.utils import check_existence_of_file
+
+
+class Start(Service):
+    """Start class."""
+
+    @validate_call
+    async def __call__(self, path: Path) -> None:
+        """Run main logic of the class.
+
+        Firstly, check for the configuration file, if it doesn't
+        exist, create it by using default fields.
+
+        Arguments:
+            path: Path to check and create config file if needed.
+
+        Returns:
+            None.
+        """
+        is_file_exist: bool = await check_existence_of_file(path=path)
+
+        if is_file_exist:
+            self.logger.warning("Configuration file already existed.")
+            return None
+
+        self.create_configuration_file(path=path)
+        self.logger.info("Configuration file is created.")
+
+    @staticmethod
+    @validate_call
+    def create_configuration_file(path: Path) -> None:
+        """Create a configuration file.
+
+        Arguments:
+            path: Path to create a file.
+
+        Returns:
+            None.
+        """
+        default_configurations: dict[str, str | dict[str, int | str]] = {
+            "database": {
+                "host": "localhost",
+                "port": 5432,
+                "user": "your_db_username",
+                "password": "your_db_password",
+                "name": "your_db_name",
+            },
+            "migration_directory": "pydbmigrations",
+        }
+        with open(path, mode="w") as file:
+            yaml.dump(default_configurations, file, default_flow_style=False)
