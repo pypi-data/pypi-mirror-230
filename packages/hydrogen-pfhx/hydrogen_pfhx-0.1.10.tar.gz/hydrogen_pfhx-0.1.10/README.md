6afeac26ec2ddef1d59d7cb6fb9fb48cc589c484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 15:07:00 +0000

    Add package hydrogen-pfhx/0.1.10/hydrogen_pfhx-0.1.10.tar.gz

---
+++ packages/hydrogen-pfhx/hydrogen_pfhx-0.1.10.tar.gz/hydrogen_pfhx-0.1.10/README.md
@@ -0,0 +1,17 @@
+# hydrogen-pfhx
+Chemical engineering model of hydrogen plate-fin heat exchanger.
+
+## Installation
+pip install hydrogen-pfhx
+
+## Usage
+hydrogen-pfhx is used to model the cryogenic plate-fin heat exchanger used within hydrogen liquefaction plants.
+Two examples are given below:
+
+1. Run with a python IDE (e.g. visual studio)
+   - see the [test file](https://github.com/fsr-uwa/hydrogen-pfhx/blob/main/tests/test_model.py)
+
+2. A jupyter notebook using google colab
+   - [open the test notebook in colab](https://colab.research.google.com/github/fsr-uwa/hydrogen-pfhx/blob/main/tests/test_model.ipynb)
+   - Click "Copy to drive" (just below filebar) then run the notebook.
+   - Note: saving data will require approval for google drive usage.
---
+++ packages/hydrogen-pfhx/hydrogen_pfhx-0.1.10.tar.gz/hydrogen_pfhx-0.1.10/src/hydrogen_pfhx/ortho_para_dynamics.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+def para_ortho_equilibrium(T):
+    # Calculate the equilibrium concentrations of Para- & Ortho- Hydrogen as a
+    # function of temperature (T)
+    xp = 0.1*(np.exp(-175./T)+0.1)**(-1) - 7.06e-9 * \
+        T**3 + 3.42e-6*T**2 - 6.2e-5*T - 0.00227
+    xo = 1 - xp
+    return xp, xo
+
+
+def first_order_kinetics(reactant):
+    # calculate first order conversion kinetics for ortho > para conversion
+    # https://onlinelibrary.wiley.com/doi/abs/10.1002/ceat.201800345
+    # extract reactant properties.
+    T = reactant.temperature
+    xo = reactant.get_xo()
+    xo_equil = reactant.get_xo_equil()
+    cH2 = reactant.molar_density
+
+    # universal gas constant
+    # TODO - import from CP
+    R = 8.31446261815324  # J/(mol.K)
+
+    # reactor kinetic constants (from Donaubauer et al.)
+    Ea = -336.45                # J/mol
+    a = 2.2e-3                 # m3s/mol
+    b = -35.11e-3              # s
+
+    # rate constant
+    k = np.exp(-Ea/(R*T))/(a*cH2 + b) * 1/(1-xo_equil)  # 1/s
+
+    # first order conversion rate. Note conversion rate defined in terms of
+    # production of para-hydrogen as positive.
+    conversion_rate = k * cH2 * (xo - xo_equil)  # m3 / (mol.s)
+
+    return conversion_rate
+
+
+# Milenko
+def autocatalytic_kinetics(reactant):
+    A0 = 18.2 # cm3/kg/h/k**n
+    n = 0.56
+    C0 = 38.5
+    D = 4.605
+    m = 2.5
+    p = 3.6
+    
+    T = reactant.temperature
+    rho = reactant.mass_density*1e-3
+    
+    A = A0 * T ** n
+    C = (C0 + D / T ** m)
+    
+    k = A*rho + C*rho**p
+    
+    return k
+    
+    
