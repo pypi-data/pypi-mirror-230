dccdcaf51d8f52cf3c467422ac6589d2c3cfb970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 05:04:16 +0000

    Add package named-arrays/0.2.0/named_arrays-0.2.0-py3-none-any.whl

---
+++ packages/named-arrays/named_arrays-0.2.0-py3-none-any.whl/named_arrays/_matrices/cartesian/matrices_cartesian_nd.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+from typing import TypeVar, Generic, Type
+import abc
+import dataclasses
+import named_arrays as na
+
+__all__ = [
+    "AbstractCartesianNdMatrixArray",
+    "CartesianNdMatrixArray",
+]
+
+
+@dataclasses.dataclass(eq=False, repr=False)
+class AbstractCartesianNdMatrixArray(
+    na.AbstractCartesianMatrixArray,
+    na.AbstractCartesianNdVectorArray,
+):
+
+    @property
+    def type_abstract(self) -> Type[AbstractCartesianNdMatrixArray]:
+        return AbstractCartesianNdMatrixArray
+
+    @property
+    def type_explicit(self) -> Type[CartesianNdMatrixArray]:
+        return CartesianNdMatrixArray
+
+    @property
+    def type_vector(self) -> Type[na.CartesianNdVectorArray]:
+        return na.CartesianNdVectorArray
+
+    @property
+    def determinant(self) -> na.ScalarLike:
+        return NotImplementedError
+
+    @property
+    def inverse(self) -> na.AbstractMatrixArray:
+        return NotImplementedError
+
+
+@dataclasses.dataclass(eq=False, repr=False)
+class CartesianNdMatrixArray(
+    na.CartesianNdVectorArray,
+    AbstractCartesianNdMatrixArray,
+    na.AbstractExplicitCartesianMatrixArray,
+):
+    pass
