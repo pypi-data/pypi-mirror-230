b0a853af8c619dba7c7f78ee116d313aea6fea7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:06:17 +0000

    Add package pycellino/0.1.0/pycellino-0.1.0.tar.gz

---
+++ packages/pycellino/pycellino-0.1.0.tar.gz/pycellino-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pycellino',
+    version='0.1.0',
+    author='Piotr Wierzgała',
+    author_email='piotr.wierzgala@gmail.com',
+    description='A package with helper functions for data processing.',
+    packages=find_packages(),
+    python_requires='>=3.6, <4',
+)
