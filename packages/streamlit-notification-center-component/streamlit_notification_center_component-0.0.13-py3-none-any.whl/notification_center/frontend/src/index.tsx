5225de9e38a50c162464e75281d5a32bed4723da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 05:04:16 +0000

    Add package streamlit-notification-center-component/0.0.13/streamlit_notification_center_component-0.0.13-py3-none-any.whl

---
+++ packages/streamlit-notification-center-component/streamlit_notification_center_component-0.0.13-py3-none-any.whl/notification_center/frontend/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import NotificationCenter from "./NotificationCenter"
+
+ReactDOM.render(
+  <React.StrictMode><NotificationCenter/></React.StrictMode>, 
+  document.getElementById("root")
+)
