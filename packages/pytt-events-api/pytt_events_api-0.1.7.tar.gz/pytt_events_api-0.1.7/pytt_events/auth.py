ed1186ee295edf586165983842d2afe320bbf19f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:06:05 +0000

    Add package pytt-events-api/0.1.7/pytt_events_api-0.1.7.tar.gz

---
+++ packages/pytt-events-api/pytt_events_api-0.1.7.tar.gz/pytt_events_api-0.1.7/pytt_events/auth.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings, constr
+from typing import Optional
+
+
+class TikTokAuth(BaseSettings):  # TODO update
+    """
+    Handles TikTok Auth data.
+    Throws a ValidationError if required environment vars are not set.
+    """
+    TIKTOK_ACCESS_TOKEN: str
+    TIKTOK_PIXEL_ID: str
+    TIKTOK_TEST_EVENT_CODE: Optional[str]
+    TIKTOK_API_VERSION: constr(strip_whitespace=True, min_length=4) = 'v1.3'
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
