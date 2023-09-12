3df046ee39ea101b97fb5379dd43ced801bc345b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:07:47 +0000

    Add package cmsgitlogs/0.0.1/cmsgitlogs-0.0.1.tar.gz

---
+++ packages/cmsgitlogs/cmsgitlogs-0.0.1.tar.gz/cmsgitlogs-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cmsgitlogs",                     # This is the name of the package
+    version="0.0.1",                        # The initial release version
+    author="mohan chinnappan",                     # Full name of the author
+    description="Getting git logs in a csv",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["cmsgitlogs"],             # Name of the python package
+    package_dir={'':'cmsgitlogs/src'},     # Directory of the source code of the package
+    install_requires=['pyperclip']                     # Install other dependencies if any
+)
