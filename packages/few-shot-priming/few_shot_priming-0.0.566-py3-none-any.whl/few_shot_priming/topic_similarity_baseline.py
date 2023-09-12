2ea73a8caae8028b42c413e35d96560d2a43b371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:07:21 +0000

    Add package few-shot-priming/0.0.566/few_shot_priming-0.0.566-py3-none-any.whl

---
+++ packages/few-shot-priming/few_shot_priming-0.0.566-py3-none-any.whl/few_shot_priming/topic_similarity_baseline.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer, util
+
+
+def calc_similarity_sentence_transformer(df_test, df_training):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    test_text = df_test["text"]
+    training_text = df_training["text"]
+    test_embeddings = model.encode(test_text)
+    training_embeddings = model.encode(training_text)
+    cosine_scores = util.cos_sim(test_embeddings, training_embeddings)
+    return cosine_scores
+
