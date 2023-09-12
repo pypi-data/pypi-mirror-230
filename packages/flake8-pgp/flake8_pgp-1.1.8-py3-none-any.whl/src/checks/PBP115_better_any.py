945f6efbe0ef6278a4a993089ecb98c6a8cae7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:06:45 +0000

    Add package flake8-pgp/1.1.8/flake8_pgp-1.1.8-py3-none-any.whl

---
+++ packages/flake8-pgp/flake8_pgp-1.1.8-py3-none-any.whl/src/checks/PBP115_better_any.py
@@ -0,0 +1,29 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring, line-too-long, too-few-public-methods, invalid-name, pointless-string-statement
+import ast
+
+from src.flake8_ast_error import PREFIX, Flake8ASTErrorInfo
+
+
+class AnyWithCompNotAllowed:
+    """
+    # Bad
+    any([x.id for x in bar])
+    all([x.id for x in bar])
+
+    # Good
+    all(x.id for x in bar)
+    any(x.id for x in bar)
+    """
+
+    msg = PREFIX + "15: Prefer-simple-any-all: remove unnecessary comprehension."
+
+    @classmethod
+    def check(cls, node: ast.Call, errors: list[Flake8ASTErrorInfo]) -> None:
+        if not (
+            isinstance(node.func, ast.Name) or
+            node.func.id not in {"all", "any"} or
+            len(node.args) != 1  # fmt: skip
+        ):
+            return
+        if isinstance(node.args[0], ast.ListComp):
+            errors.append(Flake8ASTErrorInfo(node.lineno, node.col_offset, cls.msg, type(cls)))
