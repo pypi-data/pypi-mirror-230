3fee74e825d46d17bbf151028153ef719a514def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:05:13 +0000

    Add package openmpi/0.0.1/openmpi-0.0.1.tar.gz

---
+++ packages/openmpi/openmpi-0.0.1.tar.gz/openmpi-0.0.1/README.md
@@ -0,0 +1,3 @@
+# openmpi
+
+openmpi - listed by Cycode to prevent confusion
---
+++ packages/openmpi/openmpi-0.0.1.tar.gz/openmpi-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='openmpi',
+    version="1.0.0",
+    description='openmpi - listed by Cycode to prevent confusion',
+    long_description="openmpi",
+    long_description_content_type='text/markdown',
+)
