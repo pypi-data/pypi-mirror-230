6c3ffcb8fcd2c6f19420ef394d47ddd0f9b97e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 15:06:43 +0000

    Add package ATTools/0.1.3/ATTools-0.1.3.tar.gz

---
+++ packages/ATTools/ATTools-0.1.3.tar.gz/ATTools-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ATTools",
+    version="0.1.3",
+    packages=["ATTools"],
+    install_requires=[
+        'asyncio',
+        'aiohttp',
+        'tonconnect',
+        'dedust',
+        'xjet',
+        'tontools',       
+    ],
+    python_requires='>=3.7',
+)
