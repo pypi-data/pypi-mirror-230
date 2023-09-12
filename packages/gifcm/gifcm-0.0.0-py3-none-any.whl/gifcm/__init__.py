85bc7a168c33eb47722ffce5c772070a5b4720e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 05:04:15 +0000

    Add package gifcm/0.0.0/gifcm-0.0.0-py3-none-any.whl

---
+++ packages/gifcm/gifcm-0.0.0-py3-none-any.whl/gifcm/__init__.py
@@ -0,0 +1,7 @@
+"""gifcm - Easy gif animation of matplotlib figures."""
+
+__version__ = "0.0.0"
+__author__ = "Martin Schubert <mfschubert@gmail.com>"
+__all__ = ["AnimatedFigure", "Frame"]
+
+from gifcm.animated_figure import AnimatedFigure, Frame
