1bcfba11748d0d88aa80e203c77727295d26603f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:06:22 +0000

    Add package ton-client-py/1.44.3.0/ton-client-py-1.44.3.0.tar.gz

---
+++ packages/ton-client-py/ton-client-py-1.44.3.0.tar.gz/ton-client-py-1.44.3.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ton-client-py",
+    version="1.44.3.0",
+    author="MoveTON",
+    author_email="",
+    description="Python SDK for Everscale",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/move-ton/ton-client-py",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[],
+)
