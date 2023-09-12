9a0085a186ed6ed34098c6c32c4aeb0e691c4eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:06:12 +0000

    Add package club404/0.38/club404-0.38.tar.gz

---
+++ packages/club404/club404-0.38.tar.gz/club404-0.38/club404/encoders/text.py
@@ -0,0 +1,10 @@
+
+
+from club404.encoder import Encoder
+
+
+class TextEncoder(Encoder):
+    mime = "text/plain"
+    ext = [".txt"]
+    def encode(self, data): return '{}'.format(data)
+    def decode(self, data): return '{}'.format(data)
