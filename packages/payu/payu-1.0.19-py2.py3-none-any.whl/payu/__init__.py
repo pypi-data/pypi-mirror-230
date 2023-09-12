01bdb251cdcfc6f374bdec74604ecfbe1544ce57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 15:08:25 +0000

    Add package payu/1.0.19/payu-1.0.19-py2.py3-none-any.whl

---
+++ packages/payu/payu-1.0.19-py2.py3-none-any.whl/payu/__init__.py
@@ -0,0 +1,6 @@
+"""A climate model workflow management tool.
+
+:copyright: Copyright 2011 Marshall Ward, see AUTHORS for details.
+:license: Apache License, Version 2.0, see LICENSE for details.
+"""
+__version__ = '1.0.19'
