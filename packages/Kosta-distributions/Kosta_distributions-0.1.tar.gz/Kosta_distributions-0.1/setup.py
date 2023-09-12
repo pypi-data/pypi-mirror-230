6dab8d6d3ed2e6dda462dfdeac58ca94ec6e7194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:07:21 +0000

    Add package Kosta-distributions/0.1/Kosta_distributions-0.1.tar.gz

---
+++ packages/Kosta-distributions/Kosta_distributions-0.1.tar.gz/Kosta_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Kosta_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Kosta_distributions'],
+      zip_safe=False)
