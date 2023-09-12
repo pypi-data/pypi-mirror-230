9ef9299ebcc5a30696aa88e0eb676d4c61cef60e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:07:21 +0000

    Add package kraken-analytics-rmq/1.0.1/kraken_analytics_rmq-1.0.1.tar.gz

---
+++ packages/kraken-analytics-rmq/kraken_analytics_rmq-1.0.1.tar.gz/kraken_analytics_rmq-1.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['kraken_analytics_rmq']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pika>=1.3.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'kraken-analytics-rmq',
+    'version': '1.0.1',
+    'description': 'kraken analytics rabbitmq client',
+    'long_description': '# Description of the project\n',
+    'author': 'Thomas',
+    'author_email': 'thomas@kraken-analytics.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/stakkle5/kraken-analytics-rmq',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
