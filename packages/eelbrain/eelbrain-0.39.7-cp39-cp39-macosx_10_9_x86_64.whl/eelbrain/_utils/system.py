b9ef056e7a9f4c50f714188558caae69bcca845f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:06:00 +0000

    Add package eelbrain/0.39.7/eelbrain-0.39.7-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/eelbrain/eelbrain-0.39.7-cp39-cp39-macosx_10_9_x86_64.whl/eelbrain/_utils/system.py
@@ -0,0 +1,20 @@
+# Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+import os
+import sys
+
+IS_OSX = sys.platform == 'darwin'
+IS_WINDOWS = os.name == 'nt'
+
+if IS_OSX:
+    from .macos import user_activity
+else:
+    def user_activity(func):
+        return func
+
+
+def restore_main_spec():
+    """On windows, running a multiprocessing job seems to sometimes remove this attribute"""
+    if IS_WINDOWS:
+        main_module = sys.modules['__main__']
+        if not hasattr(main_module, '__spec__'):
+            main_module.__spec__ = None
