66f7c067008f111eb604167a91ec1c5ef125b4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 16:05:10 +0000

    Add package brainpylib-cu11x/0.1.10.post1/brainpylib_cu11x-0.1.10.post1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/brainpylib-cu11x/brainpylib_cu11x-0.1.10.post1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/brainpylib/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.10.post1"
+
+
+# IMPORTANT, must import first
+from . import _register_custom_calls, _check
+
+del _check, _register_custom_calls
---
+++ packages/brainpylib-cu11x/brainpylib_cu11x-0.1.10.post1-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/brainpylib/op_register.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from ._src.op_register import (
+  compile_cpu_signature_with_numba as compile_cpu_signature_with_numba,
+  register_op_with_numba as register_op_with_numba,
+)
+
+
