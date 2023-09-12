e4fdd44eef7923bacc787d83ac3d7a89becf0d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 16:04:47 +0000

    Add package dupfilter/0.0.2/dupfilter-0.0.2-py3-none-any.whl

---
+++ packages/dupfilter/dupfilter-0.0.2-py3-none-any.whl/dupfilter/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+# author: kusen
+# email: 1194542196@qq.com
+# date: 2023/9/6
+
+from dupfilter.filters.file import FileFilter
+from dupfilter.filters.memory import MemoryFilter
+from dupfilter.filters.redis.bloomfilter import BloomFilter as RedisBloomFilter
+from dupfilter.filters.redis.bloomfilter import AsyncBloomFilter as AsyncRedisBloomFilter
+from dupfilter.filters.redis.stringfilter import StringFilter as RedisStringFilter
+from dupfilter.filters.redis.stringfilter import AsyncStringFilter as AsyncRedisStringFilter
+from dupfilter.filters.redis.setfilter import SetFilter as RedisSetFilter
+from dupfilter.filters.redis.setfilter import AsyncSetFilter as AsyncRedisSetFilter
+from dupfilter.filters.redis.sortedsetfilter import SortedSetFilter as RedisSortedSetFilter
+from dupfilter.filters.redis.sortedsetfilter import AsyncSortedSetFilter as AsyncRedisSortedSetFilter
