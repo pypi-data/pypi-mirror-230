a1da93c7eeceeb60c8dce59e218ecb45c16927f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:05:58 +0000

    Add package bpputilmodule/0.0.1/bpputilmodule-0.0.1-py3-none-any.whl

---
+++ packages/bpputilmodule/bpputilmodule-0.0.1-py3-none-any.whl/bpputilmodule/__init__.py
@@ -0,0 +1,24 @@
+# class bppUtil:
+#     """
+#     Instantiate a multiplication operation.
+#     Numbers will be multiplied by the given multiplier.
+#
+#     :param multiplier: The multiplier.
+#     :type multiplier: int
+#     """
+#
+#     def __init__(self, bppUtil):
+#         self.bppUtil = bppUtil
+#
+#     def multiply(self, number):
+#         """
+#         Multiply a given number by the multiplier.
+#
+#         :param number: The number to multiply.
+#         :type number: int
+#
+#         :return: The result of the multiplication.
+#         :rtype: int
+#         """
+#
+#         return number * self.multiplier
