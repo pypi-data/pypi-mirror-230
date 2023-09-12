489c14d82481ec7113e134fc3b98f74132c06c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:05:40 +0000

    Add package sarfile/0.1.2/sarfile-0.1.2.tar.gz

---
+++ packages/sarfile/sarfile-0.1.2.tar.gz/sarfile-0.1.2/sarfile/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.2"
+
+from ._sarfile import sarfile
+
+# Defines some API functions as stand-alone.
+pack_files = sarfile.pack_files
+pack_tar = sarfile.pack_tar
+open = sarfile.open
