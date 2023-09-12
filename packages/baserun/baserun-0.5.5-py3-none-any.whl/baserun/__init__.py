83f2ffcb00f1f4d1c64f8222bd73d27de16543bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 14:07:02 +0000

    Add package baserun/0.5.5/baserun-0.5.5-py3-none-any.whl

---
+++ packages/baserun/baserun-0.5.5-py3-none-any.whl/baserun/__init__.py
@@ -0,0 +1,9 @@
+from .baserun import Baserun
+from .unittests_class import BaserunTestCase
+
+init = Baserun.init
+test = Baserun.test
+trace = Baserun.trace
+log = Baserun.log
+flush = Baserun.flush
+evals = Baserun.evals
