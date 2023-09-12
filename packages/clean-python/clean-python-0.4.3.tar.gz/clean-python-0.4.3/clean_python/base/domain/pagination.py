c04c3262c2d0705156aacec89f139bb1a19b1d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:05:33 +0000

    Add package clean-python/0.4.3/clean-python-0.4.3.tar.gz

---
+++ packages/clean-python/clean-python-0.4.3.tar.gz/clean-python-0.4.3/clean_python/base/domain/pagination.py
@@ -0,0 +1,27 @@
+# (c) Nelen & Schuurmans
+
+from typing import Generic
+from typing import Optional
+from typing import Sequence
+from typing import TypeVar
+
+from pydantic import BaseModel
+
+__all__ = ["Page", "PageOptions"]
+
+T = TypeVar("T")
+
+
+class PageOptions(BaseModel):
+    limit: int
+    offset: int = 0
+    order_by: str = "id"
+    ascending: bool = True
+    cursor: Optional[str] = None
+
+
+class Page(BaseModel, Generic[T]):
+    total: int
+    items: Sequence[T]
+    limit: Optional[int] = None
+    offset: Optional[int] = None
