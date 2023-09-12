219258bbda27d40be87bebb831e16393c405ba6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:05:39 +0000

    Add package cool-seq-tool/0.2.0.dev0/cool_seq_tool-0.2.0.dev0.tar.gz

---
+++ packages/cool-seq-tool/cool_seq_tool-0.2.0.dev0.tar.gz/cool_seq_tool-0.2.0.dev0/cool_seq_tool/data_sources/__init__.py
@@ -0,0 +1,7 @@
+"""Module for data sources"""
+from .seqrepo_access import SeqRepoAccess
+from .mane_transcript_mappings import MANETranscriptMappings
+from .transcript_mappings import TranscriptMappings
+from .uta_database import UTADatabase
+from .mane_transcript import MANETranscript
+from .alignment_mapper import AlignmentMapper
