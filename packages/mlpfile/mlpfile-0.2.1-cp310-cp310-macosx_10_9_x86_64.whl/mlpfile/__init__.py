1f436ed040f64d7862ec974b7d9d62666202796b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 05:04:17 +0000

    Add package mlpfile/0.2.1/mlpfile-0.2.1-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/mlpfile/mlpfile-0.2.1-cp310-cp310-macosx_10_9_x86_64.whl/mlpfile/__init__.py
@@ -0,0 +1,18 @@
+"""mlpfile: Multilayer perceptron file format and evaluation."""
+
+from _mlpfile import Model, Layer, LayerType
+
+
+__all__ = ["Model", "Layer", "LayerType", "cpp_dir"]
+
+
+def cpp_dir():
+    """Prints the directory containing the C++ header and source file.
+
+    If you install the package via ``pip``, you can use the files in your build
+    system. For example, a Makefile might contain::
+
+        CXXFLAGS := -I$(shell python -c "__import__('mlpfile').cpp_dir()")
+    """
+    import os.path
+    print(os.path.dirname(__file__) + "/cpp", end="")  # no newline
