b3a73c20c3126c1ac533e9476a929fe9de301730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 03:05:24 +0000

    Add package ebel/1.0.36/ebel-1.0.36-py3-none-any.whl

---
+++ packages/ebel/ebel-1.0.36-py3-none-any.whl/ebel/web/api/ebel/v1/nsides.py
@@ -0,0 +1,33 @@
+"""nSIDES API methods."""
+
+from flask import request
+
+from ebel.manager.rdbms.models.nsides import Nsides
+from ebel.web.api.ebel.v1 import _get_data, _get_paginated_ebel_query_result
+
+
+def get_nsides():
+    """Get generic NSIDES entry."""
+    return _get_data(Nsides)
+
+
+def get_ebel_relation():
+    """Get NSIDES related eBEL relations."""
+    conf = {
+        "drugbank_id": "out.drugbank_id",
+        "drug_label": "out.label",
+        "ppr": "prr",
+        "mean_reporting_frequency": "mean_reporting_frequency",
+        "condition_meddra_id": "in.condition_meddra_id",
+        "side_effect": "in.label",
+    }
+    sql = "SELECT "
+    sql += ", ".join([f"{v} as {k}" for k, v in conf.items()])
+    sql += " FROM has_side_effect"
+
+    paras = {k: request.args[k] for k in request.args if k in conf}
+    if paras:
+        wheres = [f'{conf[k].replace(".asString()","")} like "{v}"' for k, v in paras.items()]
+        sql += " WHERE " + " AND ".join(wheres)
+
+    return _get_paginated_ebel_query_result(sql)
