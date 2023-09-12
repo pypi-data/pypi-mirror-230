7507cb821cb7bab352e01528956feb784acb8692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 04:06:45 +0000

    Add package batchscanner-siklu/0.1.6/batchscanner-siklu-0.1.6.tar.gz

---
+++ packages/batchscanner-siklu/batchscanner-siklu-0.1.6.tar.gz/batchscanner-siklu-0.1.6/src/batchscanner/__init__.py
@@ -0,0 +1,24 @@
+"""  A CLI batch tool for Siklu radios.
+
+Script usage: batchscanner [-h] [-a {scan,show,script,set_tod}] [-n NETWORK_FILENAME] [-c CONFIG_FILENAME]
+
+options:
+  -h, --help            show this help message and exit
+  -a {scan,show,script,set_tod}
+                        Action for batchscanner to take (default: show). One of:
+                            scan: scan the network and identify which IP address is a Siklu radio;
+                            show: extract key metrics from radios (parsed outputs of 'show' commands);
+                            script: execute a script: send list of commands read from text file;
+                            set_tod: set date and time.
+  -n NETWORK_FILENAME   Mandatory filename specifying the Network (range of IP addresses to scan and login credentials).
+                        Default: 'network.txt'
+  -c CONFIG_FILENAME    Optional configuration file for overriding default program parameters.
+                        Default: 'config.toml'
+
+"""
+
+__author__ = 'Daniel Ephraty'
+__version__ = '0.1.6'
+
+
+
