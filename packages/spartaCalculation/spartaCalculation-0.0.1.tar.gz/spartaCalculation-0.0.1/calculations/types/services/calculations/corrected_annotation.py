7210211870ee7774b0468b810f1a706a6f6daef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-12 15:06:16 +0000

    Add package spartaCalculation/0.0.1/spartaCalculation-0.0.1.tar.gz

---
+++ packages/spartaCalculation/spartaCalculation-0.0.1.tar.gz/spartaCalculation-0.0.1/calculations/types/services/calculations/corrected_annotation.py
@@ -0,0 +1,19 @@
+from typing_extensions import TypedDict
+
+from app.types.enums.ams_property import RELAY_TYPE
+
+
+class LapTime(TypedDict):
+    time: str
+    distance: str
+    splitTime: str
+
+
+class CalculationPayload(TypedDict):
+    video_id: str
+    lane_number: int
+    relay_type: RELAY_TYPE
+    pool_length: int
+    frame_rate: int
+    lap_times: list
+    distance: int
