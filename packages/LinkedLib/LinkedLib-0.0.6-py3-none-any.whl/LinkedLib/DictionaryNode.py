6620fc66bf1e02afbe3153428836f41ca0856e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:07:13 +0000

    Add package LinkedLib/0.0.6/LinkedLib-0.0.6-py3-none-any.whl

---
+++ packages/LinkedLib/LinkedLib-0.0.6-py3-none-any.whl/LinkedLib/DictionaryNode.py
@@ -0,0 +1,9 @@
+from Node import Node
+
+class DictionaryNode(Node):
+    def __init__(Self):
+        Self.Key: str = None
+        Self.Value: int = None
+        Self.Place: int = None
+        Self.Next: Self = None
+        Self.IsHead = False
