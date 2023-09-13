d42fd8c74bad9bf80ef8151a0b019c18b471646c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:06:50 +0000

    Add package faust-avro-serializer/1.2.0/faust_avro_serializer-1.2.0-py3-none-any.whl

---
+++ packages/faust-avro-serializer/faust_avro_serializer-1.2.0-py3-none-any.whl/faust_avro_serializer/__init__.py
@@ -0,0 +1,7 @@
+"""Faust serializer that serializes Avro compatible messages"""
+
+__version__ = "1.2.0"
+
+
+from faust_avro_serializer.avro_serializer import FaustAvroSerializer
+from faust_avro_serializer.avro_serializer import MissingSchemaException
