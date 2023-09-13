0b1237429e19b801f5c100686e9b0a11e7761659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:08:14 +0000

    Add package cdpdev-datahub/0.10.5/cdpdev_datahub-0.10.5-py3-none-any.whl

---
+++ packages/cdpdev-datahub/cdpdev_datahub-0.10.5-py3-none-any.whl/datahub/emitter/aspect.py
@@ -0,0 +1,14 @@
+from datahub.metadata.schema_classes import ASPECT_CLASSES
+
+ASPECT_MAP = {
+    AspectClass.get_aspect_name(): AspectClass for AspectClass in ASPECT_CLASSES
+}
+
+TIMESERIES_ASPECT_MAP = {
+    name: klass
+    for name, klass in ASPECT_MAP.items()
+    if klass.get_aspect_type() == "timeseries"
+}
+
+JSON_CONTENT_TYPE = "application/json"
+JSON_PATCH_CONTENT_TYPE = "application/json-patch+json"
