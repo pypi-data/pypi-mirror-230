650413347b6e1d8294cbb2199d89286e3b2e93cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:05:07 +0000

    Add package hist/2.7.2/hist-2.7.2-py3-none-any.whl

---
+++ packages/hist/hist-2.7.2-py3-none-any.whl/hist/dask/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import importlib.util
+
+if not importlib.util.find_spec("dask_histogram"):
+    raise ModuleNotFoundError(
+        """for hist.dask, install the 'dask_histogram' package with:
+        pip install dask_histogram
+        or
+        conda install dask_histogram"""
+    )
+
+from .hist import Hist
+from .namedhist import NamedHist
+
+__all__ = ["Hist", "NamedHist"]
