4dc628dddfdf02ac7685c050c27c069ed8c07f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:05:35 +0000

    Add package temporian/0.1.4/temporian-0.1.4-cp38-cp38-macosx_13_0_arm64.whl

---
+++ packages/temporian/temporian-0.1.4-cp38-cp38-macosx_13_0_arm64.whl/temporian/implementation/numpy/operators/window/moving_count.py
@@ -0,0 +1,59 @@
+# Copyright 2021 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List, Optional, Union
+import numpy as np
+from temporian.core.data.duration_utils import NormalizedDuration
+
+from temporian.core.operators.window.moving_count import (
+    MovingCountOperator,
+)
+from temporian.implementation.numpy import implementation_lib
+from temporian.implementation.numpy.operators.window.base import (
+    BaseWindowNumpyImplementation,
+)
+from temporian.implementation.numpy_cc.operators import operators_cc
+
+
+class MovingCountNumpyImplementation(BaseWindowNumpyImplementation):
+    """Numpy implementation of the moving count operator."""
+
+    def _implementation(self):
+        return operators_cc.moving_count
+
+    def _compute(
+        self,
+        src_timestamps: np.ndarray,
+        src_features: List[np.ndarray],
+        sampling_timestamps: Optional[np.ndarray],
+        dst_features: List[np.ndarray],
+        window_length: Union[NormalizedDuration, np.ndarray],
+    ) -> None:
+        assert isinstance(self.operator, MovingCountOperator)
+
+        del src_features  # Features are ignored
+
+        kwargs = {
+            "evset_timestamps": src_timestamps,
+            "window_length": window_length,
+        }
+        if sampling_timestamps is not None:
+            kwargs["sampling_timestamps"] = sampling_timestamps
+        dst_feature = operators_cc.moving_count(**kwargs)
+        dst_features.append(dst_feature)
+
+
+implementation_lib.register_operator_implementation(
+    MovingCountOperator, MovingCountNumpyImplementation
+)
