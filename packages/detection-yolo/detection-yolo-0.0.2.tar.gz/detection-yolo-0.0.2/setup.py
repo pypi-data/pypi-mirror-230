3a995ce672022058f074f30130fb0a75ebcdb9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:07:41 +0000

    Add package detection-yolo/0.0.2/detection-yolo-0.0.2.tar.gz

---
+++ packages/detection-yolo/detection-yolo-0.0.2.tar.gz/detection-yolo-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.2'
+
+setup(
+    name='detection-yolo', 
+    version=VERSION,
+    packages=find_packages(),
+    install_requires = ['ultralytics==8.0.54'],
+
+)
