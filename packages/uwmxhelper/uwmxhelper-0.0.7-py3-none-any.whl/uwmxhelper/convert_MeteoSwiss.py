0ca10d3d400a4125d263ad5bdc0ad9406ceead9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:08:27 +0000

    Add package uwmxhelper/0.0.7/uwmxhelper-0.0.7-py3-none-any.whl

---
+++ packages/uwmxhelper/uwmxhelper-0.0.7-py3-none-any.whl/uwmxhelper/convert_MeteoSwiss.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+
+def convert_MeteoSwiss(df):
+    
+    """
+    Parameters
+    ----------
+    df : DataFrame containting 3 columns: stn; time; rre150z0
+        Raw MeteoSwiss rainfall data
+
+    Returns
+    -------
+    df : DataFrame with 2 columns: "time" (timestamps) and "value" (numeric values)
+        Formatted rainfall data
+
+    """
+    
+    df.drop(df.columns[[0]],axis=1,inplace=True)
+    df.replace('-',np.nan,inplace=True)
+    df.dropna(inplace=True)
+    df.columns = ['time','value']
+    df.time = pd.to_datetime(df.time,format='%Y%m%d%H%M')
+    df.value = df.value.astype('float')
+    # Modify dataframe: addition to make file fit requirements of inflow generator input files
+    df['time'] = df.time.dt.strftime('%Y-%m-%d %H:%M')
+    
+    df.set_index('time', inplace=True) 
+   
+    
+    return df
