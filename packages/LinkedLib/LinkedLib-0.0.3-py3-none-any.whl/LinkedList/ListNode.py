1262572a714af067711d8bd908542d6d8ec7263c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:07:21 +0000

    Add package LinkedLib/0.0.3/LinkedLib-0.0.3-py3-none-any.whl

---
+++ packages/LinkedLib/LinkedLib-0.0.3-py3-none-any.whl/LinkedList/ListNode.py
@@ -0,0 +1,8 @@
+from .Node import Node
+
+class ListNode(Node):
+    def __init__(Self):
+        Self.Value: int = None
+        Self.Place: int = None
+        Self.Next: Self = None
+        Self.IsHead = False
