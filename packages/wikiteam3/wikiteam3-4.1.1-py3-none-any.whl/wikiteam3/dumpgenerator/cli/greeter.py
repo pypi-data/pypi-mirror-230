fe22050f5a8b49ba0813566ef1353ca725897156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:04:59 +0000

    Add package wikiteam3/4.1.1/wikiteam3-4.1.1-py3-none-any.whl

---
+++ packages/wikiteam3/wikiteam3-4.1.1-py3-none-any.whl/wikiteam3/dumpgenerator/cli/greeter.py
@@ -0,0 +1,40 @@
+import datetime
+
+from wikiteam3.dumpgenerator.version import getVersion
+
+
+def welcome():
+    message = ""
+    """Opening message"""
+    message += "#" * 73
+    message += "\n"
+    welcome_string = "# Welcome to DumpGenerator %s by WikiTeam3 (GPL v3)" % (
+        getVersion()
+    )
+    welcome_string += " " * (73 - len(welcome_string) - 1) + "#"
+    message += welcome_string
+    message += "\n"
+    message += (
+        "# More info at: https://github.com/saveweb/wikiteam3                  #"
+    )
+    message += "\n"
+    message += (
+        "# Copyright (C) 2011-%d WikiTeam developers                           #\n"
+        % (datetime.datetime.now().year)
+    )
+    message += "#" * 73
+
+    return message
+
+
+def bye():
+    """Closing message"""
+    print("")
+    print("---> Done <---")
+    print("")
+    print("If this is a public wiki, please, consider publishing this dump.")
+    print("Do it yourself as explained in:")
+    print("  https://github.com/WikiTeam/wikiteam/wiki/Tutorial#Publishing_the_dump")
+    print("")
+    print("Good luck! Bye!")
+    print("")
