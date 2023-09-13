6cc0c69bc519d9656734ae858d5b9c879097241a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:07:08 +0000

    Add package psyke/0.4.9.dev24/psyke-0.4.9.dev24.tar.gz

---
+++ packages/psyke/psyke-0.4.9.dev24.tar.gz/psyke-0.4.9.dev24/psyke/extraction/hypercubic/__init__.py
@@ -0,0 +1,165 @@
+from __future__ import annotations
+
+from abc import ABC
+from typing import Iterable
+import numpy as np
+import pandas as pd
+from sklearn.base import ClassifierMixin
+from sklearn.feature_selection import SelectKBest, f_regression, f_classif
+from sklearn.linear_model import LinearRegression
+from tuprolog.core import Var, Struct, clause
+from tuprolog.theory import Theory, mutable_theory
+from psyke import logger, PedagogicalExtractor
+from psyke.extraction.hypercubic.hypercube import HyperCube, RegressionCube, ClassificationCube, ClosedCube
+from psyke.utils.logic import create_variable_list, create_head, to_var, Simplifier
+from psyke.utils import Target, get_int_precision
+from psyke.extraction.hypercubic.strategy import Strategy, FixedStrategy
+
+
+class HyperCubePredictor:
+    def __init__(self, cubes=[], output=Target.CONSTANT, normalization=None):
+        self._hypercubes = cubes
+        self._output = output
+        self.normalization = normalization
+
+    def _predict(self, dataframe: pd.DataFrame) -> Iterable:
+        return np.array([self._predict_from_cubes(dict(row.to_dict())) for _, row in dataframe.iterrows()])
+
+    def _predict_from_cubes(self, data: dict[str, float]) -> float | None:
+        data = {k: v for k, v in data.items()}
+        for cube in self._hypercubes:
+            if cube.__contains__(data):
+                if self._output == Target.CLASSIFICATION:
+                    return HyperCubePredictor._get_cube_output(cube, data)
+                else:
+                    return round(HyperCubePredictor._get_cube_output(cube, data), get_int_precision())
+        return None
+
+    @property
+    def n_rules(self):
+        return len(list(self._hypercubes))
+
+    @property
+    def volume(self):
+        return sum([cube.volume() for cube in self._hypercubes])
+
+    @staticmethod
+    def _get_cube_output(cube, data: dict[str, float]) -> float:
+        return cube.output.predict(pd.DataFrame([data])).flatten()[0] if \
+            isinstance(cube, RegressionCube) else cube.output
+
+
+class HyperCubeExtractor(HyperCubePredictor, PedagogicalExtractor, ABC):
+    def __init__(self, predictor, output, normalization):
+        PedagogicalExtractor.__init__(self, predictor, normalization=normalization)
+        HyperCubePredictor.__init__(self, output=output, normalization=normalization)
+
+    def _default_cube(self) -> HyperCube | RegressionCube | ClassificationCube:
+        if self._output == Target.CONSTANT:
+            return HyperCube()
+        if self._output == Target.REGRESSION:
+            return RegressionCube()
+        return ClassificationCube()
+
+    @staticmethod
+    def _create_head(dataframe: pd.DataFrame, variables: list[Var], output: float | LinearRegression) -> Struct:
+        return create_head(dataframe.columns[-1], variables[:-1], output) \
+            if not isinstance(output, LinearRegression) else \
+            create_head(dataframe.columns[-1], variables[:-1], variables[-1])
+
+    def _ignore_dimensions(self) -> Iterable[str]:
+        return []
+
+    def _create_theory(self, dataframe: pd.DataFrame, sort: bool = True) -> Theory:
+        new_theory = mutable_theory()
+        for cube in self._hypercubes:
+            logger.info(cube.output)
+            logger.info(cube.dimensions)
+            variables = create_variable_list([], dataframe, sort)
+            variables[dataframe.columns[-1]] = to_var(dataframe.columns[-1])
+            head = HyperCubeExtractor._create_head(dataframe, list(variables.values()),
+                                                   self.unscale(cube.output, dataframe.columns[-1]))
+            body = cube.body(variables, self._ignore_dimensions(), self.unscale, self.normalization)
+            new_theory.assertZ(clause(head, body))
+        return HyperCubeExtractor._prettify_theory(new_theory)
+
+    @staticmethod
+    def _prettify_theory(theory: Theory) -> Theory:
+        visitor = Simplifier()
+        new_clauses = []
+        for c in theory.clauses:
+            body = c.body
+            structs = body.unfolded if c.body_size > 1 else [body]
+            new_structs = []
+            for s in structs:
+                new_structs.append(s.accept(visitor))
+            new_clauses.append(clause(c.head, new_structs))
+        return mutable_theory(new_clauses)
+
+
+class FeatureRanker:
+    def __init__(self, feat):
+        self.scores = None
+        self.feat = feat
+
+    def fit(self, model, samples):
+        predictions = np.array(model.predict(samples)).flatten()
+        function = f_classif if isinstance(model, ClassifierMixin) else f_regression
+        best = SelectKBest(score_func=function, k="all").fit(samples, predictions)
+        self.scores = np.array(best.scores_) / max(best.scores_)
+        return self
+
+    def fit_on_data(self, samples):
+        function = f_classif if isinstance(samples.iloc[0, -1], str) else f_regression
+        best = SelectKBest(score_func=function, k="all").fit(samples.iloc[:, :-1], samples.iloc[:, -1])
+        self.scores = np.array(best.scores_) / max(best.scores_)
+        return self
+
+    def rankings(self):
+        return list(zip(self.feat, self.scores))
+
+
+class Grid:
+    def __init__(self, iterations: int = 1, strategy: Strategy | list[Strategy] = FixedStrategy()):
+        self.iterations = iterations
+        self.strategy = strategy
+
+    def get(self, feature: str, depth: int) -> int:
+        if isinstance(self.strategy, list):
+            return self.strategy[depth].get(feature)
+        else:
+            return self.strategy.get(feature)
+
+    def iterate(self) -> range:
+        return range(self.iterations)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "Grid ({}). {}".format(self.iterations, self.strategy)
+
+
+class Node:
+    def __init__(self, dataframe: pd.DataFrame, cube: ClosedCube = None):
+        self.dataframe = dataframe
+        self.cube: ClosedCube = cube
+        self.right: Node | None = None
+        self.left: Node | None = None
+
+    @property
+    def children(self) -> list[Node]:
+        return [self.right, self.left]
+
+    def search(self, point: dict[str, float]) -> ClosedCube:
+        if self.right is None:
+            return self.cube
+        if point in self.right.cube:
+            return self.right.search(point)
+        return self.left.search(point)
+
+    @property
+    def leaves(self):
+        if self.right is None:
+            return 1
+        return self.right.leaves + self.left.leaves
