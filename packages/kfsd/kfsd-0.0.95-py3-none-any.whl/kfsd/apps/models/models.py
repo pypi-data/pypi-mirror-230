06dd6b64986ef24e962997b17e9a44927eab0236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:06:46 +0000

    Add package kfsd/0.0.95/kfsd-0.0.95-py3-none-any.whl

---
+++ packages/kfsd/kfsd-0.0.95-py3-none-any.whl/kfsd/apps/models/models.py
@@ -0,0 +1,6 @@
+from .tables.outpost import Outpost
+from .tables.reference import Reference
+from .tables.exchange import Exchange
+from .tables.route import Route
+from .tables.producer import Producer
+from .tables.queue import Queue
