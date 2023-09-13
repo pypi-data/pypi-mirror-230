3261e4cad5dd26be94da6514a31a668a4b14b2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:09:23 +0000

    Add package hyperon-das-atomdb/0.1.3/hyperon_das_atomdb-0.1.3.tar.gz

---
+++ packages/hyperon-das-atomdb/hyperon_das_atomdb-0.1.3.tar.gz/hyperon_das_atomdb-0.1.3/hyperon_das_atomdb/exceptions.py
@@ -0,0 +1,22 @@
+class BaseException(Exception):
+    """
+    Base class to exceptions
+    """
+
+    def __init__(self, message: str, details: str = ""):
+        self.message = message
+        self.details = details
+
+        super().__init__(self.message, self.details)
+
+
+class ConnectionMongoDBException(BaseException):
+    ...  # pragma no cover
+
+
+class NodeDoesNotExistException(BaseException):
+    ...  # pragma no cover
+
+
+class LinkDoesNotExistException(BaseException):
+    ...  # pragma no cover
