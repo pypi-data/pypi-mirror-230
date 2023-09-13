a929b6b0e60c5214ef4c0dab045e4051199cf77f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:08:24 +0000

    Add package league-shop-base/1.0.53/league-shop-base-1.0.53.tar.gz

---
+++ packages/league-shop-base/league-shop-base-1.0.53.tar.gz/league-shop-base-1.0.53/lsb/actions.py
@@ -0,0 +1,64 @@
+from django.contrib import messages
+from form_action import form_action
+
+from lsb.forms import ProductStatusRemarksForm
+from lsb.models import max_datetime
+from lsb.models import min_datetime
+
+
+def mark_as_handleveled(modeladmin, request, queryset):
+    updated = queryset.update(is_handleveled=True)
+    messages.info(request, f"{updated} product(s) marked as handleveled.")
+
+
+def clear_handleveled(modeladmin, request, queryset):
+    updated = queryset.update(is_handleveled=False)
+    messages.info(request, f"Cleared handleveled from {updated} product(s).")
+
+
+@form_action(ProductStatusRemarksForm, description="Mark as disabled")
+def mark_as_disabled_with_status(modeladmin, request, queryset, form):
+    status = form.cleaned_data["status"]
+    remarks = form.cleaned_data["remarks"]
+
+    to_update = {
+        "disabled_until": max_datetime,
+    }
+
+    if status != "unchanged":
+        to_update["status"] = None if status == "" else status
+    if remarks != "unchanged":
+        to_update["remarks"] = None if remarks == "" else remarks
+
+    updated = queryset.update(**to_update)
+    messages.info(
+        request,
+        (
+            f"{updated} product(s) marked as disabled with status {status} and"
+            f" remarks {remarks}."
+        ),
+    )
+
+
+@form_action(ProductStatusRemarksForm, description="Clear disabled")
+def clear_disabled_with_status(modeladmin, request, queryset, form):
+    status = form.cleaned_data["status"]
+    remarks = form.cleaned_data["remarks"]
+
+    to_update = {
+        "disabled_until": min_datetime,
+    }
+
+    if status != "unchanged":
+        to_update["status"] = None if status == "" else status
+    if remarks != "unchanged":
+        to_update["remarks"] = None if remarks == "" else remarks
+
+    updated = queryset.update(**to_update)
+    messages.info(
+        request,
+        (
+            f"Cleared disabled from {updated} product(s) marked with status"
+            f" {status} and remarks {remarks}."
+        ),
+    )
