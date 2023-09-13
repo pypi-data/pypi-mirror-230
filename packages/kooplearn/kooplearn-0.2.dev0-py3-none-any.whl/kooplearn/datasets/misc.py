8a7be7839fcfb8f9e1ec1984977c4a6b9b912e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:08:10 +0000

    Add package kooplearn/0.2.0.dev0/kooplearn-0.2.dev0-py3-none-any.whl

---
+++ packages/kooplearn/kooplearn-0.2.dev0-py3-none-any.whl/kooplearn/datasets/misc.py
@@ -0,0 +1,33 @@
+import abc
+from typing import NamedTuple
+import numpy as np
+
+from tqdm import tqdm
+
+class LinalgDecomposition(NamedTuple):
+    values: np.ndarray
+    x: np.ndarray
+    functions: np.ndarray
+
+# General utility classes
+class DataGenerator(abc.ABC):
+    @abc.abstractmethod
+    def generate(self, X0: np.ndarray, T: int = 1):
+        pass
+
+class DiscreteTimeDynamics(DataGenerator):
+    def generate(self, X0: np.ndarray, T: int = 1, show_progress: bool = False):
+        memory = np.zeros((T + 1,) + X0.shape)
+        memory[0] = X0
+        if show_progress:
+            _iter = tqdm(range(T), desc='Generating data', unit='step', leave=False)
+        else:
+            _iter = range(T)
+            
+        for t in _iter:
+            memory[t + 1] = self._step(memory[t])
+        return memory
+
+    @abc.abstractmethod
+    def _step(self, X: np.ndarray):
+        pass
