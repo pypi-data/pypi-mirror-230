09d5e1d9e4460173caf8d8f6da70e189a134420e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:09:34 +0000

    Add package cpenormalize/0.0.1/cpenormalize-0.0.1.tar.gz

---
+++ packages/cpenormalize/cpenormalize-0.0.1.tar.gz/cpenormalize-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='cpenormalize',
+    version='0.0.1',
+    description=__doc__,
+    long_description='',
+    url='http://github.com/d33tah/cpenormalize',
+    author='Jacek "d33tah" Wielemborek',
+    author_email='d33tah@gmail.com',
+    license='WTFPL',
+    packages=['cpenormalize'],
+    install_requires=[
+        'cpe',
+    ]
+)
