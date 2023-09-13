07308be4d289346402b7c9b18ff858720597a192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:05:34 +0000

    Add package act-neuron/0.1.7/act_neuron-0.1.7-py3-none-any.whl

---
+++ packages/act-neuron/act_neuron-0.1.7-py3-none-any.whl/simulation/run_simulation.py
@@ -0,0 +1,8 @@
+from simulation_configs import pospischilsPYr, pospischilsPYr_passive
+
+from act import simulator
+
+if __name__ == "__main__":
+    config = pospischilsPYr_passive
+
+    p = simulator.run(config, subprocess=False)
