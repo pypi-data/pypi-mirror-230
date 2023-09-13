f5a6d79a0fd26e74a6736f372f1379e16d1ed4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:07:14 +0000

    Add package maui-software/0.1.19/maui_software-0.1.19.tar.gz

---
+++ packages/maui-software/maui_software-0.1.19.tar.gz/maui_software-0.1.19/maui/samples/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+The module ``visualizations`` has a collection of functions to create visualizations over sudios and its acoustic indices.
+
+
+.. autosummary::
+    :toctree: generated/
+
+    get_leec_audio_sample
+
+"""
+
+from .audio_samples import (get_leec_audio_sample)
+
+
+__all__ = [ # visualizations
+           'get_leec_audio_sample']
