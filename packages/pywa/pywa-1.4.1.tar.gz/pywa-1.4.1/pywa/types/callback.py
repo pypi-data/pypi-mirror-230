a1bd30805fda65bc86201b7bc702eaf8fbcc0e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:07:50 +0000

    Add package pywa/1.4.1/pywa-1.4.1.tar.gz

---
+++ packages/pywa/pywa-1.4.1.tar.gz/pywa-1.4.1/pywa/types/callback.py
@@ -0,0 +1,303 @@
+__all__ = [
+    'CallbackButton',
+    'CallbackSelection',
+    'Button',
+    'SectionRow',
+    'Section',
+    'SectionList',
+    'CallbackData',
+    'CallbackDataT',
+    'CLB_SEP',
+    'CLB_DATA_SEP',
+]
+
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import TYPE_CHECKING, Iterable, TypeVar, Generic, Callable, Any
+from .base_update import BaseUpdate
+from .others import Metadata, User, ReplyToMessage, MessageType
+
+if TYPE_CHECKING:
+    from pywa.client import WhatsApp
+
+ALLOWED_TYPES = (str, int, bool, float)
+CLB_SEP = ';'
+CLB_DATA_SEP = ':'
+
+
+class CallbackData:
+    """
+    Base class for all callback data classes. Subclass this class to create a type-safe callback data class.
+
+        If you use dataclasses, which is the recommended way, you should not use ``kw_only=True``.
+        This is because we are limited to 200 characters in the callback data, so we need to use positional arguments.
+        So object like User(id=123, name='John') will be converted to ``123:John``.
+
+        Currently, the following types are supported:
+        ``str``, ``int``, ``bool``, ``float`` (and ``Enum`` that inherits from ``str`` e.g ``class State(str, Enum)``).
+
+        Also, you cannot use the characters ``:`` and ``;`` in the callback data, because they are used as separators.
+
+    Example:
+
+        >>> from pywa import WhatsApp
+        >>> from pywa.types import CallbackData, Button
+        >>> from dataclasses import dataclass # Use dataclass to get free ordered __init__
+        >>> @dataclass(frozen=True, slots=True) # Do not use kw_only=True
+        >>> class UserData(CallbackData): # Subclass CallbackData
+        ...     id: int
+        ...     admin: bool
+        >>> wa = WhatsApp(...)
+        >>> wa.send_message(
+        ...     to='972987654321',
+        ...     text='Click the button to get the user',
+        ...     buttons=[Button(title='Get user', callback_data=UserData(id=123, admin=True))]
+        ... )
+
+        >>> @wa.on_callback_button(factory=UserData) # Register a handler for the callback data
+        ... def on_user_data(client: WhatsApp, btn: CallbackButton[UserData]): # For autocomplete
+        ...    if btn.data.admin): ... # Access the data object as an attribute
+    """
+
+    __callback_id__: int = 0
+
+    def __init_subclass__(cls, **kwargs):
+        """Validate the callback data class and set a unique ID for it."""
+        super().__init_subclass__(**kwargs)
+        if not (annotations := cls.__annotations__.items()):
+            raise TypeError(f"Callback data class {cls.__name__} must have at least one field.")
+        if unsupported_fields := {
+            (field_name, field_type) for field_name, field_type in annotations
+            if not issubclass(field_type, ALLOWED_TYPES)
+        }:
+            raise TypeError(f"Unsupported types {unsupported_fields} in callback data. Use one of {ALLOWED_TYPES}.")
+        cls.__callback_id__ = CallbackData.__callback_id__
+        CallbackData.__callback_id__ += 1
+
+    @classmethod
+    def from_str(
+            cls,
+            data: str,
+    ) -> 'CallbackData':
+        """
+        Internal function to convert a callback string to a callback object.
+        """
+        try:
+            # noinspection PyArgumentList
+            return cls(*(
+                annotation(value) for annotation, value in zip(
+                    cls.__annotations__.values(),
+                    data.split(CLB_DATA_SEP)[1:],
+                    strict=True
+                )
+            ))
+        except (ValueError, TypeError) as e:
+            raise ValueError(f"Invalid callback data for {cls.__name__}: {data}") from e
+
+    @staticmethod
+    def _not_contains(value: Any, *not_) -> str:
+        """Internal function to validate that the value does not contain the separator."""
+        if any(sep in (str_val := str(value)) for sep in not_):
+            raise ValueError(f"Callback data cannot contain the characters {not_}.")
+        return str_val
+
+    def to_str(self) -> str:
+        """
+        Internal function to convert a callback object to a callback string.
+        """
+        return CLB_DATA_SEP.join((str(self.__callback_id__), *(
+            self._not_contains(getattr(self, field_name), CLB_SEP, CLB_DATA_SEP)
+            if not issubclass(field_type, (bool, Enum)) else (' ' if getattr(self, field_name) else '')
+            if field_type is bool else self._not_contains(getattr(self, field_name).value, CLB_SEP, CLB_DATA_SEP)
+            for field_name, field_type in self.__annotations__.items()
+        )))
+
+    @classmethod
+    def join_to_str(cls, *datas: Any) -> str:
+        """Internal function to join multiple callback objects to a callback string."""
+        return CLB_SEP.join(
+            data.to_str() if isinstance(data, CallbackData)
+            else cls._not_contains(data, CLB_DATA_SEP)
+            for data in datas
+        )
+
+
+CallbackDataT = TypeVar('CallbackDataT', bound=CallbackData | Iterable[type[CallbackData]] | str | Callable[[str], Any])
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class CallbackButton(BaseUpdate, Generic[CallbackDataT]):
+    """
+    Represents a callback button.
+
+    Attributes:
+        id: The ID of the message.
+        metadata: The metadata of the message (to which phone number it was sent).
+        type: The message type (always ``interactive``).
+        from_user: The user who sent the message.
+        timestamp: The timestamp when the message was sent.
+        reply_to_message: The message to which this callback button is a reply to.
+        data: The data of the button.
+        title: The title of the button.
+    """
+    id: str
+    type: MessageType
+    metadata: Metadata
+    from_user: User
+    timestamp: datetime
+    reply_to_message: ReplyToMessage
+    data: CallbackDataT
+    title: str
+
+    @property
+    def message_id_to_reply(self) -> str:
+        """The ID of the message to reply to."""
+        return self.reply_to_message.message_id
+
+    @classmethod
+    def from_dict(cls, client: 'WhatsApp', data: dict):
+        message = data['messages'][0]
+        return cls(
+            _client=client,
+            id=message['id'],
+            metadata=Metadata.from_dict(data['metadata']),
+            type=MessageType(message['type']),
+            from_user=User.from_dict(data['contacts'][0]),
+            timestamp=datetime.fromtimestamp(int(message['timestamp'])),
+            reply_to_message=ReplyToMessage.from_dict(message['context']),
+            data=message['interactive']['button_reply']['id'],
+            title=message['interactive']['button_reply']['title']
+        )
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class CallbackSelection(BaseUpdate, Generic[CallbackDataT]):
+    """
+    Represents a callback selection.
+
+    Attributes:
+        id: The ID of the message.
+        metadata: The metadata of the message (to which phone number it was sent).
+        type: The message type (always ``interactive``).
+        from_user: The user who sent the message.
+        timestamp: The timestamp when the message was sent.
+        reply_to_message: The message to which this callback selection is a reply to.
+        data: The data of the selection.
+        title: The title of the selection.
+        description: The description of the selection (optional).
+    """
+    id: str
+    type: MessageType
+    metadata: Metadata
+    from_user: User
+    timestamp: datetime
+    reply_to_message: ReplyToMessage
+    data: CallbackDataT
+    title: str
+    description: str | None
+
+    @classmethod
+    def from_dict(cls, client: 'WhatsApp', data: dict):
+        message = data['messages'][0]
+        return cls(
+            _client=client,
+            id=message['id'],
+            metadata=Metadata.from_dict(data['metadata']),
+            type=MessageType(message['type']),
+            from_user=User.from_dict(data['contacts'][0]),
+            timestamp=datetime.fromtimestamp(int(message['timestamp'])),
+            reply_to_message=ReplyToMessage.from_dict(message['context']),
+            data=message['interactive']['list_reply']['id'],
+            title=message['interactive']['list_reply']['title'],
+            description=message['interactive']['list_reply'].get('description')
+        )
+
+
+@dataclass(frozen=True, slots=True)
+class Button:
+    """
+    Represents a button in the button list.
+
+    Attributes:
+        title: The title of the button (up to 20 characters).
+        callback_data: The payload to send when the user clicks on the button (up to 256 characters).
+    """
+    title: str
+    callback_data: CallbackDataT
+
+    def to_dict(self) -> dict:
+        return {
+            "type": "reply",
+            "reply":
+                {
+                    "id": self.callback_data.to_str()
+                    if isinstance(self.callback_data, CallbackData) else self.callback_data
+                    if isinstance(self.callback_data, str) else self.callback_data.join_to_str(*self.callback_data),
+                    "title": self.title
+                }
+        }
+
+
+@dataclass(frozen=True, slots=True)
+class SectionRow:
+    """
+    Represents a row in a section.
+
+    Attributes:
+        title: The title of the row (up to 24 characters).
+        callback_data: The payload to send when the user clicks on the row (up to 200 characters).
+        description: The description of the row (optional, up to 72 characters).
+    """
+    title: str
+    callback_data: CallbackDataT
+    description: str | None = None
+
+    def to_dict(self) -> dict:
+        d = {
+            "id": self.callback_data.to_str() if isinstance(self.callback_data, CallbackData)
+            else self.callback_data if isinstance(self.callback_data, str)
+            else self.callback_data.join_to_str(*self.callback_data),
+            "title": self.title
+        }
+        if self.description:
+            d["description"] = self.description
+        return d
+
+
+@dataclass(frozen=True, slots=True)
+class Section:
+    """
+    Represents a section in a section list.
+
+    Attributes:
+        title: The title of the section (up to 24 characters).
+        rows: The rows in the section (at least 1, no more than 10).
+    """
+    title: str
+    rows: Iterable[SectionRow]
+
+    def to_dict(self) -> dict:
+        return {
+            "title": self.title,
+            "rows": tuple(row.to_dict() for row in self.rows)
+        }
+
+
+@dataclass(frozen=True, slots=True)
+class SectionList:
+    """
+    Represents a section list in an interactive message.
+
+    Attributes:
+        button_title: The title of the button that opens the section list (up to 20 characters).
+        sections: The sections in the section list (at least 1, no more than 10).
+    """
+    button_title: str
+    sections: Iterable[Section]
+
+    def to_dict(self) -> dict:
+        return {
+            "button": self.button_title,
+            "sections": tuple(section.to_dict() for section in self.sections)
+        }
---
+++ packages/pywa/pywa-1.4.1.tar.gz/pywa-1.4.1/pywa/webhook.py
@@ -0,0 +1,109 @@
+from __future__ import annotations
+from pywa.handlers import (
+    Handler, MessageHandler, CallbackButtonHandler, CallbackSelectionHandler, RawUpdateHandler, MessageStatusHandler
+)
+from pywa.utils import FlaskApp, FastAPIApp
+
+"""The webhook module contains the Webhook class, which is used to register a webhook to listen for incoming 
+messages."""
+
+import collections
+import threading
+from typing import TYPE_CHECKING, Callable, Any, Type
+from pywa.types import Message, CallbackButton, CallbackSelection, MessageStatus
+from pywa.types.base_update import BaseUpdate
+from pywa import utils
+
+if TYPE_CHECKING:
+    from pywa import WhatsApp
+
+__all__ = ["Webhook"]
+
+
+class Webhook:
+    """Register a webhook to listen for incoming messages."""
+
+    def __init__(
+            self,
+            wa_client: WhatsApp,
+            server: FlaskApp | FastAPIApp,
+            verify_token: str,
+            webhook_endpoint: str,
+            filter_updates: bool
+    ):
+        self.handlers: dict[str, list[Callable[[WhatsApp, BaseUpdate | dict], Any]]] = collections.defaultdict(list)
+        self.wa_client = wa_client
+        self.server = server
+        self.verify_token = verify_token
+        self.webhook_endpoint = webhook_endpoint
+        self.filter_updates = filter_updates
+
+        hub_vt = "hub.verify_token"
+        hub_ch = "hub.challenge"
+
+        if utils.is_flask_app(self.server):
+            import flask
+
+            @self.server.route(self.webhook_endpoint, methods=["GET"])
+            def verify_token():
+                if flask.request.args.get(hub_vt) == self.verify_token:
+                    return flask.request.args.get(hub_ch), 200
+                return "Error, invalid verification token", 403
+
+            @self.server.route(self.webhook_endpoint, methods=["POST"])
+            def webhook():
+                threading.Thread(target=self.call_handlers, args=(flask.request.json,)).start()
+                return "ok", 200
+
+        elif utils.is_fastapi_app(self.server):
+            import fastapi
+
+            @self.server.get(self.webhook_endpoint)
+            def verify_token(
+                    token: str = fastapi.Query(..., alias=hub_vt),
+                    challenge: str = fastapi.Query(..., alias=hub_ch)
+            ):
+                if token == self.verify_token:
+                    return fastapi.Response(content=challenge, status_code=200)
+                return fastapi.Response(content="Error, invalid verification token", status_code=403)
+
+            @self.server.post(self.webhook_endpoint)
+            def webhook(payload: dict = fastapi.Body(...)):
+                threading.Thread(target=self.call_handlers, args=(payload,)).start()
+                return fastapi.Response(content="ok", status_code=200)
+
+        else:
+            raise ValueError("The server must be a Flask or FastAPI app.")
+
+    def call_handlers(self, update: dict) -> None:
+        """Call the handlers for the given update."""
+        try:
+            if not self.filter_updates or (
+                    update["entry"][0]["changes"][0]["value"]["metadata"][
+                        "phone_number_id"] == self.wa_client.phone_id):
+                update_obj, key = self._convert_dict_to_update(client=self.wa_client, d=update)
+                if key is None:
+                    return
+                for handler in self.handlers[key.__handler_type__]:
+                    handler(self.wa_client, update_obj)
+            raise KeyError  # to always skip the except block
+        except (KeyError, IndexError):  # the update not send to this phone id or filter_updates is True
+            for raw_update_handler in self.handlers[RawUpdateHandler.__handler_type__]:
+                raw_update_handler(self.wa_client, update)
+
+    @staticmethod
+    def _convert_dict_to_update(client: WhatsApp, d: dict) -> tuple[BaseUpdate | None, Type[Handler] | None]:
+        """Convert a webhook dict to a BaseUpdate object."""
+        value = d["entry"][0]["changes"][0]["value"]
+        if 'messages' in value:
+            if value["messages"][0]["type"] != "interactive":
+                return Message.from_dict(client=client, value=value), MessageHandler
+            else:
+                if value["messages"][0]["interactive"]["type"] == "button_reply":
+                    return CallbackButton.from_dict(client=client, data=value), CallbackButtonHandler
+                elif value["messages"][0]["interactive"]["type"] == "list_reply":
+                    return CallbackSelection.from_dict(client=client, data=value), CallbackSelectionHandler
+
+        elif 'statuses' in value:
+            return MessageStatus.from_dict(client=client, data=value), MessageStatusHandler
+        return None, None  # the update is not supported
