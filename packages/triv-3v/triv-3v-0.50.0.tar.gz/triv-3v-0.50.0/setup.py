a45de5c160c16fad4d03d70cfd9752fd1c421299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:04:59 +0000

    Add package triv-3v/0.50.0/triv-3v-0.50.0.tar.gz

---
+++ packages/triv-3v/triv-3v-0.50.0.tar.gz/triv-3v-0.50.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    author='Triv Collective',
+    author_email='maintainer@triv.co',
+    description='A syntax for the web and more...',
+    install_requires=['triv'],
+    long_description='Download package triv instead',
+    name='triv-3v',
+    platforms=['all'],
+    url='https://pypi.org/project/triv/',
+    version='0.50.0',
+)
