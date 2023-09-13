81eb2bfd7b075e2f0a31479d44dc9a53586cae7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:09:49 +0000

    Add package titrate/0.5.0/titrate-0.5.0.tar.gz

---
+++ packages/titrate/titrate-0.5.0.tar.gz/titrate-0.5.0/titrate/plotting.py
@@ -0,0 +1,234 @@
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy import visualization as viz
+from astropy.table import QTable, unique
+from astropy.units import Quantity
+
+from titrate.datasets import AsimovMapDataset
+from titrate.statistics import QMuTestStatistic, QTildeMuTestStatistic
+
+STATISTICS = {"qmu": QMuTestStatistic, "qtildemu": QTildeMuTestStatistic}
+
+
+class UpperLimitPlotter:
+    def __init__(self, path, channel, uls=True, expected_uls=True, ax=None):
+        self.path = path
+        self.ax = ax if ax is not None else plt.gca()
+
+        try:
+            table = QTable.read(self.path, path=f"upperlimits/{channel}")
+        except OSError:
+            channels = list(h5py.File(self.path).keys())
+            channels = [ch for ch in channels if "meta" not in ch]
+            raise KeyError(
+                f"Channel {channel} not in dataframe. " f"Choose from {channels}"
+            )
+
+        self.channel = channel
+
+        if not uls and not expected_uls:
+            raise ValueError("Either uls or expected_uls must be True")
+
+        masses = table["mass"]
+        if uls:
+            try:
+                uls = table["ul"]
+            except KeyError:
+                raise KeyError("No upper limits in dataframe. Set uls=False")
+        else:
+            uls = None
+
+        if expected_uls:
+            try:
+                median = table["median_ul"]
+                one_sigma_minus = table["1sigma_minus_ul"]
+                one_sigma_plus = table["1sigma_plus_ul"]
+                two_sigma_minus = table["2sigma_minus_ul"]
+                two_sigma_plus = table["2sigma_plus_ul"]
+            except KeyError:
+                raise KeyError(
+                    "No expected upper limits in dataframe. Set expected_uls=False"
+                )
+        else:
+            median = None
+            one_sigma_minus = None
+            one_sigma_plus = None
+            two_sigma_minus = None
+            two_sigma_plus = None
+
+        with viz.quantity_support():
+            self.plot_channel(
+                masses,
+                uls,
+                median,
+                one_sigma_minus,
+                one_sigma_plus,
+                two_sigma_minus,
+                two_sigma_plus,
+            )
+
+        self.ax.set_xscale("log")
+        self.ax.set_yscale("log")
+
+        cl_type = unique(table[table["channel"] == self.channel], keys="cl_type")[
+            "cl_type"
+        ][0]
+        cl = unique(table[table["channel"] == self.channel], keys="cl")["cl"][0]
+        self.ax.set_xlabel(f"m / {masses.unit:latex}")
+        self.ax.set_ylabel(
+            rf"$CL_{cl_type}^{{{cl}}}$ upper limit on $< \sigma v>$ / {uls.unit:latex}"
+        )
+
+        self.ax.set_title(f"Annihilation Upper Limits for channel {self.channel}")
+
+        self.ax.legend()
+
+    def plot_channel(
+        self,
+        masses,
+        uls,
+        median,
+        one_sigma_minus,
+        one_sigma_plus,
+        two_sigma_minus,
+        two_sigma_plus,
+    ):
+        if uls is not None:
+            self.ax.plot(masses, uls, color="tab:orange", label="Upper Limits")
+        if median is not None:
+            self.ax.plot(
+                masses, median, color="tab:blue", label="Expected Upper Limits"
+            )
+            self.ax.fill_between(
+                masses,
+                median,
+                one_sigma_plus,
+                color="tab:blue",
+                alpha=0.75,
+                label=r"$1\sigma$-region",
+            )
+            self.ax.fill_between(
+                masses, median, one_sigma_minus, color="tab:blue", alpha=0.75
+            )
+            self.ax.fill_between(
+                masses,
+                one_sigma_plus,
+                two_sigma_plus,
+                color="tab:blue",
+                alpha=0.5,
+                label=r"$2\sigma$-region",
+            )
+            self.ax.fill_between(
+                masses, one_sigma_minus, two_sigma_minus, color="tab:blue", alpha=0.5
+            )
+
+
+class ValidationPlotter:
+    def __init__(
+        self,
+        measurement_dataset,
+        path,
+        channel=None,
+        mass=None,
+        statistic="qmu",
+        poi_name="scale",
+        ax=None,
+    ):
+        self.path = path
+        self.ax = ax if ax is not None else plt.gca()
+
+        asimov_dataset = AsimovMapDataset.from_MapDataset(measurement_dataset)
+
+        try:
+            table_diff = QTable.read(
+                self.path, path=f"validation/{statistic}/diff/{channel}/{mass}"
+            )
+            table_same = QTable.read(
+                self.path, path=f"validation/{statistic}/same/{channel}/{mass}"
+            )
+        except OSError:
+            if channel is None:
+                channels = list(
+                    h5py.File(self.path)["validation"][statistic]["diff"].keys()
+                )
+                channels = [ch for ch in channels if "meta" not in ch]
+                raise ValueError(f"Channel must be one of {channels}")
+            if mass is None:
+                masses = list(
+                    h5py.File(self.path)["validation"][statistic]["diff"][
+                        channel
+                    ].keys()
+                )
+                masses = [Quantity(m) for m in masses if "meta" not in m]
+                raise ValueError(f"Mass must be one of {masses}")
+
+        toys_ts_diff = table_diff["ts"]
+        toys_ts_diff_valid = table_diff["valid"]
+        toys_ts_same = table_same["ts"]
+        toys_ts_same_valid = table_same["valid"]
+
+        # apply masks
+        toys_ts_diff = toys_ts_diff[toys_ts_diff_valid]
+        toys_ts_same = toys_ts_same[toys_ts_same_valid]
+
+        max_ts = max(toys_ts_diff.max(), toys_ts_same.max())
+        bins = np.linspace(0, max_ts, 31)
+        linspace = np.linspace(0, max_ts, 1000)
+        statistic = STATISTICS[statistic](asimov_dataset, poi_name)
+        statistic_math_name = (
+            r"q_\mu" if isinstance(statistic, QMuTestStatistic) else r"\tilde{q}_\mu"
+        )
+
+        self.plot(
+            linspace, bins, toys_ts_same, toys_ts_diff, statistic, statistic_math_name
+        )
+
+        self.ax.set_yscale("log")
+        self.ax.set_xlim(0, max_ts)
+
+        self.ax.set_ylabel("pdf")
+        self.ax.set_xlabel(rf"${statistic_math_name}$")
+        self.ax.set_title(statistic.__class__.__name__)
+        self.ax.legend()
+
+    def plot(
+        self, linspace, bins, toys_ts_same, toys_ts_diff, statistic, statistic_math_name
+    ):
+        plt.hist(
+            toys_ts_diff,
+            bins=bins,
+            density=True,
+            histtype="step",
+            color="tab:blue",
+            label=(
+                rf"$f({statistic_math_name}\vert\mu^\prime)$, "
+                r"$\mu=1$, $\mu^\prime=0$"
+            ),
+        )
+        plt.hist(
+            toys_ts_same,
+            bins=bins,
+            density=True,
+            histtype="step",
+            color="tab:orange",
+            label=(
+                rf"$f({statistic_math_name}\vert\mu^\prime)$, "
+                r"$\mu=1$, $\mu^\prime=1$"
+            ),
+        )
+
+        plt.plot(
+            linspace,
+            statistic.asympotic_approximation_pdf(
+                poi_val=1, same=False, poi_true_val=0, ts_val=linspace
+            ),
+            color="tab:blue",
+            label=rf"$f({statistic_math_name}\vert\mu^\prime)$, asympotic",
+        )
+        plt.plot(
+            linspace,
+            statistic.asympotic_approximation_pdf(poi_val=1, ts_val=linspace),
+            color="tab:orange",
+            label=rf"$f({statistic_math_name}\vert\mu^\prime)$, asympotic",
+        )
---
+++ packages/titrate/titrate-0.5.0.tar.gz/titrate-0.5.0/titrate/validation.py
@@ -0,0 +1,192 @@
+from concurrent.futures import ProcessPoolExecutor
+from functools import lru_cache
+
+import h5py
+import numpy as np
+from astropy.table import QTable
+from astropy.units import Quantity
+from gammapy.astro.darkmatter import DarkMatterAnnihilationSpectralModel
+from gammapy.modeling.models import SkyModel
+
+from titrate.datasets import AsimovMapDataset
+from titrate.statistics import QMuTestStatistic, QTildeMuTestStatistic, kstest
+from titrate.utils import calc_ts_toyMC
+
+STATISTICS = {"qmu": QMuTestStatistic, "qtildemu": QTildeMuTestStatistic}
+
+
+class AsymptoticValidator:
+    def __init__(
+        self,
+        measurement_dataset,
+        statistic="qmu",
+        poi_name="scale",
+        path=None,
+        channel=None,
+        mass=None,
+        max_workers=None,
+    ):
+        if statistic not in STATISTICS.keys():
+            raise ValueError(
+                "Statistic must be one of {}".format(list(STATISTICS.keys()))
+            )
+        self.statistic_key = statistic
+        self.statistic = STATISTICS[statistic]
+
+        self.measurement_dataset = measurement_dataset
+        self.asimov_dataset = AsimovMapDataset.from_MapDataset(self.measurement_dataset)
+
+        self.path = path
+        self.channel = channel
+        self.mass = mass
+        if self.channel is None and self.path is not None:
+            channels = list(
+                h5py.File(self.path)["validation"][self.statistic_key]["diff"].keys()
+            )
+            channels = [ch for ch in channels if "meta" not in ch]
+            raise ValueError(f"Channel must be one of {channels}")
+        if self.mass is None and self.path is not None:
+            masses = list(
+                h5py.File(self.path)["validation"][self.statistic_key]["diff"][
+                    self.channel
+                ].keys()
+            )
+            masses = [Quantity(m) for m in masses if "meta" not in m]
+            raise ValueError(f"Mass must be one of {masses}")
+
+        self.poi_name = poi_name
+        self.max_workers = max_workers
+
+        self.toys_ts_diff = None
+        self.toys_ts_same = None
+        self.toys_ts_diff_valid = None
+        self.toys_ts_same_valid = None
+
+    def validate(self, n_toys=1000):
+        self.generate_datasets(n_toys)
+
+        stat = self.statistic(self.asimov_dataset, self.poi_name)
+        ks_diff = kstest(
+            self.toys_ts_diff[self.toys_ts_diff_valid],
+            lambda x: stat.asympotic_approximation_cdf(
+                poi_val=1, same=False, poi_true_val=0, ts_val=x
+            ),
+        )
+        ks_same = kstest(
+            self.toys_ts_same[self.toys_ts_same_valid],
+            lambda x: stat.asympotic_approximation_cdf(poi_val=1, ts_val=x),
+        )
+
+        valid = ks_diff > 0.05 and ks_same > 0.05
+
+        return {"pvalue_diff": ks_diff, "pvalue_same": ks_same, "valid": valid}
+
+    def generate_datasets(self, n_toys):
+        if self.path is None:
+            toys_ts_diff, toys_ts_diff_valid = self.toys_ts(n_toys, 1, 0)
+            toys_ts_same, toys_ts_same_valid = self.toys_ts(n_toys, 1, 1)
+        else:
+            (
+                toys_ts_diff,
+                toys_ts_diff_valid,
+                toys_ts_same,
+                toys_ts_same_valid,
+            ) = self.open_toys()
+
+        self.toys_ts_diff = toys_ts_diff
+        self.toys_ts_same = toys_ts_same
+        self.toys_ts_diff_valid = toys_ts_diff_valid
+        self.toys_ts_same_valid = toys_ts_same_valid
+
+    @lru_cache
+    def toys_ts(self, n_toys, poi_val, poi_true_val):
+        with ProcessPoolExecutor(self.max_workers) as pool:
+            futures = [
+                pool.submit(
+                    calc_ts_toyMC,
+                    self.measurement_dataset,
+                    self.statistic,
+                    poi_val,
+                    poi_true_val,
+                    self.poi_name,
+                )
+                for _ in range(n_toys)
+            ]
+            toys_ts = [future.result()[0] for future in futures]
+            toys_valid = [future.result()[1] for future in futures]
+
+        # to ndarray
+        toys_ts = np.array(toys_ts).ravel()
+        toys_valid = np.array(toys_valid).ravel()
+
+        return toys_ts, toys_valid
+
+    def open_toys(self):
+        toys_diff = QTable.read(
+            self.path,
+            path=f"validation/{self.statistic_key}/diff/{self.channel}/{self.mass}",
+        )
+        toys_same = QTable.read(
+            self.path,
+            path=f"validation/{self.statistic_key}/same/{self.channel}/{self.mass}",
+        )
+
+        toys_ts_diff = toys_diff["ts"]
+        toys_ts_diff_valid = toys_diff["valid"]
+        toys_ts_same = toys_same["ts"]
+        toys_ts_same_valid = toys_same["valid"]
+
+        return toys_ts_diff, toys_ts_diff_valid, toys_ts_same, toys_ts_same_valid
+
+    def save_toys(self, path, overwrite=False, **kwargs):
+        if self.toys_ts_diff is None or self.toys_ts_same is None:
+            raise ValueError("Toys not generated yet. Run validate() first.")
+
+        # collect meta data
+        for model in self.measurement_dataset.models:
+            if isinstance(model, SkyModel):
+                if isinstance(
+                    model.spectral_model, DarkMatterAnnihilationSpectralModel
+                ):
+                    channel = model.spectral_model.channel
+                    mass = model.spectral_model.mass
+        try:
+            channel
+            mass
+        except NameError:
+            raise NameError(
+                "Could not find channel and mass in measurement dataset. "
+                "Please add a DarkMatterAnnihilationSpectralModel to the dataset."
+            )
+
+        # save toys
+        toys_dict_diff = {
+            "ts": self.toys_ts_diff,
+            "valid": self.toys_ts_diff_valid,
+        }
+        toys_dict_same = {
+            "ts": self.toys_ts_same,
+            "valid": self.toys_ts_same_valid,
+        }
+
+        qtable = QTable(toys_dict_diff)
+        qtable.write(
+            path,
+            format="hdf5",
+            path=f"validation/{self.statistic_key}/diff/{channel}/{mass}",
+            overwrite=overwrite,
+            append=True,
+            serialize_meta=True,
+            **kwargs,
+        )
+
+        qtable = QTable(toys_dict_same)
+        qtable.write(
+            path,
+            format="hdf5",
+            path=f"validation/{self.statistic_key}/same/{channel}/{mass}",
+            overwrite=overwrite,
+            append=True,
+            serialize_meta=True,
+            **kwargs,
+        )
