beddb4ccd01e0c8971563cd820489a28ab4597bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:07:23 +0000

    Add package django-structlog/6.0.0.dev1/django_structlog-6.0.0.dev1-py3-none-any.whl

---
+++ packages/django-structlog/django_structlog-6.0.0.dev1-py3-none-any.whl/django_structlog/app_settings.py
@@ -0,0 +1,19 @@
+import logging
+
+from django.conf import settings
+
+
+# noinspection PyPep8Naming
+class AppSettings:
+    PREFIX = "DJANGO_STRUCTLOG_"
+
+    @property
+    def CELERY_ENABLED(self):
+        return getattr(settings, self.PREFIX + "CELERY_ENABLED", False)
+
+    @property
+    def STATUS_4XX_LOG_LEVEL(self):
+        return getattr(settings, self.PREFIX + "STATUS_4XX_LOG_LEVEL", logging.WARNING)
+
+
+app_settings = AppSettings()
