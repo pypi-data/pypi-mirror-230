f067f179e1defbd10d1eea97f785eda6ab4631b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:05:48 +0000

    Add package modelspec/0.3.2/modelspec-0.3.2.tar.gz

---
+++ packages/modelspec/modelspec-0.3.2.tar.gz/modelspec-0.3.2/test_all.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -ex
+
+## Install
+
+pip install .
+
+
+## Test main example
+
+cd examples
+python document.py
+
+cd test
+python test.py
+
+
+## Test NeuroML example
+
+cd ../neuroml2
+python neuroml2_spec.py
+
+# Requires: pip install pyneuroml
+pynml -validate hello_world_neuroml.net.nml
+pynml -validate TestNeuroML.xml
+
+cd ../..
+
+
+## Run pytest
+
+pytest tests -v
+
+## Run OMV tests
+
+omv all -V
+
+
+## Generate the docs
+
+cd docs
+python generate.py
+python contributors.py
+cd sphinx
+make clean
+make html
+cd ..
+
+## Format all file
+
+pre-commit run --all-files
