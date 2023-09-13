9bc9456a51cf8ac1800ca1e9f8aa53e710a9f10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:08:10 +0000

    Add package meta-memcache/0.9.5.dev1/meta_memcache-0.9.5.dev1-py3-none-any.whl

---
+++ packages/meta-memcache/meta_memcache-0.9.5.dev1-py3-none-any.whl/meta_memcache/__init__.py
@@ -0,0 +1,28 @@
+__version__ = "0.1.0"
+
+from meta_memcache.base.cache_pool import CachePool
+from meta_memcache.cache_pools import ShardedCachePool, ShardedWithGutterCachePool
+from meta_memcache.configuration import (
+    LeasePolicy,
+    RecachePolicy,
+    ServerAddress,
+    StalePolicy,
+    connection_pool_factory_builder,
+    socket_factory_builder,
+)
+from meta_memcache.errors import MemcacheError, MemcacheServerError
+from meta_memcache.events.write_failure_event import WriteFailureEvent
+from meta_memcache.protocol import (
+    Conflict,
+    Flag,
+    IntFlag,
+    Key,
+    Miss,
+    NotStored,
+    ServerVersion,
+    SetMode,
+    Success,
+    TokenFlag,
+    Value,
+)
+from meta_memcache.serializer import BaseSerializer, MixedSerializer
