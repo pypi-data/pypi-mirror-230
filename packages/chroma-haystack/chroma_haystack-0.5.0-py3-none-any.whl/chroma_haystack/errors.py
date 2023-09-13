b33d52dc513c5fbe9ff68633ab4a1481faa8158a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:08:28 +0000

    Add package chroma-haystack/0.5.0/chroma_haystack-0.5.0-py3-none-any.whl

---
+++ packages/chroma-haystack/chroma_haystack-0.5.0-py3-none-any.whl/chroma_haystack/errors.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2023-present John Doe <jd@example.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+from haystack.preview.document_stores.errors import DocumentStoreError, FilterError
+
+
+class ChromaDocumentStoreError(DocumentStoreError):
+    pass
+
+
+class ChromaDocumentStoreFilterError(FilterError):
+    pass
+
+
+class ChromaDocumentStoreConfigError(ChromaDocumentStoreError):
+    pass
