381d8695788ff91579fed2efdaba0634c76e28ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:05:09 +0000

    Add package py-map-gen/0.11/py-map-gen-0.11.tar.gz

---
+++ packages/py-map-gen/py-map-gen-0.11.tar.gz/py-map-gen-0.11/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='py-map-gen',
+    version='0.11',
+    author='Ford2003',
+    author_email='tommyford28@gmail.com',
+    description='A package for generating data for a world with a seed.',
+    long_description='',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=["setuptools", "numpy", "matplotlib"]
+)
