697aefe223a59c2c4e2adaad592743a3599ea6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:10:05 +0000

    Add package coloraide/2.10/coloraide-2.10.tar.gz

---
+++ packages/coloraide/coloraide-2.10.tar.gz/coloraide-2.10/coloraide/interpolate/monotone.py
@@ -0,0 +1,60 @@
+"""Monotone interpolation based on a Hermite interpolation spline."""
+from __future__ import annotations
+from .bspline import InterpolatorBSpline
+from ..interpolate import Interpolator, Interpolate
+from .. import algebra as alg
+from ..types import Vector
+from typing import Callable, Mapping, Sequence, Any, TYPE_CHECKING
+
+if TYPE_CHECKING:  # pragma: no cover
+    from ..color import Color
+
+
+class InterpolatorMonotone(InterpolatorBSpline):
+    """Interpolate with monotone spline based on Hermite."""
+
+    def setup(self) -> None:
+        """Setup."""
+
+        super().setup()
+        self.spline = alg.monotone
+
+
+class Monotone(Interpolate):
+    """Monotone interpolation plugin."""
+
+    NAME = "monotone"
+
+    def interpolator(
+        self,
+        coordinates: list[Vector],
+        channel_names: Sequence[str],
+        create: type[Color],
+        easings: list[Callable[..., float] | None],
+        stops: dict[int, float],
+        space: str,
+        out_space: str,
+        progress: Mapping[str, Callable[..., float]] | Callable[..., float] | None,
+        premultiplied: bool,
+        extrapolate: bool = False,
+        domain: list[float] | None = None,
+        padding: float | tuple[float, float] | None = None,
+        **kwargs: Any
+    ) -> Interpolator:
+        """Return the monotone interpolator."""
+
+        return InterpolatorMonotone(
+            coordinates,
+            channel_names,
+            create,
+            easings,
+            stops,
+            space,
+            out_space,
+            progress,
+            premultiplied,
+            extrapolate,
+            domain,
+            padding,
+            **kwargs
+        )
