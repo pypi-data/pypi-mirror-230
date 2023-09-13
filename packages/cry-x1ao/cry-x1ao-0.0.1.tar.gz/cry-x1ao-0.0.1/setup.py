4949e4cd23f84aa7b688e0080041f0a0e2ee9fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:08:39 +0000

    Add package cry-x1ao/0.0.1/cry-x1ao-0.0.1.tar.gz

---
+++ packages/cry-x1ao/cry-x1ao-0.0.1.tar.gz/cry-x1ao-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="cry-x1ao",
+    version="0.0.1",
+    author='x1ao',
+    author_email='810470436@qq.com',
+    long_description="text/markdown",
+    url='https://github.com',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
