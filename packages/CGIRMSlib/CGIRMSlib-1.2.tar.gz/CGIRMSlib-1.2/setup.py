3ef40b564438a39c5186bd6239767a6322a3efc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 04:05:39 +0000

    Add package CGIRMSlib/1.2/CGIRMSlib-1.2.tar.gz

---
+++ packages/CGIRMSlib/CGIRMSlib-1.2.tar.gz/CGIRMSlib-1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="CGIRMSlib",
+    version="1.2",
+    description="Derivada para uso em conversões de python para o RMS",
+    author="CGI",
+    author_email="andre.ershov@cgi.com",
+    packages=["CGIRMSlib"],
+    install_requires=[
+        'pandas',
+        'datetime',
+        'json',
+        'numpy'
+    ],
+)
+
