8c5b246d9e3bdc29d16f614f4cc06f7589c2e964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 04:06:20 +0000

    Add package brandpy/0.0.6/brandpy-0.0.6-py3-none-any.whl

---
+++ packages/brandpy/brandpy-0.0.6-py3-none-any.whl/brandpy/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present deamonpog <pog666@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .bw_auth_config import BWAuthConfig
+from .bw_api_caller import BWAPICaller
+from .bw_api_helper import BWAPIHelper
