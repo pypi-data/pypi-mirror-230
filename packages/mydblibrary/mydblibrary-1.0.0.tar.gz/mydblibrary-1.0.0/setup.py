09f1c43281a7c01a5602eadae486531a3f8880f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:07:59 +0000

    Add package mydblibrary/1.0.0/mydblibrary-1.0.0.tar.gz

---
+++ packages/mydblibrary/mydblibrary-1.0.0.tar.gz/mydblibrary-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='mydblibrary',
+    version='1.0.0',
+    packages=['dbautomate'],
+    install_requires=['psycopg2'],  # Add any other dependencies here
+)
