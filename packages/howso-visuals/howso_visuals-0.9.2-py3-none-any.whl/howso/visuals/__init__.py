d5a923040de5bcf914a0d3e37f833aabb939c3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 04:06:13 +0000

    Add package howso-visuals/0.9.2/howso_visuals-0.9.2-py3-none-any.whl

---
+++ packages/howso-visuals/howso_visuals-0.9.2-py3-none-any.whl/howso/visuals/__init__.py
@@ -0,0 +1,17 @@
+from .visuals import (
+    plot_anomalies,
+    plot_dataset,
+    plot_drift,
+    plot_feature_importances,
+    plot_interpretable_prediction,
+    plot_kl_divergence,
+)
+
+__all__ = [
+    "plot_anomalies",
+    "plot_dataset",
+    "plot_drift",
+    "plot_feature_importances",
+    "plot_interpretable_prediction",
+    "plot_kl_divergence",
+]
