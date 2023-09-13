56b0f2ae7731b8e24eba0782f0b8108fcbfa6cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:08:02 +0000

    Add package fundrive/0.9.13/fundrive-0.9.13-py3-none-any.whl

---
+++ packages/fundrive/fundrive-0.9.13-py3-none-any.whl/fundrive/base/core.py
@@ -0,0 +1,19 @@
+import os
+
+import requests
+from tqdm import tqdm
+
+
+def download_by_request(url, file_path, size, overwrite=False) -> bool:
+    if not os.path.exists(os.path.dirname(file_path)):
+        os.makedirs(os.path.dirname(file_path))
+    if not overwrite and os.path.exists(file_path) and os.path.getsize(file_path) == size:
+        return False
+    with requests.Session() as sess:
+        resp = sess.get(url, stream=True)
+
+        with open(file_path, "wb") as file:
+            with tqdm(total=size, ncols=120, desc=file_path, unit="B", unit_scale=True, unit_divisor=1024) as bar:
+                for data in resp.iter_content(chunk_size=2048):
+                    bar.update(file.write(data))
+                return True
