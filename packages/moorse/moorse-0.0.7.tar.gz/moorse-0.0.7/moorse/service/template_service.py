0177cf2129a03c829463f7eb35aad00aedb3e96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:07:21 +0000

    Add package moorse/0.0.7/moorse-0.0.7.tar.gz

---
+++ packages/moorse/moorse-0.0.7.tar.gz/moorse-0.0.7/moorse/service/template_service.py
@@ -0,0 +1,24 @@
+from moorse.service.auth_service import AuthService
+from moorse.clients.template_client import TemplateClient
+from moorse.dto.template.request.template_request import TemplateRequest
+from moorse.dto.template.template_dto import TemplateDto
+from moorse.dto.template.response.multiple.template_list import TemplateList
+
+class TemplateService:
+
+    __auth: AuthService
+
+    def __init__(self, auth: AuthService):
+        self.__auth = auth
+
+    def create(self, webhook: TemplateRequest) -> TemplateDto:
+        return TemplateClient().create(self.__auth.get_token(), webhook)
+
+    def get_all(self) -> TemplateList:
+        return TemplateClient().get_all(self.__auth.get_token())
+
+    def get_one(self, webhook_id: str) -> TemplateDto:
+        return TemplateClient().get_one(self.__auth.get_token(), webhook_id)
+    
+    def delete(self, webhook_id: str) -> TemplateDto:
+        return TemplateClient().delete(self.__auth.get_token(), webhook_id)
