2731d2d86ffc104e2fefb118d1282712120b0850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:08:33 +0000

    Add package brainpylib-cu12x/0.1.10.post1/brainpylib_cu12x-0.1.10.post1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/brainpylib-cu12x/brainpylib_cu12x-0.1.10.post1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/brainpylib/event_ops.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from ._src.event_ops.event_csr_matvec import (event_csr_matvec as event_csr_matvec)
+from ._src.event_ops.event_info_collection import (event_info as event_info)
