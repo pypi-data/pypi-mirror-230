8123dcf53a527d84df0f8b4376642c03d87e501f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:06:57 +0000

    Add package kolibri-explore-plugin/6.45.0/kolibri_explore_plugin-6.45.0.tar.gz

---
+++ packages/kolibri-explore-plugin/kolibri_explore_plugin-6.45.0.tar.gz/kolibri_explore_plugin-6.45.0/packages/welcome-screen/src/views/LoadingInitial.vue
@@ -0,0 +1,34 @@
+<template>
+  <WelcomeBase title="Welcome to Endless Key!">
+    <template #header-top>
+      <div class="w-100">
+        <img :src="loadingImage" class="mb-1" width="128">
+      </div>
+    </template>
+    <template #footer>
+      <h5 class="small text-muted text-uppercase">
+        Powered by Kolibri
+      </h5>
+      <img :src="kolibriImage" width="64">
+    </template>
+  </WelcomeBase>
+</template>
+
+<script>
+  import LoadingImage from 'ek-components/src/assets/loading-animation.gif';
+  import kolibriImage from '../assets/kolibri-logo.svg';
+  import WelcomeBase from './WelcomeBase.vue';
+
+  export default {
+    name: 'LoadingInitial',
+    components: {
+      WelcomeBase,
+    },
+    data() {
+      return {
+        loadingImage: LoadingImage,
+        kolibriImage: kolibriImage,
+      };
+    },
+  };
+</script>
