a04893e03a467806f98e661b00e699c719192db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:05:36 +0000

    Add package tweakwcs/0.8.3/tweakwcs-0.8.3.tar.gz

---
+++ packages/tweakwcs/tweakwcs-0.8.3.tar.gz/tweakwcs-0.8.3/tweakwcs/__init__.py
@@ -0,0 +1,25 @@
+"""
+This package provides support for image alignment.
+
+"""
+__docformat__ = 'restructuredtext'
+
+__taskname__ = 'tweakwcs'
+__author__ = 'Mihai Cara'
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = 'UNKNOWN'
+
+from .correctors import (WCSCorrector, JWSTWCSCorrector,  # noqa: F401
+                         FITSWCSCorrector,  # noqa: F401
+                         TPWCS, JWSTgWCS, FITSWCS)  # noqa: F401
+from .matchutils import MatchCatalogs, XYXYMatch, TPMatch  # noqa: F401
+from .imalign import fit_wcs, align_wcs  # noqa: F401
+from .wcsimage import (convex_hull, RefCatalog, WCSImageCatalog,  # noqa: F401
+                       WCSGroupCatalog)  # noqa: F401
+from .linalg import inv  # noqa: F401
+from .linearfit import iter_linear_fit, build_fit_matrix  # noqa: F401
