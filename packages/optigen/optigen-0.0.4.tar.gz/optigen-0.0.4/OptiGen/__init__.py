747db7a477f0bd0dc1eab38891b0f12d7e908532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:04:58 +0000

    Add package optigen/0.0.4/optigen-0.0.4.tar.gz

---
+++ packages/optigen/optigen-0.0.4.tar.gz/optigen-0.0.4/OptiGen/__init__.py
@@ -0,0 +1,5 @@
+from OptiGen.main import calculate_fitness 
+from OptiGen.main import Phenotype
+from OptiGen.main import next_generation
+from OptiGen.main import get_mutation_rate_round_value
+from OptiGen.main import start
---
+++ packages/optigen/optigen-0.0.4.tar.gz/optigen-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Genetic Algorithmen Framework'
+
+# Setting up
+setup(
+    name="optigen",
+    version=VERSION,
+    author="ShadowFlameFox",
+    author_email="<shadow_flame_fox@web.de>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'genetic', 'natural selection', 'algorithmen'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
