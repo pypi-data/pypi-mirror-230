067ef00ef5e3e93e17b3f25cc0a76c98e5abf486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:07:56 +0000

    Add package bloqade/0.3.4/bloqade-0.3.4-py3-none-any.whl

---
+++ packages/bloqade/bloqade-0.3.4-py3-none-any.whl/bloqade/task/braket_simulator.py
@@ -0,0 +1,43 @@
+from bloqade.builder.base import ParamType
+from .base import LocalTask
+from bloqade.submission.ir.task_results import QuEraTaskResults
+from bloqade.submission.ir.braket import (
+    from_braket_task_results,
+    BraketTaskSpecification,
+)
+from bloqade.task.base import Geometry
+from braket.devices import LocalSimulator
+from typing import Dict, Optional
+from dataclasses import dataclass
+
+## keep the old conversion for now,
+## we will remove conversion btwn QuEraTask <-> BraketTask,
+## and specialize/dispatching here.
+
+
+@dataclass
+class BraketEmulatorTask(LocalTask):
+    task_ir: BraketTaskSpecification
+    metadata: Dict[str, ParamType]
+    task_result_ir: Optional[QuEraTaskResults] = None
+
+    def _geometry(self) -> Geometry:
+        return Geometry(
+            sites=self.task_ir.program.setup.ahs_register.sites,
+            filling=self.task_ir.program.setup.ahs_register.filling,
+        )
+
+    def run(self, **kwargs) -> "BraketEmulatorTask":
+        aws_task = LocalSimulator("braket_ahs").run(
+            self.task_ir.program,
+            shots=self.task_ir.nshots,
+            **kwargs,
+        )
+        self.task_result_ir = from_braket_task_results(aws_task.result())
+        return self
+
+    def result(self):
+        if self.task_result_ir is None:
+            raise ValueError("Braket simulator job haven't submit yet.")
+
+        return self.task_result_ir
