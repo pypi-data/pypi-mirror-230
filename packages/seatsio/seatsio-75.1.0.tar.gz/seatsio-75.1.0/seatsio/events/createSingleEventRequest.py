d727ebcbfbb8e5221befcbccc6ca3d5c080199bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:06:46 +0000

    Add package seatsio/75.1.0/seatsio-75.1.0.tar.gz

---
+++ packages/seatsio/seatsio-75.1.0.tar.gz/seatsio-75.1.0/seatsio/events/createSingleEventRequest.py
@@ -0,0 +1,20 @@
+class CreateSingleEventRequest:
+    def __init__(self, chart_key, event_key=None, name=None, date=None, table_booking_config=None,
+                 object_categories=None, categories=None, channels=None):
+        if chart_key:
+            self.chartKey = chart_key
+        if event_key:
+            self.eventKey = event_key
+        if name:
+            self.name = name
+        if date:
+            self.date = date
+        if table_booking_config is not None:
+            self.tableBookingConfig = table_booking_config.to_json()
+        if object_categories is not None:
+            self.objectCategories = object_categories
+        if categories is not None:
+            self.categories = categories
+        if channels is not None:
+            self.channels = channels
+
---
+++ packages/seatsio/seatsio-75.1.0.tar.gz/seatsio-75.1.0/seatsio/events/eventProperties.py
@@ -0,0 +1,16 @@
+class EventProperties:
+    def __init__(self, event_key=None, name=None, date=None, table_booking_config=None, object_categories=None, categories=None, channels=None):
+        if event_key:
+            self.eventKey = event_key
+        if name:
+            self.name = name
+        if date:
+            self.date = date
+        if table_booking_config is not None:
+            self.tableBookingConfig = table_booking_config
+        if object_categories is not None:
+            self.objectCategories = object_categories
+        if categories is not None:
+            self.categories = categories
+        if channels is not None:
+            self.channels = channels
