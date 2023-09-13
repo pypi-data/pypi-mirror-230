7f00e4d183a809f761860f589788cd1327cc6e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:08:48 +0000

    Add package ctyun/0.0.8/ctyun-0.0.8.tar.gz

---
+++ packages/ctyun/ctyun-0.0.8.tar.gz/ctyun-0.0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='ctyun',
+    version='0.0.8',
+    author='Xizhen Du',
+    author_email='xizhendu@gmail.com',
+    url='https://github.com/xizhendu/ctyun',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    description='Python SDK for China Telecom Cloud(https://www.ctyun.cn)',
+    packages=['ctyun'],
+    package_data = {
+        'ctyun': ['config/api.yml']
+    },
+    include_package_data=True,
+    install_requires=[
+        "PyYAML",
+        "requests",
+        "datetime"
+    ]
+)
