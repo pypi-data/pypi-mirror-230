c14c4b0a159d331ee5d412ba3f41093b87d48bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:12:26 +0000

    Add package llmstudio/0.12/llmstudio-0.12-py3-none-any.whl

---
+++ packages/llmstudio/llmstudio-0.12-py3-none-any.whl/models/bedrock.py
@@ -0,0 +1,103 @@
+import os
+import requests
+
+from .models import LLMModel, LLMVendorClient
+
+
+class BedrockClient(LLMVendorClient):
+    MODEL_MAPPING = {
+        "amazon.titan-tg1-large": "Titan",
+        "anthropic.claude-v1": "Claude",
+        "anthropic.claude-instant-v1": "Claude",
+        "anthropic.claude-v2": "Claude",
+    }
+
+    def get_model(self, model_name: str):
+        model_class_name = self.MODEL_MAPPING.get(model_name)
+        if not model_class_name:
+            raise ValueError(f"Unknown model: {model_name}")
+
+        model_class = getattr(self, model_class_name)
+        return model_class(
+            model_name=model_name,
+            api_key=self.api_key,
+            api_secret=self.api_secret,
+            api_region=self.api_region,
+        )
+
+    class BedrockModel(LLMModel):
+        CHAT_URL = "http://localhost:8000/api/chat/bedrock"
+        TEST_URL = "http://localhost:8000/api/test/bedrock"
+
+        def __init__(
+            self, model_name: str, api_key: str, api_secret: str, api_region: str
+        ):
+            self.model_name = model_name
+            self.api_key = (
+                api_key
+                or os.environ.get("LS_BEDROCK_KEY")
+                or self._raise_api_error("api_key", "LS_BEDROCK_KEY")
+            )
+            self.api_secret = (
+                api_secret
+                or os.environ.get("LS_BEDROCK_SECRET")
+                or self._raise_api_error("api_secret", "LS_BEDROCK_SECRET")
+            )
+            self.api_region = (
+                api_secret
+                or os.environ.get("LS_BEDROCK_REGION")
+                or self._raise_api_error("api_region", "LS_BEDROCK_REGION")
+            )
+            self._check_api_access()
+
+        def _check_api_access(self):
+            response = requests.post(
+                self.TEST_URL,
+                json={
+                    "model_name": self.model_name,
+                    "api_key": self.api_key,
+                },
+                headers={"Content-Type": "application/json"},
+            )
+            if not response.json():
+                raise ValueError(
+                    f"The API key doesn't have access to {self.model_name}"
+                )
+
+        def chat(self, chat_input: str, parameters: dict = {}):
+            response = requests.post(
+                self.CHAT_URL,
+                json={
+                    "model_name": self.model_name,
+                    "api_key": self.api_key,
+                    "api_secret": self.api_secret,
+                    "api_region": self.api_region,
+                    "chat_input": chat_input,
+                    "parameters": parameters,
+                },
+                headers={"Content-Type": "application/json"},
+            )
+
+            return response.json()
+
+    class Claude(BedrockModel):
+        def __init__(
+            self, model_name: str, api_key: str, api_secret: str, api_region: str
+        ):
+            super().__init__(
+                model_name=model_name,
+                api_key=api_key,
+                api_secret=api_secret,
+                api_region=api_region,
+            )
+
+    class Titan(BedrockModel):
+        def __init__(
+            self, model_name: str, api_key: str, api_secret: str, api_region: str
+        ):
+            super().__init__(
+                model_name=model_name,
+                api_key=api_key,
+                api_secret=api_secret,
+                api_region=api_region,
+            )
