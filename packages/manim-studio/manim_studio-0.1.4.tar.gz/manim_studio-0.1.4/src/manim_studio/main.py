3869f4bd1347a703551b14e5ee445a022c522e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:04:59 +0000

    Add package manim-studio/0.1.4/manim_studio-0.1.4.tar.gz

---
+++ packages/manim-studio/manim_studio-0.1.4.tar.gz/manim_studio-0.1.4/src/manim_studio/main.py
@@ -0,0 +1,22 @@
+from PyQt6.QtWidgets import QApplication
+import sys
+
+from .editor_widget import EditorWidget
+from .preview_widget import PreviewWidget
+from .live_scene import LiveScene
+from .communicate import Communicate
+
+
+def main(scene_type: type[LiveScene]):
+    app = QApplication(sys.argv)
+    communicate = Communicate()
+    scene = scene_type(communicate)
+    editor_window = EditorWidget(communicate, scene)
+    preview_window = PreviewWidget(communicate, scene)
+    preview_window.showMaximized()
+    editor_window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main(LiveScene)
