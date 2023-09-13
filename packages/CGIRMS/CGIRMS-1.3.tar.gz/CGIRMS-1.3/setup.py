436e11311be317dba0b4a4b6b556dd70dee7746d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 04:07:17 +0000

    Add package CGIRMS/1.3/CGIRMS-1.3.tar.gz

---
+++ packages/CGIRMS/CGIRMS-1.3.tar.gz/CGIRMS-1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="CGIRMS",
+    version="1.3",
+    description="Derivada para uso em conversões de python para o RMS",
+    author="CGI",
+    author_email="andre.ershov@cgi.com",
+    packages=["CGIRMS"],
+    install_requires=[
+        'pandas',
+        'datetime',
+        'json',
+        'numpy'
+    ],
+)
+
