1a5e58f08d8f31db06c8c9569d8ee57e243e90b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:08:48 +0000

    Add package hypertab/0.6.2/hypertab-0.6.2.tar.gz

---
+++ packages/hypertab/hypertab-0.6.2.tar.gz/hypertab-0.6.2/hypertab/__init__.py
@@ -0,0 +1,5 @@
+from . import modules
+from .hypernetwork import Hypernetwork, HypernetworkPCA, HypernetworkEmbeddings, TrainingModes, HypernetworkWithFeatureSelector
+from . import training_utils
+from . import mask_design
+from .hypertab import HyperTabClassifier
---
+++ packages/hypertab/hypertab-0.6.2.tar.gz/hypertab-0.6.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hypertab",
+    author="Witold Wydmański",
+    author_email="wwydmanski@gmail.com",
+    version="0.6.2",
+    description="HyperTab: hypernetwork for small tabular datasets",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="MIT",
+    packages=find_packages(include=['hypertab', 'hypertab.*']),
+    install_requires=required,
+    url="https://github.com/wwydmanski/hypertab"
+)
