23bdc42805f08a445f56cdbd02ef87a072b6c0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:12:28 +0000

    Add package ncswash-fedex/2.4.1/ncswash-fedex-2.4.1.tar.gz

---
+++ packages/ncswash-fedex/ncswash-fedex-2.4.1.tar.gz/ncswash-fedex-2.4.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import fedex
+
+LONG_DESCRIPTION = open('README.rst').read()
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+KEYWORDS = 'fedex soap suds wrapper rate track avs location ship pickup country availability commitment package service'
+
+setup(name='ncswash-fedex',
+      version=fedex.VERSION,
+      description='Fedex Web Services API wrapper.',
+      long_description=LONG_DESCRIPTION,
+      author='Greg Taylor',
+      author_email='gtaylor@gc-taylor.com',
+      maintainer='Nick Joyce',
+      maintainer_email='nick@stratuscode.com',
+      url='https://github.com/python-fedex-devs/python-fedex',
+      download_url='http://pypi.python.org/pypi/fedex/',
+      packages=['fedex', 'fedex.services', 'fedex.printers'],
+      package_dir={'fedex': 'fedex'},
+      package_data={'fedex': ['wsdl/*.wsdl', 'wsdl/test_server_wsdl/*.wsdl', 'tools/*']},
+      platforms=['Platform Independent'],
+      license='BSD',
+      classifiers=CLASSIFIERS,
+      keywords=KEYWORDS,
+      requires=['suds'],
+      install_requires=['suds-community'],
+      )
