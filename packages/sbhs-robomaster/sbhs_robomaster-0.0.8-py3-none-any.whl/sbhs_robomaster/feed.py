127e7d679b75528daa32458b944b84b7e49bbeb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:07:17 +0000

    Add package sbhs-robomaster/0.0.8/sbhs_robomaster-0.0.8-py3-none-any.whl

---
+++ packages/sbhs-robomaster/sbhs_robomaster-0.0.8-py3-none-any.whl/sbhs_robomaster/feed.py
@@ -0,0 +1,32 @@
+from typing import Generic, TypeVar
+import asyncio
+
+
+FeedT = TypeVar("FeedT")
+class Feed(Generic[FeedT]):
+    queued_data: list[FeedT] = []
+    data_lock: asyncio.Lock = asyncio.Lock()
+    recv_data_flag: asyncio.Event = asyncio.Event()
+
+    async def feed(self, data: FeedT):
+        await self.data_lock.acquire()
+
+        self.queued_data.append(data)
+        self.recv_data_flag.set()
+
+        self.data_lock.release()
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        if len(self.queued_data) > 0:
+            result = self.queued_data.pop(0)
+
+            if len(self.queued_data) == 0:
+                self.recv_data_flag.clear()
+
+            return result
+        else:
+            await self.recv_data_flag.wait()
+            return await self.__anext__()
