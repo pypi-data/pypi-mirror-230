f2547c77b6df8ab6de705990b642d127ed04b3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:07:15 +0000

    Add package trolleyhq/1.0.0/trolleyhq-1.0.0.tar.gz

---
+++ packages/trolleyhq/trolleyhq-1.0.0.tar.gz/trolleyhq-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup (
+    name='trolleyhq',
+    version='1.0.0',
+    packages=["trolley", "trolley.exceptions"],
+    package_data={"trolley": ["ssl/*"]},
+    install_requires=['requests>=2.31.0'],
+    author='Trolley',
+    author_email='developer-tools@trolley.com',
+    summary='Trolley Python SDK',
+    url='https://www.trolley.com',
+    license='MIT',
+    long_description='A native Python SDK for the Trolley API',
+)
