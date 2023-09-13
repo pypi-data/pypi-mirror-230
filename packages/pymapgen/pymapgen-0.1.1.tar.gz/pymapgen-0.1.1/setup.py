7ad5224cff533734b70e83a799814d98b8d06e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:06:18 +0000

    Add package pymapgen/0.1.1/pymapgen-0.1.1.tar.gz

---
+++ packages/pymapgen/pymapgen-0.1.1.tar.gz/pymapgen-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pymapgen',
+    version='0.1.1',
+    author='Ford2003',
+    author_email='tommyford28@gmail.com',
+    description='A package for generating data for a world with a seed.',
+    long_description='',
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=["numpy", "matplotlib"]
+)
