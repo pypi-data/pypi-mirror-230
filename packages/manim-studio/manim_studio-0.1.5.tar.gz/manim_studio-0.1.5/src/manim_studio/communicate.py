1cb508ba3ee3999d05eb1aefb18a18e44d4a6499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:07:19 +0000

    Add package manim-studio/0.1.5/manim_studio-0.1.5.tar.gz

---
+++ packages/manim-studio/manim_studio-0.1.5.tar.gz/manim_studio-0.1.5/src/manim_studio/communicate.py
@@ -0,0 +1,12 @@
+from PyQt6.QtCore import pyqtSignal, QObject
+import numpy as np
+
+
+class Communicate(QObject):
+    update_scene = pyqtSignal(str)
+    update_image = pyqtSignal(np.ndarray)
+    end_scene = pyqtSignal()
+    alert = pyqtSignal(Exception)
+    save_snippet = pyqtSignal(str)
+    next_slide = pyqtSignal()
+    add_slider_to_editor = pyqtSignal(str)
