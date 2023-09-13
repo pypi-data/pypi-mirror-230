c53fe72c169d3d7228c0fff157191406735ca8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 04:05:17 +0000

    Add package cry-x1ao/0.0.10/cry-x1ao-0.0.10.tar.gz

---
+++ packages/cry-x1ao/cry-x1ao-0.0.10.tar.gz/cry-x1ao-0.0.10/x1ao/rsa.py
@@ -0,0 +1,18 @@
+from gmpy2 import *
+from Crypto.Util.number import *
+
+def know_pqec(factors=list,e=int,c=int):
+    n=1
+    phi=1
+    factor_l=[]
+    for _ in range(len(factors)):
+        n*=factors[_]
+    for _ in factors:
+        if _ not in factor_l:
+            factor_l.append(_)
+    for factor in factor_l:
+        phi=phi*(factor-1)*pow(factor,factors.count(factor)-1)
+    d=int(invert(e,phi))
+    m=pow(c,d,n)
+    print(long_to_bytes(int(m)))
+    
