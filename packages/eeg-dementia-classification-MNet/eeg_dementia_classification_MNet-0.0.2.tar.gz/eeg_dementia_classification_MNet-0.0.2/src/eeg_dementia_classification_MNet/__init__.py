9e83bd0e41a7869415318cf6a1cae65af78ce11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:07:36 +0000

    Add package eeg-dementia-classification-MNet/0.0.2/eeg_dementia_classification_MNet-0.0.2.tar.gz

---
+++ packages/eeg-dementia-classification-MNet/eeg_dementia_classification_MNet-0.0.2.tar.gz/eeg_dementia_classification_MNet-0.0.2/src/eeg_dementia_classification_MNet/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: "2023-09-12 14:53:51 (ywatanabe)"
+
+from .MNet_1000 import MNet_1000
+from . import utils
+
+__copyright__ = "Copyright (C) 2023 Yusuke Watanabe"
+__version__ = "0.0.2"
+__license__ = "GPL3.0"
+__author__ = "ywatanabe1989"
+__author_email__ = "ywata1989@gmail.com"
+__url__ = "https://github.com/yanagisawa-lab/eeg-dementia-classification"
