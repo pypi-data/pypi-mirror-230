0546c083d41aa67c9573245d7c74d671ed903fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:07:35 +0000

    Add package estatbr/0.1.0/estatbr-0.1.0.tar.gz

---
+++ packages/estatbr/estatbr-0.1.0.tar.gz/estatbr-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='estatbr',
+    version='0.1.0',
+    description="Uma biblioteca para Mudar sua experiencia em estatistica no python",
+    author='Samuel Bidjory',
+    author_email='bidjorys@gmail.com',
+    url='https://github.com/Anybosoft',
+    packages=find_packages(),  # Isso incluirá automaticamente todos os pacotes do seu projeto
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "sympy",
+        "mpmath",
+        # Lista de dependências, por exemplo: 'numpy>=1.0'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
