5d6b0c466630bcf3ae6fe89c5fb424bcf1333cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:05:51 +0000

    Add package snapatac2/2.4.0/snapatac2-2.4.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/snapatac2/snapatac2-2.4.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/snapatac2/tools/_call_peaks.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from pathlib import Path
+from snapatac2._snapatac2 import AnnData, AnnDataSet
+import snapatac2._snapatac2 as _snapatac2
+
+def call_peaks(
+    adata: AnnData | AnnDataSet,
+    groupby: str | list[str],
+    selections: set[str] | None = None,
+    q_value: float = 0.05,
+    nolambda: bool = True,
+    shift: int = -100,
+    extsize: int = 200,
+    blacklist: Path | None = None,
+    out_dir: Path | None = None,
+    key_added: str = 'peaks',
+    inplace: bool = True,
+) -> 'polars.DataFrame' | None:
+    """
+    Call peaks using MACS2.
+
+    Use the `callpeak` command in MACS2 to identify regions enriched with TN5
+    insertions. The default parameters passed to MACS2 are:
+    "-shift -100 -extsize 200 -nomodel -callsummits -nolambda -keep-dup all"
+
+    Parameters
+    ----------
+    adata
+        The (annotated) data matrix of shape `n_obs` x `n_vars`.
+        Rows correspond to cells and columns to regions.
+    groupby
+        Group the cells before peak calling. If a `str`, groups are obtained from
+        `.obs[groupby]`.
+    selections
+        Call peaks for the selected groups only.
+    q_value
+        q_value cutoff used in MACS2.
+    nolambda
+        Whether to use the `--nolambda` option in MACS2.
+    shift
+        The shift size in MACS2.
+    extsize
+        The extension size in MACS2.
+    blacklist
+        Path to the blacklist file in BED format. If provided, regions in the blacklist will be
+        removed.
+    out_dir
+        If provided, raw peak files from each group will be saved in the directory.
+        Otherwise, they will be stored in a temporary directory which will be removed
+        afterwards.
+    key_added
+        `.uns` key under which to add the peak information.
+    inplace
+        Whether to store the result inplace.
+
+    Returns
+    -------
+    'polars.DataFrame' | None
+        If `inplace=True` it stores the result in `adata.uns[`key_added`]`.
+        Otherwise, it returns the result as a dataframe.
+    """
+    if isinstance(groupby, str):
+        groupby = list(adata.obs[groupby])
+    out_dir = out_dir if out_dir is None else str(out_dir)
+    res = _snapatac2.call_peaks(adata, groupby, q_value, nolambda,
+        shift, extsize, selections, blacklist, out_dir)
+    if inplace:
+        if adata.isbacked:
+            adata.uns[key_added] = res
+        else:
+            adata.uns[key_added] = res.to_pandas()
+    else:
+        return res
