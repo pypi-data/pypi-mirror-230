38d2ee40ce545849ba10d9052e6c0e32581f7e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:07:31 +0000

    Add package alacorder/81.2.0/alacorder-81.2.0-py3-none-any.whl

---
+++ packages/alacorder/alacorder-81.2.0-py3-none-any.whl/alacorder/__init__.py
@@ -0,0 +1,15 @@
+"""
+┏┓┓ ┏┓┏┓┏┓┳┓┳┓┏┓┳┓
+┣┫┃ ┣┫┃ ┃┃┣┫┃┃┣ ┣┫
+┛┗┗┛┛┗┗┛┗┛┛┗┻┛┗┛┛┗
+(c) 2023 Sam Robson
+
+Alacorder collects case detail PDFs from Alacourt.com and processes them into
+data tables suitable for research purposes.
+
+Dependencies: Python 3.9+, Google Chrome, brotli 1.0.9+, polars 0.18.1+,
+pymupdf 1.21.1+, rich 13.3.3+, selenium 4.8.3+, typer 0.9.0+,
+xlsx2csv 0.8.1+, xlsxwriter 3.0.9+
+"""
+
+from .alac import *
