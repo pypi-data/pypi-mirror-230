06af2df9d058892f5c048f7ba73084392307da6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:05:25 +0000

    Add package invenio-vocabularies/1.6.0/invenio-vocabularies-1.6.0.tar.gz

---
+++ packages/invenio-vocabularies/invenio-vocabularies-1.6.0.tar.gz/invenio-vocabularies-1.6.0/invenio_vocabularies/contrib/affiliations/affiliations.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Vocabulary affiliations."""
+
+from invenio_records.dumpers import SearchDumper
+from invenio_records.dumpers.indexedat import IndexedAtDumperExt
+from invenio_records_resources.factories.factory import RecordTypeFactory
+
+from ...records.pidprovider import PIDProviderFactory
+from ...records.systemfields import BaseVocabularyPIDFieldContext
+from ...services.permissions import PermissionPolicy
+from .config import AffiliationsSearchOptions, service_components
+from .schema import AffiliationSchema
+
+record_type = RecordTypeFactory(
+    "Affiliation",
+    # Data layer
+    pid_field_kwargs={
+        "create": False,
+        "provider": PIDProviderFactory.create(pid_type="aff"),
+        "context_cls": BaseVocabularyPIDFieldContext,
+    },
+    schema_version="1.0.0",
+    schema_path="local://affiliations/affiliation-v1.0.0.json",
+    record_dumper=SearchDumper(
+        extensions=[
+            IndexedAtDumperExt(),
+        ]
+    ),
+    # Service layer
+    service_id="affiliations",
+    service_schema=AffiliationSchema,
+    search_options=AffiliationsSearchOptions,
+    service_components=service_components,
+    permission_policy_cls=PermissionPolicy,
+    # Resource layer
+    endpoint_route="/affiliations",
+)
