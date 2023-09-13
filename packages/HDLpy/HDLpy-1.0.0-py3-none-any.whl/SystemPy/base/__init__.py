c5570b02a308f86f7d36368e069674ec62a7645f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:08:00 +0000

    Add package HDLpy/1.0.0/HDLpy-1.0.0-py3-none-any.whl

---
+++ packages/HDLpy/HDLpy-1.0.0-py3-none-any.whl/SystemPy/base/__init__.py
@@ -0,0 +1,2 @@
+from SystemPy.base.Signal import Clk, Signal, Module
+from SystemPy.base.block import Block
---
+++ packages/HDLpy/HDLpy-1.0.0-py3-none-any.whl/SystemPy/base/save_wave.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2023/9/9 17:36
+# @Author : ChenHaHa
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def draw_wave(times, record_dict):
+    """
+    绘制波形图
+    :param times:
+    :param record_dict:
+    :return: None
+    """
+    pass
