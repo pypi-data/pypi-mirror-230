ca8abea8bb7180c3ea9006b4e6a4bec4f682d1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:05:40 +0000

    Add package pynetgear/0.10.10/pynetgear-0.10.10.tar.gz

---
+++ packages/pynetgear/pynetgear-0.10.10.tar.gz/pynetgear-0.10.10/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from codecs import open
+
+REPO_URL = "http://github.com/MatMaul/pynetgear"
+VERSION = "0.10.10"
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+with open("README.md") as f:
+    README = f.read()
+
+setup(
+    name="pynetgear",
+    version=VERSION,
+    description="Access Netgear routers using their SOAP API",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=REPO_URL,
+    download_url=REPO_URL + "/tarball/" + VERSION,
+    author="Paulus Schoutsen",
+    author_email="Paulus@PaulusSchoutsen.nl",
+    license="MIT",
+    install_requires=required,
+    packages=["pynetgear"],
+    zip_safe=True,
+)
