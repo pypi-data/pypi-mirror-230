9d30822296ef1d36b166c47e166b99c76723402b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 04:05:34 +0000

    Add package cf-view/3.2.25/cf-view-3.2.25.tar.gz

---
+++ packages/cf-view/cf-view-3.2.25.tar.gz/cf-view-3.2.25/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name = "cf-view",
+    long_description = "GUI for viewing CF files",
+    version      = "3.2.25",
+    description  = "GUI for viewing CF files",
+    author       = "Andy Heaps",
+    maintainer   = "Andy Heaps",
+    maintainer_email  = "andy.heaps@ncas.ac.uk",
+    author_email = "andy.heaps@ncas.ac.uk",
+    url          = "http://ajheaps.github.io/cf-view",
+    download_url = "https://github.com/ajheaps/CF-View",
+    platforms    = ["Linux", "MacOS"],
+    license="MIT",
+    install_requires = ["cf-python>=3.8.0",
+                        "cf-plot>=3.1.6",
+                        "PyQt5",
+                        ],
+    keywords     = ["cf","netcdf","data","science",
+                    "oceanography","meteorology","climate"],
+    scripts      = ["cfview"]
+  )
+
+
