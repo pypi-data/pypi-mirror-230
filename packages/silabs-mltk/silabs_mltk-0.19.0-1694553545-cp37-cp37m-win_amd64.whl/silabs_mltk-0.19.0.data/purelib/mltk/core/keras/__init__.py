84ab1c557585d7b910b9105c949016c493d5a73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:06:54 +0000

    Add package silabs-mltk/0.19.0/silabs_mltk-0.19.0-1694553545-cp37-cp37m-win_amd64.whl

---
+++ packages/silabs-mltk/silabs_mltk-0.19.0-1694553545-cp37-cp37m-win_amd64.whl/silabs_mltk-0.19.0.data/purelib/mltk/core/keras/__init__.py
@@ -0,0 +1,50 @@
+
+# Tensorflow keeps moving these packages ...
+# To try to import based on the current TF version
+
+
+try:
+    from keras_preprocessing import get_keras_submodule
+    DataSequence = get_keras_submodule('utils').Sequence
+except ModuleNotFoundError:
+    try:
+        from tensorflow.keras.utils.data_utils import Sequence
+        DataSequence = Sequence
+    except ModuleNotFoundError:
+        try:
+            from keras.utils.data_utils import Sequence
+            DataSequence = Sequence
+        except ModuleNotFoundError:
+            try:
+                from keras.utils import Sequence
+                DataSequence = Sequence
+            except ModuleNotFoundError:
+                DataSequence = object
+
+try:
+    from keras_preprocessing.image import ImageDataGenerator
+except ModuleNotFoundError:
+    try:
+        from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    except ModuleNotFoundError:
+        from keras.preprocessing.image import ImageDataGenerator
+
+try:
+    from keras_preprocessing.image.utils import (array_to_img, img_to_array, load_img)
+except ModuleNotFoundError:
+    try:
+        from tensorflow.keras.preprocessing.image import (array_to_img, img_to_array, load_img)
+    except ModuleNotFoundError:
+        from keras.preprocessing.image import (array_to_img, img_to_array, load_img)
+
+
+try:
+    from keras_preprocessing.image.iterator import Iterator as ImageIterator
+except ModuleNotFoundError:
+    try:
+        from tensorflow.keras.preprocessing.image import Iterator as ImageIterator
+    except ModuleNotFoundError:
+        from keras.preprocessing.image import Iterator as ImageIterator
+
+
+
---
+++ packages/silabs-mltk/silabs_mltk-0.19.0-1694553545-cp37-cp37m-win_amd64.whl/silabs_mltk-0.19.0.data/purelib/mltk/core/tflite_micro/__init__.py
@@ -0,0 +1,7 @@
+from .tflite_micro  import TfliteMicro
+from .tflite_micro_model import (
+    TfliteMicroModel, 
+    TfliteMicroLayerError, 
+    TfliteMicroModelDetails,
+    TfliteMicroProfiledLayerResult,
+)
