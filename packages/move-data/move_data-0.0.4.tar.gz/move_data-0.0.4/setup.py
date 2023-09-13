60c1021a25b106c446457596ed44cf1654ab711a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:07:16 +0000

    Add package move-data/0.0.4/move_data-0.0.4.tar.gz

---
+++ packages/move-data/move_data-0.0.4.tar.gz/move_data-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='move_data',
+    version='0.0.4',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your package requires to run
+        'snowflake-connector-python[pandas]',
+        'python-dotenv',
+        'pandas',
+        'pygsheets',
+    ],
+)
+
