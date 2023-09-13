e1a6d90b7ded9d56d29341a95c94fd72f509ad93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 04:06:00 +0000

    Add package ZYYA-Codes/2023.0.0.0/ZYYA_Codes-2023.0.0.0.tar.gz

---
+++ packages/ZYYA-Codes/ZYYA_Codes-2023.0.0.0.tar.gz/ZYYA_Codes-2023.0.0.0/src/ZYYA_Codes/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2023/9/12 21:34
+@Auth ： Alan Gong
+@File ：__init__.py
+@IDE ：PyCharm
+"""
+import importlib
+
+import pandas as pd
+
+importlib.import_module("pandas")
+
+
+def Hello():
+    print("Hello from src/ZYYA_Codes/__init__.py")
