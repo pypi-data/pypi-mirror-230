3254a2214cd460538ea7cb6dd584db1693d4f3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:07:23 +0000

    Add package curated-transformers/1.2.0/curated-transformers-1.2.0.tar.gz

---
+++ packages/curated-transformers/curated-transformers-1.2.0.tar.gz/curated-transformers-1.2.0/curated_transformers/tokenizers/auto_tokenizer.py
@@ -0,0 +1,133 @@
+from typing import Dict, Optional, Type, cast
+
+from huggingface_hub.utils import EntryNotFoundError
+
+from ..util.hf import TOKENIZER_JSON, get_file_metadata, get_hf_config_model_type
+from .hf_hub import FromHFHub, get_tokenizer_config
+from .legacy.bert_tokenizer import BERTTokenizer
+from .legacy.camembert_tokenizer import CamemBERTTokenizer
+from .legacy.llama_tokenizer import LlamaTokenizer
+from .legacy.roberta_tokenizer import RoBERTaTokenizer
+from .legacy.xlmr_tokenizer import XLMRTokenizer
+from .tokenizer import Tokenizer, TokenizerBase
+
+HF_TOKENIZER_MAPPING: Dict[str, Type[FromHFHub]] = {
+    "BertTokenizer": BERTTokenizer,
+    "BertTokenizerFast": BERTTokenizer,
+    "CamembertTokenizer": CamemBERTTokenizer,
+    "CamembertTokenizerFast": CamemBERTTokenizer,
+    "LlamaTokenizer": LlamaTokenizer,
+    "LlamaTokenizerFast": LlamaTokenizer,
+    "RobertaTokenizer": RoBERTaTokenizer,
+    "RobertaTokenizerFast": RoBERTaTokenizer,
+    "XLMRobertaTokenizer": XLMRTokenizer,
+    "XLMRobertaTokenizerFast": XLMRTokenizer,
+}
+
+HF_MODEL_MAPPING: Dict[str, Type[FromHFHub]] = {
+    "bert": BERTTokenizer,
+    "camembert": CamemBERTTokenizer,
+    "llama": LlamaTokenizer,
+    "roberta": RoBERTaTokenizer,
+    "xlm-roberta": XLMRTokenizer,
+}
+
+
+class AutoTokenizer:
+    """
+    Tokenizer loaded from the Hugging Face Model Hub.
+    """
+
+    # NOTE: We do not inherit from FromHFHub, because its from_hf_hub method
+    #       requires that the return type is Self.
+
+    @classmethod
+    def from_hf_hub_to_cache(
+        cls,
+        *,
+        name: str,
+        revision: str = "main",
+    ):
+        """
+        Download the tokenizer's serialized model, configuration and vocab files
+        from Hugging Face Hub into the local Hugging Face cache directory.
+        Subsequent loading of the tokenizer will read the files from disk. If the
+        files are already cached, this is a no-op.
+
+        :param name:
+            Model name.
+        :param revision:
+            Model revision.
+        """
+        tokenizer_cls = _resolve_tokenizer_class(name, revision)
+        tokenizer_cls.from_hf_hub_to_cache(name=name, revision=revision)
+
+    @classmethod
+    def from_hf_hub(cls, *, name: str, revision: str = "main") -> TokenizerBase:
+        """
+        Infer a tokenizer type and load it from Hugging Face Hub.
+
+        :param name:
+            Model name.
+        :param revision:
+            Model revision.
+        :returns:
+            The tokenizer.
+        """
+
+        tokenizer_cls = _resolve_tokenizer_class(name, revision)
+        # This cast is safe, because we only return tokenizers.
+        return cast(
+            TokenizerBase, tokenizer_cls.from_hf_hub(name=name, revision=revision)
+        )
+
+
+def _get_tokenizer_class_from_config(
+    *, name: str, revision: str
+) -> Optional[Type[FromHFHub]]:
+    """
+    Infer the tokenizer class from the tokenizer configuration.
+
+    :param name:
+        Model name.
+    :param revision:
+        Model revision.
+    :returns:
+        Inferred class.
+    """
+
+    try:
+        tokenizer_config = get_tokenizer_config(name=name, revision=revision)
+    except EntryNotFoundError:
+        return None
+
+    return HF_TOKENIZER_MAPPING.get(tokenizer_config.get("tokenizer_class", None), None)
+
+
+def _resolve_tokenizer_class(name: str, revision: str) -> Type[FromHFHub]:
+    tokenizer_cls: Optional[Type[FromHFHub]] = None
+    try:
+        # We will try to fetch metadata to avoid potentially downloading
+        # the tokenizer file twice (here and Tokenizer.from_hf_hub).
+        get_file_metadata(filename=TOKENIZER_JSON, name=name, revision=revision)
+    except EntryNotFoundError:
+        pass
+    else:
+        tokenizer_cls = Tokenizer
+
+    if tokenizer_cls is None:
+        tokenizer_cls = _get_tokenizer_class_from_config(name=name, revision=revision)
+
+    if tokenizer_cls is None:
+        try:
+            model_type = get_hf_config_model_type(name=name, revision=revision)
+        except EntryNotFoundError:
+            pass
+        else:
+            tokenizer_cls = HF_MODEL_MAPPING.get(model_type, None)
+
+    if tokenizer_cls is None:
+        raise ValueError(
+            f"Cannot infer tokenizer for repository '{name}' with revision '{revision}'"
+        )
+    return tokenizer_cls
---
+++ packages/curated-transformers/curated-transformers-1.2.0.tar.gz/curated-transformers-1.2.0/curated_transformers/tokenizers/legacy/llama_tokenizer.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+from typing import Any, Dict, Optional, Type, TypeVar
+
+from curated_tokenizers import SentencePieceProcessor
+
+from ..hf_hub import LegacyFromHFHub
+from .legacy_tokenizer import AddBosEosPreEncoder
+from .sentencepiece_tokenizer import SentencePieceTokenizer
+
+# Only provided as typing.Self in Python 3.11+.
+Self = TypeVar("Self", bound="LlamaTokenizer")
+
+DEFAULT_BOS_PIECE = "<s>"
+
+
+class LlamaTokenizer(SentencePieceTokenizer, LegacyFromHFHub):
+    """
+    Legacy tokenizer for Llama (`Touvron et al., 2023 [a]`_, `Touvron et al., 2023 [b]`_) models.
+
+    .. _Touvron et al., 2023 [a]: https://arxiv.org/abs/2302.13971
+    .. _Touvron et al., 2023 [b]: https://arxiv.org/abs/2307.09288
+    """
+
+    vocab_files: Dict[str, str] = {"model": "tokenizer.model"}
+
+    def __init__(
+        self,
+        *,
+        processor: SentencePieceProcessor,
+        add_bos_piece: bool = True,
+        add_eos_piece: bool = False,
+    ):
+        """
+        Construct a Llama tokenizer from a ``curated-tokenizers`` SentencePiece processor.
+
+        :param processor:
+            The processor to wrap.
+        :param add_bos_piece:
+            Add a begin-of-sequence piece.
+        :param add_eos_piece:
+            Add an end-of-sequence piece.
+        """
+        super().__init__(processor=processor)
+
+        self.processor = processor
+
+        bos_piece = processor.id_to_piece(processor.bos_id()) if add_bos_piece else None
+        eos_piece = processor.id_to_piece(processor.eos_id()) if add_eos_piece else None
+
+        self.pre_encoder = AddBosEosPreEncoder(bos_piece=bos_piece, eos_piece=eos_piece)
+
+    @classmethod
+    def from_files(
+        cls: Type[Self],
+        *,
+        model_path: Path,
+        add_bos_piece: bool = True,
+        add_eos_piece: bool = False,
+    ) -> Self:
+        """
+        Construct a Llama tokenizer from a SentencePiece model.
+
+        :param model_path:
+            Path to the SentencePiece model file.
+        :param add_bos_piece:
+            Add a begin-of-sequence piece.
+        :param add_eos_piece:
+            Add an end-of-sequence piece.
+        """
+        processor = SentencePieceProcessor.from_file(str(model_path))
+        return cls(
+            processor=processor,
+            add_bos_piece=add_bos_piece,
+            add_eos_piece=add_eos_piece,
+        )
+
+    @classmethod
+    def _load_from_vocab_files(
+        cls: Type[Self],
+        *,
+        vocab_files: Dict[str, Path],
+        tokenizer_config: Optional[Dict[str, Any]],
+    ) -> Self:
+        if tokenizer_config is None:
+            return cls.from_files(model_path=vocab_files["model"])
+
+        add_bos_piece = tokenizer_config.get("add_bos_token", True)
+        add_eos_piece = tokenizer_config.get("add_eos_token", False)
+
+        return cls.from_files(
+            model_path=vocab_files["model"],
+            add_bos_piece=add_bos_piece,
+            add_eos_piece=add_eos_piece,
+        )
