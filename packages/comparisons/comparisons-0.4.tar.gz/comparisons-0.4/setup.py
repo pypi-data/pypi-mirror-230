7b3466013b84786b9437190b2b0a6508707bdc0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:08:16 +0000

    Add package comparisons/0.4/comparisons-0.4.tar.gz

---
+++ packages/comparisons/comparisons-0.4.tar.gz/comparisons-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="comparisons",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "regex",
+        "python-Levenshtein",
+    ],
+)
