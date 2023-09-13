42c4cb9fca56b4574627edc504f4b4d59c513821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:08:21 +0000

    Add package SG1AB/1.0.1.1/SG1AB-1.0.1.1.tar.gz

---
+++ packages/SG1AB/SG1AB-1.0.1.1.tar.gz/SG1AB-1.0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+import shutil
+
+target_directory = 'SG1AB'
+
+setup(
+    name=target_directory,
+    version='1.0.1.1',
+    description='nnfw_api binding',
+    long_description='you can use nnfw_api by python',
+    url='https://github.com/Samsung/ONE/tree/master/runtime',
+    author='Your Name',
+    author_email='your@email.com',
+    license='Samsung',
+    packages=[target_directory],
+    package_data={target_directory: ['nnfw_api_pybind.cpython-38-x86_64-linux-gnu.so', 'libnnfw-dev.so', 'nlibonert_core.so', 'nlibbackend_cpu.so', 'nnfw_api_pybind.cpython-38-arm-linux-gnueabihf.so','nnfw_api_pybind.cpython-38-aarch64-linux-gnueabihf.so']},
+    install_requires=[
+        # 필요한 의존성 패키지를 여기에 추가
+    ],
+)
+
