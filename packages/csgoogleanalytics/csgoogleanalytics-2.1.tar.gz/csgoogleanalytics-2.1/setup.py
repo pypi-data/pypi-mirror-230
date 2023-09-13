826a8f8ddff0498c8ad4b19fea0a2f7bfedecd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:08:58 +0000

    Add package csgoogleanalytics/2.1/csgoogleanalytics-2.1.tar.gz

---
+++ packages/csgoogleanalytics/csgoogleanalytics-2.1.tar.gz/csgoogleanalytics-2.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = "2.1"
+
+setup(
+    name="csgoogleanalytics",
+    version=version,
+    description=(
+        "Google analyticseko gure webguneko datuak hileka edo asteka jasotzeko"
+        " tresna"
+    ),
+    long_description="""\
+""",
+    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords="",
+    author="Jatsu Argarate",
+    author_email="jargarate@codesyntax.com",
+    url="",
+    license="",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "google-analytics-data",
+        "urllib3",
+        "google-auth",
+        "requests-oauthlib",
+        "jsonpickle",
+        "python-gflags",
+        "pytz",
+        "django-object-actions",
+    ],
+    entry_points="""
+      # -*- Entry points: -*-
+      """,
+)
