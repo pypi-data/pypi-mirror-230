f38b718b2f5961215ee7f2d6991ebac8ab9c83b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:06:53 +0000

    Add package mime-guess/0.1.0/mime_guess-0.1.0.tar.gz

---
+++ packages/mime-guess/mime_guess-0.1.0.tar.gz/mime_guess-0.1.0/src/lib.rs
@@ -0,0 +1,16 @@
+use pyo3::prelude::*;
+use ::mime_guess::from_path;
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn guess_type(path:String) -> PyResult<Option<&'static str>> {
+    let result = from_path(path).first_raw();
+    Ok(result)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn mime_guess(_py: Python, m: &PyModule) -> PyResult<()> {
+    #[cfg(Py_3_7)]
+    m.add_function(wrap_pyfunction!(guess_type, m)?)?;
+    Ok(())
+}
