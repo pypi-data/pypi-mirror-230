809efed8c7fd698c82b35a3659c65864ea8832b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 03:07:47 +0000

    Add package intelliw-onnx/0.0.2/intelliw_onnx-0.0.2-py3-none-any.whl

---
+++ packages/intelliw-onnx/intelliw_onnx-0.0.2-py3-none-any.whl/intelliw_onnx/common/mish_convert.py
@@ -0,0 +1,195 @@
+import onnx
+import sys
+import argparse
+from intelliw_onnx.utils.logger import get_logger
+
+logger = get_logger("CONVERT")
+
+
+def merge_mish_old(onnxfile, export_onnx):
+    model = onnx.load(onnxfile)
+
+    dict_sp = {}
+    dict_tanh = {}
+    dict_mul = {}
+    dict_mish_next = {}
+    mish_next_list = []
+
+    got_mish = False
+
+    for node_id, node in enumerate(model.graph.node):
+        # print(node_id, ", name:", node.name, ", input:", node.input, ", output:", node.output,  \
+        #         ", op:", node.op_type, ', len(input):', len(node.input))
+
+        if got_mish == True:
+            if dict_mul['output'][0] in node.input:
+                logger.debug('got next node----')
+                dict_mish_next['input'] = node.input
+                dict_mish_next['output'] = node.output
+                dict_mish_next['id'] = node_id
+
+                mish_next_list.append(dict_mish_next)
+
+            continue
+
+        if node.op_type == 'Softplus':
+            dict_sp['input'] = node.input
+            dict_sp['output'] = node.output
+            dict_sp['id'] = node_id
+
+        if node.op_type == 'Tanh':
+            if dict_sp and node.input == dict_sp['output']:
+                dict_tanh['input'] = node.input
+                dict_tanh['output'] = node.output
+                dict_tanh['id'] = node_id
+                logger.debug('got first pair: {} {}'.format(dict_tanh['input'], dict_tanh['output']))
+            else:
+                logger.debug('clear Softplus')
+                dict_sp = {}
+
+        if node.op_type == 'Mul':
+            if dict_sp and dict_tanh and node.input[1] == dict_tanh['output'][0] and node.input[0] == dict_sp['input'][
+                0]:
+                logger.debug('got second pair: {}'.format(dict_tanh['output']))
+                dict_mul['input'] = node.input
+                dict_mul['output'] = node.output
+                dict_mul['id'] = node_id
+
+                got_mish = True
+
+    if got_mish == True:
+        old_node = model.graph.node[dict_sp['id']]
+        model.graph.node.remove(old_node)
+
+        mish_node = onnx.helper.make_node(
+            name='',
+            op_type='Mish',
+            inputs=dict_sp['input'],
+            outputs=dict_mul['output'],
+        )
+
+        model.graph.node.insert(dict_sp['id'], mish_node)
+
+        # for node in mish_next_list:
+        # next_node = model.graph.node[node['id']]
+        # next_node.input[0] = dict_sp['output'][0]
+
+        old_node = model.graph.node[dict_mul['id']]
+        model.graph.node.remove(old_node)
+
+        old_node = model.graph.node[dict_tanh['id']]
+        model.graph.node.remove(old_node)
+
+        onnx.save(model, export_onnx)
+
+    return got_mish
+
+
+def merge_mish(model):
+    dict_sp = {}
+    dict_tanh = {}
+    dict_mul = {}
+
+    got_mish = False
+
+    search = True
+
+    while search == True:
+        search = False
+        for node_id, node in enumerate(model.graph.node):
+            # print(node_id, ", name:", node.name, ", input:", node.input, ", output:", node.output,  \
+            #        ", op:", node.op_type, ', len(input):', len(node.input))
+
+            if node.op_type == 'Softplus':
+                dict_sp['input'] = node.input
+                dict_sp['output'] = node.output
+                dict_sp['id'] = node_id
+
+            if node.op_type == 'Tanh':
+                if dict_sp and node.input == dict_sp['output']:
+                    dict_tanh['input'] = node.input
+                    dict_tanh['output'] = node.output
+                    dict_tanh['id'] = node_id
+                    logger.debug('got first pair: {} {}'.format(dict_tanh['input'], dict_tanh['output']))
+                else:
+                    logger.debug('clear Softplus, dict_sp: {}'.format(dict_sp))
+                    dict_sp = {}
+
+            if node.op_type == 'Mul':
+                if dict_sp and dict_tanh and node.input[1] == dict_tanh['output'][0] and node.input[0] == \
+                        dict_sp['input'][0]:
+                    dict_mul['input'] = node.input
+                    dict_mul['output'] = node.output
+                    dict_mul['id'] = node_id
+
+                    logger.debug('got second pair: {} {}'.format(dict_mul['input'], dict_mul['output']))
+
+                    got_mish = True
+
+                    old_node = model.graph.node[dict_sp['id']]
+                    model.graph.node.remove(old_node)
+
+                    mish_node = onnx.helper.make_node(
+                        name='',
+                        op_type='Mish',
+                        inputs=dict_sp['input'],
+                        outputs=dict_mul['output'],
+                        domain='com.metax-tech'
+                    )
+
+                    model.graph.node.insert(dict_sp['id'], mish_node)
+
+                    # for node in mish_next_list:
+                    # next_node = model.graph.node[node['id']]
+                    # next_node.input[0] = dict_sp['output'][0]
+
+                    old_node = model.graph.node[dict_mul['id']]
+                    model.graph.node.remove(old_node)
+
+                    old_node = model.graph.node[dict_tanh['id']]
+                    model.graph.node.remove(old_node)
+
+                    dict_sp = {}
+                    dict_tanh = {}
+                    dict_mul = {}
+
+                    search = True
+                    break
+                else:
+                    logger.debug('clear Softplus and Tanh')
+                    logger.debug('dict_sp: {}'.format(dict_sp))
+                    logger.debug('dict_tanh: {}'.format(dict_tanh))
+                    dict_sp = {}
+                    dict_tanh = {}
+
+    if got_mish == True:
+        op_set = model.opset_import.add()
+        op_set.domain = 'com.metax-tech'
+        op_set.version = 1
+
+        # onnx.save(model, export_onnx)
+
+    return model
+
+
+def Test():
+    export_onnx = './tmp.onnx'
+
+    merge_mish('./v4_no_mish.onnx', export_onnx)
+
+
+'''
+    loop = True
+
+    loop = merge_mish_old('./v4_no_mish.onnx', export_onnx)   
+
+    while loop:
+        loop = merge_mish_old(export_onnx, export_onnx)
+'''
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert Softplus+Tanh+Mul to Mish')
+    parser.add_argument('--onnx_file', type=str, default='', help='source onnx model')
+    parser.add_argument('--output_file', type=str, default='mish.onnx', help='dest onnx file')
+    args = parser.parse_args()
+    merge_mish(onnxfile=args.onnx_file, export_onnx=args.output_file)
---
+++ packages/intelliw-onnx/intelliw_onnx-0.0.2-py3-none-any.whl/intelliw_onnx/main.py
@@ -0,0 +1,132 @@
+import argparse
+from intelliw_onnx.convert.convert import ONNXConvert
+
+
+def handle_convert(args):
+    ONNXConvert(args).convert()
+
+
+def handle_default(args):
+    if args.version:
+        exit(0)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    # parser #
+    parser.set_defaults(func=handle_default)
+    # show package version
+    parser.add_argument('--version', '-v',
+                        action='store_true',
+                        default=False,
+                        help='Show intelliw-onnx version')
+    subparsers = parser.add_subparsers(help='commands')
+
+    # convert #
+    convert_parser = subparsers.add_parser(name='convert', help='Convert torch/paddle model to ONNX.')
+
+    convert_parser.add_argument("--model_path",
+                                type=str,
+                                help="Input path(model file or folder)")
+    convert_parser.add_argument("--model_type",
+                                type=str,
+                                help="Input model type(ex: paddle/pytorch)")
+    convert_parser.add_argument("--output",
+                                type=str,
+                                help="Output path(ex: ./output.onnx)")
+    convert_parser.add_argument("--op_set",
+                                type=int, required=False,
+                                help="Set op_set version(default: 11)")
+    # for pytorch/dynamic_paddle
+    convert_parser.add_argument("--input_shape",
+                                type=str,
+                                required=False,
+                                default='',
+                                help="Input shape for pytorch/paddle(ex: [1,3,224,224] or [1,3,224,224]/[1,3,56,56])")
+    # for paddle dynamic model or pytorch
+    convert_parser.add_argument("--model_def_file",
+                                type=str,
+                                required=False,
+                                default='',
+                                help="Paddle/pytorch model definition file location(ex: --model_def_file ./cnn.py)")
+
+    convert_parser.add_argument("--model_weights_file",
+                                type=str,
+                                required=False,
+                                default='',
+                                help="Paddle/pytorch model weights file location(ex: --model_weights_file ./0.99667.pth)")
+
+    convert_parser.add_argument("--model_class_name",
+                                type=str,
+                                required=False,
+                                default='',
+                                help="Paddle/pytorch model class name(ex: --model_class_name CNN)")
+
+    convert_parser.add_argument("--model_input_type",
+                                type=str,
+                                required=False,
+                                default='',
+                                help="Paddle/pytorch input type(default float, choice is ['float', 'float32', 'float16', 'uint8', 'int8', 'uint16', 'int16', 'uint32', 'int32', 'uint64', 'int64', 'bool'])")
+
+    convert_parser.add_argument("--params_file",
+                                type=str,
+                                required=False,
+                                default='',
+                                help="Paddle/pytorch params declaration file location(ex: --params_file ./params.py)")
+
+    # for pytorch/paddle
+    convert_parser.add_argument("--output_num",
+                                type=int,
+                                required=False,
+                                default=1,
+                                help="If output num of pytorch model > 1, you can specify it by --output_num")
+
+    # for pytorch
+    convert_parser.add_argument("--keep_batch",
+                                type=int,
+                                choices=[0, 1],
+                                required=False,
+                                default=1,
+                                help="For pytorch, if set 1, the tool will keep model batch size(if 0, set it to dynamic(-1))")
+
+    # for dynamic batch size
+    convert_parser.add_argument("--dynamic_batch",
+                                type=int,
+                                required=False,
+                                default=0,
+                                choices=[0, 1],
+                                help="If set 1, the tool will convert batch size to -1")
+
+    # for simplify
+    convert_parser.add_argument("--simplify",
+                                type=int, required=False,
+                                choices=[0, 1, 2],
+                                default=0,
+                                help="Simplify the model(0:no simplify;1:do simplify; 2:for dynamic model)")
+
+    convert_parser.add_argument("--simplify_hw",
+                                type=str,
+                                required=False,
+                                default='',
+                                help="When h/w is -1, you can specify h/w as you expected(together with --simplify 2)")
+
+    # force simplify
+    convert_parser.add_argument("--force_simplify",
+                                type=int, required=False,
+                                choices=[0, 1, 2],
+                                default=0,
+                                help="Force simplify the model(0:no simplify;1:do simplify; 2:for dynamic model)")
+
+    convert_parser.set_defaults(func=handle_convert)
+
+    args = parser.parse_args()
+
+    # 执行函数功能
+    try:
+        args.func(args)
+    except AttributeError:
+        print("try use `intelliw-onnx -h`")
+
+
+if __name__ == '__main__':
+    main()
