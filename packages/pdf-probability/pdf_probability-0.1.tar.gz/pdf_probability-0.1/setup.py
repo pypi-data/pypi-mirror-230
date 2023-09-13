b93697d3d140df3afae7ce0d47b5338bf37accec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:06:47 +0000

    Add package pdf-probability/0.1/pdf_probability-0.1.tar.gz

---
+++ packages/pdf-probability/pdf_probability-0.1.tar.gz/pdf_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pdf_probability',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['pdf_probability'],
+      zip_safe=False)
