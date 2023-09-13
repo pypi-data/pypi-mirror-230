96212b1ae57e8a0af087b4bccf9d51a85c2e5064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 04:05:37 +0000

    Add package resulting/1.1.1/resulting-1.1.1-py3-none-any.whl

---
+++ packages/resulting/resulting-1.1.1-py3-none-any.whl/resulting/__init__.py
@@ -0,0 +1,17 @@
+class UnwrapError(Exception):
+    pass
+
+from ._result import Result, Ok, Err
+from ._option import Option, Some, none
+from ._wrappers import catch, optional
+
+__all__ = [
+    "Result",
+    "Ok",
+    "Err",
+    "Option",
+    "Some",
+    "none",
+    "catch",
+    "optional",
+]
