f5e81a0fd42443ada8f41f1f7be5f984989cd18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:06:54 +0000

    Add package patch-target/0.1.3/patch_target-0.1.3.tar.gz

---
+++ packages/patch-target/patch_target-0.1.3.tar.gz/patch_target-0.1.3/patch_target/__init__.py
@@ -0,0 +1,43 @@
+import typing
+from types import ModuleType
+from typing import Protocol, NewType, Any, TypeGuard, Callable
+from typing_extensions import assert_never
+
+PatchTarget = NewType("PatchTarget", str)
+import logging
+
+logger = logging.getLogger(__name__)
+
+NamedCallable: typing.TypeAlias = Callable[[Any], Any]
+
+class Named(Protocol):
+    __name__: str
+
+
+class InvalidPatchTargetException(Exception):
+    pass
+
+
+def is_named(value: Named | NamedCallable | str) -> TypeGuard[Named]:
+    return hasattr(value, "__name__")
+
+
+def patch_target(
+        host_module: ModuleType, object_to_be_patched: Named | NamedCallable | str
+) -> PatchTarget:
+    match object_to_be_patched:
+        case v if is_named(v):
+            name = v.__name__
+        case str(v):
+            name = v
+        case _:
+            raise TypeError
+
+
+    if not hasattr(host_module, name):
+        raise InvalidPatchTargetException(f"'{name}' not found within {host_module.__name__}")
+
+    return PatchTarget(f"{host_module.__name__}.{name}")
+
+
+__all__ = ["patch_target"]
