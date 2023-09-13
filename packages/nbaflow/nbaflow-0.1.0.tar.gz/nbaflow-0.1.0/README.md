46e64f6f4554fb5ec350881615bbf4b257a10d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 05:05:14 +0000

    Add package nbaflow/0.1.0/nbaflow-0.1.0.tar.gz

---
+++ packages/nbaflow/nbaflow-0.1.0.tar.gz/nbaflow-0.1.0/README.md
@@ -0,0 +1,18 @@
+![NBAFlow logo](./docs/assets/imgs/header-readme.png)
+
+<div align="center">  
+
+  [![PyPI](https://img.shields.io/pypi/v/nbaflow?color=orange)](https://pypi.org/project/nbaflow/)
+  ![PyPI - Downloads](https://img.shields.io/pypi/dm/nbaflow?color=orange)
+  ![PyPI - Status](https://img.shields.io/pypi/status/nbaflow?color=orange)
+
+</div>
+
+<div align="center">
+  <strong>:basketball: Helping users to get and analyse NBA data using the <a href="https://github.com/swar/nba_api"> nba_api</a> Python package :basketball:</strong>
+</div>
+<br/>
+
+
+> **Note**
+> This project is under construction. Soon as possible it will be ready to go.
---
+++ packages/nbaflow/nbaflow-0.1.0.tar.gz/nbaflow-0.1.0/setup.py
@@ -0,0 +1,45 @@
+"""Package setup script.
+
+This script handles everything about package publishing on PyPI.
+"""
+
+# Importing libraries
+from setuptools import setup, find_packages
+
+# Reading README.md for project description
+with open("README.md", "r", encoding='utf-8') as f:
+    __long_description__ = f.read()
+
+# Setting up package information
+setup(
+    name='nbaflow',
+    version='0.1.0',
+    author='Thiago Panini',
+    author_email='panini.development@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        "nba_api"
+    ],
+    license='MIT',
+    description="Making it easier to extract and analyse NBA data.",
+    long_description=__long_description__,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ThiagoPanini/nbaflow',
+    keywords='Python, NBA',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: Portuguese (Brazilian)",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    python_requires=">=3.0.0"
+)
