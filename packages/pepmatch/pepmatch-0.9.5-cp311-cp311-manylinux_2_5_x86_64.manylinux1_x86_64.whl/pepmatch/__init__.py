66a39a6e408ce93e670e485331922dc291744bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 15:06:45 +0000

    Add package pepmatch/0.9.5/pepmatch-0.9.5-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/pepmatch/pepmatch-0.9.5-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.whl/pepmatch/__init__.py
@@ -0,0 +1,7 @@
+from .preprocessor import Preprocessor
+from .matcher import Matcher
+from .benchmarker import Benchmarker
+
+from .version import __version__
+
+__all__ = ['Preprocessor', 'Matcher', 'Benchmarker', '__version__']
---
+++ packages/pepmatch/pepmatch-0.9.5-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.whl/pepmatch/benchmarker.py
@@ -0,0 +1,66 @@
+from .preprocessor import Preprocessor
+from .matcher import Matcher
+
+
+class Benchmarker(Matcher):
+  """
+  Object used for benchmarking the PEPMatch code for the various applications.
+  It uses three methods: 2 for preprocessing and 1 for searching as the benchmarking
+  code is structured. PEPMatch does not do any query preprocessing so it raises
+  a TypeError which is excepted in the benchmarking code.
+  Inherits from Matcher object.
+  """
+  def __init__(self, query, proteome, lengths, max_mismatches, algorithm_parameters):
+    self.query = query
+    self.proteome = proteome
+    self.max_mismatches = max_mismatches
+    self.lengths = lengths
+    self.algorithm_parameters = algorithm_parameters
+    
+    super().__init__(
+      query, proteome, max_mismatches, 
+      output_format=algorithm_parameters['output_format'])
+
+  def __str__(self):
+    return 'PEPMatch'
+
+  def preprocess_query(self) -> None:
+    """No query preprocessing, raise TypeError"""
+    raise TypeError(self.__str__() + ' does not preprocess queries.\n')
+
+  def preprocess_proteome(self) -> None:
+    """Preprocess proteome once or multiple times for each split calculated."""
+    preprocessor = Preprocessor(self.proteome)
+    if self.max_mismatches == -1:
+      for k in self._best_match_ks():
+        preprocessor.pickle_proteome(k)
+    else:
+      for k in self._batch_query().keys():
+        preprocessor.pickle_proteome(k)
+
+  def search(self) -> list:
+    """Call overarching match function. Then convert results into the standard format
+    needed to calculate accuracy.
+    """
+    if self.max_mismatches == -1:
+      self.k_specified = True
+      self.k = 2
+      self.max_mismatches = 7
+
+    matches = self.match()
+
+    all_matches = []
+    for i, match in matches.iterrows():
+      match_string = ''
+      match_string += match['Query Sequence'] + ','
+      match_string += match['Matched Sequence'] + ','
+      match_string += match['Protein ID'] + ','
+      match_string += str(match['Mismatches']) + ','
+      try:
+        match_string += str(match['Index start'] - 1)
+      except TypeError:
+        match_string += ''
+        
+      all_matches.append(match_string)
+    
+    return all_matches
