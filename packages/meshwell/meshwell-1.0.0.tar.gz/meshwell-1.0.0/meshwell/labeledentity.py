826efed93a5c145ac0e824ef9406f1df19a51a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-13 14:07:01 +0000

    Add package meshwell/1.0.0/meshwell-1.0.0.tar.gz

---
+++ packages/meshwell/meshwell-1.0.0.tar.gz/meshwell-1.0.0/meshwell/labeledentity.py
@@ -0,0 +1,44 @@
+import gmsh
+
+
+class LabeledEntities:
+    """Class to track entities, boundaries, and physical labels during meshing."""
+
+    def __init__(self, index, dimtags, label, base_resolution, resolution, keep, model):
+        self.index = index
+        self.model = model
+        self.dimtags = self._fuse_self(dimtags)
+        self.label = label
+        self.base_resolution = base_resolution
+        self.resolution = resolution
+        self.boundaries = self.update_boundaries()
+        self.interfaces = []
+        self.keep = keep
+
+    def _fuse_self(self, dimtags):
+        if len(dimtags) == 0:
+            return []
+        elif len(dimtags) != 1:
+            dimtags = gmsh.model.occ.fuse(
+                [dimtags[0]],
+                dimtags[1:],
+                removeObject=True,
+                removeTool=True,
+            )[0]
+            self.model.occ.synchronize()
+        return dimtags
+
+    def get_tags(self):
+        tags = [tag for dim, tag in self.dimtags]
+        if any(isinstance(el, list) for el in tags):
+            tags = [item for sublist in tags for item in sublist]
+        return tags
+
+    def get_dim(self):
+        return [dim for dim, tag in self.dimtags][0]
+
+    def update_boundaries(self):
+        self.boundaries = [
+            tag
+            for dim, tag in gmsh.model.getBoundary(self.dimtags, False, False, False)
+        ]
---
+++ packages/meshwell/meshwell-1.0.0.tar.gz/meshwell-1.0.0/meshwell/polysurface.py
@@ -0,0 +1,63 @@
+class PolySurface:
+    """
+    Creates bottom-up GMSH polygonal surfaces formed by list of shapely (multi)polygon.
+
+    Attributes:
+        polygons: list of shapely (Multi)Polygon
+        model: GMSH model to synchronize
+    """
+
+    def __init__(
+        self,
+        polygons,
+        model,
+    ):
+        # Parse (multi)polygons
+        self.polygons = list(
+            polygons.geoms if hasattr(polygons, "geoms") else [polygons]
+        )
+
+        # Track gmsh entities for bottom-up volume definition
+        self.model = model
+
+    def _parse_coords(self, coords):
+        """Chooses z=0 if the provided coordinates are 2D."""
+        return (coords[0], coords[1], 0) if len(coords) == 2 else coords
+
+    def get_gmsh_polygons(self):
+        """Returns the fused GMSH surfaces within model from the polygons."""
+        surfaces = [self.add_surface_with_holes(entry) for entry in self.polygons]
+        if len(surfaces) <= 1:
+            return surfaces
+        dimtags = self.model.occ.fuse(
+            [(2, surfaces[0])],
+            [(2, tag) for tag in surfaces[1:]],
+            removeObject=True,
+            removeTool=True,
+        )[0]
+        self.model.occ.synchronize()
+        return [tag for dim, tag in dimtags]
+
+    def add_surface_with_holes(self, polygon):
+        """Returns surface, removing intersection with hole surfaces."""
+        exterior = self.model.add_surface(
+            [self._parse_coords(coords) for coords in polygon.exterior.coords]
+        )
+        interior_tags = [
+            self.model.add_surface(
+                [self._parse_coords(coords) for coords in interior.coords],
+            )
+            for interior in polygon.interiors
+        ]
+        for interior_tag in interior_tags:
+            exterior = self.model.occ.cut(
+                [(2, exterior)], [(2, interior_tag)], removeObject=True, removeTool=True
+            )
+            self.model.occ.synchronize()
+            exterior = exterior[0][0][1]  # Parse `outDimTags', `outDimTagsMap'
+        return exterior
+
+    def instanciate(self):
+        polysurface = self.get_gmsh_polygons()
+        self.model.occ.synchronize()
+        return [(2, polysurface)]
