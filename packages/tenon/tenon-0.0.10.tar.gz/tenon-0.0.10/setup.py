90103d3f3f03e515ca07d09fcd5c4954642ae275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:07:54 +0000

    Add package tenon/0.0.10/tenon-0.0.10.tar.gz

---
+++ packages/tenon/tenon-0.0.10.tar.gz/tenon-0.0.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tenon',
+    version='0.0.10',
+    package_dir={'': 'src'},
+    packages=['tenon'],
+    url='https://github.com/Juannie-PP/jigsaw',
+    license='MIT',
+    author='Juannie-PP',
+    author_email='2604868278@qq.com',
+    description='A package to solve tenon',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["requests>=2.19.1", "numpy>=1.23.0", "opencv-python==4.5.5.64"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    keywords=["rotate_captcha", "notch_captcha", "captcha identify", "captcha"],
+    python_requires='>=3.6',
+)
