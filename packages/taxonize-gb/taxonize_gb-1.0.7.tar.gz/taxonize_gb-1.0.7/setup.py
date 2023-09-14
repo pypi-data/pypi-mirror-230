bd753725e5431f717e031b5a0994ff68d4996c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:09:59 +0000

    Add package taxonize-gb/1.0.7/taxonize_gb-1.0.7.tar.gz

---
+++ packages/taxonize-gb/taxonize_gb-1.0.7.tar.gz/taxonize_gb-1.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='taxonize_gb',
+    version='1.0.7',
+    description='Python package to download and filter GenBank database based on taxonomy',
+    author='Mohamed S. Sarhan',
+    author_email='mohamed.sarhan@eurac.edu',
+    url='https://github.com/msabrysarhan/taxonize_genbank',
+    packages=find_packages(),
+    package_data = { 'taxonize_gb' : [
+        'taxonize_gb/utils/*',
+    ] },
+
+    install_requires=[
+        'biopython==1.81',
+        'tqdm==4.64.1',
+        'ete3==3.1.3',
+        'networkx==2.6.3',
+        'six==1.16.0'
+    ],
+    entry_points={
+        "console_scripts": [
+            "taxonize_gb = taxonize_gb.taxonize_gb:main",
+            "get_db = taxonize_gb.utils.get_db:main",
+            "get_taxonomy = taxonize_gb.utils.get_taxonomy:main",
+        ],
+    },
+
+)
