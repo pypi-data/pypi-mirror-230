b77d446855567acc191aaa96865028d7becc4c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 14:06:24 +0000

    Add package private-values/0.3.0/private_values-0.3.0-py3-none-any.whl

---
+++ packages/private-values/private_values-0.3.0-py3-none-any.whl/private_values/env.py
@@ -0,0 +1,65 @@
+from .main import *
+
+import os
+
+
+# =====================================================================================================================
+class PrivateEnv(PrivateBase):
+    """
+    read exact environ from Os Environment
+    """
+    @classmethod
+    def get(cls, name: str, _raise_exx: Optional[bool] = None) -> Type_Value:
+        if _raise_exx is None:
+            _raise_exx = cls.RAISE_EXX
+
+        result = os.getenv(name)
+        if result is None:
+            cls.show()
+
+            msg = f"[CRITICAL]no [{name=}] in environment!"
+            msg += f"\n\tIf you just now add it - dont forget reboot!"
+            if _raise_exx:
+                raise Exx_PvNotAccepted(msg)
+            else:
+                print(msg)
+        return result
+
+    @staticmethod
+    def show(prefix: Optional[str] = None) -> Type_PvDict:
+        """
+        mainly it is only for PRINTing and debugging! don't use result!
+
+        NOTE: be careful to use result as dict! especially if you have lowercase letters!
+
+        REASON:
+            import os
+
+            name_lowercase = "name_lowercase"
+            os.environ[name_lowercase] = name_lowercase
+
+            print(os.getenv(name_lowercase))    # name_lowercase
+            print(os.getenv(name_lowercase.upper()))    # name_lowercase
+
+            print(os.environ[name_lowercase])   # name_lowercase
+            print(os.environ[name_lowercase.upper()])   # name_lowercase
+
+            print(dict(os.environ)[name_lowercase])     # KeyError: 'name_lowercase'
+        """
+        envs_all = os.environ
+        result: Type_PvDict = {}
+
+        # filter ---------------
+        for name, value in envs_all.items():
+            if not prefix or (prefix and name.upper().startswith(prefix.upper())):
+                result.update({name: value})
+
+        # print ---------------
+        print()     # to pretty print in pytest only
+        for name, value in result.items():
+            print(f"{name}    ={value}")
+        print()     # to pretty print in pytest only
+        return result
+
+
+# =====================================================================================================================
---
+++ packages/private-values/private_values-0.3.0-py3-none-any.whl/private_values/json.py
@@ -0,0 +1,37 @@
+from .main import *
+
+import json
+
+
+# =====================================================================================================================
+class PrivateJson(PrivateBaseWFile):
+    FILENAME: str = "pv.json"
+
+    def _get_value_unsafe(self, name: str, section: str, text: str) -> Optional[str]:
+        """
+        section only in first level!
+        """
+        json_data = self._get_section_unsafe(section, text)
+
+        if json_data:
+            value = json_data.get(name)
+            return value
+
+    def _get_section_unsafe(self, section: str, text: str) -> Optional[Dict[str, Any]]:
+        """
+        section only in first level!
+        """
+        try:
+            json_data = json.loads(text)
+        except Exception as exx:
+            msg = f"[CRITICAL] incorrect format file!\n{exx!r}"
+            print(msg)
+            raise Exx_PvNotAccepted(msg)
+
+        if section:
+            json_data = json_data.get(section)
+
+        return json_data
+
+
+# =====================================================================================================================
