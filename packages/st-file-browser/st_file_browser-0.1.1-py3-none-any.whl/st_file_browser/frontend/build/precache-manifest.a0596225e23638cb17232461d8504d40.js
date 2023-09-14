76de80f45a92419622ffbb8443654be194a7c5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 04:06:44 +0000

    Add package st-file-browser/0.1.1/st_file_browser-0.1.1-py3-none-any.whl

---
+++ packages/st-file-browser/st_file_browser-0.1.1-py3-none-any.whl/st_file_browser/frontend/build/precache-manifest.a0596225e23638cb17232461d8504d40.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cca5067d8c6de5d954432987ef27a01",
+    "url": "./index.html"
+  },
+  {
+    "revision": "441153df53207b8c38ee",
+    "url": "./static/css/2.5a3919a9.chunk.css"
+  },
+  {
+    "revision": "441153df53207b8c38ee",
+    "url": "./static/js/2.0e6934a9.chunk.js"
+  },
+  {
+    "revision": "53ec02d0e91d8bc718b5061335433fac",
+    "url": "./static/js/2.0e6934a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51e5392a52097788e7e",
+    "url": "./static/js/main.4551d177.chunk.js"
+  },
+  {
+    "revision": "44e7e8c245c21e8f352f",
+    "url": "./static/js/runtime-main.5606d314.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
