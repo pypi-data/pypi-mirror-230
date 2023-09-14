54f26639948d0595a992fcc36cdcd6659b850d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 15:08:04 +0000

    Add package pcre2/0.3.0/pcre2-0.3.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pcre2/pcre2-0.3.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pcre2/__init__.py
@@ -0,0 +1,7 @@
+from .methods import compile, findall, match, scan, split, substitute
+from .consts import (
+    __libpcre2_version__,
+    CompileOption, MatchOption, SubstituteOption, ExpandOption,
+    A, I, G, M, NE, NS, U, S, X
+)
+__version__ = "0.3.0"
