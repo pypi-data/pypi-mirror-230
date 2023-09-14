a0b97284e3ee7236681129f9817c7713cef58b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 15:07:23 +0000

    Add package linkhub/1.6.0/linkhub-1.6.0-py3-none-any.whl

---
+++ packages/linkhub/linkhub-1.6.0-py3-none-any.whl/linkhub/__init__.py
@@ -0,0 +1,24 @@
+__version__ = '1.6.0'
+Version = __version__  # for backward compatibility
+__all__ = ["Token","LinkhubException"]
+from .linkhub import Token, LinkhubException
+
+TokenInstance = Token()
+
+def generateToken(LinkID, SecretKey, ServiceID, AccessID, Scope, forwardIP = None, UseStaticIP=False, UseLocalTimeYN=True, UseGAIP=False):
+    return TokenInstance.get(LinkID, SecretKey, ServiceID, AccessID, Scope, forwardIP, UseStaticIP, UseLocalTimeYN, UseGAIP)
+
+def getBalance(_Token, UseStaticIP=False, UseGAIP=False):
+    return TokenInstance.balance(_Token, UseStaticIP, UseGAIP)
+
+def getPartnerBalance(_Token, UseStaticIP=False, UseGAIP=False):
+    return TokenInstance.partnerBalance(_Token, UseStaticIP, UseGAIP)
+
+def getPartnerURL(_Token, TOGO, UseStaticIP=False, UseGAIP=False):
+    return TokenInstance.getPartnerURL(_Token, TOGO, UseStaticIP, UseGAIP)
+
+def getTime(UseStaticIP=False, UseLocalTimeYN=True, UseGAIP=False):
+    return TokenInstance.getTime(UseStaticIP, UseLocalTimeYN, UseGAIP)
+
+def authURL(AuthURL):
+    return TokenInstance.serviceURL(AuthURL)
