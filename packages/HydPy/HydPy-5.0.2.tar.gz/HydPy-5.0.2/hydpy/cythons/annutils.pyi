131ec386c09a3fb9de2196cf65852eb9357abd0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:05:39 +0000

    Add package HydPy/5.0.2/HydPy-5.0.2.tar.gz

---
+++ packages/HydPy/HydPy-5.0.2.tar.gz/HydPy-5.0.2/hydpy/cythons/annutils.pyi
@@ -0,0 +1,22 @@
+from numpy import float_
+from numpy.typing import NDArray
+
+class ANN:
+    # required for usage as an "algorithm" by interputils:
+    nmb_inputs: int
+    nmb_outputs: int
+    inputs: NDArray[float_]
+    outputs: NDArray[float_]
+    output_derivatives: NDArray[float_]
+    def calculate_values(self) -> None: ...
+    def calculate_derivatives(self, __idx_input: int) -> None: ...
+    # algorithm-specific requirements:
+    nmb_layers: int
+    nmb_neurons: NDArray[float_]
+    weights_input: NDArray[float_]
+    weights_output: NDArray[float_]
+    weights_hidden: NDArray[float_]
+    intercepts_hidden: NDArray[float_]
+    intercepts_output: NDArray[float_]
+    activation: NDArray[float_]
+    neurons: NDArray[float_]
