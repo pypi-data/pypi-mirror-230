59464f59c45f6994e64a91fd88d11e79b6f9da5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:08:59 +0000

    Add package mkdocs-nav-weight/0.0.9/mkdocs-nav-weight-0.0.9.tar.gz

---
+++ packages/mkdocs-nav-weight/mkdocs-nav-weight-0.0.9.tar.gz/mkdocs-nav-weight-0.0.9/mkdocs_nav_weight/__init__.py
@@ -0,0 +1,19 @@
+from numbers import Number
+import mkdocs
+from mkdocs_nav_weight.util import Util
+
+
+class MkDocsNavWeight(mkdocs.plugins.BasePlugin):
+
+    config_scheme = (
+        ('section_renamed', mkdocs.config.config_options.Type(bool, default=False)),
+        ('index_weight', mkdocs.config.config_options.Type(Number, default=-10)),
+        ('warning', mkdocs.config.config_options.Type(bool, default=True)),
+        ('reverse', mkdocs.config.config_options.Type(bool, default=False)),
+        ('headless_included', mkdocs.config.config_options.Type(bool, default=False)),
+    )
+
+    def on_nav(self, nav, config, files, **kwargs):
+        util = Util(self.config, nav.items, nav.pages, config)
+        util.set_nav()
+        return nav
