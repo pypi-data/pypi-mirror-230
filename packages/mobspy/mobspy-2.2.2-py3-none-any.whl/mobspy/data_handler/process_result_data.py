d6b6964c005b68ff29e6a3da296f48c66d0131cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 04:07:53 +0000

    Add package mobspy/2.2.2/mobspy-2.2.2-py3-none-any.whl

---
+++ packages/mobspy/mobspy-2.2.2-py3-none-any.whl/mobspy/data_handler/process_result_data.py
@@ -0,0 +1,61 @@
+"""
+    mobspy.data_handler.process_result_data.py
+
+    Handles converting the output data from a simulation into desired-units or concentration
+"""
+from mobspy.modules.mobspy_expressions import u
+from scipy.constants import N_A
+from copy import deepcopy
+
+
+def convert_data_to_desired_unit(data, unit_x=None, unit_y=None, output_concentration=False, volume=1):
+    """Converts the simulation output data from the MobsPy standard units
+    to the desired units specified by the user
+
+    :param data: (dict) resulting data from a MobsPy simulation execution
+    :param unit_x: (str) unit that the user desires the time in, defaults to dimensionless (None)
+    :param unit_y: (str) unit that the user desires the y axis to be in (Concentration or counts)
+    :param output_concentration: (bool) decide if output should be a concentration or count
+    :param volume: (float) - volume set as parameter to the simulation
+    :return: converted_data - input data converted to the desired units
+    :rtype: (dict) Dictionary meta-species as key and run as value
+    """
+    ur = u.unit_registry_object
+    converted_data = deepcopy(data)
+
+    if unit_x is not None:
+        new_time = []
+        for time in data['Time']:
+            quantity = time*ur.seconds
+            new_time.append(quantity.to(unit_x).magnitude)
+        converted_data['Time'] = new_time
+
+    def multiply_data_by_factor(data, factor):
+        for key in data:
+            if key == 'Time':
+                continue
+
+            converted_data[key] = [count*factor for count in data[key]]
+
+    def convert_data(data, from_unit, to_unit):
+        for key in data:
+            if key == 'Time':
+                continue
+
+            converted_data[key] = [(count*from_unit).to(to_unit).magnitude for count in data[key]]
+
+    if output_concentration:
+        multiply_data_by_factor(converted_data, volume ** -1)
+
+    if unit_y is not None:
+        if 'mol' in unit_y:
+            multiply_data_by_factor(converted_data, N_A ** -1)
+            if output_concentration:
+                convert_data(converted_data, ur.molar, unit_y)
+            else:
+                convert_data(converted_data, ur.moles, unit_y)
+
+    return converted_data
+
+
+
---
+++ packages/mobspy/mobspy-2.2.2-py3-none-any.whl/mobspy/parameter_scripts/parameter_reader.py
@@ -0,0 +1,93 @@
+"""
+    This module is responsible for processing the parameters given to MobsPy before starting the simulation
+"""
+import json
+import sys
+from datetime import datetime
+import os
+from pathlib import Path
+import numpy as np
+import mobspy.simulation_logging.log_scripts as simlog
+from pint import Quantity
+
+
+def read_json(json_file_name):
+    """
+        Reads json file
+
+        :param plot_json_filename: json file name
+
+        :raise simlog.error: If the file was not able to be read
+
+        :return: plot parameter dictionary
+    """
+    with open(json_file_name, 'r') as file:
+        try:
+            json_data = json.load(file)
+        except json.decoder.JSONDecodeError as e:
+            simlog.error('Error reading file')
+            exit(1)
+
+    return json_data
+
+
+def __name_output_file(params):
+    """
+        Gives a name to the output file - just date time in case the user has not specified one
+
+        :param params: (dict) Dictionary with simulation parameters
+    """
+
+    file_name = "r_"
+    file_name += str(datetime.now()) + '.json'
+    params["absolute_output_file"] = params["output_dir"] + file_name
+
+
+def __check_stochastic_repetitions_seeds(params):
+    """
+        The list of seeds must be equal to the number of repetitions specified
+
+        :param params (dict) = Dictionary with simulation parameters
+    """
+    if 'seeds' in params:
+        try:
+            if params['repetitions'] != len(params['seeds']):
+                simlog.error('Seeds must be equal to the number of repetitions')
+        except Exception:
+            simlog.error('Parameter seeds must be a list')
+
+
+def __convert_parameters_for_COPASI(params):
+    """
+        Converts parameters units to MobsPy standard units (basiCO needs seconds for simulation duration)
+
+        :param params: (dict) = Dictionary with simulation parameters
+    """
+    for key, p in params.items():
+        if key == 'duration' and isinstance(p, Quantity):
+            if p.dimensionality != '[time]':
+                simlog.error('The duration of the simulation is not in units of time')
+
+        if isinstance(p, Quantity):
+            if str(p.dimensionality) == '[time]':
+                params[key] = p.convert('second').magnitude
+                continue
+
+
+def __convert_unit_parameters(params):
+    units = ['unit_x', 'unit_y']
+    for u in units:
+        if u in params and params[u] is not None:
+            if isinstance(params[u], Quantity):
+                params[u] = str(params[u].units)
+            else:
+                params[u] = str(params[u])
+
+
+def parameter_process(params):
+    __convert_unit_parameters(params)
+    __name_output_file(params)
+    __check_stochastic_repetitions_seeds(params)
+    __convert_parameters_for_COPASI(params)
+
+
