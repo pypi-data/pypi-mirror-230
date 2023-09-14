7506c4a1dc960521af386c46da5688754fc8a383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 05:06:04 +0000

    Add package hcai-nova-server/1.0.2/hcai_nova_server-1.0.2-py3-none-any.whl

---
+++ packages/hcai-nova-server/hcai_nova_server-1.0.2-py3-none-any.whl/nova_server/__init__.py
@@ -0,0 +1,10 @@
+# We follow Semantic Versioning (https://semver.org/)
+_MAJOR_VERSION = '1'
+_MINOR_VERSION = '0'
+_PATCH_VERSION = '2'
+
+__version__ = '.'.join([
+    _MAJOR_VERSION,
+    _MINOR_VERSION,
+    _PATCH_VERSION,
+])
