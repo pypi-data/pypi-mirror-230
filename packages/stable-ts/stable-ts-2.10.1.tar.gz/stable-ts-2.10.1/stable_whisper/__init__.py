6d8d53363b7574bcb4d5443156b42c95a2ddcdf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 14:06:20 +0000

    Add package stable-ts/2.10.1/stable-ts-2.10.1.tar.gz

---
+++ packages/stable-ts/stable-ts-2.10.1.tar.gz/stable-ts-2.10.1/stable_whisper/__init__.py
@@ -0,0 +1,7 @@
+from .whisper_word_level import *
+from .result import *
+from .text_output import *
+from .video_output import *
+from .stabilization import visualize_suppression
+from .non_whisper import transcribe_any
+from ._version import __version__
