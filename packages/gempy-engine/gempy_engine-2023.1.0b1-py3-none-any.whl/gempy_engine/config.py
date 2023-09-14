a77afb2400ab50bc8f37cf2c42048e906ea0609e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 16:07:42 +0000

    Add package gempy-engine/2023.1.0b1/gempy_engine-2023.1.0b1-py3-none-any.whl

---
+++ packages/gempy-engine/gempy_engine-2023.1.0b1-py3-none-any.whl/gempy_engine/config.py
@@ -0,0 +1,27 @@
+from importlib.util import find_spec
+from enum import Enum, auto, Flag
+
+
+class AvailableBackends(Flag):
+    numpy = auto()
+    tensorflow = auto()
+    # Legacy
+    aesara = auto()
+    legacy = auto()
+
+
+# ! Careful what we commit here!
+DEBUG_MODE = True
+OPTIMIZE_MEMORY = True
+DEFAULT_BACKEND = AvailableBackends.numpy
+DEFAULT_PYKEOPS = True
+DEFAULT_TENSOR_DTYPE = 'float32'
+LINE_PROFILER_ENABLED = False
+SET_RAW_ARRAYS_IN_SOLUTION = True
+
+is_numpy_installed = find_spec("numpy") is not None
+is_tensorflow_installed = find_spec("tensorflow") is not None
+is_pykeops_installed = find_spec("pykeops") is not None
+
+
+
