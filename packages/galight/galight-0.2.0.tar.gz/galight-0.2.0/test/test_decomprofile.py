41c9936af6386471a810e6967e74a8ccedfc1358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 16:07:24 +0000

    Add package galight/0.2.0/galight-0.2.0.tar.gz

---
+++ packages/galight/galight-0.2.0.tar.gz/galight-0.2.0/test/test_decomprofile.py
@@ -0,0 +1,19 @@
+"""
+Tests for `galight` module.
+"""
+import pytest
+from galight import galight
+
+
+class TestGalight(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
