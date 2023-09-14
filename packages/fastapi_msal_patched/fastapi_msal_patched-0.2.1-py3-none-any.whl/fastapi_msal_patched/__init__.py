ea4a87e8bac0cdab6eb5508dd34874189297ea6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:10:06 +0000

    Add package fastapi_msal_patched/0.2.1/fastapi_msal_patched-0.2.1-py3-none-any.whl

---
+++ packages/fastapi_msal_patched/fastapi_msal_patched-0.2.1-py3-none-any.whl/fastapi_msal_patched/__init__.py
@@ -0,0 +1,11 @@
+"""
+FastAPI/MSAL - The MSAL (Microsoft Authentication Library) plugin for FastAPI!
+
+FastAPI - https://github.com/tiangolo/fastapi
+MSAL for Python - https://github.com/AzureAD/microsoft-authentication-library-for-python
+"""
+
+__version__ = "0.2.1"
+from .core import MSALClientConfig
+from .models import UserInfo, IDTokenClaims
+from .auth import MSALAuthorization
---
+++ packages/fastapi_msal_patched/fastapi_msal_patched-0.2.1-py3-none-any.whl/fastapi_msal_patched/models/common.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from fastapi_msal_patched.core import StrsDict, OptStr
+
+
+class BearerToken(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+    def generate_header(self) -> StrsDict:
+        return {"Authorization": f"{self.token_type} {self.access_token}"}
+
+
+class AuthResponse(BaseModel):
+    state: OptStr = None
+    code: OptStr = None
