94ef19a5e24bb41f8e11ecfac0be7ac805bf648d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 04:06:49 +0000

    Add package siesta-tools/0.1.0/siesta_tools-0.1.0.tar.gz

---
+++ packages/siesta-tools/siesta_tools-0.1.0.tar.gz/siesta_tools-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="Raúl Guerrero",
+    description ="package for siesta utilities",
+    name="siesta_tools",
+    version='0.1.0',
+    packages=find_packages(include=["utilities","utilities.*","electronic_utils","electronic_utils*"]),
+    install_requires=[
+        "pandas>=1.5.3",
+        "numpy>=1.23.5",
+        "ase>=3.22.1",
+        "sisl>=0.13"
+    ],
+    python_requires='>=3.8',
+)
