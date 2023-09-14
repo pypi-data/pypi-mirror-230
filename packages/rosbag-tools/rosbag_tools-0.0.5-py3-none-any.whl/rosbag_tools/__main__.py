f3de620678ee06b3ee9b7ea23a4362ce891292f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 05:05:40 +0000

    Add package rosbag-tools/0.0.5/rosbag_tools-0.0.5-py3-none-any.whl

---
+++ packages/rosbag-tools/rosbag_tools-0.0.5-py3-none-any.whl/rosbag_tools/__main__.py
@@ -0,0 +1,22 @@
+import click
+
+from . import __version__, clip, compute_duration, topic_compare, topic_remove
+
+CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(__version__)
+def cli_main():
+    """A ROS-agnostic toolbox for common rosbag operations"""
+    pass
+
+
+cli_main.add_command(clip)
+cli_main.add_command(compute_duration)
+cli_main.add_command(topic_compare)
+cli_main.add_command(topic_remove)
+
+
+if __name__ == "__main__":
+    cli_main()
