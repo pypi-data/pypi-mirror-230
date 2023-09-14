590c9d8d4d54a6386f6e765e69eca4f11b5e1f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 04:07:32 +0000

    Add package faisslite/0.0.2/faisslite-0.0.2.tar.gz

---
+++ packages/faisslite/faisslite-0.0.2.tar.gz/faisslite-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# 学习了这篇文章：https://zhuanlan.zhihu.com/p/276461821
+# 总结一下就是先写个 setup.py 文件，然后按照下面步骤操作：
+# python3 setup.py sdist
+# python3 setup.py register
+# python3 setup.py sdist upload
+setup(
+    name='faisslite',
+    version='0.0.2',
+    author='may.xiaoya.zhang',
+    author_email='may.xiaoya.zhang@gmail.com',
+    url='https://pypi.org/user/May.xiaoya.zhang/',
+    description='Faiss向量数据库建立',
+    packages=find_packages(),
+    data_files=[
+        ('.', [
+            'document.py',
+            'Faiss.py'
+        ])
+    ]
+)
