011f74473965f3995021fb00858f1f69104dee78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 16:07:34 +0000

    Add package pybaseutils/0.9.2/pybaseutils-0.9.2.tar.gz

---
+++ packages/pybaseutils/pybaseutils-0.9.2.tar.gz/pybaseutils-0.9.2/test_py/converter/convert_labelme2coco.py
@@ -0,0 +1,26 @@
+# -*-coding: utf-8 -*-
+"""
+    @Author : Pan
+    @E-mail : 390737991@qq.com
+    @Date   : 2022-10-12 16:21:55
+    @Brief  :
+"""
+import sys
+import os
+
+sys.path.insert(0, os.getcwd())
+import cv2
+from pybaseutils.converter.convert_labelme2coco import Labelme2COCO
+
+
+def demo_for_voc():
+    image_dir = "/media/PKing/新加卷1/SDK/base-utils/data/person"
+    anno_dir = "/media/PKing/新加卷1/SDK/base-utils/data/person"
+    save_coco_file = os.path.join(os.path.dirname(image_dir), "person.json")
+    build = Labelme2COCO(image_dir=image_dir, anno_dir=anno_dir, init_id=None)
+    build.build_keypoints_dataset()
+    build.save_coco(save_coco_file)
+
+
+if __name__ == '__main__':
+    demo_for_voc()
