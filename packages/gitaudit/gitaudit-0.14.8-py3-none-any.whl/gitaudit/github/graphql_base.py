7cf2fe12926b5b2e00533bd02a7fe3258e12e19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 16:07:25 +0000

    Add package gitaudit/0.14.8/gitaudit-0.14.8-py3-none-any.whl

---
+++ packages/gitaudit/gitaudit-0.14.8-py3-none-any.whl/gitaudit/github/graphql_base.py
@@ -0,0 +1,33 @@
+"""Root GraphQL Class
+"""
+
+from pydantic import BaseModel, root_validator, Extra
+from humps.camel import case
+
+
+class GraphQlBase(BaseModel):
+    """Root Graph QL Class"""
+
+    class Config:
+        """Pydantic Config for GraphQlBase"""
+
+        alias_generator = case
+        allow_population_by_field_name = True
+        populate_by_name = True
+        extra = Extra.forbid
+
+    @root_validator(pre=True)
+    def unwap_nodes(cls, data: dict):  # pylint: disable=no-self-argument
+        """Unwraps <object>.nodes.<items> to <object>.<items>
+
+        Args:
+            data (dict): The to be validated input data
+
+        Returns:
+            dict: The valiated and transformed input data
+        """
+        for key, value in data.items():
+            if isinstance(value, dict) and "nodes" in value:
+                data[key] = value["nodes"]
+
+        return data
