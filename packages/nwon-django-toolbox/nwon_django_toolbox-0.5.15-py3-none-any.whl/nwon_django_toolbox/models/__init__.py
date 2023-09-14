5fa0868512d9e4164007b47ae79936133eb062c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 16:07:29 +0000

    Add package nwon-django-toolbox/0.5.15/nwon_django_toolbox-0.5.15-py3-none-any.whl

---
+++ packages/nwon-django-toolbox/nwon_django_toolbox-0.5.15-py3-none-any.whl/nwon_django_toolbox/models/__init__.py
@@ -0,0 +1,4 @@
+from nwon_django_toolbox.models.all_app_models import all_app_models
+from nwon_django_toolbox.models.nwon_base_model import NWONBaseModel
+
+__all__ = ["NWONBaseModel", "all_app_models"]
