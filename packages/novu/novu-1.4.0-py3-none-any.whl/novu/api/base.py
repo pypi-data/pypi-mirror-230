c1c8a19ce64a7f8f9ecd6bfc61ebb2e20a668830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:06:40 +0000

    Add package novu/1.4.0/novu-1.4.0-py3-none-any.whl

---
+++ packages/novu/novu-1.4.0-py3-none-any.whl/novu/api/base.py
@@ -0,0 +1,94 @@
+"""This module is used to defined an abstract class for all reusable methods to communicate with the Novu API"""
+import copy
+import logging
+import os
+from json.decoder import JSONDecodeError
+from typing import Optional
+
+import requests
+
+from novu.config import NovuConfig
+from novu.helpers import SentryProxy
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Api:  # pylint: disable=R0903
+    """Base class for all API in the Novu client"""
+
+    requests_timeout: int = 5
+    """This field allow you to change the :param:`~requests.request.timeout` params which is used during API calls."""
+
+    session: Optional[requests.Session] = None
+    """This field allow you to use a :class:`~requests.Session` during API calls."""
+
+    def __init__(
+        self,
+        url: Optional[str] = None,
+        api_key: Optional[str] = None,
+        requests_timeout: Optional[int] = None,
+        session: Optional[requests.Session] = None,
+    ) -> None:
+        config = NovuConfig()
+
+        url = url or config.url
+        api_key = api_key or config.api_key
+
+        self._url = url
+        self._headers = {"Authorization": f"ApiKey {api_key}"}
+
+        self.requests_timeout = requests_timeout or int(os.getenv("NOVU_PYTHON_REQUESTS_TIMEOUT", "5"))
+        self.session = session
+
+    def handle_request(
+        self,
+        method: str,
+        url: str,
+        json: Optional[dict] = None,
+        payload: Optional[dict] = None,
+        headers: Optional[dict] = None,
+        **kwargs,
+    ) -> dict:
+        """Handle a request to the API.
+
+        This method can handle all cases of request and is used to authenticate the request and raise
+        an error on bad status.
+
+        Args:
+            method: The HTTP method used during the request (e.g. "POST")
+            url: The URL to reach during the request
+            json: The body to send, in json format. Defaults to None.
+            payload: Params to send, in json format. Defaults to None.
+            headers: Headers to send, in json format. Defaults to None.
+
+        Returns:
+            Return parsed response.
+        """
+        if headers:
+            _headers = copy.deepcopy(self._headers)
+            _headers.update(copy.deepcopy(headers))
+        else:
+            _headers = self._headers
+
+        res = (self.session.request if self.session else requests.request)(
+            method=method,
+            url=url,
+            headers=_headers,
+            json=json,
+            params=payload,
+            timeout=self.requests_timeout,
+            **kwargs,
+        )
+
+        if not res.ok:
+            try:
+                detail = res.json()
+                SentryProxy().set_extra("error_details", detail)
+            except JSONDecodeError:
+                pass
+            res.raise_for_status()
+
+        if res.status_code == 204:
+            return {}
+
+        return res.json()
---
+++ packages/novu/novu-1.4.0-py3-none-any.whl/novu/api/notification_group.py
@@ -0,0 +1,50 @@
+"""
+This module is used to define the ``NotificationGroupApi``, a python wrapper
+to interact with ``NotificationGroup`` in Novu.
+"""
+from typing import Optional
+
+import requests
+
+from novu.api.base import Api
+from novu.constants import NOTIFICATION_GROUPS_ENDPOINT
+from novu.dto.notification_group import (
+    NotificationGroupDto,
+    PaginatedNotificationGroupDto,
+)
+
+
+class NotificationGroupApi(Api):
+    """This class aims to handle all API methods around notification groups in API"""
+
+    def __init__(
+        self,
+        url: Optional[str] = None,
+        api_key: Optional[str] = None,
+        requests_timeout: Optional[int] = None,
+        session: Optional[requests.Session] = None,
+    ) -> None:
+        super().__init__(url=url, api_key=api_key, requests_timeout=requests_timeout, session=session)
+
+        self._notification_group_url = f"{self._url}{NOTIFICATION_GROUPS_ENDPOINT}"
+
+    def list(self) -> PaginatedNotificationGroupDto:
+        """Method to list notification groups
+
+        Returns:
+            Paginated notification groups
+        """
+        return PaginatedNotificationGroupDto.from_camel_case(self.handle_request("GET", self._notification_group_url))
+
+    def create(self, name: str) -> NotificationGroupDto:
+        """Method to create a new notification group
+
+        Args:
+            name: The name of the notification group.
+
+        Returns:
+            The created notification group.
+        """
+        return NotificationGroupDto.from_camel_case(
+            self.handle_request("POST", self._notification_group_url, {"name": name})["data"]
+        )
