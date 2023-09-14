67ed07544463d72d8d34527053b69367c31766d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:06:20 +0000

    Add package libstored/1.6.0/libstored-1.6.0.tar.gz

---
+++ packages/libstored/libstored-1.6.0.tar.gz/libstored-1.6.0/libstored/__init__.py
@@ -0,0 +1,18 @@
+# libstored, distributed debuggable data stores.
+# Copyright (C) 2020-2023  Jochem Rutgers
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from .zmq_server import ZmqServer
+from .zmq_client import ZmqClient
+from .stdio2zmq import Stdio2Zmq
+from .serial2zmq import Serial2Zmq
+from .stream2zmq import Stream2Zmq
+from .csv import CsvExport, generateFilename
+from . import protocol
+from .version import __version__, libstored_version
+
+protocol.registerLayerType(ZmqServer)
+
