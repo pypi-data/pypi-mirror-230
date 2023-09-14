8ba48ea4a7b5b0c026e1a2d78e4b3fe0cc52f7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:10:04 +0000

    Add package teamhack-nmap/0.0.189/teamhack_nmap-0.0.189.tar.gz

---
+++ packages/teamhack-nmap/teamhack_nmap-0.0.189.tar.gz/teamhack_nmap-0.0.189/src/teamhack_nmap/server.py
@@ -0,0 +1,33 @@
+from tempfile import NamedTemporaryFile
+from flask import Flask, request
+from subprocess import run, PIPE
+from os import unlink
+
+def portscan_daemon(file):
+  # /usr/bin/env
+  p = run(['nmap', '-A', '-sV', '--script=vulners/vulners.nse', '-p-', '-iL', '-', '-oX', '-'], stdin=file, stdout=PIPE)
+  p.check_returncode()
+  return p.stdout
+
+def create_app():
+  app = Flask(__name__)
+
+  @app.route('/upload', methods=['PUT'])
+  def upload():
+    file = NamedTemporaryFile()
+    try:
+      file.write(request.get_data())
+      file.flush()
+      return portscan_daemon(file), 200
+    finally:
+      file.close()
+      #unlink(file.name)
+
+  return app
+
+def start_server(host="0.0.0.0", port=55432, *args, **kwargs):
+  app = create_app(**kwargs)
+  app.run(debug=True, host=host, port=port, *args)
+
+# https://www.easydevguide.com/posts/curl_upload_flask
+
