b838c61a874850746d490315db2b1f3a459ec8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 04:07:28 +0000

    Add package oblate/1.1.0/oblate-1.1.0-py3-none-any.whl

---
+++ packages/oblate/oblate-1.1.0-py3-none-any.whl/oblate/__init__.py
@@ -0,0 +1,19 @@
+"""
+Oblate
+~~~~~~
+
+Python library for data validation.
+
+Copyright (C) Izhar Ahmad 2023, licensed under MIT license.
+See LICENSE at project's root for more information.
+"""
+
+__version__ = '1.1.0'
+__author__  = 'Izhar Ahmad (izxxr)'
+
+from oblate import fields as fields
+from oblate import validate as validate
+from oblate.schema import *
+from oblate.contexts import *
+from oblate.exceptions import *
+from oblate.configs import *
