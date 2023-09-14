c3b32e103af0506c8409f374f1c8e449193d9814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 15:08:17 +0000

    Add package strada/0.1.2/strada-0.1.2.tar.gz

---
+++ packages/strada/strada-0.1.2.tar.gz/strada-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="strada",
+    version="0.1.2",
+    packages=find_packages(where="strada"),
+    package_dir={"": "strada"},
+    install_requires=[
+        # your dependencies here
+        "google-api-python-client",
+        "google-auth",
+        "google-auth-oauthlib",
+        "google-auth-httplib2",
+    ],
+    author="Strada",
+    description="Strada SDK",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
