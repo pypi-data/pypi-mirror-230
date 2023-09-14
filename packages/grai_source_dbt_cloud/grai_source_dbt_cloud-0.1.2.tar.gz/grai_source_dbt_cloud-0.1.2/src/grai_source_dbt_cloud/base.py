7bb1908a871f1824d4d03d0e372d3d44f30f2509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 05:05:39 +0000

    Add package grai_source_dbt_cloud/0.1.2/grai_source_dbt_cloud-0.1.2.tar.gz

---
+++ packages/grai_source_dbt_cloud/grai_source_dbt_cloud-0.1.2.tar.gz/grai_source_dbt_cloud-0.1.2/src/grai_source_dbt_cloud/base.py
@@ -0,0 +1,44 @@
+from typing import List, Optional, Tuple
+
+from grai_client.integrations.base import EventMixin
+from grai_schemas.integrations.base import GraiIntegrationImplementation
+from grai_schemas.base import SourcedEdge, SourcedNode, Event
+from grai_schemas.v1.source import SourceV1
+from grai_source_dbt_cloud.loader import DbtCloudConnector
+from functools import cached_property
+
+
+class DbtCloudIntegration(EventMixin, GraiIntegrationImplementation):
+    def __init__(
+        self,
+        api_key: str,
+        source: SourceV1,
+        version: Optional[str] = None,
+        namespace: Optional[str] = "default",
+    ):
+        super().__init__(source, version)
+
+        self.connector = DbtCloudConnector(
+            namespace=namespace,
+            api_key=api_key,
+            source=source,
+        )
+
+    @cached_property
+    def get_nodes_and_edges(self) -> Tuple[List[SourcedNode], List[SourcedEdge]]:
+        nodes, edges = self.connector.get_nodes_and_edges()
+        return nodes, edges
+
+    def nodes(self) -> List[SourcedNode]:
+        return self.get_nodes_and_edges[0]
+
+    def edges(self) -> List[SourcedEdge]:
+        return self.get_nodes_and_edges[1]
+
+    def events(self, last_event_date: Optional[str]) -> List[Event]:
+        events = self.connector.get_events(last_event_date=last_event_date)
+        return events
+
+    def ready(self) -> bool:
+        _ = self.connector.default_account
+        return True
