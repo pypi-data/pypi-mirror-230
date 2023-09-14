95db8fef81ff2554a67bc0a2c2d5ff9aa17f3c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:07:53 +0000

    Add package cmdcomp/2.3.3/cmdcomp-2.3.3-py3-none-any.whl

---
+++ packages/cmdcomp/cmdcomp-2.3.3-py3-none-any.whl/cmdcomp/v2/command/argument/file_argument.py
@@ -0,0 +1,25 @@
+from typing import Annotated, Literal
+
+from pydantic import Field
+
+from cmdcomp.model import Model
+from cmdcomp.v2.mixin.has_alias import HasAlias
+
+
+class V2FileArgument(HasAlias, Model):
+    type: Literal["file"]
+
+    description: Annotated[
+        str | None,
+        Field(title="description of the argument."),
+    ] = None
+
+    alias: Annotated[
+        str | list[str] | None,
+        Field(title="alias of the argument."),
+    ] = None
+
+    base_path: Annotated[
+        str | None,
+        Field(title="path of the directory from which to base filename completion."),
+    ] = None
