4289579c763b0b0d2ebbee21e104b72fd550d069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 16:08:29 +0000

    Add package conntextual/0.3.2/conntextual-0.3.2-py3-none-any.whl

---
+++ packages/conntextual/conntextual-0.3.2-py3-none-any.whl/conntextual/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.3
+# hash=a41a4835dc612466a8a7f5f175830f13
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A network-application TUI using textual."
+PKG_NAME = "conntextual"
+VERSION = "0.3.2"
