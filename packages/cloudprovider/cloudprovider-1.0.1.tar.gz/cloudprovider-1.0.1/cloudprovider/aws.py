729111d7e239b03a8886da75e7e831eaa89406fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:06:38 +0000

    Add package cloudprovider/1.0.1/cloudprovider-1.0.1.tar.gz

---
+++ packages/cloudprovider/cloudprovider-1.0.1.tar.gz/cloudprovider-1.0.1/cloudprovider/aws.py
@@ -0,0 +1,77 @@
+import boto3
+
+class AWSResourceManager:
+    def __init__(self, region,aws_access,aws_secret):
+        self.region = region
+        self.aws_access=aws_access
+        self.aws_secret=aws_secret
+        self.ec2 = self._configure_aws_credentials(region)
+
+    def _configure_aws_credentials(self, region):
+        try:
+            session = boto3.Session(
+                region_name=region,
+                aws_access_key_id=self.aws_access,
+                aws_secret_access_key=self.aws_secret,
+                aws_session_token=None  # If using temporary session credentials
+            )
+            ec2_client = session.client('ec2')
+            return ec2_client
+        except Exception as e:
+            print(f"Error configuring AWS credentials: {str(e)}")
+            return None
+
+    def create_instance(self, instance_name):
+        try:
+            response = self.ec2.run_instances(
+                ImageId='ami-0a0c8eebcdd6dcbd0',  # Replace with a valid AMI ID
+                InstanceType='t2.micro',
+                MinCount=1,
+                MaxCount=1,
+                TagSpecifications=[
+                    {
+                        'ResourceType': 'instance',
+                        'Tags': [
+                            {
+                                'Key': 'Name',
+                                'Value': instance_name
+                            },
+                        ]
+                    },
+                ]
+            )
+            instance_id = response['Instances'][0]['InstanceId']
+            return instance_id
+        except Exception as e:
+            print(f"Error creating EC2 instance: {str(e)}")
+            return None
+
+    def terminate_instance(self, instance_id):
+        try:
+            self.ec2.terminate_instances(InstanceIds=[instance_id])
+            print(f"EC2 instance {instance_id} terminated")
+        except Exception as e:
+            print(f"Error terminating EC2 instance: {str(e)}")
+
+    def update_instance(self, instance_id, new_instance_type):
+        try:
+            self.ec2.modify_instance_attribute(
+                InstanceId=instance_id,
+                InstanceType={'Value': new_instance_type}
+            )
+            print(f"EC2 instance {instance_id} updated with instance type: {new_instance_type}")
+        except Exception as e:
+            print(f"Error updating EC2 instance: {str(e)}")
+
+    def read_instance(self, instance_id):
+        try:
+            response = self.ec2.describe_instances(InstanceIds=[instance_id])
+            if 'Reservations' in response and response['Reservations']:
+                instance = response['Reservations'][0]['Instances'][0]
+                return instance
+            else:
+                print(f"EC2 instance {instance_id} not found.")
+                return None
+        except Exception as e:
+            print(f"Error reading EC2 instance: {str(e)}")
+            return None
---
+++ packages/cloudprovider/cloudprovider-1.0.1.tar.gz/cloudprovider-1.0.1/tests/test_aws.py
@@ -0,0 +1,49 @@
+import unittest
+from my_cloud_lib.aws import AWSResourceManager
+from my_cloud_lib.azure import AzureResourceManager
+
+class TestCloudResourceManagement(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Initialize AWS and Azure resource managers
+        cls.aws_manager = AWSResourceManager(region='us-east-1')
+        cls.azure_manager = AzureResourceManager(subscription_id='your-subscription-id')
+
+    def test_aws_instance_lifecycle(self):
+        # Test AWS EC2 instance creation
+        instance_name = 'my-aws-instance'
+        instance_id = self.aws_manager.create_instance(instance_name)
+        self.assertIsNotNone(instance_id)
+
+        # Test AWS EC2 instance update
+        new_instance_type = 't2.small'
+        self.aws_manager.update_instance(instance_id, new_instance_type)
+        updated_instance = self.aws_manager.read_instance(instance_id)
+        self.assertEqual(updated_instance['InstanceType'], new_instance_type)
+
+        # Test AWS EC2 instance termination
+        self.aws_manager.terminate_instance(instance_id)
+        terminated_instance = self.aws_manager.read_instance(instance_id)
+        self.assertIsNone(terminated_instance)
+
+    def test_azure_vm_lifecycle(self):
+        # Test Azure VM creation
+        vm_name = 'my-azure-vm'
+        resource_group_name = 'my-resource-group'
+        location = 'eastus'
+        created_vm_name = self.azure_manager.create_vm(vm_name, resource_group_name, location)
+        self.assertEqual(created_vm_name, vm_name)
+
+        # Test Azure VM update
+        new_vm_size = 'Standard_DS2_v2'
+        self.azure_manager.update_vm(vm_name, resource_group_name, new_vm_size)
+        updated_vm = self.azure_manager.read_vm(vm_name, resource_group_name)
+        self.assertEqual(updated_vm.hardware_profile.vm_size, new_vm_size)
+
+        # Test Azure VM deletion
+        self.azure_manager.delete_vm(vm_name, resource_group_name)
+        deleted_vm = self.azure_manager.read_vm(vm_name, resource_group_name)
+        self.assertIsNone(deleted_vm)
+
+if __name__ == '__main__':
+    unittest.main()
