d6566869468d266e7583a690df96f75737b954dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 04:07:28 +0000

    Add package microsoft-kiota-abstractions/0.8.2/microsoft_kiota_abstractions-0.8.2.tar.gz

---
+++ packages/microsoft-kiota-abstractions/microsoft_kiota_abstractions-0.8.2.tar.gz/microsoft_kiota_abstractions-0.8.2/kiota_abstractions/api_error.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import Dict, Optional
+
+
+@dataclass
+class APIError(Exception):
+    """The base class for all API errors."""
+
+    message: Optional[str] = None
+    response_status_code: Optional[int] = None
+    response_headers: Optional[Dict[str, str]] = None
+
+    def __str__(self) -> str:
+        return f"""APIError {self.response_status_code}: {self.message} {getattr('error', '')}"""
