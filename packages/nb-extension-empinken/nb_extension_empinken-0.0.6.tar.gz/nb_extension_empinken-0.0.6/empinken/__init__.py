f33713a17acea20306c4b37c9da58d2af893d1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 16:07:50 +0000

    Add package nb-extension-empinken/0.0.6/nb_extension_empinken-0.0.6.tar.gz

---
+++ packages/nb-extension-empinken/nb_extension_empinken-0.0.6.tar.gz/nb_extension_empinken-0.0.6/empinken/__init__.py
@@ -0,0 +1,14 @@
+import pkg_resources
+
+__version__ = '0.0.6'
+#__version__ = pkg_resources.require("empinken")[0].version
+#version_info = pkg_resources.parse_version(__version__)
+
+def _jupyter_nbextension_paths():
+    return [dict(section="notebook",
+                 src="static",
+                 dest="empinken",
+                 require="empinken/index")]
+
+def load_jupyter_server_extension(nbapp):
+    nbapp.log.info("empinken enabled!")
