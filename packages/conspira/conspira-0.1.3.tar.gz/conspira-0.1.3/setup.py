b5ece538289566b94e1abfc3939c2678469b931a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 15:08:17 +0000

    Add package conspira/0.1.3/conspira-0.1.3.tar.gz

---
+++ packages/conspira/conspira-0.1.3.tar.gz/conspira-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='conspira',
+    version='0.1.3',
+    author='Meng Xiao',
+    description='This is a package for conspiratory theory content identification.',
+    packages=find_packages(),
+    install_requires=[
+        'jieba',
+        'scikit-learn',
+    ],
+    package_data={'conspira': ['resources/*']},
+    
+)
