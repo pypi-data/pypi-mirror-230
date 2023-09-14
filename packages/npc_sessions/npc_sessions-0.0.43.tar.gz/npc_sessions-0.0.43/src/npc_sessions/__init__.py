502c48ce90bb6b2ba028f9bb535f19b596ce14b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:06:51 +0000

    Add package npc_sessions/0.0.43/npc_sessions-0.0.43.tar.gz

---
+++ packages/npc_sessions/npc_sessions-0.0.43.tar.gz/npc_sessions-0.0.43/src/npc_sessions/__init__.py
@@ -0,0 +1,14 @@
+import dotenv
+from npc_lims import tracked
+from npc_session import *
+
+from npc_sessions.sessions import *
+from npc_sessions.trials import *
+from npc_sessions.utils import *
+
+_ = dotenv.load_dotenv(
+    dotenv.find_dotenv(usecwd=True)
+)  # take environment variables from .env
+
+Session = DynamicRoutingSession  # temp alias for backwards compatibility
+sessions = [DynamicRoutingSession(info.session) for info in tracked]
