8d8396b58db277f12b1bf9c7d7e223831cb9120f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 14:05:48 +0000

    Add package sqlmesh/0.31.2/sqlmesh-0.31.2-py3-none-any.whl

---
+++ packages/sqlmesh/sqlmesh-0.31.2-py3-none-any.whl/sqlmesh/core/model/seed.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import typing as t
+import zlib
+from io import StringIO
+from pathlib import Path
+
+import pandas as pd
+from sqlglot import exp
+from sqlglot.optimizer.normalize_identifiers import normalize_identifiers
+
+from sqlmesh.utils.pandas import columns_to_types_from_df
+from sqlmesh.utils.pydantic import PydanticModel
+
+
+class Seed(PydanticModel):
+    """Represents content of a seed.
+
+    Presently only CSV format is supported.
+    """
+
+    content: str
+    dialect: str = ""
+    _df: t.Optional[pd.DataFrame] = None
+
+    @property
+    def columns_to_types(self) -> t.Dict[str, exp.DataType]:
+        return columns_to_types_from_df(self._get_df())
+
+    @property
+    def column_hashes(self) -> t.Dict[str, str]:
+        df = self._get_df()
+        return {
+            column_name: str(zlib.crc32(df[column_name].to_json().encode("utf-8")))
+            for column_name in df.columns
+        }
+
+    def read(self, batch_size: t.Optional[int] = None) -> t.Generator[pd.DataFrame, None, None]:
+        df = self._get_df()
+
+        batch_size = batch_size or df.size
+        batch_start = 0
+        while batch_start < df.shape[0]:
+            yield df.iloc[batch_start : batch_start + batch_size, :]
+            batch_start += batch_size
+
+    def _get_df(self) -> pd.DataFrame:
+        if self._df is None:
+            self._df = pd.read_csv(
+                StringIO(self.content),
+                index_col=False,
+                on_bad_lines="error",
+            )
+            self._df = self._df.rename(
+                columns={
+                    col: normalize_identifiers(col, dialect=self.dialect).name
+                    for col in self._df.columns
+                },
+            )
+
+        return self._df
+
+
+def create_seed(path: str | Path, dialect: t.Optional[str] = "") -> Seed:
+    with open(Path(path), "r") as fd:
+        return Seed(content=fd.read(), dialect=dialect)
