966d3bb68d8e335c3f22cf4d62dbf53eaf517ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 14:06:02 +0000

    Add package mct-nightly/1.9.0.20230914.post349/mct_nightly-1.9.0.20230914.post349-py3-none-any.whl

---
+++ packages/mct-nightly/mct_nightly-1.9.0.20230914.post349-py3-none-any.whl/model_compression_toolkit/data_generation/pytorch/constants.py
@@ -0,0 +1,22 @@
+# Copyright 2023 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+# Axis indices for tensor dimensions
+BATCH_AXIS, CHANNEL_AXIS, H_AXIS, W_AXIS = 0, 1, 2, 3
+
+# Default initial learning rate constant.
+DEFAULT_PYTORCH_INITIAL_LR = 5
+
+# Default output loss multiplier.
+DEFAULT_PYTORCH_OUTPUT_LOSS_MULTIPLIER = 1e-4
