d7d1c0a6d2a6f48bc8a11d6073bb3197c5dfc8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:06:50 +0000

    Add package medcom-ble/0.1.0/medcom_ble-0.1.0.tar.gz

---
+++ packages/medcom-ble/medcom_ble-0.1.0.tar.gz/medcom_ble-0.1.0/medcom_ble/__init__.py
@@ -0,0 +1,8 @@
+"""Parser for Medcom BLE radiation monitors."""
+from __future__ import annotations
+
+from .parser import MedcomBleDevice, MedcomBleDeviceData
+
+__version__ = "0.1.0"
+
+__all__ = ["MedcomBleDeviceData", "MedcomBleDevice"]
