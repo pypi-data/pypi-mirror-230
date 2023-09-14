96270a49dcb7fb9e3c369477ec4114599f65edb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:09:24 +0000

    Add package pyday/0.0.25/pyday-0.0.25.tar.gz

---
+++ packages/pyday/pyday-0.0.25.tar.gz/pyday-0.0.25/pyday/basic/config.py
@@ -0,0 +1,40 @@
+import os
+
+# 獲取 目前腳本pyday所在目錄的父目錄的絕對路徑
+basedir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+worKdir = os.getcwd()
+
+# 字體的絕對路徑
+font_path = os.path.join(basedir, "basic/font")
+Noto_Sans_TC = os.path.join(font_path, "Noto_Sans_TC")
+Noto_Sans_SC = os.path.join(font_path, "Noto_Sans_SC")
+
+font_otf = {
+    "Noto_Sans_TC_Black": os.path.join(Noto_Sans_TC, "NotoSansTC-Black.otf"),
+    "Noto_Sans_TC_Bold": os.path.join(Noto_Sans_TC, "NotoSansTC-Bold.otf"),
+    "Noto_Sans_TC_Light": os.path.join(Noto_Sans_TC, "NotoSansTC-Light.otf"),
+    "Noto_Sans_TC_Medium": os.path.join(Noto_Sans_TC, "NotoSansTC-Medium.otf"),
+    "Noto_Sans_TC_Regular": os.path.join(Noto_Sans_TC, "NotoSansTC-Regular.otf"),
+    "Noto_Sans_TC_Thin": os.path.join(Noto_Sans_TC, "NotoSansTC-Thin.otf"),
+    "Noto_Sans_SC_Black": os.path.join(Noto_Sans_SC, "NotoSansSC-Black.otf"),
+    "Noto_Sans_SC_Bold": os.path.join(Noto_Sans_SC, "NotoSansSC-Bold.otf"),
+    "Noto_Sans_SC_Light": os.path.join(Noto_Sans_SC, "NotoSansSC-Light.otf"),
+    "Noto_Sans_SC_Medium": os.path.join(Noto_Sans_SC, "NotoSansSC-Medium.otf"),
+    "Noto_Sans_SC_Regular": os.path.join(Noto_Sans_SC, "NotoSansSC-Regular.otf"),
+    "Noto_Sans_SC_Thin": os.path.join(Noto_Sans_SC, "NotoSansSC-Thin.otf"),
+}
+
+font_ttf = {
+    "Noto_Sans_TC_Black": os.path.join(Noto_Sans_TC, "NotoSansTC-Black.ttf"),
+    "Noto_Sans_TC_Bold": os.path.join(Noto_Sans_TC, "NotoSansTC-Bold.ttf"),
+    "Noto_Sans_TC_Light": os.path.join(Noto_Sans_TC, "NotoSansTC-Light.ttf"),
+    "Noto_Sans_TC_Medium": os.path.join(Noto_Sans_TC, "NotoSansTC-Medium.ttf"),
+    "Noto_Sans_TC_Regular": os.path.join(Noto_Sans_TC, "NotoSansTC-Regular.ttf"),
+    "Noto_Sans_TC_Thin": os.path.join(Noto_Sans_TC, "NotoSansTC-Thin.ttf"),
+    "Noto_Sans_SC_Black": os.path.join(Noto_Sans_SC, "NotoSansSC-Black.ttf"),
+    "Noto_Sans_SC_Bold": os.path.join(Noto_Sans_SC, "NotoSansSC-Bold.ttf"),
+    "Noto_Sans_SC_Light": os.path.join(Noto_Sans_SC, "NotoSansSC-Light.ttf"),
+    "Noto_Sans_SC_Medium": os.path.join(Noto_Sans_SC, "NotoSansSC-Medium.ttf"),
+    "Noto_Sans_SC_Regular": os.path.join(Noto_Sans_SC, "NotoSansSC-Regular.ttf"),
+    "Noto_Sans_SC_Thin": os.path.join(Noto_Sans_SC, "NotoSansSC-Thin.ttf"),
+}
