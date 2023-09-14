951b870064d2ee811f4a2c00e5fc2e85651d67d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 16:07:44 +0000

    Add package sumbeanTest/0.0.1/sumbeanTest-0.0.1.tar.gz

---
+++ packages/sumbeanTest/sumbeanTest-0.0.1.tar.gz/sumbeanTest-0.0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(name='sumbeanTest',# 包名
+      version='0.0.1',
+      description='测试',
+      author='sumbean',
+      author_email='1041267952@qq.com',
+      packages=setuptools.find_packages(),
+      ) 
