02e6d3eaa4294518d7333c2345be6189c5033f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 05:05:13 +0000

    Add package gbdf-probability/0.1/gbdf_probability-0.1.tar.gz

---
+++ packages/gbdf-probability/gbdf_probability-0.1.tar.gz/gbdf_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gbdf_probability',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['gbdf_probability'],
+      zip_safe=False)
