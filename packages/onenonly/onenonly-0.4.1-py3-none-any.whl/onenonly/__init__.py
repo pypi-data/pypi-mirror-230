d3649ef7183980ad336ad9056f244037c31df216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 04:06:45 +0000

    Add package onenonly/0.4.1/onenonly-0.4.1-py3-none-any.whl

---
+++ packages/onenonly/onenonly-0.4.1-py3-none-any.whl/onenonly/__init__.py
@@ -0,0 +1,31 @@
+r"""
+One-n-Only, a library for Python
+================================
+
+Modules:
+-------
+`areas`
+`array`
+`binary`
+`const`
+`data_toolkit`
+`learn` >> `learn.preprocessing`
+`LinkedList`
+`List`
+`maths`
+`matrix`
+`number`
+`perimeter`
+`physics`
+`queue`
+`search`
+`sort`
+`stack`
+`stats`
+`utils`
+`vector`
+"""
+
+__version__ = "0.4.1"
+AUTHOR = "Sahil Rajwar"
+HOMEPAGE = "https://github.com/Sahil-Rajwar-2004/One-n-Only"
