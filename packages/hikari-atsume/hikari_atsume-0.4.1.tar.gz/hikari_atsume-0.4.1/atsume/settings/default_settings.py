f61791bd0da14a21bced53dfa316f8b2d7b7fd79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 14:05:59 +0000

    Add package hikari-atsume/0.4.1/hikari_atsume-0.4.1.tar.gz

---
+++ packages/hikari-atsume/hikari_atsume-0.4.1.tar.gz/hikari_atsume-0.4.1/atsume/settings/default_settings.py
@@ -0,0 +1,28 @@
+import hikari
+from atsume.settings.type_hints import *
+
+COMPONENTS = []
+
+COMPONENT_PERMISSIONS_CLASS = None
+
+COMPONENT_ALL_GUILDS_PERMISSIONS = []
+
+COMPONENT_DM_PERMISSIONS = []
+
+COMPONENT_GUILD_PERMISSIONS = {}
+
+MIDDLEWARE = []
+
+HIKARI_LOGGING = False
+
+INTENTS = hikari.Intents.ALL_UNPRIVILEGED
+
+MESSAGE_PREFIX = None
+
+DATABASE_URL = "sqlite:///db.sqlite"
+
+DEBUG = False
+
+GLOBAL_COMMANDS = False
+
+DISABLE_UVLOOP = False
