80925bb681033f979d81f986668fe7d4433b9c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 14:07:28 +0000

    Add package ATTools/0.1.8/ATTools-0.1.8.tar.gz

---
+++ packages/ATTools/ATTools-0.1.8.tar.gz/ATTools-0.1.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ATTools",
+    version="0.1.8",
+    packages=["ATTools"],
+    install_requires=[
+        'asyncio',
+        'aiohttp',
+        'tonconnect',
+        'dedust',
+        'xjet',
+        'tontools',     
+    ],
+    project_urls={
+        'Documentation': 'https://github.com/BEWARETHEDEAD/ATTools/tree/main'
+    },
+    python_requires='>=3.7',
+)
