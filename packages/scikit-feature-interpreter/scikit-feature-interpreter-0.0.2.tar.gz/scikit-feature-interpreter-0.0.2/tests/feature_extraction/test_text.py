a2094b05eee642dfd806ba843bb85134925469e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:05:50 +0000

    Add package scikit-feature-interpreter/0.0.2/scikit-feature-interpreter-0.0.2.tar.gz

---
+++ packages/scikit-feature-interpreter/scikit-feature-interpreter-0.0.2.tar.gz/scikit-feature-interpreter-0.0.2/tests/feature_extraction/test_text.py
@@ -0,0 +1,35 @@
+from skinterpret.feature_extraction.text import TfidfInterpreter
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+CORPUS = [
+    "This is the first document.",
+    "This document is the second document.",
+    "And this is the third one.",
+    "Is this the first document?",
+]
+
+
+def test_can_create_from_fitted_vectorizer():
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(CORPUS)
+
+    interpreter = TfidfInterpreter(vectorizer)
+
+    assert interpreter.vectorizer is vectorizer
+
+
+def test_interpret():
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(CORPUS)
+
+    interpreter = TfidfInterpreter(vectorizer)
+    actual = interpreter.interpret(CORPUS[0])
+
+    expected = [
+        ("first", 0.5802858236844359),
+        ("document", 0.46979138557992045),
+        ("this", 0.38408524091481483),
+        ("the", 0.38408524091481483),
+        ("is", 0.38408524091481483),
+    ]
+    assert actual == expected
