446276959f4ae23c6c452632a1df933d4c87aad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 14:06:32 +0000

    Add package pollination-annual-irradiance/0.4.3/pollination-annual-irradiance-0.4.3.tar.gz

---
+++ packages/pollination-annual-irradiance/pollination-annual-irradiance-0.4.3.tar.gz/pollination-annual-irradiance-0.4.3/tests/validation_test.py
@@ -0,0 +1,8 @@
+from pollination.annual_irradiance.entry import AnnualIrradianceEntryPoint
+from queenbee.recipe.dag import DAG
+
+
+def test_annual_irradiance():
+    recipe = AnnualIrradianceEntryPoint().queenbee
+    assert recipe.name == 'annual-irradiance-entry-point'
+    assert isinstance(recipe, DAG)
