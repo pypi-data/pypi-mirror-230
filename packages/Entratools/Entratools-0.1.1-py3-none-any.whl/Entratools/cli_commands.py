f7029f59148ec5f1e9020ca56a4febf727c1f385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 05:05:44 +0000

    Add package Entratools/0.1.1/Entratools-0.1.1-py3-none-any.whl

---
+++ packages/Entratools/Entratools-0.1.1-py3-none-any.whl/Entratools/cli_commands.py
@@ -0,0 +1,11 @@
+# Utils/utils/cli_commands.py
+import argparse
+
+def cli_command():
+    # Define your CLI command using argparse
+    parser = argparse.ArgumentParser(description='Description of your command')
+    # Add arguments and options
+    parser.add_argument('--option', help='Description of an option')
+    args = parser.parse_args()
+    print("it worked!")
+    
