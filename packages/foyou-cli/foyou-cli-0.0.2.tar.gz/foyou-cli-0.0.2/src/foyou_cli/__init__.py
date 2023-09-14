9339a6f0aff70ce4c39bada76c6cb87a102db984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 03:09:37 +0000

    Add package foyou-cli/0.0.2/foyou-cli-0.0.2.tar.gz

---
+++ packages/foyou-cli/foyou-cli-0.0.2.tar.gz/foyou-cli-0.0.2/src/foyou_cli/__init__.py
@@ -0,0 +1,13 @@
+__title__ = 'foyou-cli'
+__author__ = 'foyoux'
+__version__ = '0.0.2'
+
+import fire
+
+from .ebook import add_nav_for_epub
+
+
+def main():
+    fire.Fire({
+        'epub': add_nav_for_epub,
+    })
