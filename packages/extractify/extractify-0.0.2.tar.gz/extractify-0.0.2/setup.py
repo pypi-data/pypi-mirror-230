197c249f401ef23c9cf4b0834f08ee7b647bd58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 05:06:10 +0000

    Add package extractify/0.0.2/extractify-0.0.2.tar.gz

---
+++ packages/extractify/extractify-0.0.2.tar.gz/extractify-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the content of README.md
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as f:
+    readme = f.read()
+
+setup(
+    name="extractify",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[
+        "tika",
+        "openpyxl",
+        "argparse"
+    ],
+    entry_points={
+        "console_scripts": [
+            "extractify = extractify.main:main"
+        ]
+    },
+    long_description=readme,
+    long_description_content_type="text/markdown",
+)
