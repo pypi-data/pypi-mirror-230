f8e09118bb69aa8f1e6fc844c1ee339ff9f1403d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:07:50 +0000

    Add package larray-eurostat/0.34.1/larray_eurostat-0.34.1-py3-none-any.whl

---
+++ packages/larray-eurostat/larray_eurostat-0.34.1-py3-none-any.whl/larray_eurostat/tsv.py
@@ -0,0 +1,77 @@
+import gzip
+from io import StringIO
+from urllib.request import urlopen
+
+from larray import Session, read_eurostat
+
+
+def _remove_chars(s, chars):
+    return s.translate({ord(c): None for c in chars})
+
+
+EUROSTAT_BASEURL = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&file="
+
+
+def _get_one(indicator, *, drop_markers=True):
+    """Get one Eurostat indicator and return it as an array."""
+    url = f"{EUROSTAT_BASEURL}data/{indicator}.tsv.gz"
+    with urlopen(url) as f, gzip.open(f, mode='rt') as fgz:    # noqa: S310
+        try:
+            s = fgz.read()
+            if drop_markers:
+                first_line_end = s.index('\n')
+                # strip markers except on first line
+                s = s[:first_line_end] + _remove_chars(s[first_line_end:], ' dbefcuipsrzn:')
+            return read_eurostat(StringIO(s))
+        except Exception as e:
+            e.args = (e.args[0] + f"\nCan't open file {f.geturl()}",)
+            raise
+
+
+def eurostat_get(indicators, *, drop_markers=True):
+    """Get one or several Eurostat indicators and return them as an array or a session.
+
+    Parameters
+    ----------
+    indicators : str or list/tuple of str
+        Name(s) of eurostat indicator(s). When requesting a single indicator, the result is an Array, otherwise it is a
+        Session.
+    drop_markers : bool, optional
+        Whether or not to drop special markers. Defaults to True.
+
+    Returns
+    -------
+    Array or Session
+
+    Examples
+    --------
+    >>> data = eurostat_get('avia_ec_enterp')
+    >>> data.info
+    2 x 16 x 13
+     enterpr [2]: 'AIRP' 'AVIA'
+     geo [16]: 'CY' 'CZ' 'EE' ... 'BG' 'FI' 'SE'
+     time [13]: 2013 2012 2011 ... 2003 2002 2001
+    dtype: float64
+    memory used: 3.25 Kb
+    >>> indicators = eurostat_get(['avia_ec_enterp', 'apro_mt_lsequi'])
+    >>> indicators.names
+    ['apro_mt_lsequi', 'avia_ec_enterp']
+    >>> indicators.avia_ec_enterp.info
+    2 x 16 x 13
+     enterpr [2]: 'AIRP' 'AVIA'
+     geo [16]: 'CY' 'CZ' 'EE' ... 'BG' 'FI' 'SE'
+     time [13]: 2013 2012 2011 ... 2003 2002 2001
+    dtype: float64
+    memory used: 3.25 Kb
+    >>> indicators.apro_mt_lsequi.info
+    3 x 1 x 28 x 38
+     animals [3]: 'A1000' 'A1100' 'A1200'
+     unit [1]: 'THS_HD'
+     geo [28]: 'AL' 'BE' 'BG' ... 'SI' 'SK' 'UK'
+     time [38]: 1997 1996 1995 ... 1962 1961 1960
+    dtype: float64
+    memory used: 24.94 Kb
+    """
+    if isinstance(indicators, (tuple, list)):
+        return Session({i: _get_one(i, drop_markers=drop_markers) for i in indicators})
+    return _get_one(indicators, drop_markers=drop_markers)
