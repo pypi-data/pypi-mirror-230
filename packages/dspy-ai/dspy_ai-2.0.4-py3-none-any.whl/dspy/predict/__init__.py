18448eaac8ef34316717d42d071e25bd6ca413e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:08:17 +0000

    Add package dspy-ai/2.0.4/dspy_ai-2.0.4-py3-none-any.whl

---
+++ packages/dspy-ai/dspy_ai-2.0.4-py3-none-any.whl/dspy/predict/__init__.py
@@ -0,0 +1,4 @@
+from .predict import Predict
+from .chain_of_thought import ChainOfThought
+from .chain_of_thought_with_hint import ChainOfThoughtWithHint
+from .aggregation import majority 
---
+++ packages/dspy-ai/dspy_ai-2.0.4-py3-none-any.whl/dspy/teleprompt/__init__.py
@@ -0,0 +1,5 @@
+from .teleprompt import *
+from .bootstrap import *
+from .vanilla import *
+from .random_search import *
+from .finetune import *
