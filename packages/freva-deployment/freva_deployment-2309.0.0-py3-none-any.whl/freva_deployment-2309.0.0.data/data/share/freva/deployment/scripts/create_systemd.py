031eee3fe4fed46d54a57a28df667d7fab99ec42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:09:23 +0000

    Add package freva-deployment/2309.0.0/freva_deployment-2309.0.0-py3-none-any.whl

---
+++ packages/freva-deployment/freva_deployment-2309.0.0-py3-none-any.whl/freva_deployment-2309.0.0.data/data/share/freva/deployment/scripts/create_systemd.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+import argparse
+from pathlib import Path
+import os
+import shlex
+import sys
+import subprocess
+from typing import Dict, List, Tuple
+
+SYSTEMD_TMPL = dict(
+    Unit=dict(
+        Description="Start/Stop freva services containers",
+        After="network-online.target",
+    ),
+    Service=dict(
+        TimeoutStartSec="35s",
+        TimeoutStopSec="35s",
+        ExecStartPre="{delete_command}",
+        ExecStart='/bin/sh -c "{container_cmd} {container_args}"',
+        Restart="no",
+    ),
+    Install=dict(WantedBy="default.target"),
+)
+
+
+def parse_args() -> Tuple[str, str, List[str], bool]:
+    """Parse the commandline arguments."""
+
+    app = argparse.ArgumentParser(
+        prog=sys.argv[0], description="Create a new systemd unit."
+    )
+    app.add_argument("name", type=str, help="Set the container name")
+    app.add_argument(
+        "--enable",
+        action="store_true",
+        default=False,
+        help="Enable this unit.",
+    )
+    app.add_argument("--requires", type=str, nargs="+", default=[])
+    args, other = app.parse_known_args()
+    return " ".join(other), args.name, args.requires, args.enable
+
+
+def _parse_dict(tmp_dict: Dict[str, Dict[str, str]]) -> str:
+    systemd_unit = ""
+    for section, keys in tmp_dict.items():
+        systemd_unit += "[{}]\n".format(section)
+        for key, values in keys.items():
+            systemd_unit += "{}={}\n".format(key, values)
+    return systemd_unit
+
+
+def load_unit(unit: str, content: str, enable: bool = True) -> None:
+    """Load a given systemd unit."""
+    files = (
+        "/etc/systemd/system/{}.service".format(unit),
+        "~/.local/share/systemd/user/{}.service".format(unit),
+    )
+    flags = ("", "--user")
+    for file, flag in zip(files, flags):
+        out_file = Path(file).expanduser()
+        try:
+            out_file.parent.mkdir(exist_ok=True, parents=True)
+            with out_file.open(mode="w", encoding="utf-8") as f_obj:
+                f_obj.write(content)
+        except PermissionError:
+            continue
+        cmd = ["systemctl"]
+        if flag:
+            cmd += [flag]
+
+        subprocess.run(cmd + ["daemon-reload"], check=True)
+        if enable:
+            subprocess.run(cmd + ["enable", unit], check=True)
+        subprocess.run(cmd + ["restart", unit], check=True)
+        return
+
+
+def get_container_cmd(args: str) -> Tuple[str, str]:
+    """Get the correct container command for the system."""
+    path = os.environ.get("PATH", "") + ":" + "/usr/local/bin"
+    env = os.environ.copy()
+    env["PATH"] = path
+    cmd = ["/tmp/docker-or-podman", "--print-only"] + shlex.split(args)
+    res = subprocess.run(
+        cmd,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=env,
+    )
+    out = res.stdout.decode().split()
+    if out:
+        return out[0], " ".join(out[1:]).replace("%", "%%")
+    return "", ""
+
+
+def create_unit(
+    args: str, unit: str, requires: List[str], enable: bool
+) -> None:
+    """Create the systemd unit."""
+    container_cmd, container_args = get_container_cmd(args)
+    _, delete_command = get_container_cmd("rm -f {}".format(unit))
+    if delete_command:
+        delete_command = "{} {}".format(container_cmd, delete_command)
+    else:
+        delete_command = ""
+    if "docker" in container_cmd:
+        SYSTEMD_TMPL["Unit"]["Requires"] = "docker.service"
+        SYSTEMD_TMPL["Unit"]["After"] += " docker.service"
+    for key in ("ExecStart",):
+        SYSTEMD_TMPL["Service"][key] = SYSTEMD_TMPL["Service"][key].format(
+            container_cmd=container_cmd,
+            container_args=container_args,
+            unit=unit,
+        )
+    SYSTEMD_TMPL["Service"]["ExecStartPre"] = SYSTEMD_TMPL["Service"][
+        "ExecStartPre"
+    ].format(delete_command=delete_command)
+    for service in requires:
+        for key in ("After", "Requires"):
+            try:
+                SYSTEMD_TMPL["Unit"][key] += " {}.service".format(service)
+            except KeyError:
+                SYSTEMD_TMPL["Unit"][key] = " {}.service".format(service)
+    load_unit(unit, _parse_dict(SYSTEMD_TMPL), enable)
+
+
+if __name__ == "__main__":
+    create_unit(*parse_args())
---
+++ packages/freva-deployment/freva_deployment-2309.0.0-py3-none-any.whl/freva_deployment/cli/_deploy.py
@@ -0,0 +1,81 @@
+"""Command line interface for the deployment."""
+from __future__ import annotations
+
+import argparse
+import sys
+from pathlib import Path
+
+from freva_deployment import __version__
+
+from ..deploy import DeployFactory
+from ..utils import config_dir, guess_map_server, set_log_level
+
+
+def parse_args(argv: list[str] | None) -> argparse.Namespace:
+    """Consturct command line argument parser."""
+
+    ap = argparse.ArgumentParser(
+        prog="deploy-freva-cmd",
+        description="Deploy freva and its services on different machines.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    ap.add_argument(
+        "--server-map",
+        type=str,
+        default=None,
+        help=(
+            "Hostname of the service mapping the freva server "
+            "archtiecture, Note: you can create a server map by "
+            "running the deploy-freva-map command"
+        ),
+    )
+    ap.add_argument(
+        "--config",
+        "-c",
+        type=Path,
+        help="Path to ansible inventory file.",
+        default=config_dir / "inventory.toml",
+    )
+    ap.add_argument(
+        "--steps",
+        type=str,
+        nargs="+",
+        default=["db", "solr", "web", "core"],
+        choices=["web", "core", "db", "solr"],
+        help="The services/code stack to be deployed",
+    )
+    ap.add_argument(
+        "--ask-pass",
+        help="Connect to server via ssh passwd instead of public key.",
+        action="store_true",
+        default=False,
+    )
+    ap.add_argument(
+        "-v", "--verbose", action="count", help="Verbosity level", default=0
+    )
+    ap.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version="%(prog)s {version}".format(version=__version__),
+    )
+    return ap.parse_args()
+
+
+def cli(argv: list[str] | None = None) -> None:
+    """Run the command line interface."""
+    args = parse_args(argv)
+    if args.server_map:
+        server_map = guess_map_server(args.server_map, mandatory=False)
+    else:
+        server_map = ""
+    set_log_level(args.verbose)
+    with DeployFactory(
+        steps=args.steps,
+        config_file=args.config,
+    ) as DF:
+        DF.play(server_map, args.ask_pass, args.verbose)
+
+
+if __name__ == "__main__":
+    cli(sys.argv[1:])
