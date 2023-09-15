7575cbb62c167bd4a31c6508f69b036bd150d60d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 05:06:06 +0000

    Add package candor-test/1.0.6/candor_test-1.0.6-py3-none-any.whl

---
+++ packages/candor-test/candor_test-1.0.6-py3-none-any.whl/candor/candor.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def verifyLicense(api_key: str, license_key: str, product_id: str):
+    try:
+        data = {
+            'product_id': product_id,
+            'key': license_key
+        }
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': api_key
+        }
+        response = requests.post("https://candorian.app/api/licenses/verify", json=data, headers=headers)
+        json_response = response.json()
+        isLicenseValid = json_response.get('success', False)
+        return isLicenseValid
+    except requests.exceptions.RequestException:
+        print("Error: Unable to fetch license information.")
+        return False
+
+def getConfigs(api_key: str, config_id: str):
+    print('test')
