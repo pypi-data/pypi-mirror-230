284b6747827b1c236e6fb6ddbeb75de94b0f3996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:07:23 +0000

    Add package mqtasks/0.0.1.dev15/mqtasks-0.0.1.dev15-py3-none-any.whl

---
+++ packages/mqtasks/mqtasks-0.0.1.dev15-py3-none-any.whl/mqtasks/headers.py
@@ -0,0 +1,6 @@
+class MqTaskHeaders:
+    TASK: str = "task"
+    ID: str = "id"
+    RELAY_TO: str = "relay_to"
+    RESPONSE_TO_MESSAGE_ID: str = "response_to_message_id"
+    RESPONSE_TYPE: str = "response_type"
