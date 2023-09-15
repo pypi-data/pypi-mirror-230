46f3f570aa380ec3426395d5f6717cbbcf04f47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 05:06:36 +0000

    Add package candor-test/1.1.2/candor-test-1.1.2.tar.gz

---
+++ packages/candor-test/candor-test-1.1.2.tar.gz/candor-test-1.1.2/src/candor/candor.py
@@ -0,0 +1,33 @@
+import requests
+
+def verifyLicense(api_key: str, license_key: str, product_id: str):
+    #try:
+        data = {
+            'product_id': product_id,
+            'key': license_key
+        }
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': api_key
+        }
+        response = requests.post("https://dashboard.candorservices.net/api/licenses/verify", json=data, headers=headers)
+        json_response = response.json()
+        isLicenseValid = json_response.get('success', False)
+        return isLicenseValid
+    #except requests.exceptions.RequestException:
+    #    print("Error: Unable to fetch license information.")
+    #    return False
+
+def getConfigs(api_key: str, config_id: str):
+    try:
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': api_key
+        }
+        response = requests.get(f"https://dashboard.candorservices.net/api/configs/{config_id}", headers=headers)
+        json_response = response.json()
+        values = json_response.get('values', {})
+        return values
+    except requests.exceptions.RequestException:
+        print("Error: Unable to fetch config information.")
+        return {}
