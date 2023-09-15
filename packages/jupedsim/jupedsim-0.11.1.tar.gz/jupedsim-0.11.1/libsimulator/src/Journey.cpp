e37defabac902465e78bc271daa45a237daab679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 15:06:24 +0000

    Add package jupedsim/0.11.1/jupedsim-0.11.1.tar.gz

---
+++ packages/jupedsim/jupedsim-0.11.1.tar.gz/jupedsim-0.11.1/libsimulator/src/Journey.cpp
@@ -0,0 +1,37 @@
+// Copyright © 2012-2023 Forschungszentrum Jülich GmbH
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "Journey.hpp"
+
+#include "GenericAgent.hpp"
+#include "RoutingEngine.hpp"
+#include "SimulationError.hpp"
+#include "Stage.hpp"
+#include "StageDescription.hpp"
+#include "TemplateHelper.hpp"
+
+#include <algorithm>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+#include <tuple>
+#include <vector>
+
+#include "fmt/ranges.h"
+
+////////////////////////////////////////////////////////////////////////////////
+/// SimpleJourney
+////////////////////////////////////////////////////////////////////////////////
+Journey::Journey(std::vector<BaseStage*> stages_) : stages(std::move(stages_))
+{
+}
+
+std::tuple<Point, size_t, BaseStage::ID> Journey::Target(const GenericAgent& agent) const
+{
+    for(size_t idx = agent.currentJourneyStageIdx; idx < stages.size(); ++idx) {
+        if(stages[idx]->IsCompleted(agent)) {
+            continue;
+        }
+        return std::make_tuple(stages[idx]->Target(agent), idx, stages[idx]->Id());
+    }
+    return std::make_tuple(stages.back()->Target(agent), stages.size() - 1, stages.back()->Id());
+}
