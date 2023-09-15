60f8314dcb70ab568185c01cc03c5cc2a43beaed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 14:07:24 +0000

    Add package python-pulse/0.1.0/python-pulse-0.1.0.tar.gz

---
+++ packages/python-pulse/python-pulse-0.1.0.tar.gz/python-pulse-0.1.0/pypulse/View/setview.py
@@ -0,0 +1,27 @@
+from .view import View
+
+
+class SetView:
+    def __init__(self, name: str, view: object, requirement_view: list, path_trigger: str) -> None:
+        self._validate_new_view_consistance(name, path_trigger)
+        View(name, view, requirement_view, path_trigger)
+
+    def _validate_new_view_consistance(self, name, path_trigger):
+        if len(View.instances) == 0:
+            return
+
+        if name in [i.name for i in View.instances]:
+            raise KeyError(f'The view with the name {name} already exists')
+
+        if path_trigger in [i.path_trigger for i in View.instances]:
+            raise KeyError(
+                f'The view with the path_trigger {path_trigger} already exists')
+
+
+def view(name: str, path_trigger: str):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)  # Call the original function
+            return result
+        return wrapper
+    return decorator
