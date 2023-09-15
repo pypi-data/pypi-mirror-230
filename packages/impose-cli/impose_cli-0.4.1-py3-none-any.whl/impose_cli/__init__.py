faef2e4790e1121340950c2091b3acd8fc4f1e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 05:06:03 +0000

    Add package impose-cli/0.4.1/impose_cli-0.4.1-py3-none-any.whl

---
+++ packages/impose-cli/impose_cli-0.4.1-py3-none-any.whl/impose_cli/__init__.py
@@ -0,0 +1,4 @@
+from impose_cli.impose_cli import impose_cli
+from impose_cli.decorators import require_flags, impose, hide, cleanup
+
+__all__ = ["impose_cli", "require_flags", "impose", "hide", "cleanup"]
