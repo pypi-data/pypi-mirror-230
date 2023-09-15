544af9720240d348ab430522826b4e32ebd26f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 04:07:17 +0000

    Add package dmlcloud/0.1.1/dmlcloud-0.1.1-py3-none-any.whl

---
+++ packages/dmlcloud/dmlcloud-0.1.1-py3-none-any.whl/dmlcloud/__init__.py
@@ -0,0 +1,14 @@
+from .config import ArgparseVar, BaseConfig, ConfigVar, DefaultConfig, SubConfig
+from .training import BaseTrainer, ClassificationTrainer
+
+__version__ = "0.1.1"
+
+__all__ = [
+    'ArgparseVar',
+    'BaseConfig',
+    'BaseTrainer',
+    'ClassificationTrainer',
+    'ConfigVar',
+    'DefaultConfig',
+    'SubConfig',
+]
