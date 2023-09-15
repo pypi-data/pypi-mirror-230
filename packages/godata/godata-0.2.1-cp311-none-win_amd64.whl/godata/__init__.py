9b81eee893e88cbd4c77c7377794ba3e0653d34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 14:08:18 +0000

    Add package godata/0.2.1/godata-0.2.1-cp311-none-win_amd64.whl

---
+++ packages/godata/godata-0.2.1-cp311-none-win_amd64.whl/godata/__init__.py
@@ -0,0 +1,15 @@
+from .project import (
+    create_project,
+    list_collections,
+    list_projects,
+    load_project,
+    remove_project,
+)
+
+__all__ = [
+    "load_project",
+    "list_projects",
+    "create_project",
+    "list_collections",
+    "remove_project",
+]
