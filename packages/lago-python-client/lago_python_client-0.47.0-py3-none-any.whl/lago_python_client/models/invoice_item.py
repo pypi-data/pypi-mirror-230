37b3c8d4c825045e1ef42176b3588c3a9797df54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 04:08:02 +0000

    Add package lago-python-client/0.47.0/lago_python_client-0.47.0-py3-none-any.whl

---
+++ packages/lago-python-client/lago_python_client-0.47.0-py3-none-any.whl/lago_python_client/models/invoice_item.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from ..base_model import BaseResponseModel
+
+
+class InvoiceItemResponse(BaseResponseModel):
+    type: Optional[str]
+    code: Optional[str]
+    name: Optional[str]
+    lago_item_id: Optional[str]
+    item_type: Optional[str]
---
+++ packages/lago-python-client/lago_python_client-0.47.0-py3-none-any.whl/lago_python_client/version.py
@@ -0,0 +1,1 @@
+LAGO_VERSION = '0.47.0'
