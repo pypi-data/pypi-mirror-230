67be07a55e417a923a6f87a26f57230c7f4c06f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 04:07:49 +0000

    Add package peaq-py/0.0.3/peaq-py-0.0.3.tar.gz

---
+++ packages/peaq-py/peaq-py-0.0.3.tar.gz/peaq-py-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+
+setup(
+    name='peaq-py',
+    version=VERSION,
+    url='https://github.com/yourusername/mylibrary',
+    author='Jay Pan',
+    author_email='pan@eotlabs.io',
+    description='peaq python sdk',
+    packages=find_packages(),
+    install_requires=[],
+)
