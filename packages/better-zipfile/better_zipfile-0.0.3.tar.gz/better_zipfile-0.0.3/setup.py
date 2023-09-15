c7a03a188b430e98e39a17b5b80d9a3433361e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 15:06:57 +0000

    Add package better-zipfile/0.0.3/better_zipfile-0.0.3.tar.gz

---
+++ packages/better-zipfile/better_zipfile-0.0.3.tar.gz/better_zipfile-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+__version__ = '0.0.3'
+
+setup(
+    name="better_zipfile",
+    version=__version__,
+    description="本项目旨在对优化python中自带的zipfile，以处理mnbvc项目解压遇到的问题",
+    url="https://github.com/aplmikex/better_zipfile",
+    author="aplmikex",
+    author_email="2738186032@qq.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    packages=['better_zipfile'],
+    python_requires=">=3.7",
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/aplmikex/better_zipfile/issues",
+        "Source": "https://github.com/aplmikex/better_zipfile/",
+    },
+)
