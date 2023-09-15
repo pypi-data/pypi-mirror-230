9853eeb9e731e25b2d1f466734eb54db2fdff359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:08:47 +0000

    Add package jmcomic/2.2.10/jmcomic-2.2.10-py3-none-any.whl

---
+++ packages/jmcomic/jmcomic-2.2.10-py3-none-any.whl/jmcomic/__init__.py
@@ -0,0 +1,9 @@
+# 模块依赖关系如下:
+# 被依赖方 <--- 使用方
+# config <--- entity <--- toolkit <--- client <--- option <--- downloader
+
+__version__ = '2.2.10'
+
+from .api import *
+from .jm_plugin import *
+from .cl import main
