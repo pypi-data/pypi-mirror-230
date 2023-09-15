266e25a707b29ab8d9ddc980c118aa8caa6d03d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:09:38 +0000

    Add package pyholaclient/1.1.4/pyholaclient-1.1.4-py3-none-any.whl

---
+++ packages/pyholaclient/pyholaclient-1.1.4-py3-none-any.whl/pyholaclient/__init__.py
@@ -0,0 +1,6 @@
+from .pyholaclient import HolaClient
+# from .Classes import User, Coupon, Package
+from .Classes.user import User
+from .Classes.package import Package
+from .Classes.coupon import Coupon
+from .Classes.sysinfo import SysInfo
