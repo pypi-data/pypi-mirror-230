8462ff75ec412570aba7d4facdf641b128310273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 05:06:03 +0000

    Add package impose-cli/0.4.1/impose-cli-0.4.1.tar.gz

---
+++ packages/impose-cli/impose-cli-0.4.1.tar.gz/impose-cli-0.4.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+DESCRIPTION = 'Create a CLI tool easily.'
+
+# Setting up
+setup(
+    name="impose-cli",
+    version='0.4.1',
+    author="scdev",
+    author_email="samuel.chai.development@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=requirements,
+    keywords=['python', 'cli', 'click'],
+)
