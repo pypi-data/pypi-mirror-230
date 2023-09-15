46223d08e864ad0fa2f68d5b54fda1d7a8d54358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 04:07:12 +0000

    Add package pyuff-ustb/2.0.3/pyuff_ustb-2.0.3-py3-none-any.whl

---
+++ packages/pyuff-ustb/pyuff_ustb-2.0.3-py3-none-any.whl/pyuff_ustb/objects/scans/scan.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from pyuff_ustb.objects.uff import Uff, compulsory_property, dependent_property
+from pyuff_ustb.readers import LazyArray
+
+
+class Scan(Uff):
+    """:class:`Uff` class to define a scan.
+
+    :class:`Scan` contains the position of a collection of pixels. It is asuperclass for more easy-to-handle classes such as :class:`~pyuff_ustb.objects.scans.linear_scan.LinearScan` or :class:`~pyuff_ustb.objects.scans.sector_scan.SectorScan`.
+    """
+
+    # Compulsory properties
+    @compulsory_property
+    def x(self) -> np.ndarray:
+        "Vector containing the x coordinates of each pixel in [m]"
+        return LazyArray(self._reader["x"])
+
+    @compulsory_property
+    def y(self) -> np.ndarray:
+        "Vector containing the y coordinates of each pixel in [m]"
+        return LazyArray(self._reader["y"])
+
+    @compulsory_property
+    def z(self) -> np.ndarray:
+        "Vector containing the z coordinates of each pixel in [m]"
+        return LazyArray(self._reader["z"])
+
+    # Dependent properties
+    @dependent_property
+    def xyz(self) -> np.ndarray:
+        "Vector containing the [x, y, z] coordinates of each pixel in [m]"
+        return np.stack([self.x, self.y, self.z], axis=-1)
