5ea1640c3bbd7f3f111f2f06d20d786d65759cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 05:05:52 +0000

    Add package num2textilka/0.1.1/num2textilka-0.1.1.tar.gz

---
+++ packages/num2textilka/num2textilka-0.1.1.tar.gz/num2textilka-0.1.1/src/packages/lang.py
@@ -0,0 +1,16 @@
+languages = {
+
+"persian" : {"add_text" : " و ",
+"big_number_format" : ('', 'هزار', 'میلیون', 'میلیارد', 'بیلیون', 'بیلیارد', 'تریلیون', 'تریلیارد', 'کوآدریلیون', 'کادریلیارد', 'کوینتیلیون', 'کوانتینیارد', 'سکستیلیون', 'سکستیلیارد', 'سپتیلیون', 'سپتیلیارد', 'اکتیلیون', 'اکتیلیارد',
+                     'نانیلیون', 'نانیلیارد', 'دسیلیون', 'دسیلیارد', 'آندسیلیون', 'آندسیلیارد', 'دودسیلیون', 'دودسیلیارد', 'تریدسیلیون', 'تریدسیلیارد', 'کوادردسیلیون', 'کوادردسیلیارد', 'کویندسیلیون', 'کویندسیلیارد', 'سیدسیلیون', 'سیدسیلیارد', 'گوگول'),
+
+"small_number_format" : {'0': 'صفر', '1': 'یک', '2': 'دو', '3': 'سه', '4': 'چهار', '5': 'پنج', '6': 'شش', '7': 'هفت', '8': 'هشت', '9': 'نه', '10': 'ده', '11': 'یازده', '12': 'دوازده', '13': 'سیزده', '14': 'چهارده', '15': 'پانزده', '16': 'شانزده', '17': 'هفده', '18': 'هجده',
+                             '19': 'نوزده', '20': 'بیست', '30': 'سی', '40': 'چهل', '50': 'پنجاه', '60': 'شصت', '70': 'هفتاد', '80': 'هشتاد', '90': 'نود', '100': 'یکصد', '200': 'دویست', '300': 'سیصد', '400': 'چهارصد', '500': 'پانصد', '600': 'ششصد', '700': 'هفتصد', '800': 'هشتصد', '900': 'نهصد'}},
+                             
+"english" : {"add_text" : " ",
+"big_number_format" : ('', 'thousand', 'million','billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quattuordecillion', 'quindecillion', 'sedecillion', 'septendecillion', 'octodecillion', 'novendecillion', 'vigintillion', 'unvigintillion', 'duovigintillion', 'tresvigintillion', 'quattuor­vigint­illion', 'quinvigintillion', 'sesvigintillion', 'septemvigintillion', 'octovigintillion', 'novemvigintillion', 'trigintillion', 'untrigintillion', 'duotrigintillion', 'googol'),
+             
+"small_number_format" : {'0': 'zero', '1': 'one', '2': 'two', '3': 'three', '4': 'four', '5': 'five', '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine', '10': 'ten', '11': 'eleven', '12': 'twelve', '13': 'thirteen', '14': 'fourteen', '15': 'fifteen', '16': 'sixteen', '17': 'seventeen', '18': 'eighteen',
+                            '19': 'nineteen', '20': 'twenty', '30': 'thirty', '40': 'forty', '50': 'fifty', '60': 'sixty', '70': 'seventy', '80': 'eighty', '90': 'ninty', '100': 'one hundred', '200': 'two hundred', '300': 'three hundred', '400': 'four hundred', '500': 'five hunder', '600': 'six hundred', '700': 'seven hundred', '800': 'eight hundred', '900': 'nine hundred'}}
+}
+                         
