bcf255a203f5d4c28b479261e52bd3d4f464be7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 14:06:30 +0000

    Add package prefect/2.13.1/prefect-2.13.1-py3-none-any.whl

---
+++ packages/prefect/prefect-2.13.1-py3-none-any.whl/prefect/server/ui/assets/usePageTitle-ad3dbf10.js
@@ -0,0 +1,1 @@
+import{c,u as o,al as u}from"./index-562c8470.js";function i(...t){const r=[...t,"Prefect Server"],a=c(()=>r.map(e=>o(e)).filter(e=>e!==null).join(" • "));u(()=>document.title=a.value)}export{i as u};
