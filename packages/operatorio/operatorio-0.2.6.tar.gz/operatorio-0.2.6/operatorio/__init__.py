11a6998d15ff0cbb40969617cd413e5f1b67f683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 04:07:08 +0000

    Add package operatorio/0.2.6/operatorio-0.2.6.tar.gz

---
+++ packages/operatorio/operatorio-0.2.6.tar.gz/operatorio-0.2.6/operatorio/__init__.py
@@ -0,0 +1,13 @@
+from .main import (
+    EntityType,
+    Address,
+    AddressMatch,
+    Addresses,
+    DescribeInput,
+    DescribeOutput,
+    Query,
+    ValidationError,
+    HTTPValidationError,
+    ApiException,
+    OperatorSearch,
+)
