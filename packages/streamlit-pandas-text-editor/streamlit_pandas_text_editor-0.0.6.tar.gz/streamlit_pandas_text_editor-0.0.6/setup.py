a5e7c493b0f7d8c2709d1ba9029f44d295d0e334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 04:07:10 +0000

    Add package streamlit-pandas-text-editor/0.0.6/streamlit_pandas_text_editor-0.0.6.tar.gz

---
+++ packages/streamlit-pandas-text-editor/streamlit_pandas_text_editor-0.0.6.tar.gz/streamlit_pandas_text_editor-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit_pandas_text_editor",
+    version="0.0.6",
+    author="Anonymous",
+    author_email="anonymous_paper_submission@outlook.com",
+    description="Rich Text Editor with customizable Dropdown Options for Streamlit",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
