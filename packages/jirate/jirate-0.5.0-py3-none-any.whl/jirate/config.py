f3ee4ced9d767f40ee43500f9b85b8a25be3f719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 04:08:03 +0000

    Add package jirate/0.5.0/jirate-0.5.0-py3-none-any.whl

---
+++ packages/jirate/jirate-0.5.0-py3-none-any.whl/jirate/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import json
+import os
+
+
+def get_config():
+    try:
+        config_file = open(os.path.expanduser('~/.jirate.json'))
+    except FileNotFoundError:
+        config_file = open(os.path.expanduser('~/.trolly.json'))
+    config_data = config_file.read()
+    config_file.close()
+    config = json.loads(config_data)
+
+    return config
