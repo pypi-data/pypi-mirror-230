9f14efbd77edfa7d21fe7b47ce196784193cf693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 05:06:38 +0000

    Add package DekuDealsAPI/0.1/dekudealsapi-0.1.tar.gz

---
+++ packages/DekuDealsAPI/dekudealsapi-0.1.tar.gz/dekudealsapi-0.1/README.md
@@ -0,0 +1,12 @@
+# DekuDeals-Python-API
+
+An unofficial Python 3 API for the Nintendo Switch game price tracker website DekuDeals.com
+
+WIP, so install and usage instruction may change
+
+## Installation 
+
+`git clone https://github.com/randomcoder67/DekuDealsAPI.git`
+`pip install DekuDealsAPI`
+
+## Usage
---
+++ packages/DekuDealsAPI/dekudealsapi-0.1.tar.gz/dekudealsapi-0.1/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import json
+from DekuDealsAPI import search
+from DekuDealsAPI import page
+
+a = search.PerformSearch("lego star wars")
+b = page.ItemDetails("https://www.dekudeals.com/items/my-time-at-portia")
+
+#print(a["results"])
+
+final = json.dumps(a, indent=2)
+
+print(final)
+
+final2 = json.dumps(b, indent=2)
+print(final2)
+
+print(b.keys())
