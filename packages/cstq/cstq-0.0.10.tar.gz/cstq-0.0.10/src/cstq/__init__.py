84f8c82fc0ffbf6dff616b8d5155480660ca1eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 14:07:28 +0000

    Add package cstq/0.0.10/cstq-0.0.10.tar.gz

---
+++ packages/cstq/cstq-0.0.10.tar.gz/cstq-0.0.10/src/cstq/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023-present Alvaro Leiva <aleivag@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from cstq.query import Query
+from cstq.obj2cst import obj2cst
+
+__all__ = ["Query", "obj2cst"]
