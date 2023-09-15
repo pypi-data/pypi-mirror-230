b535ac5f20b992bed43273b53d8a28636574a9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 15:06:19 +0000

    Add package sqlconstructor/1.0.40/sqlconstructor-1.0.40.tar.gz

---
+++ packages/sqlconstructor/sqlconstructor-1.0.40.tar.gz/sqlconstructor-1.0.40/tests/test_sql_section/test_init.py
@@ -0,0 +1,21 @@
+import pytest
+from sqlconstructor import SqlSection
+
+SECTION_HEADER = 'select'
+
+
+def test_init_with_no_header():
+    section = SqlSection()
+    assert section.header == ''
+    assert section.container is None
+
+
+@pytest.mark.SqlSection
+@pytest.mark.parametrize("args, kwargs", [
+    ([SECTION_HEADER], {}),
+    ([], {'header': SECTION_HEADER}),
+])
+def test_init_with_section_header(args, kwargs):
+    section = SqlSection(*args, **kwargs)
+    assert section.header == SECTION_HEADER
+    assert section.container is None
