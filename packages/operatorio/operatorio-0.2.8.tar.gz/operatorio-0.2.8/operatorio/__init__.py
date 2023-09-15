8c4218cf16393ba0ebe1aa0bd0c5a6aaa75e7760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 14:06:32 +0000

    Add package operatorio/0.2.8/operatorio-0.2.8.tar.gz

---
+++ packages/operatorio/operatorio-0.2.8.tar.gz/operatorio-0.2.8/operatorio/__init__.py
@@ -0,0 +1,12 @@
+from .main import (
+    Address,
+    AddressMatch,
+    Addresses,
+    DescribeInput,
+    DescribeOutput,
+    Query,
+    ValidationError,
+    HTTPValidationError,
+    ApiException,
+    OperatorSearch,
+)
---
+++ packages/operatorio/operatorio-0.2.8.tar.gz/operatorio-0.2.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="operatorio",
+    version="0.2.8",
+    description="A Python SDK for the Operator Search API.",
+    author="David Shi",
+    author_email="david@operator.io",
+    url="https://github.com/operatorlabs/sdk/python",
+    packages=['operatorio'],  
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+    test_suite="tests",
+)
+
