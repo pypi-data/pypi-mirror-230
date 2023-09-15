691b97379f2c4aefdebd237f5ca390d76122c820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 15:06:51 +0000

    Add package unblockedGPT/0.3.5/unblockedGPT-0.3.5-py3-none-any.whl

---
+++ packages/unblockedGPT/unblockedGPT-0.3.5-py3-none-any.whl/unblockedGPT/run_app.py
@@ -0,0 +1,7 @@
+import os
+import streamlit as st
+
+def run():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    app_path = os.path.join(dir_path, 'app.py')
+    os.system(f"streamlit run {app_path}")
