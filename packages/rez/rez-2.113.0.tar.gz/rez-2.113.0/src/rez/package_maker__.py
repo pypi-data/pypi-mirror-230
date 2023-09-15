03089287a42875fd885ebf511723dc20a0970284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 14:06:34 +0000

    Add package rez/2.113.0/rez-2.113.0.tar.gz

---
+++ packages/rez/rez-2.113.0.tar.gz/rez-2.113.0/src/rez/package_maker__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the Rez Project
+
+
+import warnings
+from rez.package_maker import *  # noqa
+
+
+warnings.warn(
+    "rez.package_maker__ is deprecated; import rez.package_maker instead",
+    DeprecationWarning
+)
---
+++ packages/rez/rez-2.113.0.tar.gz/rez-2.113.0/src/rez/packages_.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the Rez Project
+
+
+import warnings
+from rez.packages import *  # noqa
+
+
+warnings.warn(
+    "rez.packages_ is deprecated; import rez.packages instead",
+    DeprecationWarning
+)
