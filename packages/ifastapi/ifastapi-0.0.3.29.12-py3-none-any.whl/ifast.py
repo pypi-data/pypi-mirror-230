3d69df01064ad25a96f96cea2e3eec7ecf121bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 15:06:51 +0000

    Add package ifastapi/0.0.3.29.12/ifastapi-0.0.3.29.12-py3-none-any.whl

---
+++ packages/ifastapi/ifastapi-0.0.3.29.12-py3-none-any.whl/ifast.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import sys
+
+
+def main():
+    print(sys.argv, '123333')
+    copy_file()
+
+
+def copy_file():
+    # 拷贝文件到运行目录
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    package_dir = os.path.dirname(__file__)
+
+    # 要复制的目录名称
+    directory_to_copy = 'init_builder'
+
+    source_dir = current_directory
+    source_dir = os.path.join(source_dir, directory_to_copy)
+    shutil.copytree(source_dir, package_dir)
