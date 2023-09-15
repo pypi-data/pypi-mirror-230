e5a576563d809656ff2801afa656a286033840c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 05:06:06 +0000

    Add package teleport-clienit/1.15.0/teleport-clienit-1.15.0.tar.gz

---
+++ packages/teleport-clienit/teleport-clienit-1.15.0.tar.gz/teleport-clienit-1.15.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+
+
+setup(name="teleport-clienit",
+      version='1.15.0',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False
+)
+
