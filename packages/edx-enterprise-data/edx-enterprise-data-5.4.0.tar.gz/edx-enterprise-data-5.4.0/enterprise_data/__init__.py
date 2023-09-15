b2388065300127bb3ad2ec91063066871a0ab14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 15:06:00 +0000

    Add package edx-enterprise-data/5.4.0/edx-enterprise-data-5.4.0.tar.gz

---
+++ packages/edx-enterprise-data/edx-enterprise-data-5.4.0.tar.gz/edx-enterprise-data-5.4.0/enterprise_data/__init__.py
@@ -0,0 +1,7 @@
+"""
+Enterprise data api application. This Django app exposes API endpoints used by enterprises.
+"""
+
+__version__ = "5.4.0"
+
+default_app_config = "enterprise_data.apps.EnterpriseDataAppConfig"  # pylint: disable=invalid-name
