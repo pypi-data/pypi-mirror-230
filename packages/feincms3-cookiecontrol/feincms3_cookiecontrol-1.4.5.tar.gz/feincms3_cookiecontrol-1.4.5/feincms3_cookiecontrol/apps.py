6b7ea555bd80f15246b38db33bd849a3a691d987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:08:20 +0000

    Add package feincms3-cookiecontrol/1.4.5/feincms3_cookiecontrol-1.4.5.tar.gz

---
+++ packages/feincms3-cookiecontrol/feincms3_cookiecontrol-1.4.5.tar.gz/feincms3_cookiecontrol-1.4.5/feincms3_cookiecontrol/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+class CookiecontrolConfig(AppConfig):
+    name = "feincms3_cookiecontrol"
+    verbose_name = "feincms3 Cookie Control Panel"
+    default_auto_field = "django.db.models.AutoField"
+
+    def ready(self):
+        from feincms3_cookiecontrol.models import clobber_cookiecontrol_data
+
+        clobber_cookiecontrol_data()
