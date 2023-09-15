e226e4ca2ed0651a322fc93c326ebf59d483912a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 15:06:14 +0000

    Add package support-toolbox/0.1.7/support-toolbox-0.1.7.tar.gz

---
+++ packages/support-toolbox/support-toolbox-0.1.7.tar.gz/support-toolbox-0.1.7/support_toolbox/main.py
@@ -0,0 +1,44 @@
+import os
+import importlib
+import sys
+from support_toolbox import utils
+import signal
+
+
+def exit_app(signum, frame):
+    print("\nExiting...")
+    sys.exit(0)
+
+
+def main():
+    print("\nWelcome to the Support Toolbox!\n")
+
+    # Get the absolute path to the 'support_toolbox' package directory
+    package_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # List available tools
+    tools = [f[:-3] for f in os.listdir(package_dir) if f.endswith('.py') and f != '__init__.py' and f != 'main.py' and f != 'utils.py']
+
+    # Display the available tools
+    print("Available tools:")
+    for idx, tool in enumerate(tools, start=1):
+        print(f"{idx}. {tool}")
+
+    # Ask the user to select a tool
+    selection = input("\nEnter the number corresponding with the tool you want to use: ")
+
+    try:
+        selected_tool = tools[int(selection) - 1]
+
+        # Check if the tokens for the selected tool exist and set them up if needed
+        utils.check_tokens(selected_tool)
+
+        module = importlib.import_module(f"support_toolbox.{selected_tool}")
+        module.run()
+    except (ValueError, IndexError):
+        print("Invalid selection. Please enter a valid number.")
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, exit_app)
+    main()
