1b073fb1cf00fa85eabcad5047b3bbc0f140fff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 14:06:42 +0000

    Add package richclass/1.0.0/richclass-1.0.0.tar.gz

---
+++ packages/richclass/richclass-1.0.0.tar.gz/richclass-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("app/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="richclass",
+    version="1.0.0",
+    description="Provides feature-rich classes for simplifying",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UpAllNate/richclass",
+    author="UpAllNate",
+    author_email="upallnate@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords = ["class", "features", "tutorial"],
+    extras_require={
+        "dev": ["twine>=4.0.2"],
+    },
+)
