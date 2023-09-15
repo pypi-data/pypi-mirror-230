1f00705469ab7e3bce053cd7891c627a7b494c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:09:18 +0000

    Add package pytest-info-plugin/1.0.1/pytest-info-plugin-1.0.1.tar.gz

---
+++ packages/pytest-info-plugin/pytest-info-plugin-1.0.1.tar.gz/pytest-info-plugin-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+VERSION = '1.0.1'
+
+setup(
+    name='pytest-info-plugin',  # package name
+    version=VERSION,  # package version
+    description='Get executed interface information in pytest interface automation framework',  # package description
+    zip_safe=False,
+    install_requires=[
+        "pytest",
+    ],
+    entry_points={
+        'pytest11': [
+            'pytest_info_plugin = pytest_info_plugin',
+        ],
+    },
+
+)
