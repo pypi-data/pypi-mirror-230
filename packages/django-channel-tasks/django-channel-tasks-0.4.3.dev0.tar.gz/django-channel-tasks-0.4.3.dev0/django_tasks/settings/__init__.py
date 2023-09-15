276ccb5e72f47dc4a0f5c49e09565b23b093a107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 05:06:09 +0000

    Add package django-channel-tasks/0.4.3.dev0/django-channel-tasks-0.4.3.dev0.tar.gz

---
+++ packages/django-channel-tasks/django-channel-tasks-0.4.3.dev0.tar.gz/django-channel-tasks-0.4.3.dev0/django_tasks/settings/__init__.py
@@ -0,0 +1,53 @@
+import configparser
+import os
+
+
+class SettingsIni:
+    def __init__(self):
+        ini_key = 'CHANNEL_TASKS_INI_PATH'
+        assert ini_key in os.environ, f'Settings are to be specified with the {ini_key} envvar.'
+        self.ini = configparser.ConfigParser()
+        self.ini.read(os.environ[ini_key])
+
+    def get_array(self, section, key, default):
+        return ([line.strip() for line in self.ini[section][key].splitlines()]
+                if self.ini.has_option(section, key) else default)
+
+    def get_boolean(self, section, key, default):
+        return self.ini[section].getboolean(key, default) if self.ini.has_section(section) else default
+
+    def get_text(self, section, key, default):
+        return self.ini[section][key].strip() if self.ini.has_option(section, key) else default
+
+    @property
+    def allowed_hosts(self):
+        return self.get_array('security', 'allowed-hosts', ['localhost'])
+
+    @property
+    def debug(self):
+        return self.get_boolean('security', 'debug', False)
+
+    @property
+    def proxy_route(self):
+        return self.get_text('security', 'proxy-route', '')
+
+    @property
+    def expose_doctask_api(self):
+        return self.get_boolean('asgi', 'expose-doctask-api', False)
+
+    def set_databases(self, settings):
+        if not self.ini.has_section('database'):
+            return
+
+        default = dict(self.ini['database'])
+
+        if 'install-apps' in default:
+            install_apps = [s.strip() for s in default.pop('install-apps').strip().splitlines()]
+            settings.INSTALLED_APPS.extend(install_apps)
+
+        settings.DATABASES = dict(default={k.upper(): v for k, v in default.items()})
+
+    def apply(self, settings):
+        settings.ALLOWED_HOSTS = self.allowed_hosts
+        settings.DEBUG = self.debug
+        self.set_databases(settings)
