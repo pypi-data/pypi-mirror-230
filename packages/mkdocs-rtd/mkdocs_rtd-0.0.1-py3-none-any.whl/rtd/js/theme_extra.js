aa2f0cc53dbe91646cff52b6eafeea153fdec1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 14:08:28 +0000

    Add package mkdocs-rtd/0.0.1/mkdocs_rtd-0.0.1-py3-none-any.whl

---
+++ packages/mkdocs-rtd/mkdocs_rtd-0.0.1-py3-none-any.whl/rtd/js/theme_extra.js
@@ -0,0 +1,25 @@
+/*
+ * Assign 'docutils' class to tables so styling and
+ * JavaScript behavior is applied.
+ *
+ * https://github.com/mkdocs/mkdocs/issues/2028
+ */
+(function($) {
+    $('div.wy-social').css(
+        'left',
+        parseInt($("nav.wy-nav-side").outerWidth() + $("div.wy-nav-content").outerWidth()) + 'px'
+    );
+    $('div.rst-content table').addClass('docutils');
+    $('div.wy-side-scroll li.toctree-l1').on('click', '>a:has(button)', function(e) {
+        let $button = $(this).children('button');
+        if ($button.length > 0) {
+            $button.click();
+        }
+    });
+    $('.rst-content a[href^="http"]').attr('target', '_blank');
+
+    const $current = $('li.current,a.current');
+    if ($current.length > 0) {
+        $current[0].scrollIntoView();
+    }
+})(jQuery);
