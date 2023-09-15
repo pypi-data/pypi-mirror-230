4cfa2d26cd60ed78edbde8114e33e84d14b12a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:08:35 +0000

    Add package sqlconstructor/1.0.37/sqlconstructor-1.0.37-py3-none-any.whl

---
+++ packages/sqlconstructor/sqlconstructor-1.0.37-py3-none-any.whl/sqlconstructor/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+"""Initialize all classes which are prepared for API"""
+
+__author__ = 'https://github.com/akvilary'
+
+from .sql_query import SqlQuery
+from .sql_section import SqlSection
+from .sql_container import SqlContainer
+from .sql_cte import SqlCte
+from .sql_enum import SqlEnum
+from .sql_cols import SqlCols
+from .sql_vals import SqlVals
