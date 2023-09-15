4459aab8e3a466ff16b791686bdbdc778664da6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:09:28 +0000

    Add package aaabbbccc/0.1/aaabbbccc-0.1.tar.gz

---
+++ packages/aaabbbccc/aaabbbccc-0.1.tar.gz/aaabbbccc-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aaabbbccc",
+    version="0.1",
+    author="Your Name",
+    author_email="your_email@example.com",
+    description="A brief description of your package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
