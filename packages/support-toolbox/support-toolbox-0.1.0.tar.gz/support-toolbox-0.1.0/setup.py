2f7b522533d89c4c7264c120fdd1dbda26b4e24c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 14:08:31 +0000

    Add package support-toolbox/0.1.0/support-toolbox-0.1.0.tar.gz

---
+++ packages/support-toolbox/support-toolbox-0.1.0.tar.gz/support-toolbox-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="support-toolbox",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "certifi==2023.7.22",
+        "charset-normalizer==3.2.0",
+        "defusedxml==0.7.1",
+        "idna==3.4",
+        "jira==3.5.2",
+        "oauthlib==3.2.2",
+        "packaging==23.1",
+        "requests==2.31.0",
+        "requests-oauthlib==1.3.1",
+        "requests-toolbelt==1.0.0",
+        "typing_extensions==4.7.1",
+        "urllib3==2.0.4"
+    ],
+    entry_points={
+        "console_scripts": [
+            "support-toolbox = support_toolbox.main:main"
+        ]
+    },
+)
