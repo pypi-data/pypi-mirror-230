b0cc245c37df10dbdb02162c24e91668bd3648e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 15:06:53 +0000

    Add package lmstudio/0.0.1/lmstudio-0.0.1.tar.gz

---
+++ packages/lmstudio/lmstudio-0.0.1.tar.gz/lmstudio-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='lmstudio',
+    version='0.0.1',
+    description='LM Studio API Python Client',
+    author='LM Studio',
+    author_email='team@lmstudio.ai',
+    zip_safe=False
+)
