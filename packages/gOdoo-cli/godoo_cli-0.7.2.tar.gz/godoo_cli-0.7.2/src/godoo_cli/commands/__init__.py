80178fe68f4067333c579eb517d5d044c8fbec22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 03:09:08 +0000

    Add package gOdoo-cli/0.7.2/godoo_cli-0.7.2.tar.gz

---
+++ packages/gOdoo-cli/godoo_cli-0.7.2.tar.gz/godoo_cli-0.7.2/src/godoo_cli/commands/__init__.py
@@ -0,0 +1,9 @@
+from .bootstrap import bootstrap_odoo
+from .config import set_odoo_config
+from .db import db_cli_app
+from .launch import launch_import, launch_odoo
+from .rpc import rpc_cli_app
+from .shell import odoo_shell, uninstall_modules
+from .source_get import source_cli_app
+from .test import odoo_test
+from .backup import backup_cli_app
