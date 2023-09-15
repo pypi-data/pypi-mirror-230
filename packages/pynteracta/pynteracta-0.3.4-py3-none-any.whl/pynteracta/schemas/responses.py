9c49e16b3bf3227e3a49fdd528de6c506038e567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-15 05:06:05 +0000

    Add package pynteracta/0.3.4/pynteracta-0.3.4-py3-none-any.whl

---
+++ packages/pynteracta/pynteracta-0.3.4-py3-none-any.whl/pynteracta/schemas/responses.py
@@ -0,0 +1,98 @@
+from pydantic import HttpUrl
+
+from .core import InteractaOut, PagedItemsOut
+from .models import (
+    BaseListPostsElement,
+    Group,
+    Hashtag,
+    ListSystemGroupsElement,
+    ListSystemUsersElement,
+    Post,
+    PostDefinition,
+    PostDetail,
+    PostWorkflowDefinitionState,
+    PostWorkflowDefinitionTransition,
+    User,
+)
+
+
+class ListSystemGroupsOut(PagedItemsOut):
+    # ListSystemGroupsResponseDTO
+    items: list[ListSystemGroupsElement] | None = []
+
+
+class ListSystemUsersOut(PagedItemsOut):
+    # ListSystemUsersResponseDTO
+    items: list[ListSystemUsersElement] | None
+
+
+class ListUsersOut(PagedItemsOut):
+    # ListUsersResponseDTO ##OK##
+    items: list[User] | None
+
+
+class ListGroupMembersOut(PagedItemsOut):
+    # ListGroupMembersResponseDTO
+    items: list[User] | None = []
+
+
+class PostDetailOut(InteractaOut, Post):
+    # GetPostDetailResponseDTO
+    current_workflow_state: PostWorkflowDefinitionState | None = None
+    current_workflow_screen_data: dict | None = None
+    mentions: list[User] | None = None
+    comment_mentions: ListUsersOut | None = None
+    watchers: list[User] | None = None
+    watcher_users: list[User] | None = None
+    watcher_groups: list[Group] | None = None
+    hashtags: list[Hashtag] | None = None
+
+
+class PostsOut(PagedItemsOut):
+    # PagedListPostsResponseDTO
+    items: list[BaseListPostsElement] | None = []
+
+
+class HashtagsOut(PagedItemsOut):
+    items: list[Hashtag] | None = []
+
+
+class PostCreatedOut(InteractaOut):
+    # CreatePostResponseDTO
+    post_id: int
+    next_occ_token: int | None = None
+    post_data: PostDetail | None = None
+
+
+class GetPostDefinitionOut(InteractaOut, PostDefinition):
+    # -- GetPostDefinitionResponseDTO
+    pass
+
+
+class CreateUserOut(InteractaOut):
+    # CreateUserResponseDTO
+    # ID dell'utente creato.
+    user_id: int
+    # Url dell'immagine utente.
+    account_photo_url: HttpUrl | None = None
+    # Nel caso in cui sono state create delle crenziali custom (username / password) riporta
+    # la password generata.
+    generated_password: list[str] | None = None
+    # Nel caso in cui sono state create delle crenziali custom (username / password) indica se le
+    # credenziali devono essere cambiate al prossimo login.
+    expired_credentials: bool | None = None
+    # Nel caso in cui sono state create delle crenziali custom (username / password) indica se è
+    # stata spedita un'email di notifica.
+    sent_email_notify: bool | None = None
+    # Token per il controllo della conconcorrenza
+    next_occ_token: int | None = None
+
+
+class ExecutePostWorkflowOperationResponse(InteractaOut):
+    # ExecutePostWorkflowOperationResponseDTO
+    new_current_state: PostWorkflowDefinitionState | None = None
+    new_screen_data: dict | None = None
+    new_current_workflow_permitted_operations: list[PostWorkflowDefinitionTransition] | None = None
+    new_can_edit_workflow_screen_data: bool | None = None
+    new_last_modify_timestamp: int | None = None
+    post_data_has_changed: bool | None = None
