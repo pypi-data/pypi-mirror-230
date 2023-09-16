287076d3d632da92ba5129efefda89919cc45522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:06:13 +0000

    Add package plhtools/1.1.26/plhtools-1.1.26-py3-none-any.whl

---
+++ packages/plhtools/plhtools-1.1.26-py3-none-any.whl/plhtools/helloword.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''                                                                                                             
+Author: penglinhan                                        
+Email: 2453995079@qq.com                                
+File: helloword.py
+Date: 2022/6/16 5:32 下午
+'''
+
+def show():
+    print('hello word')
