34bf1f6137238005a337bb82b386ccb017e3b666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 04:07:50 +0000

    Add package pcsinfo/0.1.2/pcsinfo-0.1.2.tar.gz

---
+++ packages/pcsinfo/pcsinfo-0.1.2.tar.gz/pcsinfo-0.1.2/README.md
@@ -0,0 +1,8 @@
+# News of updates
+#### Init update 0.1.2
+- Into `__init__.py` added a script for install a requirements modules and fixed bugs
+
+# pcsinfo - what is this?
+`pcsinfo` **- a module for obtaining information about the computer and computer users. It has a clear interface, thanks to which there is no need to delve into the module.**
+
+- **Examples and more in the [GitHub](https://github.com/barlin41k/pcsinfo)**
---
+++ packages/pcsinfo/pcsinfo-0.1.2.tar.gz/pcsinfo-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="pcsinfo",
+    version='0.1.2',
+    description='A easy module for obtaining information about a PC',
+    packages=['pcsinfo'],
+    author_email='sasaigrypocta@gmail.com',
+    author="barlin41k",
+    zip_safe=False,
+    long_description="""
+# News of updates
+#### Init update 0.1.2
+- Into `__init__.py` added a script for install a requirements modules and fixed bugs
+
+# pcsinfo - what is this?
+`pcsinfo` **- a module for obtaining information about the computer and computer users. It has a clear interface, thanks to which there is no need to delve into the module.**
+
+- **Examples and more in the [GitHub](https://github.com/barlin41k/pcsinfo)**
+""",
+    long_description_content_type="text/markdown",
+    url="https://github.com/barlin41k/pcsinfo",
+    project_urls={
+        "GitHub": "https://github.com/barlin41k/pcsinfo",
+    })
