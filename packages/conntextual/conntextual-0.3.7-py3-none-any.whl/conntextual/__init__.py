6efb62df06647801e5cf529977b9d042a3b4b0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 15:06:36 +0000

    Add package conntextual/0.3.7/conntextual-0.3.7-py3-none-any.whl

---
+++ packages/conntextual/conntextual-0.3.7-py3-none-any.whl/conntextual/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.3
+# hash=90212e3e8f3c8279961d32a943343e3d
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A network-application TUI using textual."
+PKG_NAME = "conntextual"
+VERSION = "0.3.7"
---
+++ packages/conntextual/conntextual-0.3.7-py3-none-any.whl/conntextual/ui/task.py
@@ -0,0 +1,55 @@
+"""
+A module implementing a TUI application task.
+"""
+
+# built-in
+import asyncio
+
+# third-party
+from runtimepy.net.arbiter import AppInfo
+from runtimepy.net.arbiter.task import ArbiterTask, TaskFactory
+
+# internal
+from conntextual.ui.base import Base
+
+
+class TuiDispatchTask(ArbiterTask):
+    """A class implementing a periodic task for a textual TUI."""
+
+    tui: Base
+    tui_task: asyncio.Task[None]
+
+    async def init(self, app: AppInfo) -> None:
+        """Initialize this task with application information."""
+
+        await super().init(app)
+        self.tui = Base.create(app, self.env)
+
+        # Create application task.
+        self.tui_task = asyncio.create_task(
+            self.tui.run_async(
+                headless=app.config.get("headless", False),  # type: ignore
+            )
+        )
+
+        # Wait for the application to be composed.
+        await self.tui.composed.wait()
+
+    async def dispatch(self) -> bool:
+        """Dispatch an iteration of this task."""
+
+        self.tui.dispatch()
+        return True
+
+    async def stop_extra(self) -> None:
+        """Extra actions to perform when this task is stopping."""
+
+        # Ensure that the app task is awaited.
+        await self.tui.action_quit()
+        await self.tui_task
+
+
+class TuiDispatch(TaskFactory[TuiDispatchTask]):
+    """A factory for the TUI dispatch task."""
+
+    kind = TuiDispatchTask
