c4233cb7ba2e4b3092db3c7eb08b8c731fea72be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:07:10 +0000

    Add package shell-whiz/0.6.2/shell_whiz-0.6.2-py3-none-any.whl

---
+++ packages/shell-whiz/shell_whiz-0.6.2-py3-none-any.whl/shell_whiz/constants.py
@@ -0,0 +1,11 @@
+SW_DESCRIPTION = "Shell Whiz: AI assistant right in your terminal"
+
+SW_ERROR = "[bold yellow]Error[/]"
+
+INV_CLI_ARGS_EXIT_CODE = 1
+OPENAI_ERROR_EXIT_CODE = 2
+SW_ERROR_EXIT_CODE = 3
+
+SW_WAIT_MSG = "Wait, Shell Whiz is thinking"
+
+DELIMITER = "####"
