1f0d5d6a8f6efe0d6c77b4fbf1e4380129a7cb87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:06:53 +0000

    Add package datatiles/0.0.0/datatiles-0.0.0.tar.gz

---
+++ packages/datatiles/datatiles-0.0.0.tar.gz/datatiles-0.0.0/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(name='datatiles',
+      version='0.0.0',
+      description='Data transformations and pipelines for data science and machine learning.',
+      maintainer="Polyaxon, Inc.",
+      maintainer_email="contact@polyaxon.com",
+      author="Polyaxon, Inc.",
+      author_email="contact@polyaxon.com",
+      url="https://github.com/polyaxon/vents",
+      license="Apache 2.0",
+      platforms="any",
+      packages=find_packages(),
+      keywords=[
+          'polyaxon',
+          'data-transformations',
+          'data-pipelines',
+          'data-science',
+          'machine-learning',
+          'workflows',
+      ],
+      install_requires=[],
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ],
+      tests_require=[],
+      cmdclass={'test': PyTest})
