e128abf29813539fbfc34da5dfe367378195072d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 14:08:13 +0000

    Add package sequelframe/0.3/sequelframe-0.3.tar.gz

---
+++ packages/sequelframe/sequelframe-0.3.tar.gz/sequelframe-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sequelframe',  
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'pandas', 
+    ],
+    author='Sina Mirshahi',
+    author_email='sina7th@gmail.com',
+    description='SQL interface for CSV and Excel data using SQLite.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
