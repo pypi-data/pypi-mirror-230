17feab6e5194e5b0de07bfe434064a2276d2937a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 05:08:04 +0000

    Add package serpycow/1.2.20/serpycow-1.2.20.tar.gz

---
+++ packages/serpycow/serpycow-1.2.20.tar.gz/serpycow-1.2.20/setup.py
@@ -0,0 +1,17 @@
+from setuptools import Extension, setup
+from Cython.Build import build_ext, cythonize
+
+
+extensions = [
+    Extension("serpyco.serializer", sources=["serpyco/serializer.pyx"]),
+    Extension("serpyco.encoder", sources=["serpyco/encoder.pyx"]),
+]
+extensions = cythonize(extensions)
+
+setup(
+    ext_modules=[
+        Extension("serpyco.serializer", sources=["serpyco/serializer.pyx"]),
+        Extension("serpyco.encoder", sources=["serpyco/encoder.pyx"]),
+    ],
+    cmdclass={'build_ext': build_ext},
+)
