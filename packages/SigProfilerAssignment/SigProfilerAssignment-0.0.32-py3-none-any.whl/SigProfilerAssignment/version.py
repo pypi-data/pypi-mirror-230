0cb3545cc90a9dfc09849d3f0aee0d7204c00770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 05:07:13 +0000

    Add package SigProfilerAssignment/0.0.32/SigProfilerAssignment-0.0.32-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.32-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.32'
+version = '0.0.32'
+Update = 'v0.0.32: Resolve with SBS4608 plots showing up as custom plots.'
+
+    
+    
