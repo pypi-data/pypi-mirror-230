7ba56f895555f65ad787a70b0474a5c40d1ca30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 14:07:18 +0000

    Add package battery-tools/0.0.1/battery-tools-0.0.1.tar.gz

---
+++ packages/battery-tools/battery-tools-0.0.1.tar.gz/battery-tools-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "battery-tools",
+    version = "0.0.1",
+    author = "PABLO GONZALEZ PILA",
+    author_email = "pablogonzalezpila@gmail.com",
+    url = "https://github.com/PaulFilms/battery-tools.git",
+    # packages = "tools"
+)
