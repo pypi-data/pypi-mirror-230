b724829e85623bebc1b4d831817ab64c9627333d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:07:57 +0000

    Add package gempy-engine/2023.1.0b5/gempy_engine-2023.1.0b5.tar.gz

---
+++ packages/gempy-engine/gempy_engine-2023.1.0b5.tar.gz/gempy_engine-2023.1.0b5/gempy_engine/modules/kernel_constructor/_b_vector_assembler.py
@@ -0,0 +1,19 @@
+from gempy_engine.core.backend_tensor import BackendTensor as bt, BackendTensor
+from gempy_engine.core.data.kernel_classes.orientations import OrientationsInternals
+
+
+def b_vector_assembly(ori: OrientationsInternals, cov_size: int) -> bt.tensor_types:
+    g_s = ori.n_orientations_tiled
+    n_dim = ori.orientations.n_dimensions
+
+    if n_dim == 3:
+        g_vector = ori.orientations.gx, ori.orientations.gy, ori.orientations.gz
+    elif n_dim == 2:
+        g_vector = ori.orientations.gx, ori.orientations.gy
+    else:
+        raise ValueError("Wrong number of dimensions in the gradients.")
+
+    b_vector = bt.tfnp.concatenate([*g_vector, bt.tfnp.zeros(cov_size - g_s, dtype=BackendTensor.dtype)], -1)
+    b_vector = bt.tfnp.expand_dims(b_vector, axis=1)
+
+    return b_vector
