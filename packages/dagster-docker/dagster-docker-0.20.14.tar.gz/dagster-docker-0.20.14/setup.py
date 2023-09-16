282b92b33e9596984641a61465fd388d989ed017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 05:07:17 +0000

    Add package dagster-docker/0.20.14/dagster-docker-0.20.14.tar.gz

---
+++ packages/dagster-docker/dagster-docker-0.20.14.tar.gz/dagster-docker-0.20.14/setup.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from typing import Dict
+
+from setuptools import find_packages, setup
+
+
+def get_version() -> str:
+    version: Dict[str, str] = {}
+    with open(Path(__file__).parent / "dagster_docker/version.py", encoding="utf8") as fp:
+        exec(fp.read(), version)
+
+    return version["__version__"]
+
+
+ver = get_version()
+# dont pin dev installs to avoid pip dep resolver issues
+pin = "" if ver == "1!0+dev" else f"=={ver}"
+setup(
+    name="dagster-docker",
+    version=ver,
+    author="Dagster Labs",
+    author_email="hello@dagsterlabs.com",
+    license="Apache-2.0",
+    description="A Dagster integration for docker",
+    url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-docker",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=["dagster_docker_tests*"]),
+    # urllib3<2 pin needed until docker-py is updated
+    # see: https://github.com/docker/docker-py/issues/3113
+    install_requires=["dagster==1.4.14", "docker", "docker-image-py", "urllib3<2"],
+    zip_safe=False,
+)
