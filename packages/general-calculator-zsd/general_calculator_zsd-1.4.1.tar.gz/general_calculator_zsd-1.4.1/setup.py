ec8ba5558fd23561e0acd43ff00e949ac1106efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:06:43 +0000

    Add package general-calculator-zsd/1.4.1/general_calculator_zsd-1.4.1.tar.gz

---
+++ packages/general-calculator-zsd/general_calculator_zsd-1.4.1.tar.gz/general_calculator_zsd-1.4.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='general_calculator_zsd',
+    version='1.4.1',
+    author='harden.jiang',
+    author_email='zsd0830@163.com',
+    description='单独测试',
+    long_description_content_type="""text/markdown""",
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
