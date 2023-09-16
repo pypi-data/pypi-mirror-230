f6c7a72e4128a97671a0b8bc630889446f9ba50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 04:08:34 +0000

    Add package python-jsonschema-objects/0.4.6/python_jsonschema_objects-0.4.6.tar.gz

---
+++ packages/python-jsonschema-objects/python_jsonschema_objects-0.4.6.tar.gz/python_jsonschema_objects-0.4.6/test/test_nondefault_resolver_validator.py
@@ -0,0 +1,24 @@
+from jsonschema import Draft3Validator, RefResolver
+from jsonschema._utils import URIDict, load_schema
+
+import python_jsonschema_objects as pjo
+
+
+def test_non_default_resolver_validator(markdown_examples):
+    ms = URIDict()
+    draft3 = load_schema("draft3")
+    draft4 = load_schema("draft4")
+    ms[draft3["id"]] = draft3
+    ms[draft4["id"]] = draft4
+    resolver_with_store = RefResolver(draft3["id"], draft3, ms)
+
+    # 'Other' schema should be valid with draft3
+    builder = pjo.ObjectBuilder(
+        markdown_examples["Other"],
+        resolver=resolver_with_store,
+        validatorClass=Draft3Validator,
+        resolved=markdown_examples,
+    )
+    klasses = builder.build_classes()
+    a = klasses.Other(MyAddress="where I live")
+    assert a.MyAddress == "where I live"
