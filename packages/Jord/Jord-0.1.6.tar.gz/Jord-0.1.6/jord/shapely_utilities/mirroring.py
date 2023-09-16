082d83758ab7b65aa37b10fea3cbce61b611d052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:06:40 +0000

    Add package Jord/0.1.6/Jord-0.1.6.tar.gz

---
+++ packages/Jord/Jord-0.1.6.tar.gz/Jord-0.1.6/jord/shapely_utilities/mirroring.py
@@ -0,0 +1,30 @@
+__all__ = ["reflection"]
+
+import numpy
+
+# pts = numpy.array([[0, 0], [1, 1], [1, 2], [0, 1]])
+# display(Polygon(pts.dot([[1,0],[0,-1]])))
+
+
+def principal_axis():
+    ...
+
+
+from shapely.affinity import scale
+from shapely.ops import transform
+from shapely.geometry import Polygon
+
+
+def reflection(x0):
+    return lambda x, y: (2 * x0 - x, y)
+
+
+P = Polygon([[0, 0], [1, 1], [1, 2], [0, 1]])
+print(P)  # POLYGON ((0 0, 1 1, 1 2, 0 1, 0 0))
+
+Q1 = scale(P, xfact=-1, origin=(1, 0))  # X-axis
+# scale(P, yfact = -1, origin = (1, 0)) # Y-axis
+Q2 = transform(reflection(1), P)
+
+print(Q1)  # POLYGON ((2 0, 1 1, 1 2, 2 1, 2 0))
+print(Q2)  # POLYGON ((2 0, 1 1, 1 2, 2 1, 2 0))
