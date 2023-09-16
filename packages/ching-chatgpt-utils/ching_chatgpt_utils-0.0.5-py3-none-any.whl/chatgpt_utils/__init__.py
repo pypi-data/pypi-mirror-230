4010735038f0dd7dee1381e3c64999edaac044b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:06:37 +0000

    Add package ching-chatgpt-utils/0.0.5/ching_chatgpt_utils-0.0.5-py3-none-any.whl

---
+++ packages/ching-chatgpt-utils/ching_chatgpt_utils-0.0.5-py3-none-any.whl/chatgpt_utils/__init__.py
@@ -0,0 +1,2 @@
+from .parse import extract_json_from_string, extract_text_by_delimiter
+from .prompt import PromptDesigner, read_prompt
---
+++ packages/ching-chatgpt-utils/ching_chatgpt_utils-0.0.5-py3-none-any.whl/chatgpt_utils/parse/delimiter_parser.py
@@ -0,0 +1,12 @@
+"""
+Helpful functions for parsing ChatGPT Output
+"""
+import re
+
+def extract_text_by_delimiter(target_string, delimiter):
+    pattern = re.escape(delimiter) + r'(.*?)' + re.escape(delimiter)
+    matches = re.findall(pattern, target_string, re.DOTALL)
+    if len(matches) > 0:
+        return matches[0].strip()
+    return None
+        
