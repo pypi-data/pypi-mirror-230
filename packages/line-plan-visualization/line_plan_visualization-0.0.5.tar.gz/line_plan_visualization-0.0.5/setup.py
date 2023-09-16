237052ae7f4c3cd59b6307a2a2627bf95dc9c083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:07:28 +0000

    Add package line-plan-visualization/0.0.5/line_plan_visualization-0.0.5.tar.gz

---
+++ packages/line-plan-visualization/line_plan_visualization-0.0.5.tar.gz/line_plan_visualization-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r', encoding="utf-8") as ld:
+    long_description = ld.read()
+
+setup(name="line_plan_visualization",
+      version="0.0.5",
+      author="Macroda",
+      author_email="SteppePrince@outlook.com",
+      description="A visualization tool for line plan",
+      long_description_content_type="text/markdown",
+      long_description=long_description,
+      url="https://github.com/SteppePrince/LinePlanVisualization",
+      packages=find_packages(),
+      install_requires=["matplotlib>=3.7.3"],
+      python_requires=">=3.9"
+      )
