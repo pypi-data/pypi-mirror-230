c1a830c81630b1333491cba6f982f8d7cbea1907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 14:07:17 +0000

    Add package toycoronagraph/1.6.2/toycoronagraph-1.6.2.tar.gz

---
+++ packages/toycoronagraph/toycoronagraph-1.6.2.tar.gz/toycoronagraph-1.6.2/toycoronagraph/toycoronagraph_para.py
@@ -0,0 +1,23 @@
+
+fits_filename = None # The address of the FITS file containing the target data. If None, the default file `DATADIR/example.fit` will be used.
+taregt_pixel = 512 # The pixel number of the target image in the x-direction and y-direction
+num_cores = 16 # The number of cores used in parallel calculation.
+rot_number = taregt_pixel # The number of rotations for generating the circular symmetry final image.
+
+# Calculate the psf scale in arcsecs per pixel
+wavelength = 1.0e-6 # meter
+D = 2.4 # aperture size in meter
+rad_to_arcsecond = 206264.806247
+visual_range = 32 # in lambda/D
+psf_scale = wavelength/D*rad_to_arcsecond*visual_range/taregt_pixel # arcsec per pixel
+
+# Example .fits file has unit W/m^2/pixel
+# vF_v(W/m^2/pixel) to F_v(Jy/arcsec^2)
+light_speed = 299792458 # m/s
+jy=10**26 # The conversion factor from W / m^2 / sr / Hz to Jy
+F_transfer = jy*wavelength/light_speed/psf_scale**2
+
+# Coronagraph setting
+coronagraph_type='vortex'
+lyot_mask_size = 0.95
+psf_range=16
