c418f82c54d6af1bfdc10a760f601de466c3f6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 15:06:34 +0000

    Add package classfig/0.1.5/classfig-0.1.5.tar.gz

---
+++ packages/classfig/classfig-0.1.5.tar.gz/classfig-0.1.5/classfig/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__name__ = "classFig"
+__author__ = "Fabian Stutzki"
+__email__ = "fast@fast-apps.de"
+__version__ = "0.1.5"
+
+from classfig.classfig import classfig
