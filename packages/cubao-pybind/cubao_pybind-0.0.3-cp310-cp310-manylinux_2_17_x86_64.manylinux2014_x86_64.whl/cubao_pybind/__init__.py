d5d75d908e2b55088b8668e6dc32215c683921ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 15:06:45 +0000

    Add package cubao-pybind/0.0.3/cubao_pybind-0.0.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cubao-pybind/cubao_pybind-0.0.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cubao_pybind/__init__.py
@@ -0,0 +1,10 @@
+from _cubao_pybind import *  # noqa: F403
+from _cubao_pybind import __version__  # noqa: F401
+
+
+def pure_python_func(
+    arg1: int = 42,
+    arg2: float = 3.14,
+    arg3: str = "you shall not pass",
+) -> str:
+    return f"int: {arg1}, float: {arg2}, str: {arg3}"
