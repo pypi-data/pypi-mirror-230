ad9126cf524ee16b2c478f821a7de594da27d11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 14:08:00 +0000

    Add package SigProfilerExtractor/1.1.22/SigProfilerExtractor-1.1.22.tar.gz

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.22.tar.gz/SigProfilerExtractor-1.1.22/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.22'
+version = '1.1.22'
+Update = 'Upgrade v1.1.22: Resolve issue with 4608 plotting as custom'
+    
+    
