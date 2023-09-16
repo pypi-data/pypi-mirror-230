0cbb970bf45c7aedda0aa40b4b3c0bc3fa766b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:06:12 +0000

    Add package cloudprovider/1.1.0/cloudprovider-1.1.0.tar.gz

---
+++ packages/cloudprovider/cloudprovider-1.1.0.tar.gz/cloudprovider-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cloudprovider',
+    packages= find_packages(),
+    version='1.1.0',
+    author="Maha Arunachalam",
+    description="Multi cloud provider",
+    long_description="Multi Cloud provider",
+    install_requires=[
+        'boto3',  # Add any other required dependencies here
+        'azure-identity',
+        'azure-mgmt-compute',
+        'jsonify'
+    ],
+    license='MIT',
+)
