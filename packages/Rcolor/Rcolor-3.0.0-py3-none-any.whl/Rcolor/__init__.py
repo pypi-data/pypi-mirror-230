0c5b764c30c1b24baab26ce75aa49241706dd7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:07:21 +0000

    Add package Rcolor/3.0.0/Rcolor-3.0.0-py3-none-any.whl

---
+++ packages/Rcolor/Rcolor-3.0.0-py3-none-any.whl/Rcolor/__init__.py
@@ -0,0 +1,2 @@
+# !/usr/bin/python
+from .rcolor import foreground, background, standard_fg, standard_bg, style, auto_fg, auto_bg, foreground_background, foreground_style, foreground_background_style, background_style, rainbow_fg, standard_rainbow_fg
