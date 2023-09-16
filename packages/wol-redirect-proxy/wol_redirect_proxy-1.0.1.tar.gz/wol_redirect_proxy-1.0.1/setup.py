f3a96a0f0066d4f16acfc0789104d8640e6b8258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:07:35 +0000

    Add package wol-redirect-proxy/1.0.1/wol_redirect_proxy-1.0.1.tar.gz

---
+++ packages/wol-redirect-proxy/wol_redirect_proxy-1.0.1.tar.gz/wol_redirect_proxy-1.0.1/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['wol_proxy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.94.1,<0.95.0',
+ 'ping3>=4.0.4,<5.0.0',
+ 'pyyaml>=6.0,<7.0',
+ 'uvicorn>=0.21.0,<0.22.0',
+ 'wakeonlan>=3.0.0,<4.0.0']
+
+entry_points = \
+{'console_scripts': ['wol-proxy = wol_proxy.app:main']}
+
+setup_kwargs = {
+    'name': 'wol-redirect-proxy',
+    'version': '1.0.1',
+    'description': 'A simple python Wake-on-LAN proxy',
+    'long_description': '## Wake-on-LAN redirect proxy\n\nBrutally simple, single file, Fast API based proxy service. Create a simple `path` and/or  `hostname` based\nredirects that send WoL packets if target URL is not reachable. Only works for `HTTP(S)` redirects.\n\nGreat complement for a simple [auto-suspend](https://autosuspend.readthedocs.io/) setup,\ne.g. your home jellyfin server or any other `HTTP` service.\n\nThat\'s it!\n\n### Install\n```shell\n# 1. install script - install wol-proxy into a new virtualenv inside your $HOME\n curl https://raw.githubusercontent.com/xyzjonas/wol-redirect-proxy/main/install.sh | bash\n```\n```shell\n# 2. from PyPi\npip install wol-redirect-proxy\n```\n```shell\n# 3. from source using Poetry\npoetry install\n```\n### ...and run\n```shell\nwol-proxy --host "0.0.0.0" --port 12345\n```\n```shell\nwol-proxy --help\n```\n\n### Configuration\nSpecify a list of proxy mappings using a yaml config, e.g.:\n\n```yaml\n#  This will WoL redirect any HTTP request and carry over the path.\n#  e.g.: POST: http://my-proxy.home/login -> POST "http://my-jellyfin.home:8096/login"\n\ntargets:\n- handler: "wol"\n  source_url: "http://my-proxy.home/*"\n  target_url: "http://my-jellyfin.home:8096"\n  methods: [GET, POST, DELETE, PATCH]\n  options:\n    mac: "75:55:39:a4:33:27"\n    timeout_s: 2\n```\n\n```yaml\n#  Multiple redirects to different services by hostname\n#  (requires working DNS setup)\n\ntargets:\n- handler: "plain"\n  source_url: "http://jellyfin.home/*"\n  target_url: "http://192.168.0.124:8096"\n  methods: [GET, POST, DELETE, PATCH]\n  \n- handler: "plain"\n  source_url: "http://nextcloud.home/*"\n  target_url: "http://192.168.0.129:8080"\n  methods: [GET, POST]\n  \n- handler: "plain"\n  source_url: "http://torrent-box.home/*"\n  target_url: "http://192.168.0.135:8080"\n  methods: [GET, POST]\n```\n\n### Use as a systemd service\n\n```shell\n# use install script and follow instructions\ncurl https://raw.githubusercontent.com/xyzjonas/wol-redirect-proxy/main/install.sh | bash\n```\n\n```shell\n# use the sample systemd unit file\ngit clone https://github.com/jonasbrauer/wol-redirect-proxy.git && cd wol-redirect-proxy \nsudo cp sample-systemd.unit /etc/systemd/system/wol-redirect-proxy.service  # ...and edit user/group\nsudo systemctl daemon-reload\nsudo systemctl enable --now wol-redirect-proxy.service\nsudo systemctl status wol-redirect-proxy.service\n```\n',
+    'author': 'None',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
