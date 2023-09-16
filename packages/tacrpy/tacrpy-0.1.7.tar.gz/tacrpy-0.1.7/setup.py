e7a85faa42d8f76cf09ce6ce531232b61e05100f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 04:07:49 +0000

    Add package tacrpy/0.1.7/tacrpy-0.1.7.tar.gz

---
+++ packages/tacrpy/tacrpy-0.1.7.tar.gz/tacrpy-0.1.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tacrpy',
+    version='0.1.7',
+    description='Analytická knihovna pro potřeby TA ČR',
+    long_description='Knihovna, která slouží pro práci s daty a vypracování analýz TA ČR.',
+    author='RozumDoKapsy',
+    author_email='david.sulc@tacr.cz',
+    packages=['tacrpy']
+)
