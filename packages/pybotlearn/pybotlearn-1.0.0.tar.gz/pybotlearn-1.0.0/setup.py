20e1651c724aa34b3ce03b93cacb1bb972d19dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 15:06:25 +0000

    Add package pybotlearn/1.0.0/pybotlearn-1.0.0.tar.gz

---
+++ packages/pybotlearn/pybotlearn-1.0.0.tar.gz/pybotlearn-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybotlearn',
+    version='1.0.0',
+    description='Uma biblioteca Python para criar bots mais ou menos inteligentes.',
+    author='jose',
+    author_email='reginaldasmcosta@gmail.com',
+    packages=['pybotlearn'],
+    python_requires='>=3.6',
+)
