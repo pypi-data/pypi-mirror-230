cca6ad99948c2e8e59de64c89c989faedfaa22e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:07:00 +0000

    Add package sse-relay-server/0.1.3/sse_relay_server-0.1.3-py3-none-any.whl

---
+++ packages/sse-relay-server/sse_relay_server-0.1.3-py3-none-any.whl/sse_relay_server/main.py
@@ -0,0 +1,97 @@
+import json
+
+import uvicorn
+import psycopg
+import argparse
+from loguru import logger
+
+
+from sse_starlette.sse import EventSourceResponse, ServerSentEvent
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+from starlette.routing import Route
+from .config import get_database_params, get_allowed_origins, get_debug_value
+
+
+DATABASE_PARAMS = get_database_params()
+SSE_SERVER_DEBUG = get_debug_value()
+ALLOWED_ORIGINS = get_allowed_origins()
+
+
+async def event_publisher(request: Request):
+    aconnection = await psycopg.AsyncConnection.connect(
+        **DATABASE_PARAMS,
+        autocommit=True,
+    )
+    channel = request.query_params.get("channel")
+    if not channel:
+        return
+
+    async with aconnection.cursor() as acursor:
+        await acursor.execute(f"LISTEN {channel}")
+        generator = aconnection.notifies()
+        async for notify_message in generator:
+            payload = json.loads(notify_message.payload)
+            logger.debug(f"Received from {channel}: {payload}")
+            yield ServerSentEvent(**payload)
+
+
+async def sse(request: Request):
+    return EventSourceResponse(event_publisher(request))
+
+
+routes = [Route("/", endpoint=sse)]
+
+
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=ALLOWED_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["GET"],
+        allow_headers=["*"],
+    )
+]
+
+app = Starlette(debug=SSE_SERVER_DEBUG, routes=routes, middleware=middleware)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="sse-relay-server",
+        description="Relay server for SSE events",
+    )
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="0.0.0.0",
+        help="server host",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=8001,
+        help="server port",
+    )
+    parser.add_argument(
+        "--reload",
+        action="store_true",
+        help="enable auto-reload",
+        default=False,
+    )
+    parser.add_argument("--workers", type=int)
+    parser.add_argument(
+        "--log-level",
+        default="info",
+        choices=["critical", "error", "warning", "info", "debug", "trace"],
+    )
+
+    args = parser.parse_args()
+
+    uvicorn.run("sse_relay_server.main:app", **args.__dict__)
+
+
+if __name__ == "__main__":
+    main()
