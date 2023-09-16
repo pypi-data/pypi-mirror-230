f7549e033283f666a7646964ca2c2fa1f91d43e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 04:09:43 +0000

    Add package xmind2testcasezen/0.0.1/xmind2testcasezen-0.0.1.tar.gz

---
+++ packages/xmind2testcasezen/xmind2testcasezen-0.0.1.tar.gz/xmind2testcasezen-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="xmind2testcasezen",
+    version="0.0.1",
+    python_requires=">=3.6.0",
+    author="maiyajj",
+    author_email="",
+    url="https://github.com/maiyajj/xmind2testcasezen",
+    description="XMind2TestCaseZen基于XMind2TestCase做二次开发，提供了一个高效测试用例设计的解决方案！兼容XMindZen",
+    long_description=r"",
+    long_description_content_type="text/markdown",
+    packages=["xmind2testcasezen"],
+    entry_points={},
+    install_requires=[],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
