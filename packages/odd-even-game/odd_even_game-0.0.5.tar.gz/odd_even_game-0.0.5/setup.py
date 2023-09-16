2eea11475481bb9882f1fd3084160666d04774e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 04:09:32 +0000

    Add package odd-even-game/0.0.5/odd_even_game-0.0.5.tar.gz

---
+++ packages/odd-even-game/odd_even_game-0.0.5.tar.gz/odd_even_game-0.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.5'
+DESCRIPTION = 'A simple python game'
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+# Setting up
+setup(
+    name="odd_even_game",
+    version=VERSION,
+    author="Siddharth Yadav",
+    author_email="siddharthdis3432@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/siddharthdis/odd_even_game",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['game', 'odd even', 'odd even game', 'siddharth', 'siddharth yadav'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
