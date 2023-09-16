f2351d5f4a5a6afc24ff27207c8320efdb513469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 04:07:55 +0000

    Add package simple-aws-wrapper/2.2.2/simple_aws_wrapper-2.2.2.tar.gz

---
+++ packages/simple-aws-wrapper/simple_aws_wrapper-2.2.2.tar.gz/simple_aws_wrapper-2.2.2/src/simple_aws_wrapper/services/s3.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from simple_aws_wrapper.config import AWSConfig
+from simple_aws_wrapper.const import services
+from simple_aws_wrapper.exceptions.exceptions import MissingConfigurationException, GenericException
+from simple_aws_wrapper.resource_manager import ResourceManager
+
+
+class S3:
+    """
+    Classe per la gestione di bucket S3 su AWS
+    """
+
+    def __init__(self):
+        if not AWSConfig().is_configured():
+            raise MissingConfigurationException
+        self.region_name = AWSConfig().get_region_name()
+        self.endpoint_url = AWSConfig().get_endpoint_url()
+        if self.endpoint_url and self.endpoint_url != "":
+            self.client = ResourceManager.get_client(
+                services.S3, self.region_name, self.endpoint_url
+            )
+        else:
+            self.client = ResourceManager.get_client(services.S3, self.region_name)
+
+    def put_object(self, body: bytes | str, bucket_name: str, object_key: str) -> bool:
+        """
+        Funzione per inserire un oggetto all'interno di un bucket
+        :param body: contenuto del file codificato in byte
+        :param bucket_name: nome del buket su cui effettuare l'upload
+        :param object_key: objectkey per identificare l'oggetto all'interno del bucket
+        :return: bool True se l'upload è andato OK, False altrimenti
+        """
+        if isinstance(body, str):
+            body = body.encode("utf-8")
+        try:
+            self.client.put_object(Body=body, Bucket=bucket_name, Key=object_key)
+            return True
+        except Exception as e:
+            print(str(e))
+            raise GenericException
