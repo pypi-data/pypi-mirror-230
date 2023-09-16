7d55bee2584fcfdffacbc29ead2fbdfc7dc14063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 03:07:54 +0000

    Add package logprep/6.8.1/logprep-6.8.1.tar.gz

---
+++ packages/logprep/logprep-6.8.1.tar.gz/logprep-6.8.1/logprep/abc/processor.py
@@ -0,0 +1,314 @@
+"""Abstract module for processors"""
+import copy
+from abc import abstractmethod
+from logging import DEBUG, Logger
+from multiprocessing import current_process
+from pathlib import Path
+from typing import TYPE_CHECKING, List, Optional
+
+from attr import define, field, validators
+
+from logprep.abc.component import Component
+from logprep.framework.rule_tree.rule_tree import RuleTree
+from logprep.metrics.metric import Metric, calculate_new_average
+from logprep.processor.base.exceptions import (
+    FieldExistsWarning,
+    ProcessingCriticalError,
+    ProcessingWarning,
+)
+from logprep.processor.processor_strategy import SpecificGenericProcessStrategy
+from logprep.util import getter
+from logprep.util.helper import (
+    add_and_overwrite,
+    add_field_to,
+    get_dotted_field_value,
+    pop_dotted_field_value,
+)
+from logprep.util.json_handling import list_json_files_in_directory
+from logprep.util.time_measurement import TimeMeasurement
+
+if TYPE_CHECKING:
+    from logprep.processor.base.rule import Rule  # pragma: no cover
+
+
+class Processor(Component):
+    """Abstract Processor Class to define the Interface"""
+
+    @define(kw_only=True, slots=False)
+    class Config(Component.Config):
+        """Common Configurations"""
+
+        specific_rules: List[str] = field(
+            validator=[
+                validators.instance_of(list),
+                validators.deep_iterable(member_validator=validators.instance_of(str)),
+            ]
+        )
+        """List of rule locations to load rules from.
+        In addition to paths to file directories it is possible to retrieve rules from a URI.
+        For valid URI formats see :ref:`getters`.
+        """
+        generic_rules: List[str] = field(
+            validator=[
+                validators.instance_of(list),
+                validators.deep_iterable(member_validator=validators.instance_of(str)),
+            ]
+        )
+        """List of rule locations to load rules from.
+        In addition to paths to file directories it is possible to retrieve rules from a URI.
+        For valid URI formats see :ref:`getters`.
+        """
+        tree_config: Optional[str] = field(
+            default=None, validator=[validators.optional(validators.instance_of(str))]
+        )
+        """Path to a JSON file with a valid rule tree configuration.
+        For string format see :ref:`getters`"""
+        apply_multiple_times: Optional[bool] = field(
+            default=False, validator=[validators.optional(validators.instance_of(bool))]
+        )
+        """Set if the processor should be applied multiple times. This enables further processing
+        of an output with the same processor."""
+
+    @define(kw_only=True)
+    class ProcessorMetrics(Metric):
+        """Tracks statistics about this processor"""
+
+        _prefix: str = "logprep_processor_"
+
+        number_of_processed_events: int = 0
+        """Number of events that were processed by the processor"""
+        mean_processing_time_per_event: float = 0.0
+        """Mean processing time for one event"""
+        _mean_processing_time_sample_counter: int = 0
+        number_of_warnings: int = 0
+        """Number of warnings that occurred while processing events"""
+        number_of_errors: int = 0
+        """Number of errors that occurred while processing events"""
+        generic_rule_tree: RuleTree.RuleTreeMetrics
+        """Tracker of the generic rule tree metrics"""
+        specific_rule_tree: RuleTree.RuleTreeMetrics
+        """Tracker of the specific rule tree metrics"""
+
+        def update_mean_processing_time_per_event(self, new_sample):
+            """Updates the mean processing time per event"""
+            new_avg, new_sample_counter = calculate_new_average(
+                self.mean_processing_time_per_event,
+                new_sample,
+                self._mean_processing_time_sample_counter,
+            )
+            self.mean_processing_time_per_event = new_avg
+            self._mean_processing_time_sample_counter = new_sample_counter
+
+    __slots__ = [
+        "rule_class",
+        "has_custom_tests",
+        "metrics",
+        "metric_labels",
+        "_event",
+        "_specific_tree",
+        "_generic_tree",
+    ]
+
+    rule_class: "Rule"
+    has_custom_tests: bool
+    metrics: ProcessorMetrics
+    metric_labels: dict
+    _event: dict
+    _specific_tree: RuleTree
+    _generic_tree: RuleTree
+    _strategy = None
+
+    def __init__(self, name: str, configuration: "Processor.Config", logger: Logger):
+        super().__init__(name, configuration, logger)
+        self._strategy = SpecificGenericProcessStrategy(self._config.apply_multiple_times)
+        self.metric_labels, specific_tree_labels, generic_tree_labels = self._create_metric_labels()
+        self._specific_tree = RuleTree(
+            config_path=self._config.tree_config, metric_labels=specific_tree_labels
+        )
+        self._generic_tree = RuleTree(
+            config_path=self._config.tree_config, metric_labels=generic_tree_labels
+        )
+        self.load_rules(
+            generic_rules_targets=self._config.generic_rules,
+            specific_rules_targets=self._config.specific_rules,
+        )
+        self.metrics = self.ProcessorMetrics(
+            labels=self.metric_labels,
+            generic_rule_tree=self._generic_tree.metrics,
+            specific_rule_tree=self._specific_tree.metrics,
+        )
+        self.has_custom_tests = False
+
+    def _create_metric_labels(self):
+        """Reads out the metrics from the configuration and sets up labels for the rule trees"""
+        metric_labels = self._config.metric_labels
+        metric_labels.update({"processor": self.name})
+        specif_tree_labels = copy.deepcopy(metric_labels)
+        specif_tree_labels.update({"rule_tree": "specific"})
+        generic_tree_labels = copy.deepcopy(metric_labels)
+        generic_tree_labels.update({"rule_tree": "generic"})
+        return metric_labels, specif_tree_labels, generic_tree_labels
+
+    @property
+    def _specific_rules(self):
+        """Returns all specific rules
+
+        Returns
+        -------
+        specific_rules: list[Rule]
+        """
+        return self._specific_tree.rules
+
+    @property
+    def _generic_rules(self):
+        """Returns all generic rules
+
+        Returns
+        -------
+        generic_rules: list[Rule]
+        """
+        return self._generic_tree.rules
+
+    @property
+    def rules(self):
+        """Returns all rules
+
+        Returns
+        -------
+        rules: list[Rule]
+        """
+        return [*self._generic_rules, *self._specific_rules]
+
+    @TimeMeasurement.measure_time()
+    def process(self, event: dict):
+        """Process a log event by calling the implemented `process` method of the
+        strategy object set in  `_strategy` attribute.
+
+        Parameters
+        ----------
+        event : dict
+           A dictionary representing a log event.
+
+        """
+        if self._logger.isEnabledFor(DEBUG):  # pragma: no cover
+            self._logger.debug(f"{self.describe()} processing event {event}")
+        self._strategy.process(
+            event,
+            generic_tree=self._generic_tree,
+            specific_tree=self._specific_tree,
+            callback=self._apply_rules_wrapper,
+            processor_metrics=self.metrics,
+        )
+
+    def _apply_rules_wrapper(self, event, rule):
+        try:
+            self._apply_rules(event, rule)
+        except ProcessingWarning as error:
+            self._handle_warning_error(event, rule, error)
+        except ProcessingCriticalError as error:
+            raise error
+        except BaseException as error:
+            raise ProcessingCriticalError(self, str(error), event) from error
+        if not hasattr(rule, "delete_source_fields"):
+            return
+        if rule.delete_source_fields:
+            for dotted_field in rule.source_fields:
+                pop_dotted_field_value(event, dotted_field)
+
+    @abstractmethod
+    def _apply_rules(self, event, rule):
+        ...  # pragma: no cover
+
+    def test_rules(self) -> dict:
+        """Perform custom rule tests.
+
+        Returns a dict with a list of test results as tuples containing a result and an expected
+        result for each rule, i.e. {'RULE REPR': [('Result string', 'Expected string')]}
+        Optional: Can be used in addition to regular rule tests.
+
+        """
+
+    @staticmethod
+    def resolve_directories(rule_paths: list) -> list:
+        resolved_paths = []
+        for rule_path in rule_paths:
+            getter_instance = getter.GetterFactory.from_string(rule_path)
+            if getter_instance.protocol == "file":
+                if Path(getter_instance.target).is_dir():
+                    paths = list_json_files_in_directory(getter_instance.target)
+                    for file_path in paths:
+                        resolved_paths.append(file_path)
+                else:
+                    resolved_paths.append(rule_path)
+            else:
+                resolved_paths.append(rule_path)
+        return resolved_paths
+
+    def load_rules(self, specific_rules_targets: List[str], generic_rules_targets: List[str]):
+        """method to add rules from directories or urls"""
+        specific_rules_targets = self.resolve_directories(specific_rules_targets)
+        generic_rules_targets = self.resolve_directories(generic_rules_targets)
+        for specific_rules_target in specific_rules_targets:
+            rules = self.rule_class.create_rules_from_target(specific_rules_target)
+            for rule in rules:
+                self._specific_tree.add_rule(rule, self._logger)
+        for generic_rules_target in generic_rules_targets:
+            rules = self.rule_class.create_rules_from_target(generic_rules_target)
+            for rule in rules:
+                self._generic_tree.add_rule(rule, self._logger)
+        if self._logger.isEnabledFor(DEBUG):  # pragma: no cover
+            number_specific_rules = self._specific_tree.metrics.number_of_rules
+            self._logger.debug(
+                f"{self.describe()} loaded {number_specific_rules} "
+                f"specific rules ({current_process().name})"
+            )
+            number_generic_rules = self._generic_tree.metrics.number_of_rules
+            self._logger.debug(
+                f"{self.describe()} loaded {number_generic_rules} generic rules "
+                f"generic rules ({current_process().name})"
+            )
+
+    @staticmethod
+    def _field_exists(event: dict, dotted_field: str) -> bool:
+        fields = dotted_field.split(".")
+        dict_ = event
+        for field_ in fields:
+            if field_ in dict_ and isinstance(dict_, dict):
+                dict_ = dict_[field_]
+            else:
+                return False
+        return True
+
+    def _handle_warning_error(self, event, rule, error, failure_tags=None):
+        tags = get_dotted_field_value(event, "tags")
+        if failure_tags is None:
+            failure_tags = rule.failure_tags
+        if tags is None:
+            add_and_overwrite(event, "tags", sorted(list({*failure_tags})))
+        else:
+            add_and_overwrite(event, "tags", sorted(list({*tags, *failure_tags})))
+        if isinstance(error, ProcessingWarning):
+            self._logger.warning(str(error))
+        else:
+            self._logger.warning(str(ProcessingWarning(self, str(error), rule, event)))
+
+    def _has_missing_values(self, event, rule, source_field_dict):
+        missing_fields = list(
+            dict(filter(lambda x: x[1] in [None, ""], source_field_dict.items())).keys()
+        )
+        if missing_fields:
+            error = BaseException(f"{self.name}: no value for fields: {missing_fields}")
+            self._handle_warning_error(event, rule, error)
+            return True
+        return False
+
+    def _write_target_field(self, event: dict, rule: "Rule", result: any) -> None:
+        add_successful = add_field_to(
+            event,
+            output_field=rule.target_field,
+            content=result,
+            extends_lists=rule.extend_target_list,
+            overwrite_output_field=rule.overwrite_target,
+        )
+        if not add_successful:
+            raise FieldExistsWarning(self, rule, event, [rule.target_field])
---
+++ packages/logprep/logprep-6.8.1.tar.gz/logprep-6.8.1/logprep/connector/confluent_kafka/output.py
@@ -0,0 +1,232 @@
+"""
+ConfluentKafkaOutput
+====================
+
+This section contains the connection settings for ConfluentKafka, the default
+index, the error index and a buffer size. Documents are sent in batches to Elasticsearch to reduce
+the amount of times connections are created.
+
+Example
+^^^^^^^
+..  code-block:: yaml
+    :linenos:
+
+    output:
+      my_confluent_kafka_output:
+        type: confluentkafka_output
+        bootstrapservers: [127.0.0.1:9200]
+        topic: my_default_topic
+        error_topic: my_error_topic
+        flush_timeout: 0.2
+        send_timeout: 0
+        compression: gzip
+        maximum_backlog: 100000
+        ack_policy: -1
+        linger_duration: 0.5
+        ssl: {"cafile": None, "certfile": None, "keyfile": None, "password": None}
+"""
+
+import json
+from datetime import datetime
+from functools import cached_property, partial
+from socket import getfqdn
+from typing import List, Optional
+
+import msgspec
+from attrs import define, field, validators
+from confluent_kafka import KafkaException, Producer
+
+from logprep.abc.output import CriticalOutputError, FatalOutputError, Output
+from logprep.util.validators import dict_with_keys_validator
+
+
+class ConfluentKafkaOutput(Output):
+    """A kafka connector that serves as output connector."""
+
+    @define(kw_only=True, slots=False)
+    class Config(Output.Config):
+        """Confluent Kafka Output Config"""
+
+        bootstrapservers: List[str]
+        topic: str = field(validator=validators.instance_of(str))
+        error_topic: str
+        flush_timeout: float
+        send_timeout: int = field(validator=validators.instance_of(int), default=0)
+        compression: str = field(
+            validator=[
+                validators.instance_of(str),
+                validators.in_(["snappy", "gzip", "lz4", "zstd", "none"]),
+            ],
+            default="none",
+        )
+        maximum_backlog: int = field(
+            validator=[validators.instance_of(int), validators.gt(0)], default=100000
+        )
+        ack_policy: int = field(
+            validator=[validators.instance_of(int), validators.in_([0, 1, -1])],
+            converter=lambda x: -1 if x == "all" else x,
+            default=-1,
+        )
+        linger_duration: float = field(
+            validator=[validators.instance_of(float)], converter=float, default=0.5
+        )
+        ssl: dict = field(
+            validator=[
+                validators.instance_of(dict),
+                partial(
+                    dict_with_keys_validator,
+                    expected_keys=["cafile", "certfile", "keyfile", "password"],
+                ),
+            ],
+            default={"cafile": None, "certfile": None, "keyfile": None, "password": None},
+        )
+        kafka_config: Optional[dict] = field(
+            validator=[
+                validators.instance_of(dict),
+                validators.deep_mapping(
+                    key_validator=validators.instance_of(str),
+                    value_validator=validators.instance_of((str, dict)),
+                ),
+            ],
+            factory=dict,
+        )
+        """ (Optional) Additional kafka configuration for the kafka client. 
+        This is for advanced usage only. For possible configuration options see: 
+        <https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md>
+        """
+
+    @cached_property
+    def _client_id(self):
+        return getfqdn()
+
+    @cached_property
+    def _producer(self):
+        return Producer(self._confluent_settings)
+
+    @cached_property
+    def _confluent_settings(self) -> dict:
+        """generate confluence settings mapping
+
+        Returns
+        -------
+        dict
+            the translated confluence settings
+        """
+        configuration = {
+            "bootstrap.servers": ",".join(self._config.bootstrapservers),
+            "queue.buffering.max.messages": self._config.maximum_backlog,
+            "compression.type": self._config.compression,
+            "acks": self._config.ack_policy,
+            "linger.ms": self._config.linger_duration,
+        }
+        ssl_settings_are_setted = any(self._config.ssl[key] for key in self._config.ssl)
+        if ssl_settings_are_setted:
+            configuration.update(
+                {
+                    "security.protocol": "SSL",
+                    "ssl.ca.location": self._config.ssl["cafile"],
+                    "ssl.certificate.location": self._config.ssl["certfile"],
+                    "ssl.key.location": self._config.ssl["keyfile"],
+                    "ssl.key.password": self._config.ssl["password"],
+                }
+            )
+        return self._config.kafka_config | configuration
+
+    def describe(self) -> str:
+        """Get name of Kafka endpoint with the bootstrap server.
+
+        Returns
+        -------
+        kafka : ConfluentKafka
+            Acts as input and output connector.
+
+        """
+        base_description = super().describe()
+        return f"{base_description} - Kafka Output: {self._config.bootstrapservers[0]}"
+
+    def store(self, document: dict) -> Optional[bool]:
+        """Store a document in the producer topic.
+
+        Parameters
+        ----------
+        document : dict
+           Document to store.
+
+        Returns
+        -------
+        Returns True to inform the pipeline to call the batch_finished_callback method in the
+        configured input
+        """
+        self.store_custom(document, self._config.topic)
+        self.metrics.number_of_processed_events += 1
+        if self.input_connector:
+            self.input_connector.batch_finished_callback()
+
+    def store_custom(self, document: dict, target: str) -> None:
+        """Write document to Kafka into target topic.
+
+        Parameters
+        ----------
+        document : dict
+            Document to be stored in target topic.
+        target : str
+            Topic to store document in.
+        Raises
+        ------
+        CriticalOutputError
+            Raises if any error except a BufferError occurs while writing into Kafka.
+
+        """
+        try:
+            self._producer.produce(target, value=self._encoder.encode(document))
+            self._producer.poll(self._config.send_timeout)
+        except BufferError:
+            # block program until buffer is empty
+            self._producer.flush(timeout=self._config.flush_timeout)
+        except BaseException as error:
+            raise CriticalOutputError(
+                self, f"Error storing output document -> {error}", document
+            ) from error
+
+    def store_failed(
+        self, error_message: str, document_received: dict, document_processed: dict
+    ) -> None:
+        """Write errors into error topic for documents that failed processing.
+
+        Parameters
+        ----------
+        error_message : str
+           Error message to write into Kafka document.
+        document_received : dict
+            Document as it was before processing.
+        document_processed : dict
+            Document after processing until an error occurred.
+
+        """
+        value = {
+            "error": error_message,
+            "original": document_received,
+            "processed": document_processed,
+            "timestamp": str(datetime.now()),
+        }
+        try:
+            self._producer.produce(
+                self._config.error_topic,
+                value=json.dumps(value, separators=(",", ":")).encode("utf-8"),
+            )
+            self._producer.poll(self._config.send_timeout)
+        except BufferError:
+            # block program until buffer is empty
+            self._producer.flush(timeout=self._config.flush_timeout)
+
+    def setup(self):
+        super().setup()
+        try:
+            _ = self._producer
+        except (KafkaException, ValueError) as error:
+            raise FatalOutputError(self, str(error)) from error
+
+    def shut_down(self) -> None:
+        """ensures that all messages are flushed"""
+        if self._producer is not None:
+            self._producer.flush(self._config.flush_timeout)
