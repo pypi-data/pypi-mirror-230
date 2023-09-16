ccf5adb329932bfc698667a0daa13b4a8a968306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 15:07:23 +0000

    Add package horde-client/1.0.2/horde-client-1.0.2.tar.gz

---
+++ packages/horde-client/horde-client-1.0.2.tar.gz/horde-client-1.0.2/horde_client/__init__.py
@@ -0,0 +1,9 @@
+from horde_client.client import HordeClient
+from horde_client.async_client import AsyncHordeClient
+
+from horde_client.model import (
+    ModelType,
+    TextGenParams
+)
+
+from horde_client.langchain_llm import HordeClientLLM
