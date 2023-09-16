4fe177807f25050ff41fe2e81c3fd3388d934b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-16 05:08:01 +0000

    Add package agent1c-metrics/0.1.7/agent1c_metrics-0.1.7-py3-none-any.whl

---
+++ packages/agent1c-metrics/agent1c_metrics-0.1.7-py3-none-any.whl/agent1c_metrics/__init__.py
@@ -0,0 +1,13 @@
+from importlib import resources
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+settings = load(resources.read_text("agent1c_metrics", "config.yaml"),Loader=Loader)
+
+# Version of the package
+__version__ = "0.1.7"
+
+#settings = {'folders':['c:\\Program Files\\1cv8\\srvinfo\\reg_1541']}
