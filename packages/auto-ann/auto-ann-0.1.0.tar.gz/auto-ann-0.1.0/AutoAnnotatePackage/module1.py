15ef3d4cafb8d2ca34f5ea978ecb0407cae31d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:09:05 +0000

    Add package auto-ann/0.1.0/auto-ann-0.1.0.tar.gz

---
+++ packages/auto-ann/auto-ann-0.1.0.tar.gz/auto-ann-0.1.0/AutoAnnotatePackage/module1.py
@@ -0,0 +1,3 @@
+# module1.py
+def function1():
+    return "Hello from function1"
---
+++ packages/auto-ann/auto-ann-0.1.0.tar.gz/auto-ann-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='auto-ann',
+    version='0.1.0',
+    url='https://github.com/essamsleiman/Auto_Annotate',
+    author='Essam Sleiman',
+    author_email='essamfsleiman@gmail.com',
+    description='An open-source Python library designed to label your Image datasets tailored for a given task.',
+    packages=find_packages(),    
+    install_requires=[],
+)
