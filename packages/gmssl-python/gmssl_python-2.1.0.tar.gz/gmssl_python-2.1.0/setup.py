28c60175f996528da993f4696de2eb07dfb5b71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:06:42 +0000

    Add package gmssl-python/2.1.0/gmssl_python-2.1.0.tar.gz

---
+++ packages/gmssl-python/gmssl_python-2.1.0.tar.gz/gmssl_python-2.1.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# Copyright 2023 The GmSSL Project. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the License); you may
+# not use this file except in compliance with the License.
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# pyGmSSL - the Python binding of the GmSSL library
+
+
+from distutils.core import setup
+
+setup(
+	name='GmSSL-Python',
+	version='2.1',
+	description='GmSSL Python API',
+	author='GmSSL Project',
+	author_email='guan@pku.edu.cn',
+	url='https://GmSSL.github.io/GmSSL-Python/',
+	py_modules=['gmssl'],
+)
