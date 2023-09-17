75e6f595f0068c0fb9e332c2a7545c44a5703e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:08:17 +0000

    Add package mango-pycore/0.1.13/mango_pycore-0.1.13.tar.gz

---
+++ packages/mango-pycore/mango_pycore-0.1.13.tar.gz/mango_pycore-0.1.13/mango_pycore/api/response.py
@@ -0,0 +1,33 @@
+import decimal
+import json
+
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        return super().default(o)
+
+
+class Response:
+    def __init__(self, code=200, encoded=False, body=None, headers={}):
+        self.headers = {
+            "Content-Type": "application/json",
+        }
+        self.headers.update(headers)
+        self.code = code
+        self.encoded = encoded
+        self.body = body
+
+    def to_dict(self):
+        response = {
+            "isBase64Encoded": self.encoded,
+            "statusCode": self.code,
+            "headers": self.headers,
+        }
+        if self.body is not None:
+            response.update({
+                "body": json.dumps(self.body, cls=CustomEncoder)
+            })
+
+        return response
