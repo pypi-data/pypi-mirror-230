eb90d85125a8c714c93af02eb436391cc126c98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:08:05 +0000

    Add package henrymistert-selfbot-embeds/1.0.0/henrymistert_selfbot_embeds-1.0.0.tar.gz

---
+++ packages/henrymistert-selfbot-embeds/henrymistert_selfbot_embeds-1.0.0.tar.gz/henrymistert_selfbot_embeds-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='henrymistert_selfbot_embeds',
+    version='1.0.0',
+    author='henrymistert',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+)
