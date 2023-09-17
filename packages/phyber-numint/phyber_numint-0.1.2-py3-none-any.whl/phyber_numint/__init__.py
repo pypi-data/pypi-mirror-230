113c517af8d86ba134630bd4131bfdce9a68bd84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:07:54 +0000

    Add package phyber-numint/0.1.2/phyber_numint-0.1.2-py3-none-any.whl

---
+++ packages/phyber-numint/phyber_numint-0.1.2-py3-none-any.whl/phyber_numint/__init__.py
@@ -0,0 +1,6 @@
+from .integrators import (
+    ODEIntegrator,
+    IntegrationMethod
+)
+
+__version__ = '0.1.2'
---
+++ packages/phyber-numint/phyber_numint-0.1.2-py3-none-any.whl/phyber_numint/examples/__init__.py
@@ -0,0 +1,5 @@
+from .examples import (
+    lorenz_system,
+    damped_oscillator,
+    damped_oscillator_analytical
+)
