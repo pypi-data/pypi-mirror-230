e608617be58c6568242e3269d8c6986d7a71ea92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:06:46 +0000

    Add package flash-attention-srf/0.0.7/flash_attention_srf-0.0.7-py3-none-any.whl

---
+++ packages/flash-attention-srf/flash_attention_srf-0.0.7-py3-none-any.whl/flash_attention_srf/ops/torch/kernels.py
@@ -0,0 +1,23 @@
+import torch
+
+def softmax_kernel(data, *, projection_matrix, is_query, normalize_data=True, eps=1e-4, device = None):
+    b, h, *_ = data.shape
+
+    data_normalizer = (data.shape[-1] ** -0.25) if normalize_data else 1.
+
+    ratio = (projection_matrix.shape[0] ** -0.5)
+
+    data_dash = torch.matmul(data_normalizer * data, projection_matrix.type_as(data))
+
+    diag_data = torch.square(data).sum(-1) / 2.0 * (data_normalizer ** 2)
+    diag_data = diag_data.unsqueeze(dim=-1)
+
+    if is_query:
+        data_dash = ratio * (
+            torch.exp(data_dash - diag_data -
+                    torch.amax(data_dash, dim=-1, keepdim=True).detach()) + eps)
+    else:
+        data_dash = ratio * (
+            torch.exp(data_dash - diag_data - torch.amax(data_dash, dim=(-1, -2), keepdim=True).detach()) + eps)
+
+    return data_dash.type_as(data)
