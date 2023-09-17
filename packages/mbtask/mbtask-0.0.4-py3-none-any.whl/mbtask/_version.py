099ab2fa2b03999b7d741a589f3a4c02eb86868b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:08:17 +0000

    Add package mbtask/0.0.4/mbtask-0.0.4-py3-none-any.whl

---
+++ packages/mbtask/mbtask-0.0.4-py3-none-any.whl/mbtask/_version.py
@@ -0,0 +1,12 @@
+"""
+Provides mbtask version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update mbtask` to change this file.
+
+from incremental import Version
+
+
+__version__ = Version("mbtask", 0, 0, 4)
+__all__ = ["__version__"]
