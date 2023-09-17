9e0424d9e124d169556c3bc810a8f039e0901c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:08:09 +0000

    Add package axabc/0.0.39/axabc-0.0.39.tar.gz

---
+++ packages/axabc/axabc-0.0.39.tar.gz/axabc-0.0.39/axabc/initialization/lazy_initializable_collection.py
@@ -0,0 +1,26 @@
+from abc import ABC
+from typing import Any, Optional
+from axabc.initialization.autopass import AutoPass
+from axabc.initialization.annotation import get_initializable_annotations
+
+
+class LazyInitializableCollection(ABC):
+    BaseItemClass: Optional[Any] = None
+    use_previous: bool = True
+
+    @classmethod
+    def create(
+        cls, 
+        args: Optional[list] = None, 
+        kwargs: Optional[dict] = None,
+        apass: Optional[AutoPass] = None,
+    ):
+        args = args or []
+        apass = apass or AutoPass()
+
+        for name_, type_ in get_initializable_annotations(cls, cls.BaseItemClass):
+            args_, kwargs_ = apass.retrieve(type_, *args)
+
+            
+
+
