144272e76765a745fb5a07c627c5a9a7e4b11b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:09:33 +0000

    Add package mb-test-task/0.0.0/mb_test_task-0.0.0-py3-none-any.whl

---
+++ packages/mb-test-task/mb_test_task-0.0.0-py3-none-any.whl/mb_test/__init__.py
@@ -0,0 +1,4 @@
+from mb_test_task._version import __version__
+
+
+PUB_VERSION = __version__.public()
---
+++ packages/mb-test-task/mb_test_task-0.0.0-py3-none-any.whl/mb_test/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides mb_test version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update mb_test` to change this file.
+
+from incremental import Version
+
+__version__ = Version("mb_test", 0, 0, 0)
+__all__ = ["__version__"]
