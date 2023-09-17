a5331e486642683f7aefb5cf06571b18d07d2b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:08:30 +0000

    Add package python-pulse/0.1.5/python_pulse-0.1.5-py3-none-any.whl

---
+++ packages/python-pulse/python_pulse-0.1.5-py3-none-any.whl/pypulse/Aplication/vars.py
@@ -0,0 +1,3 @@
+class Vars:
+    APLICATION_PATH = None
+    INTERNAL_HTTP_SERVER_PORT = 63333
---
+++ packages/python-pulse/python_pulse-0.1.5-py3-none-any.whl/pypulse/Template/__init__.py
@@ -0,0 +1,3 @@
+from .render_template import RenderTemplate
+from .template import Template
+from .redirect_path import Redirect
