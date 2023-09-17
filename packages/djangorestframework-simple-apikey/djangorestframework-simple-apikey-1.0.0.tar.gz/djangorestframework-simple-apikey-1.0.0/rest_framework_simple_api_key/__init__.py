909378de3026cdf7495831f155d758d8652f2b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:09:01 +0000

    Add package djangorestframework-simple-apikey/1.0.0/djangorestframework-simple-apikey-1.0.0.tar.gz

---
+++ packages/djangorestframework-simple-apikey/djangorestframework-simple-apikey-1.0.0.tar.gz/djangorestframework-simple-apikey-1.0.0/rest_framework_simple_api_key/__init__.py
@@ -0,0 +1,13 @@
+"""
+Django REST framework Simple API Key Configurations
+"""
+
+import django
+
+
+__title__ = "Django REST framework Simple API Key"
+__author__ = ["Kolawole Mangabo", "Ruben Atinho"]
+
+
+# Header encoding (see RFC5987)
+HTTP_HEADER_ENCODING = "iso-8859-1"
