c80f6acb772bc4ad626d19a622cfaba3bb78e110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:09:33 +0000

    Add package botlogger/0.0.5/botlogger-0.0.5-py3-none-any.whl

---
+++ packages/botlogger/botlogger-0.0.5-py3-none-any.whl/botlogger/middlewares.py
@@ -0,0 +1,33 @@
+from typing import Any, Callable, Dict, Awaitable
+
+from aiogram import types
+try:
+    from aiogram.dispatcher.middlewares import BaseMiddleware
+except ImportError:
+    from aiogram import BaseMiddleware
+
+from .logger import logger
+
+
+class MessageLogger(BaseMiddleware):
+    logger = logger
+
+    async def on_pre_process_message(self, message: types.Message, data: dict) -> Any:
+        await self.logger.send_message(data=message, text=message.text)
+
+    async def on_pre_process_callback_query(self, query: types.CallbackQuery, data: dict):
+        await self.logger.send_message(data=query, text=query.data)
+
+    async def on_post_process_callback_query(self, query: types.CallbackQuery, data: dict):
+        await self.logger.send_message(data=query, text=query.data, answer=query.message.text)
+
+    async def on_post_process_message(self, message: types.Message, data: dict) -> Any:
+        await self.logger.send_message(data=message, text=message.text, answer=message.reply_to_message.text)
+
+    async def __call__(
+            self,
+            handler: Callable[[types.Message, Dict[str, Any]], Awaitable[Any]],
+            event: types.Message,
+            data: Dict[str, Any]
+    ) -> Any:
+        return await handler(event, data)
