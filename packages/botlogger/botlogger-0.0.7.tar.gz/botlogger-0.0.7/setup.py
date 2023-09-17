09d96e6fbc7726c4b4a3ab556bc98a8f0025e16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:09:35 +0000

    Add package botlogger/0.0.7/botlogger-0.0.7.tar.gz

---
+++ packages/botlogger/botlogger-0.0.7.tar.gz/botlogger-0.0.7/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="botlogger",
+    version="0.0.7",
+    author="justacold",
+    description="Library to log messages from telegram bots",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'loguru',
+        'aiohttp',
+        # Add more dependencies here
+    ],
+    exclude=['README.md', '*.txt']
+)
