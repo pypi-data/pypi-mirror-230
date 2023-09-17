31479d8bbf1aa9cf04160bee4d968c148ab21105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:08:20 +0000

    Add package qabot/0.4.0/qabot-0.4.0.tar.gz

---
+++ packages/qabot/qabot-0.4.0.tar.gz/qabot-0.4.0/qabot/config.py
@@ -0,0 +1,14 @@
+from typing import List, Optional
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    OPENAI_API_KEY: str
+
+    QABOT_DATABASE_URI: Optional[str] = None
+    QABOT_CACHE_DATABASE_URI = "duckdb:///:memory:"
+    QABOT_MODEL_NAME = "gpt-4"
+    QABOT_TABLES: Optional[List[str]]
+    QABOT_ENABLE_WIKIDATA: bool = True
+    QABOT_ENABLE_HUMAN_CLARIFICATION: bool = True
