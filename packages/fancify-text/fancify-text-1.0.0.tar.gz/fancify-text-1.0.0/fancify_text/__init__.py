8d6276d887263b432b70b2ae74fbd146efbc081b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:09:27 +0000

    Add package fancify-text/1.0.0/fancify-text-1.0.0.tar.gz

---
+++ packages/fancify-text/fancify-text-1.0.0.tar.gz/fancify-text-1.0.0/fancify_text/__init__.py
@@ -0,0 +1,3 @@
+from .fontData import fonts, modifiers
+from .functions import *
+from . import cli
---
+++ packages/fancify-text/fancify-text-1.0.0.tar.gz/fancify-text-1.0.0/setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+# Packages
+from setuptools import setup
+
+setup(
+    name="fancify-text",
+    version="1.0.0",
+    author="Secret-chest",
+    url="https://github.com/Secret-chest/fancify-text",
+    description=(
+        "Convert text to one of 27 fancy unicode representations"
+    ),
+    long_description=open("README.md", encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    packages=["fancify_text"],
+    install_requires=[],
+    entry_points = {
+        'console_scripts': [
+            'fancify = fancify_text.cli:fancify_cmd'
+        ]
+    },
+)
