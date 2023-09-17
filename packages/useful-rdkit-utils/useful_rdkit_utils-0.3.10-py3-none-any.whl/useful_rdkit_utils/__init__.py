946d699de1a289487a19d365d09058d5faa49b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:06:30 +0000

    Add package useful-rdkit-utils/0.3.10/useful_rdkit_utils-0.3.10-py3-none-any.whl

---
+++ packages/useful-rdkit-utils/useful_rdkit_utils-0.3.10-py3-none-any.whl/useful_rdkit_utils/__init__.py
@@ -0,0 +1,11 @@
+"""Some useful RDKit functions"""
+
+# Add imports here
+from .useful_rdkit_utils import *
+from .reos import REOS
+from .pandas_utils import *
+from .ring_systems import *
+from .seaborn_utils import *
+
+__version__="0.3.10"
+
