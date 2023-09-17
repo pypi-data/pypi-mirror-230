81d0b1d212c8690e79b3744e75733b50efa25ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:08:20 +0000

    Add package helloworlddojuanz/1.0.0/helloworlddojuanz-1.0.0.tar.gz

---
+++ packages/helloworlddojuanz/helloworlddojuanz-1.0.0.tar.gz/helloworlddojuanz-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helloworlddojuanz',
+    version='1.0.0',
+    description='Biblioteca de exemplo que imprime "Hello, World!"',
+    author='Juan',
+    author_email='juan.santanaa@outlook.com',
+    packages=find_packages(),
+)
