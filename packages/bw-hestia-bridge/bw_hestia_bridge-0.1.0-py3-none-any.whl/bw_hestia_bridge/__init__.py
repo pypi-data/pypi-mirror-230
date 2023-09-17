4099927d5b71d996069e52298fd0b2158543b6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:09:25 +0000

    Add package bw-hestia-bridge/0.1.0/bw_hestia_bridge-0.1.0-py3-none-any.whl

---
+++ packages/bw-hestia-bridge/bw_hestia_bridge-0.1.0-py3-none-any.whl/bw_hestia_bridge/__init__.py
@@ -0,0 +1,28 @@
+"""bw_hestia_bridge."""
+
+__version__ = "0.1.0"
+
+
+_config: dict = {
+    "http_proxy": "",
+    "https_proxy": "",
+    "logging_level": "WARNING",
+    "hestia_token": None,
+    "hestia_api": "https://api.hestia.earth"
+}
+
+
+from .hestia_api import (
+    get_hestia_node, login_to_hestia, search_hestia, set_hestia_token)
+from .utils import get_config, set_config
+
+
+__all__ = (
+    "__version__",
+    "get_config",
+    "get_hestia_node",
+    "login_to_hestia",
+    "search_hestia",
+    "set_config",
+    "set_token"
+)
