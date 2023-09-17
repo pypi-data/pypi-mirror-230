987df52b51c07e071fe086cde259bd073584ba5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:08:55 +0000

    Add package db-analytics-tools/0.1.4.8/db_analytics_tools-0.1.4.8.tar.gz

---
+++ packages/db-analytics-tools/db_analytics_tools-0.1.4.8.tar.gz/db_analytics_tools-0.1.4.8/db_analytics_tools/plotting.py
@@ -0,0 +1,22 @@
+# coding : utf-8
+
+"""
+    DB Analytics Tools Data Visualization
+"""
+
+# import matplotlib.pyplot as plt
+#
+# def pie_chart(label, size, data):
+#     pass
+#     # plt.pie()
+#
+# def donut_chart(label, size, data):
+#     pass
+#     # plt.pie()
+#
+# def bar_plot(x, height, data):
+#     pass
+#
+# def line_plot(x, y, data):
+#     pass
+#     # fig = plt.plot(data[x], data[y])
