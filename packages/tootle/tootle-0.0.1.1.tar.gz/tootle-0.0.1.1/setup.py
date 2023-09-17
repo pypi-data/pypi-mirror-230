7082d4b2a5f815af954a241492cbf6773edc1761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:08:14 +0000

    Add package tootle/0.0.1.1/tootle-0.0.1.1.tar.gz

---
+++ packages/tootle/tootle-0.0.1.1.tar.gz/tootle-0.0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements():
+    with open('requirements.txt', 'r') as f:
+        requirements = f.readlines()
+    return [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
+
+
+setup(
+    name='tootle',
+    version='0.0.1.1',
+    include_package_data=True,
+    packages=find_packages(exclude=['test', 'test.*']),
+    package_data={
+        'tootle.dev_bot': ['.autom/*.json'],
+    },
+    install_requires=read_requirements(),
+    author='',
+    author_email='',
+    description='',
+    long_description=open('./README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+)
+
+# To build:
+# python setup.py sdist bdist_wheel
+# To upload to pypi:
+# twine upload dist/*
