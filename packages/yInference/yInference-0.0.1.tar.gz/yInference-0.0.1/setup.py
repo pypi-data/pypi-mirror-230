1312eea59cb151d1e4c9ce5b013025063c5f90a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:08:00 +0000

    Add package yInference/0.0.1/yInference-0.0.1.tar.gz

---
+++ packages/yInference/yInference-0.0.1.tar.gz/yInference-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(name="yInference",
+      version="0.0.1",
+      description="Inference your Fine tuned LLM",
+      author="Shuvam Mandal",
+      author_email="shuvammandal121@gmail.com",
+      packages=["yInference"],
+      long_description= long_description,
+      long_description_content_type = "text/markdown",
+      install_requires = ['transformers','pandas','tensorflow'],
+      
+      )
