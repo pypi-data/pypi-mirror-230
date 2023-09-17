9a451f73557b14a96f26a0a355f179278d3f305f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:07:53 +0000

    Add package iotcore/0.3.0/iotcore-0.3.0-cp37-none-win_amd64.whl

---
+++ packages/iotcore/iotcore-0.3.0-cp37-none-win_amd64.whl/iotcore/djangoiot/apps.py
@@ -0,0 +1,18 @@
+from django.apps import AppConfig
+from iotcore import IotCore
+import os
+
+
+class IotConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "iotcore.djangoiot"
+
+    # def ready(self):
+    #     server_status = os.environ.get("MQTT_SERVER_RUNNING", None)
+    #     if server_status is None:
+    #         print("Starting MQTT server!")
+    #         os.environ["MQTT_SERVER_RUNNING"] = "true"
+    #         iot = IotCore()
+    #         iot.background_loop_forever()
+    #     else:
+    #         print("Server already running!")
