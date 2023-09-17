088bd82064651c098219820303980cdf7e06c806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:08:03 +0000

    Add package ml3m/0.0.18/ml3m-0.0.18-py3-none-any.whl

---
+++ packages/ml3m/ml3m-0.0.18-py3-none-any.whl/ml3m/__init__.py
@@ -0,0 +1,13 @@
+from ._version import show_versions
+
+__version__ = "0.0.18"
+
+__all__ = [
+    # Submodules
+    "base",
+    "errors",
+    "mcq",
+    "utils",
+    # Functions
+    "show_versions",
+]
