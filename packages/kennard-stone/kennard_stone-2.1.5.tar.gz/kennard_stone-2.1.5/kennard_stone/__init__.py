303c5677f820a9edd2713d444e81bcc580653a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:07:31 +0000

    Add package kennard-stone/2.1.5/kennard_stone-2.1.5.tar.gz

---
+++ packages/kennard-stone/kennard_stone-2.1.5.tar.gz/kennard_stone-2.1.5/kennard_stone/__init__.py
@@ -0,0 +1,15 @@
+from .kennard_stone import KFold, train_test_split
+
+__version__ = "2.1.5"
+__license__ = "MIT"
+__author__ = "yu9824"
+__copyright__ = "Copyright © 2021 yu9824"
+__author_email__ = "yu.9824.job@gmail.com"
+__user_id__ = "yu9824"
+__app_name__ = "kennard_stone"
+__url__ = "https://github.com/yu9824/kennard_stone"
+
+__all__ = [
+    "KFold",
+    "train_test_split",
+]
