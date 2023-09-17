bce4b6c38ceae8cfa3cdc9070cba269f5b4b01c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:09:01 +0000

    Add package kamui/0.1/kamui-0.1.tar.gz

---
+++ packages/kamui/kamui-0.1.tar.gz/kamui-0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kamui",
+    version="0.1",
+    author="Chin-Yun Yu",
+    author_email="chin-yun.yu@qmul.ac.uk",
+    description="A Python package for phase unwrapping",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yoyololicon/kamui",
+    packages=["kamui"],
+    install_requires=["numpy", "scipy"],
+    extras_require={
+        "extra": ["PyMaxflow"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
