562acdcb2f44e9e6d94efd40a16476d153721193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:07:42 +0000

    Add package ezpydb/1.0/ezpydb-1.0.tar.gz

---
+++ packages/ezpydb/ezpydb-1.0.tar.gz/ezpydb-1.0/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    name='ezpydb',
+
+    packages=['ezpydb'],
+
+    version='1.0',
+
+    license='MIT',
+
+    description='EzPyDB - Easy Python DataBase.',
+
+    author='Ivan Perzhinsky.',
+    author_email='name1not1found.com@gmail.com',
+
+    url='https://github.com/xzripper/EzPyDB',
+    download_url='https://github.com/xzripper/EzPyDB/archive/refs/tags/v1.0-beta.tar.gz',
+
+    keyword =['database'],
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Database',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
