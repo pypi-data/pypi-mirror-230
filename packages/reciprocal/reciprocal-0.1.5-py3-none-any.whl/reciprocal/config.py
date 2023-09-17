935e9fbf3fb27622d4e7397a1d09104ace977fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:09:15 +0000

    Add package reciprocal/0.1.5/reciprocal-0.1.5-py3-none-any.whl

---
+++ packages/reciprocal/reciprocal-0.1.5-py3-none-any.whl/reciprocal/config.py
@@ -0,0 +1,4 @@
+# These colors are used to highlight the option being hovered, the ways to specify colors are
+# detailed in the click documentation: https://click.palletsprojects.com/en/latest/api/#click.style
+HOVERED_FG = "green"
+HOVERED_BG = None
