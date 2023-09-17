96c3baff77fcf0541a18d6a8e8abd10552e402fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:07:08 +0000

    Add package openai-pricing-draft/0.6/openai_pricing_draft-0.6.tar.gz

---
+++ packages/openai-pricing-draft/openai_pricing_draft-0.6.tar.gz/openai_pricing_draft-0.6/publish.sh
@@ -0,0 +1,2 @@
+python3.10 setup.py sdist bdist_wheel
+twine upload dist/*
---
+++ packages/openai-pricing-draft/openai_pricing_draft-0.6.tar.gz/openai_pricing_draft-0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="openai_pricing_draft",
+    version="0.6",
+    packages=find_packages(),
+    py_modules=['pricing_calc'],
+    install_requires=[
+        # List your dependencies here
+    ],
+)
