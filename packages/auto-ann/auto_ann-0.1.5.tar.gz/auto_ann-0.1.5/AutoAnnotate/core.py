3bf1058af09c68fd64d80a162b8aef9ea76f75a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:07:52 +0000

    Add package auto-ann/0.1.5/auto_ann-0.1.5.tar.gz

---
+++ packages/auto-ann/auto_ann-0.1.5.tar.gz/auto_ann-0.1.5/AutoAnnotate/core.py
@@ -0,0 +1,16 @@
+# module1.py
+def greet(name):
+    return f"Hello from {name}"
+
+
+
+# """
+# Test: Manager sends messages to workers which print they have received.
+
+# Run on at least two processors:
+# eg. For 4 processors:
+# mpiexec -np 2 module_mpi4py_1.py
+# """
+
+# # if __name__ == "__main__":
+# #   function1()
