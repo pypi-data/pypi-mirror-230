50643d7cc661adf56a4e55208fb5a433325c19de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:07:42 +0000

    Add package diambra-engine/2.1.0rc23/diambra_engine-2.1.0rc23-py3-none-any.whl

---
+++ packages/diambra-engine/diambra_engine-2.1.0rc23-py3-none-any.whl/diambra/engine/__init__.py
@@ -0,0 +1,11 @@
+import grpc
+import diambra.engine.interface_pb2_grpc as service
+import diambra.engine.interface_pb2 as model
+
+class Client(service.EnvServerStub):
+    """DIAMBRA Arena Engine API Client"""
+
+    def __init__(self, address, timeout=60):
+        self.channel = grpc.insecure_channel(address)
+        super().__init__(self.channel)
+        grpc.channel_ready_future(self.channel).result(timeout=timeout)
