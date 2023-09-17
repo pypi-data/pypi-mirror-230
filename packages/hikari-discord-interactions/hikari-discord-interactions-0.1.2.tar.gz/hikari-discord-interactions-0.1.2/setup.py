9802daa8a8d20d1b26d0bc7cbf4a624a47a2c334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:09:05 +0000

    Add package hikari-discord-interactions/0.1.2/hikari-discord-interactions-0.1.2.tar.gz

---
+++ packages/hikari-discord-interactions/hikari-discord-interactions-0.1.2.tar.gz/hikari-discord-interactions-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hikari-discord-interactions",
+    version="0.1.2",
+    url="https://github.com/KeenanOH/hikari-discord-interactions/",
+    author="KeenanOH",
+    author_email="86394469+KeenanOH@users.noreply.github.com",
+    description="A Discord interactions library built with hikari.",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    install_requires=["hikari[server]~=2.0.0.dev121"],
+    python_requires=">=3.10",
+)
