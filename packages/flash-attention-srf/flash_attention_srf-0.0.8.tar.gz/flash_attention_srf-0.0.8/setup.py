6a878a8f386fd7b4d65bbb2de8346d0dd4831ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:07:12 +0000

    Add package flash-attention-srf/0.0.8/flash_attention_srf-0.0.8.tar.gz

---
+++ packages/flash-attention-srf/flash_attention_srf-0.0.8.tar.gz/flash_attention_srf-0.0.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="flash_attention_srf",
+    version="0.0.8",
+    author="Alexander Levenston",
+    author_email="alexlevenston2021@gmail.com",
+    description="Softmax-less Flash Attention w/ SRFs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexjlevenston/flash-attention-srf",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "scipy",
+        "torch"
+    ]
+)
+
