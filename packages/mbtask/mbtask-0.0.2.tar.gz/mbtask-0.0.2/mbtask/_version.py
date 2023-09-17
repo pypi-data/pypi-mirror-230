f06caa6bee62d8ee7118a9c37844c325bc6b0ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:07:39 +0000

    Add package mbtask/0.0.2/mbtask-0.0.2.tar.gz

---
+++ packages/mbtask/mbtask-0.0.2.tar.gz/mbtask-0.0.2/mbtask/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides mbtask version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update mbtask` to change this file.
+
+from incremental import Version
+
+__version__ = Version("mbtask", 0, 0, 2)
+__all__ = ["__version__"]
