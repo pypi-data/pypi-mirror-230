f08ceb51318039ffa414d777c3d7c02633b15aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 03:07:37 +0000

    Add package portalocker/2.8.1/portalocker-2.8.1.tar.gz

---
+++ packages/portalocker/portalocker-2.8.1.tar.gz/portalocker-2.8.1/portalocker_tests/test_combined.py
@@ -0,0 +1,13 @@
+import sys
+
+from portalocker import __main__
+
+
+def test_combined(tmpdir):
+    output_file = tmpdir.join('combined.py')
+    __main__.main(['combine', '--output-file', output_file.strpath])
+    sys.path.append(output_file.dirname)
+    # Combined is being generated above but linters won't understand that
+    import combined  # type: ignore
+
+    assert combined
