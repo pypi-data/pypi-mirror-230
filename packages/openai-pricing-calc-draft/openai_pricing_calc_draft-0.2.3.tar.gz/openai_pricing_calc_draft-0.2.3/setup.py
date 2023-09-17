1a6db14b6c40b1a5f8c4d1e2197789f089403339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:07:44 +0000

    Add package openai-pricing-calc-draft/0.2.3/openai_pricing_calc_draft-0.2.3.tar.gz

---
+++ packages/openai-pricing-calc-draft/openai_pricing_calc_draft-0.2.3.tar.gz/openai_pricing_calc_draft-0.2.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='openai_pricing_calc_draft',
+                 version='0.2.3',
+                 description='Pricing Calc for OpenAI API',
+                 author='M.Ali Koken',
+                 author_email='ali@koken-consulting.com',
+                 url='https://github.com/kokenconsulting/openai-api-pricing',
+                 python_requires='>=3.6',
+                 packages=find_packages(),
+                 #classifiers=['Packages', 'Boilerplate']
+                 )
