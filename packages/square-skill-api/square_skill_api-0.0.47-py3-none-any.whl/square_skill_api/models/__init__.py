db20d55ebfccb2bac1da50e30676e1e0887e7764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:07:41 +0000

    Add package square-skill-api/0.0.47/square_skill_api-0.0.47-py3-none-any.whl

---
+++ packages/square-skill-api/square_skill_api-0.0.47-py3-none-any.whl/square_skill_api/models/__init__.py
@@ -0,0 +1,8 @@
+from square_skill_api.models.heartbeat import HeartbeatResult
+from square_skill_api.models.prediction import (
+    Prediction,
+    PredictionDocument,
+    PredictionOutput,
+    QueryOutput,
+)
+from square_skill_api.models.request import QueryRequest
