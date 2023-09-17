49aa1513b6fd831696604e9024cf2abdefb3e072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:07:10 +0000

    Add package dimtown-spider/0.1.1/dimtown_spider-0.1.1-py3-none-any.whl

---
+++ packages/dimtown-spider/dimtown_spider-0.1.1-py3-none-any.whl/dimtown/__init__.py
@@ -0,0 +1,44 @@
+from .utils import (
+    AnimeAvatar,
+    ArtAlbum,
+    Cosplay,
+    CoupleAvatar,
+    FemaleAvatar,
+    Figure,
+    Hanfu,
+    JK,
+    LoliTa,
+    MaleAvatar,
+    PcPic,
+    PhonePic,
+    SelectedIllustrations,
+    SelectedPixiv,
+    Search,
+    ReturnImage
+)
+
+__all__ = [
+    "AnimeAvatar",
+    "ArtAlbum",
+    "Cosplay",
+    "CoupleAvatar",
+    "FemaleAvatar",
+    "Figure",
+    "Hanfu",
+    "JK",
+    "LoliTa",
+    "MaleAvatar",
+    "PcPic",
+    "PhonePic",
+    "SelectedIllustrations",
+    "SelectedPixiv",
+    "Search",
+    "ReturnImage",
+]
+__title__ = "dimtown"
+__description__ = "A Spider for https://dimtown.com"
+__url__ = "https://github.com/Cvandia/dimtown-spider"
+__version__ = "0.1.1"
+__author__ = "Cvandia"
+__author_email__ = "1141538825@qq.com"
+__license__ = "Apache License 2.0"
