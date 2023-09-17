cd6f09de9a2654ab33f1403009eedd2bb1a1159c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 14:07:11 +0000

    Add package saffronorchestrator/0.0.2/saffronorchestrator-0.0.2.tar.gz

---
+++ packages/saffronorchestrator/saffronorchestrator-0.0.2.tar.gz/saffronorchestrator-0.0.2/saffronorchestrator/orchestration.py
@@ -0,0 +1,8 @@
+from rankexpansion import *
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from basilsweepercrawler import *
+
+def candidate_search_flow(user_query:str, mongo_query:dict, mongo_collection:Collection, n_queries:int):
+    return
+    
