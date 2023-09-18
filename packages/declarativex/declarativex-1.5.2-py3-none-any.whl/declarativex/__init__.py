119600253718f8ad1c09357eb5d18ea99bd8147f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:07:32 +0000

    Add package declarativex/1.5.2/declarativex-1.5.2-py3-none-any.whl

---
+++ packages/declarativex/declarativex-1.5.2-py3-none-any.whl/declarativex/__init__.py
@@ -0,0 +1,27 @@
+from .client import BaseClient
+from .dependencies import (
+    Path,
+    JsonField,
+    Json,
+    Query,
+    Header,
+    Cookie,
+    Timeout,
+    Dependency,
+)
+from .exceptions import (
+    DeclarativeException,
+    MisconfiguredException,
+    AnnotationException,
+    DependencyValidationError,
+    HTTPException,
+    TimeoutException,
+    UnprocessableEntityException,
+    RateLimitExceeded,
+)
+from .methods import http
+from .middlewares import Middleware
+from .rate_limiter import rate_limiter
+from .retry import retry
+
+__version__ = "1.5.2"
