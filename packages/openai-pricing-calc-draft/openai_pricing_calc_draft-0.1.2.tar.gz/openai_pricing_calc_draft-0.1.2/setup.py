69d6c82caa2cad0babee3a98884a7aef48c3d78f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:07:33 +0000

    Add package openai-pricing-calc-draft/0.1.2/openai_pricing_calc_draft-0.1.2.tar.gz

---
+++ packages/openai-pricing-calc-draft/openai_pricing_calc_draft-0.1.2.tar.gz/openai_pricing_calc_draft-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.2'
+DESCRIPTION = 'Open AI Calculated Cost'
+LONG_DESCRIPTION = 'A package that helps calculate costs for Open AI API'
+
+# Setting up
+setup(
+    name="openai_pricing_calc_draft",
+    version=VERSION,
+    author="Koken Consultng",
+    author_email="<ali@koken-consulting.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=["requests"],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
