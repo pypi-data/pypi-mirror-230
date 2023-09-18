4831ebd25ea19f6ea0bb77e9544cdb598ae2e7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:09:27 +0000

    Add package phi-torch/0.0.4/phi_torch-0.0.4-py3-none-any.whl

---
+++ packages/phi-torch/phi_torch-0.0.4-py3-none-any.whl/phi/model.py
@@ -0,0 +1,24 @@
+from phicore.transformer import Transformer, Decoder
+from phicore.autoregressive_wrapper import AutoregressiveWrapper 
+
+Phi = Transformer(
+    num_tokens=64007,
+    max_seq_len=8192,
+    use_abs_pos_emb=False,
+    attn_layers = Decoder(
+        dim=2560, # 2048
+        depth=32, # 16
+        dim_head=128,
+        heads=24,
+        alibi_pos_bias=True,
+        alibi_num_heads=12,
+        rotary_xpos=True,
+        attn_flash = True,
+        attn_one_kv_head = True,
+        qk_norm=True,
+        attn_qk_norm=True,
+        attn_qk_norm_dim_scale=True # set this to True, in addition to `attn_qk_norm = True`
+    )
+)
+
+Phi = AutoregressiveWrapper(Phi)
