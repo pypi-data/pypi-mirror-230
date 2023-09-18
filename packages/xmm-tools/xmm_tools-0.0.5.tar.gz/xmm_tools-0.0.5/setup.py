b997d961cdc1a1c1a5029db3b6879e028d09ccbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:10:07 +0000

    Add package xmm-tools/0.0.5/xmm_tools-0.0.5.tar.gz

---
+++ packages/xmm-tools/xmm_tools-0.0.5.tar.gz/xmm_tools-0.0.5/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="xmm_tools",
+    version="0.0.5",
+    author="剑爆是十条街",
+    author_email="author@example.com",
+    description="A small example package",
+    packages=["io"],
+    clean={"all": True}
+)
