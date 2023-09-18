de59a4a6a3f690ec4812c8cd20296c8767ba79af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:10:57 +0000

    Add package zcbot-crawl-core/2.0.3/zcbot-crawl-core-2.0.3.tar.gz

---
+++ packages/zcbot-crawl-core/zcbot-crawl-core-2.0.3.tar.gz/zcbot-crawl-core-2.0.3/zcbot_crawl_core/service/batch_spider.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from ..util import logger
+from ..dao import batch_spider as batch_spider_dao
+from ..model.entity import BatchSpider
+
+LOGGER = logger.get('批次爬虫')
+
+
+def get_batch_spider(spider_id: str):
+    row = batch_spider_dao.get_batch_spider(spider_id=spider_id)
+    return row
