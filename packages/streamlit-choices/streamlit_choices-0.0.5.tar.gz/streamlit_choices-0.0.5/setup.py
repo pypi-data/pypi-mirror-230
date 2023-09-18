26447822c83d64bd632adc5b725ecc9c12da21fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:09:48 +0000

    Add package streamlit-choices/0.0.5/streamlit_choices-0.0.5.tar.gz

---
+++ packages/streamlit-choices/streamlit_choices-0.0.5.tar.gz/streamlit_choices-0.0.5/setup.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit_choices",
+    version="0.0.5",
+    author="Brienna Herold",
+    author_email="brienna.herold+pip@gmail.com",
+    description="Streamlit component that allows you to add a multiple choice question to your app.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.8",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+    extras_require={
+        "devel": [
+            "wheel",
+            "pytest==7.4.0",
+            "playwright==1.36.0",
+            "requests==2.31.0",
+            "pytest-playwright-snapshot==1.0",
+            "pytest-rerunfailures==12.0",
+        ]
+    }
+)
