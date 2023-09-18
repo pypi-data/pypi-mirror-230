68b90528ef1ba920e3c643c96a10228def4840b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:09:48 +0000

    Add package dg-itest/0.0.13/dg-itest-0.0.13.tar.gz

---
+++ packages/dg-itest/dg-itest-0.0.13.tar.gz/dg-itest-0.0.13/dg_itest/servers/dg_servers/dg_requsts.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/27 11:35
+# @Author  : yaitza
+# @Email   : yaitza@foxmail.com
+
+import requests
+import allure
+from dg_itest import get_logger, host_url
+
+class DgRequests(object):
+	def __init__(self):
+		self.server_url = host_url
+
+	def http_request(self, method, url, headers=None, **kwargs):
+		"""
+		针对接口封装.
+		"""
+		method = method.upper()
+
+		request_headers = {'finger': 'auto_test'}
+		if headers is not None:
+			request_headers.update(headers)
+		allure.attach(str(kwargs))
+		get_logger().info(f'inputs params: {str(kwargs)}')
+		result = requests.request(method, url=self.server_url + url, headers=request_headers, **kwargs)
+		get_logger().info(f'response: {str(result.json())}')
+		allure.attach(str(result.json()))
+		return result
