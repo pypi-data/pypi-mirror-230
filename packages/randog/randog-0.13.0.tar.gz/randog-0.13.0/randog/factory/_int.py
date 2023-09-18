fa9e27f84acccf07eb7d148dbe473027c8bc04e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:09:21 +0000

    Add package randog/0.13.0/randog-0.13.0.tar.gz

---
+++ packages/randog/randog-0.13.0.tar.gz/randog-0.13.0/randog/factory/_int.py
@@ -0,0 +1,71 @@
+import typing as t
+from random import Random
+
+from ._base import Factory, decide_rnd
+from ..exceptions import FactoryConstructionError
+
+
+def randint(
+    minimum: int,
+    maximum: int,
+    *,
+    rnd: t.Optional[Random] = None,
+) -> Factory[int]:
+    """Return a factory generating random int values.
+
+    Parameters
+    ----------
+    minimum : int
+        the minimum
+    maximum : int
+        the maximum
+    rnd : Random, optional
+        random number generator to be used
+
+    Raises
+    ------
+    FactoryConstructionError
+        When the specified generating conditions are inconsistent.
+    """
+    return IntRandomFactory(minimum, maximum, rnd=rnd)
+
+
+class IntRandomFactory(Factory[int]):
+    """factory generating random int values"""
+
+    _random: Random
+    _min: int
+    _max: int
+
+    def __init__(
+        self,
+        minimum: int,
+        maximum: int,
+        *,
+        rnd: t.Optional[Random] = None,
+    ):
+        """Return a factory generating random int values.
+
+        Parameters
+        ----------
+        minimum : int
+            the minimum
+        maximum : int
+            the maximum
+        rnd : Random, optional
+            random number generator to be used
+
+        Raises
+        ------
+        FactoryConstructionError
+            When the specified generating conditions are inconsistent.
+        """
+        self._random = decide_rnd(rnd)
+        self._min = minimum
+        self._max = maximum
+
+        if minimum > maximum:
+            raise FactoryConstructionError("empty range for randint")
+
+    def _next(self) -> int:
+        return self._random.randint(self._min, self._max)
