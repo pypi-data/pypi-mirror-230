b2e83444d84d234d794abd711a4674b314dbe4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:11:20 +0000

    Add package deepsport-utilities/4.10.0/deepsport_utilities-4.10.0.tar.gz

---
+++ packages/deepsport-utilities/deepsport_utilities-4.10.0.tar.gz/deepsport_utilities-4.10.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deepsport_utilities',
+    author='Gabriel Van Zandycke',
+    author_email="gabriel.vanzandycke@uclouvain.be",
+    url="https://gitlab.com/deepsport/deepsport_utilities",
+    licence="LGPL",
+    python_requires='>=3.8',
+    # 3.8 required for
+    #    - PEP 572 – Assignment Expressions (:=)
+    #    - `functools.cached_property` (although `mlworkflow.lazyproperty` could be used for older python versions)
+    # 3.7 required for
+    #    - EvalAI ... but we don't care anymore, do we?
+    description="",
+    version='4.10.0',
+    packages=find_packages(),
+    install_requires=[
+        "numpy", # was >=1.20", ... but I don't understand why
+        "scipy",
+        "opencv-python",
+        "imageio",
+        "m3u8",
+        "requests",
+        "calib3d>=2.10.0",
+        "mlworkflow>=0.6.0",
+        "shapely",
+        "scikit-image",
+        "aleatorpy"
+    ],
+    extras_require={
+        'dev': [
+            "pdoc3",
+            "pytest",
+            "twine",
+        ]
+    }
+)
