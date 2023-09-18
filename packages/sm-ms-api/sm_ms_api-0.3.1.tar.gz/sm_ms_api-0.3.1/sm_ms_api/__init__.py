5d48cf2d4a248f96c7aad64869da47f79ce274bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:11:02 +0000

    Add package sm-ms-api/0.3.1/sm_ms_api-0.3.1.tar.gz

---
+++ packages/sm-ms-api/sm_ms_api-0.3.1.tar.gz/sm_ms_api-0.3.1/sm_ms_api/__init__.py
@@ -0,0 +1,30 @@
+from io import BytesIO
+from pathlib import Path
+
+import requests
+
+
+class SMMS(object):
+    root: str = "https://sm.ms/api/v2/"
+
+    def __init__(self, token: str):
+        self.header = {"Authorization": token}
+
+    def upload_image(self, file: Path | str | BytesIO) -> str:
+        if not isinstance(file, BytesIO):
+            with open(file, "rb") as file:
+                files = {"smfile": file}
+        else:
+            files = {"smfile": file}
+        res = requests.post(self.root + "upload", files=files, headers=self.header)
+        res = res.json()
+        if res["success"]:
+            return res["data"]["url"]
+        elif res["code"] == "image_repeated":
+            return res["images"]
+        else:
+            raise ImageUploadError(res["message"])
+
+
+class ImageUploadError(Exception):
+    pass
