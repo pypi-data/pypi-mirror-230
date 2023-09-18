9717b7547b86f992b93542ecc12b68604e83a2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 14:10:04 +0000

    Add package boneIO/0.7.0.dev6/boneIO-0.7.0.dev6.tar.gz

---
+++ packages/boneIO/boneIO-0.7.0.dev6.tar.gz/boneIO-0.7.0.dev6/boneio/input/__init__.py
@@ -0,0 +1,7 @@
+"""Input classes."""
+
+from boneio.input.gpio import GpioEventButton
+from boneio.input.gpio_beta import GpioEventButtonBeta
+
+
+__all__ = ["GpioEventButton", "GpioEventButtonBeta"]
