e5e1824cd11b9f2002e8ca4f55c3ac790c109bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:09:30 +0000

    Add package duspider/0.0.3/duspider-0.0.3.tar.gz

---
+++ packages/duspider/duspider-0.0.3.tar.gz/duspider-0.0.3/duspider/base.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @project: duspider
+# @Author：dyz
+# @date：2023/9/18 9:42
+
+
+HEADERS = {
+    'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Microsoft Edge";v="116"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.76',
+}
