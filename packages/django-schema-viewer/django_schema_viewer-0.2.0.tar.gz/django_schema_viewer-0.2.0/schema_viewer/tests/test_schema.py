6201401277883a0a842503dc7f83845de20763a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 14:10:16 +0000

    Add package django-schema-viewer/0.2.0/django_schema_viewer-0.2.0.tar.gz

---
+++ packages/django-schema-viewer/django_schema_viewer-0.2.0.tar.gz/django_schema_viewer-0.2.0/schema_viewer/tests/test_schema.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+from schema_viewer.schema import get_schema
+from schema_viewer.export_drawio import export
+
+
+class SchemaTest(TestCase):
+    def test_schema(self):
+        tables = {
+            'auth_permission',
+            'django_admin_log',
+            'auth_group',
+            'auth_user',
+            'django_content_type',
+            'django_session',
+        }
+        schema = get_schema()
+        self.assertTrue({t['name'] for t in schema['resources']}, tables)
+
+    def test_export_drawio(self):
+        schema = export(get_schema())
+        print(schema)
