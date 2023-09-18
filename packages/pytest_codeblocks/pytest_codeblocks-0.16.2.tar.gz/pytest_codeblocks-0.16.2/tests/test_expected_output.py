c76c8fd5dffacdb8dbb8aef1569235ef8febc362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 14:10:05 +0000

    Add package pytest_codeblocks/0.16.2/pytest_codeblocks-0.16.2.tar.gz

---
+++ packages/pytest_codeblocks/pytest_codeblocks-0.16.2.tar.gz/pytest_codeblocks-0.16.2/tests/test_expected_output.py
@@ -0,0 +1,58 @@
+def test_expected_output(testdir):
+    string = """
+    Lorem ipsum
+    ```python
+    print(1 + 3)
+    print(1 - 3)
+    print(1 * 3)
+    ```
+    dolor sit amet
+    <!--pytest-codeblocks:expected-output-->
+    ```
+    4
+    -2
+    3
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(passed=1)
+
+
+def test_expected_output_fail(testdir):
+    string = """
+    Lorem ipsum
+    ```python
+    print(1 + 3)
+    ```
+    dolor sit amet
+    <!--pytest-codeblocks:expected-output-->
+    ```
+    5
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(failed=1)
+
+
+def test_expected_output_ignore_whitespace(testdir):
+    string = """
+Lorem ipsum
+```python
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+```
+dolor sit amet
+<!--pytest-codeblocks:expected-output-ignore-whitespace-->
+```
+ 4
+     -2
+
+  3
+```
+"""
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(passed=1)
---
+++ packages/pytest_codeblocks/pytest_codeblocks-0.16.2.tar.gz/pytest_codeblocks-0.16.2/tests/test_shell.py
@@ -0,0 +1,105 @@
+import sys
+
+import pytest
+
+if sys.platform.startswith("win"):
+    pytest.skip("skipping shell tests", allow_module_level=True)
+
+
+def test_shell(testdir):
+    string = """
+    Lorem ipsum
+    ```sh
+    ls
+    ```
+    dolor sit amet
+    ```sh
+    cd
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(passed=2)
+
+
+def test_shell_fail(testdir):
+    string = """
+    ```sh
+    cdc
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(failed=1)
+
+
+def test_shell_expect_fail(testdir):
+    string = """
+    <!--pytest.mark.xfail-->
+    ```sh
+    cdc
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(xfailed=1)
+
+
+def test_shell_expect_fail_passed(testdir):
+    string = """
+    <!--pytest.mark.xfail-->
+    ```sh
+    cd
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(xpassed=1)
+
+
+def test_shell_expect_output(testdir):
+    string = """
+    ```sh
+    echo abc
+    ```
+    <!--pytest-codeblocks:expected-output-->
+    ```sh
+    abc
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(passed=1)
+
+
+def test_shell_expect_output_fail(testdir):
+    string = """
+    ```sh
+    echo abc
+    ```
+    <!--pytest-codeblocks:expected-output-->
+    ```sh
+    ac
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(failed=1)
+
+
+def test_bash(testdir):
+    string = """
+    ```bash
+    foo=1
+    if [[ $foo == 1 ]]; then
+        echo abc
+    fi
+    ```
+    <!--pytest-codeblocks:expected-output-->
+    ```sh
+    abc
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(passed=1)
