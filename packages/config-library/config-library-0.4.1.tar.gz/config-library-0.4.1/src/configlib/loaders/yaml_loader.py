75df82bcece13705f64645d246bb9d9948d26773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:06:51 +0000

    Add package config-library/0.4.1/config-library-0.4.1.tar.gz

---
+++ packages/config-library/config-library-0.4.1.tar.gz/config-library-0.4.1/src/configlib/loaders/yaml_loader.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+try:
+    import yaml
+except ModuleNotFoundError:
+    raise NotImplementedError('please install config-library[yaml] for this')
+from . import baseloader
+
+
+class YamlLoader(baseloader.BaseLoader):
+    _FILE_EXTENSIONS = ('.yaml',)
+
+    def load(self):
+        with open(self.fp, 'r') as file:
+            return yaml.safe_load(file)
