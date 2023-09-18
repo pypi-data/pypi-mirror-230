d8a47525a94ee2cd1551a3c8a9d8480000226408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:07:50 +0000

    Add package pypipr/1.0.34/pypipr-1.0.34-py3-none-any.whl

---
+++ packages/pypipr/pypipr-1.0.34-py3-none-any.whl/pypipr/libstd.py
@@ -0,0 +1,41 @@
+import asyncio
+import collections.abc
+import datetime
+import json
+import math
+import multiprocessing
+import operator
+import os
+import pathlib
+import platform
+import pprint
+import queue
+import random
+import re
+import io
+import shutil
+import subprocess
+import sys
+import threading
+import time
+import timeit
+import urllib.request
+import urllib.parse
+import uuid
+import warnings
+import webbrowser
+import zoneinfo
+import textwrap
+import array
+import types
+import string
+
+
+''' platform '''
+WINDOWS = platform.system() == 'Windows'
+LINUX = not WINDOWS
+
+
+
+if __name__ == "__main__":
+    print_colorize("Anda menjalankan module pypipr", color=colorama.Fore.RED)
