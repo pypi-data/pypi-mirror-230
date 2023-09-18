d605f5ed89fe12e36b205b74ba99943d8cf6e725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:10:08 +0000

    Add package cimsparql/2.9.0/cimsparql-2.9.0-py3-none-any.whl

---
+++ packages/cimsparql/cimsparql-2.9.0-py3-none-any.whl/cimsparql/async_sparql_wrapper.py
@@ -0,0 +1,46 @@
+from http import HTTPStatus
+from typing import Optional
+
+import httpx
+from SPARQLWrapper import JSON, SPARQLWrapper
+from SPARQLWrapper.SPARQLExceptions import (
+    EndPointInternalError,
+    EndPointNotFound,
+    QueryBadFormed,
+    Unauthorized,
+    URITooLong,
+)
+
+exceptions = {
+    400: QueryBadFormed,
+    401: Unauthorized,
+    404: EndPointNotFound,
+    414: URITooLong,
+    500: EndPointInternalError,
+}
+
+
+class AsyncSparqlWrapper(SPARQLWrapper):
+    def __init__(self, *args, **kwargs):
+        self.ca_bundle: Optional[str] = kwargs.pop("ca_bundle", None)
+        super().__init__(*args, **kwargs)
+
+    async def queryAndConvert(self) -> dict:
+        if self.returnFormat != JSON:
+            raise NotImplementedError("Async client only support JSON return format")
+
+        request = self._createRequest()
+        url = request.get_full_url()
+        method = request.get_method()
+
+        args = {"timeout": self.timeout} | ({"verify": self.ca_bundle} if self.ca_bundle else {})
+        async with httpx.AsyncClient(**args) as client:
+            response = await client.request(
+                method, url, headers=request.headers, content=request.data
+            )
+
+        status = response.status_code
+        if status != HTTPStatus.OK:
+            raise exceptions.get(status, Exception)(response.content)
+
+        return response.json()
---
+++ packages/cimsparql/cimsparql-2.9.0-py3-none-any.whl/cimsparql/url.py
@@ -0,0 +1,70 @@
+"""Functions used to configure GraphDB client
+
+Will handle authenticated instances of GraphDB where user and password is given in environment
+variables ("GRAPHDB_USER" & "GRAPHDB_USER_PASSWD").
+
+"""
+
+import os
+from typing import Dict, List, Optional
+
+import requests
+
+
+def service(
+    repo: Optional[str] = None,
+    server: str = "127.0.0.1:7200",
+    protocol: str = "https",
+    path: str = "",
+) -> str:
+    """Returns service url for GraphdDBClient
+
+    Args:
+       repo: Repo on server
+       server: server ip/name
+       protocol: http or https
+    """
+    url = f"{protocol}://{server}/{path}repositories"
+    if repo:
+        url += f"/{repo}"
+    return url
+
+
+def service_blazegraph(server: str, repo: str, protocol: str = "https"):
+    return f"{protocol}://{server}/{repo}/sparql"
+
+
+class GraphDbConfig:
+    def __init__(
+        self,
+        server: str = "127.0.0.1:7200",
+        protocol: str = "https",
+        auth: requests.auth.AuthBase = None,
+    ) -> None:
+        """Get repo configuration from GraphDB
+
+        Args:
+           server: GraphDB server
+           protocol: http or https
+
+        """
+        self._service = service(None, server, protocol)
+        if auth is None:
+            auth = requests.auth.HTTPBasicAuth(
+                os.getenv("GRAPHDB_USER"), os.getenv("GRAPHDB_USER_PASSWD")
+            )
+        try:
+            response = requests.get(
+                self._service, headers={"Accept": "application/json"}, auth=auth
+            )
+            response.raise_for_status()
+            self._repos = response.json()["results"]["bindings"]
+        except (requests.exceptions.RequestException, KeyError):
+            self._repos: List[Dict[str, Dict[str, str]]] = []
+
+    @property
+    def repos(self) -> List[str]:
+        """List of available repos on GraphDB server."""
+        if self._repos:
+            return [repo["id"]["value"] for repo in self._repos]
+        return []
