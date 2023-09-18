ae6612494251045becbc016bb65c308651b99f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 14:09:29 +0000

    Add package gamecreator/1.0.0/gamecreator-1.0.0.tar.gz

---
+++ packages/gamecreator/gamecreator-1.0.0.tar.gz/gamecreator-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gamecreator',
+    version='1.0.0',
+    description='python game creator using pygame but simple',
+    author='icarogamer2001',
+    author_email='deiamota050@gmail.com',
+    packages=['gamecreator'],
+    python_requires='>=3.6',
+)
