240a10ab1fb5f70f08b79edbd1bac8d806a17856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:07:32 +0000

    Add package autonomy-dev/0.2.10/autonomy_dev-0.2.10.tar.gz

---
+++ packages/autonomy-dev/autonomy_dev-0.2.10.tar.gz/autonomy_dev-0.2.10/tests/conftest.py
@@ -0,0 +1,21 @@
+"""
+Conftest for testing command-line interfaces.
+"""
+
+import pytest
+
+from auto_dev.utils import isolated_filesystem
+
+
+@pytest.fixture
+def test_filesystem():
+    """Fixture for invoking command-line interfaces."""
+    with isolated_filesystem(copy_cwd=True) as directory:
+        yield directory
+
+
+@pytest.fixture
+def test_clean_filesystem():
+    """Fixture for invoking command-line interfaces."""
+    with isolated_filesystem() as directory:
+        yield directory
