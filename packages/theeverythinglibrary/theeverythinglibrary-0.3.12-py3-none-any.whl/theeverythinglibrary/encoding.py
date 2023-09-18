9004e888f0032465f7445467286e8e48001c7cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 14:11:03 +0000

    Add package theeverythinglibrary/0.3.12/theeverythinglibrary-0.3.12-py3-none-any.whl

---
+++ packages/theeverythinglibrary/theeverythinglibrary-0.3.12-py3-none-any.whl/theeverythinglibrary/encoding.py
@@ -0,0 +1,181 @@
+import base64
+
+class TELEncoding:
+    '''
+    ## Encoding & Decoding
+    ---
+    This class provides utility functions for encoding and decoding in different formats.
+
+    **Note:** This class is a work in progress and subject to further development.
+    '''
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def base64_encode(plaintext: str, encoding: str = 'utf-8') -> str:
+        '''
+        ## Base64 Encode
+        ---
+        ### Description
+        Encode the given plaintext using Base64 encoding.\n
+        ---
+        ### Arguments
+            - `plaintext`: The text to encode.
+            - `encoding` (optional): The character encoding to use (default is 'utf-8').\n
+        ---
+        ### Return
+            - The Base64 encoded string.\n
+        ---
+        ### Exceptions
+            - If an error occurs during encoding.\n
+        '''
+        try:
+            encoded_bytes = base64.b64encode(plaintext.encode(encoding=encoding))
+            encoded_string = encoded_bytes.decode(encoding=encoding)
+            return encoded_string
+        except Exception as e:
+            raise Exception(f"Something whent wrong: {e}")
+
+    @staticmethod
+    def base64_decode(encoded_text: str, encoding: str = 'utf-8') -> str:
+        '''
+        ## Base64 Decode
+        ---
+        ### Description
+        Decode a Base64 encoded string.\n
+        ---
+        ### Arguments
+            - `encoded_text`: The Base64 encoded string to decode.
+            - `encoding` (optional): The character encoding to use (default is 'utf-8').\n
+        ---
+        ### Return
+            - The decoded string.\n
+        ---
+        ### Exceptions
+            - If an error occurs during decoding.\n
+        '''
+        try:
+            decoded_bytes = base64.b64decode(encoded_text.encode(encoding=encoding))
+            decoded_string = decoded_bytes.decode(encoding=encoding)
+            return decoded_string
+        except Exception as e:
+            raise Exception(f"Something whent wrong: {e}")
+
+    @staticmethod
+    def hex_encode(plaintext: str, encoding: str = 'utf-8') -> str:
+        '''
+        ## Hexadecimal Encode
+        ---
+        ### Description
+        Encode the given plaintext into a hexadecimal string.\n
+        ---
+        ### Arguments
+            - `plaintext`: The text to encode.
+            - `encoding` (optional): The character encoding to use (default is 'utf-8').\n
+        ---
+        ### Return
+            - The hexadecimal encoded string.\n
+        ---
+        ### Exceptions
+            - If an error occurs during encoding.\n
+        '''
+        try:
+            encoded_string = plaintext.encode(encoding=encoding).hex()
+            return encoded_string
+        except Exception as e:
+            raise Exception(f"Something whent wrong: {e}")
+
+    @staticmethod
+    def hex_decode(encoded_text: str, encoding: str = 'utf-8') -> str:
+        '''
+        ## Hexadecimal Decode
+        ---
+        ### Description
+        Decode a hexadecimal encoded string.\n
+        ---
+        ### Arguments
+            - `encoded_text`: The hexadecimal encoded string to decode.
+            - `encoding` (optional): The character encoding to use (default is 'utf-8').\n
+        ---
+        ### Return
+            - The decoded string.\n
+        ---
+        ### Exceptions
+            - If an error occurs during decoding.\n
+        '''
+        try:
+            decoded_string = bytes.fromhex(encoded_text).decode(encoding=encoding)
+            return decoded_string
+        except Exception as e:
+            raise Exception(f"Something whent wrong: {e}")
+
+    @staticmethod
+    def binary_encode(plaintext: str, as_bytes: bool = False, chunk_size: int = 8) -> str | bytes:
+        '''
+        ## Binary Encode
+        ---
+        ### Description
+        Encode the given plaintext into binary representation.\n
+        ---
+        ### Arguments
+            - `plaintext`: The text to encode.
+            - `as_bytes` (optional): If `True`, return the encoded result as bytes (default is `False`).\n
+            - `chunk_size` (optional): Size of each binary chunk (default is 8 bits).\n
+        ---
+        ### Return
+            - The binary encoded result as a string or bytes.\n
+        ---
+        ### Exceptions
+            - If an error occurs during encoding.\n
+        '''
+        try:
+            if as_bytes:
+                encoded_bytes = bytes([ord(char) for char in plaintext])
+            else:
+                binary_str = ''.join(format(ord(char), '08b') for char in plaintext)
+                if chunk_size > 0:
+                    chunks = [binary_str[i:i+chunk_size] for i in range(0, len(binary_str), chunk_size)]
+                    encoded_bytes = ' '.join(chunks)
+                else:
+                    encoded_bytes = binary_str
+            return encoded_bytes
+        except Exception as e:
+            raise Exception(f"Something went wrong: {e}")
+
+    @staticmethod
+    def binary_decode(encoded_text: str | bytes, as_text: bool = True, chunk_size: int = 8) -> str:
+        '''
+        ## Binary Decode
+        ---
+        ### Description
+        Decode binary data into text representation.\n
+        ---
+        ### Arguments
+            - `encoded_text`: The binary data to decode (either string or bytes).
+            - `as_text` (optional): If `True`, return the decoded result as text (default is `True`).\n
+            - `chunk_size` (optional): Size of each binary chunk (default is 8 bits).\n
+        ---
+        ### Return
+            - The decoded result as text.\n
+        ---
+        ### Exceptions
+            - If an error occurs during decoding.\n
+        '''
+        try:
+            if isinstance(encoded_text, bytes):
+                binary_str = ''.join(format(byte, '08b') for byte in encoded_text)
+            else:
+                binary_str = ''.join(encoded_text.split())
+
+            if chunk_size > 0:
+                chunks = [binary_str[i:i+chunk_size] for i in range(0, len(binary_str), chunk_size)]
+            else:
+                chunks = [binary_str]
+
+            decoded_text = ''.join([chr(int(chunk, 2)) for chunk in chunks])
+
+            return decoded_text if as_text else decoded_text.encode('utf-8')
+        except Exception as e:
+            raise Exception(f"Something went wrong: {e}")
+
---
+++ packages/theeverythinglibrary/theeverythinglibrary-0.3.12-py3-none-any.whl/theeverythinglibrary/files.py
@@ -0,0 +1,487 @@
+import os
+import re
+import shutil
+import datetime
+
+class TELFileManager:
+    '''
+    ## File Manager
+    ---
+    This class provides utility functions for managing files and directories.
+
+    **Note:** This class is a work in progress and subject to further development.
+    '''
+
+    class File:
+        '''
+        ## File Class
+        ---
+        ### Description
+        Represents a file with various attributes such as name, extension, path, size, permissions, user ID, group ID,
+        last access time, last modification time, and creation time.\n
+        ---
+        ### Constructor
+        - `name`: The name of the file.
+        - `extension`: The file extension (not including the dot).
+        - `path`: The path to the file.
+        - `size`: The size of the file in bytes.
+        - `permissions`: The file's permissions or access rights.
+        - `user_id`: The user ID of the file owner.
+        - `group_id`: The group ID of the file owner's group.
+        - `last_access_time`: The timestamp of the last access to the file.
+        - `last_modification_time`: The timestamp of the last modification of the file.
+        - `creation_time`: The timestamp of the file's creation.
+        '''
+        def __init__(self, name, extension, file_type, path, size, permissions, user_id, group_id, last_access_time, last_modification_time, creation_time) -> None:
+            self.name = name
+            self.extension = extension
+            self.file_type = file_type
+            self.path = path
+            self.size = size
+            self.permissions = permissions
+            self.user_id = user_id
+            self.group_id = group_id
+            self.last_access_time = last_access_time
+            self.last_modification_time = last_modification_time
+            self.creation_time = creation_time
+        
+        def display(self):
+            '''
+            ## Display
+            ---
+            ### Description
+            Prints info about a `File` class to the console.\n
+            ---
+            ### Arguments
+                - None\n
+            ---
+            ### Return
+                - None.\n
+            ---
+            ### Exceptions
+                - None\n
+            '''
+            print(f'---------------------- | Info for "{self.name}" | ----------------------')
+            print(f'Name:                     {self.name}')
+            print(f'Extension:                {self.extension}')
+            print(f'File Type:                {self.file_type}')
+            print(f'Path:                     {self.path}')
+            print(f'Size:                     {self.size} bytes | {round(self.size / 1024, 4)} KB | {round(self.size / (1024 * 1024), 4)} MB | {round(self.size / (1024 * 1024 * 1024), 4)} GB')
+            print(f'Permissions:              {self.permissions}')
+            print(f'User ID:                  {self.user_id}')
+            print(f'Group ID:                 {self.group_id}')
+            print(f'Last Access Time:         {self.last_access_time}')
+            print(f'Last Modification Time:   {self.last_modification_time}')
+            print(f'Creation Time:            {self.creation_time}')
+        
+        def read(self):
+            '''
+            ## Read
+            ---
+            ### Description
+            Read the contents of a file form the `File` class.\n
+            ---
+            ### Arguments
+                - None\n
+            ---
+            ### Return
+                - Contents of the file.\n
+            ---
+            ### Exceptions
+                - If an error occurs during reading of the file.\n
+            '''
+            try:
+                if os.path.isfile(self.path):
+                    with open(self.path, 'r') as f:
+                        return f.read()
+            except Exception as e:
+                raise Exception(f"Something went wrong: {e}")
+    
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def create_dir(dir: str) -> str:
+        '''
+        ## Create Directory
+        ---
+        ### Description
+        Create a directory if it doesn't exist.\n
+        ---
+        ### Arguments
+            - `dir`: The directory path to create.\n
+        ---
+        ### Return
+            - The created directory path.\n
+        ---
+        ### Exceptions
+            - If an error occurs during directory creation.\n
+        '''
+        try:
+            os.makedirs(dir, exist_ok=True)
+            return dir
+        except Exception as e:
+            raise Exception(f"Something went wrong: {e}")
+
+    @staticmethod
+    def delete_dir(dir: str) -> bool:
+        '''
+        ## Delete Directory
+        ---
+        ### Description
+        Delete a directory and its contents.\n
+        ---
+        ### Arguments
+            - `dir`: The directory path to delete.\n
+        ---
+        ### Return
+            - `True` if the directory was deleted successfully, otherwise `False`.\n
+        ---
+        ### Exceptions
+            - If an error occurs during directory deletion.\n
+        '''
+        try:
+            if os.path.exists(dir) and os.path.isdir(dir):
+                shutil.rmtree(dir)
+                return True
+            return False
+        except Exception as e:
+            raise Exception(f"Something went wrong: {e}")
+
+    @staticmethod
+    def create_file(dir: str, name: str, type: str) -> None:
+        '''
+        ## Create File
+        ---
+        ### Description
+        Create a file in the specified directory.\n
+        ---
+        ### Arguments
+            - `dir`: The directory path where the file will be created.
+            - `name`: The name of the file.
+            - `type`: The file type (extension).\n
+        ---
+        ### Exceptions
+            - If the provided file name and type are not valid.\n
+        '''
+        dir = dir.replace("/", "\\")
+        type.replace(".", "")
+        if not re.match(r'^[a-zA-Z0-9_]+\.[a-z]+$', f'{name}.{type}'):
+            raise Exception(f'The file name "{name+"."+type}" is not a valid file name')
+        try:
+            with open(os.path.join(dir, f'{name}.{type}'), 'w+') as file:
+                file.close()
+        except Exception as e:
+            raise Exception(f"Something went wrong: {e}")
+    
+    @staticmethod
+    def delete_file(file: str) -> None:
+        '''
+        ## Delete File
+        ---
+        ### Description
+        Delete a file.\n
+        ---
+        ### Arguments
+            - `file`: The path of the file to delete.\n
+        ---
+        ### Exceptions
+            - If the provided path does not lead to a valid file.\n
+        '''
+        file = file.replace("/", "\\")
+        file_name = file.split('\\')[len(file.split('\\'))-1]
+        if not os.path.isfile(file):
+            raise Exception(f'"{file_name}" is not a valid file or path')
+        try:
+            os.remove(file)
+        except Exception as e:
+            raise Exception(f"Something went wrong: {e}")
+    
+    @staticmethod
+    def list_files(dir: str) -> list[str]:
+        '''
+        ## List Files
+        ---
+        ### Description
+        List all files in a directory.\n
+        ---
+        ### Arguments
+            - `dir`: The directory path to list files from.\n
+        ---
+        ### Return
+            - A list of file names in the directory.\n
+        ---
+        ### Exceptions
+            - If an error occurs during listing.\n
+        '''
+        try:
+            if os.path.exists(dir) and os.path.isdir(dir):
+                files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
+                return files
+            return []
+        except Exception as e:
+            raise Exception(f"Something went wrong: {e}")
+    
+    @staticmethod
+    def copy_directory(dir: str, dest_dir: str) -> bool:
+        '''
+        ## Copy Directory
+        ---
+        ### Description
+        Copy a directory and its contents to a destination directory.\n
+        ---
+        ### Arguments
+            - `dir`: The path of the directory to copy.
+            - `dest_dir`: The destination directory path.\n
+        ---
+        ### Return
+            - `True` if the directory was copied successfully, otherwise `False`.\n
+        ---
+        ### Exceptions
+            - If an error occurs during copying.\n
+        '''
+        dir = dir.replace("/", "\\")
+        try:
+            if os.path.exists(dir) and os.path.isdir(dir):
+                shutil.copytree(dir, os.path.join(dest_dir, os.path.basename(dir)))
+                return True
+            return False
+        except Exception as e:
+            raise Exception(f"Something went wrong: {e}")
+    
+    @staticmethod
+    def move_file(file: str, dest_dir: str) -> bool:
+        '''
+        ## Move File
+        ---
+        ### Description
+        Move a file to a destination directory.\n
+        ---
+        ### Arguments
+            - `file`: The path of the file to move.
+            - `dest_dir`: The destination directory path.\n
+        ---
+        ### Return
+            - `True` if the file was moved successfully, otherwise `False`.\n
+        ---
+        ### Exceptions
+            - If an error occurs during moving.\n
+        '''
+        file = file.replace("/", "\\")
+        file_name = file.split('\\')[len(file.split('\\'))-1]
+        path_to_file = file.replace(file_name, "")
+        try:
+            if os.path.exists(path_to_file) and os.path.isfile(file):
+                shutil.move(file, dest_dir)
+                return True
+            return False
+        except Exception as e:
+            raise Exception(f"Something went wrong: {e}")
+
+    @staticmethod
+    def move_directory(dir: str, dest_dir: str) -> bool:
+        '''
+        ## Move Directory
+        ---
+        ### Description
+        Move a directory and its contents to a destination directory.\n
+        ---
+        ### Arguments
+            - `dir`: The path of the directory to move.
+            - `dest_dir`: The destination directory path.\n
+        ---
+        ### Return
+            - `True` if the directory was moved successfully, otherwise `False`.\n
+        ---
+        ### Exceptions
+            - If an error occurs during moving.\n
+        '''
+        dir = dir.replace("/", "\\")
+        try:
+            if os.path.exists(dir) and os.path.isdir(dir):
+                shutil.move(dir, dest_dir)
+                return True
+            return False
+        except Exception as e:
+            raise Exception(f"Error moving directory: {e}")
+    
+    @staticmethod
+    def search(dir: str, extensions: list[str] = None, keywords: list[str] = None,
+               exclude_extensions: list[str] = None, exclude_keywords: list[str] = None,
+               exclude_dirs: list[str] = None, depth: int = None,
+               min_size: int = None, max_size: int = None,
+               list_dirs: bool = False, divider: str = "\\",
+               last_item: bool = False):
+        '''
+        ## Search Files
+        ---
+        ### Description
+        Recursively search for files in a directory based on specified criteria.\n
+        ---
+        ### Arguments
+            - `dir`: The directory in which to start the search.
+            - `extensions`: A list of file extensions to filter by (optional).
+            - `keywords`: A list of keywords to filter files by name (optional).
+            - `exclude_extensions`: A list of file extensions to exclude (optional).
+            - `exclude_keywords`: A list of keywords to exclude from file names (optional).
+            - `exclude_dirs`: A list of directory names to exclude (optional).
+            - `depth`: The maximum depth of recursion in the file structure (optional).
+            - `min_size`: The minimum file size in bytes (optional).
+            - `max_size`: The maximum file size in bytes (optional).
+            - `list_dirs`: Whether to list directories even if there are no files in them (optional). ! PERFORMANCE ISSUES !
+            - `last_item`: Whether to list the last item when depth is used (optional).
+            - `divider`: The directory separator character (optional).
+        ---
+        ### Return
+            - A list of file paths that meet the specified search criteria.\n
+        ---
+        ### Exceptions
+            - If an error occurs during searching.\n
+        '''
+        try:
+            if depth is not None and not isinstance(depth, int):
+                raise TypeError(f'Depth must be "int" not "{type(depth).__name__}"')
+
+            def is_valid_extension(extension):
+                return re.match(r'^[a-zA-Z0-9]+$', extension)
+
+            if extensions:
+                extensions = [ext.lstrip('.').lower() for ext in extensions if is_valid_extension(ext)]
+            if exclude_extensions:
+                exclude_extensions = [ext.lstrip('.').lower() for ext in exclude_extensions if is_valid_extension(ext)]
+
+            def should_exclude(file):
+                file_extension = os.path.splitext(file)[1].lower()
+                return (exclude_extensions and file_extension.lstrip('.') in exclude_extensions) or \
+                    (exclude_keywords and any(keyword in file for keyword in exclude_keywords))
+
+            def should_include(file):
+                file_extension = os.path.splitext(file)[1].lower()
+                return (not extensions or file_extension.lstrip('.') in extensions) and \
+                    (not keywords or all(keyword in file for keyword in keywords)) and \
+                    (min_size is None or os.path.getsize(file) >= min_size) and \
+                    (max_size is None or os.path.getsize(file) <= max_size)
+
+            max_depth = depth - 1 if depth is not None else None
+
+            def file_generator(root):
+                try:
+                    for entry in os.scandir(root):
+                        if entry.is_file():
+                            file_path = entry.path
+                            if should_include(file_path) and not should_exclude(file_path):
+                                yield normalize_path(file_path)
+                        elif entry.is_dir():
+                            if max_depth is None or root[len(dir):].count(os.path.sep) <= max_depth:
+                                if not exclude_dirs or not any(exclude_dir in entry.name for exclude_dir in exclude_dirs):
+                                    yield from file_generator(entry.path)
+                except PermissionError:
+                    return
+
+            def dir_generator(root):
+                try:
+                    for entry in os.scandir(root):
+                        if entry.is_dir():
+                            if max_depth is None or root[len(dir):].count(os.path.sep) <= max_depth:
+                                if not exclude_dirs or not any(exclude_dir in entry.name for exclude_dir in exclude_dirs):
+                                    if last_item:
+                                        yield normalize_path(entry.path)
+                                    else:
+                                        yield from dir_generator(entry.path)
+                except PermissionError:
+                    return
+
+            normalize_path = lambda path: path.replace("\\", divider)
+
+            found_files = []
+            found_dirs = []
+
+            for entry in os.scandir(dir):
+                if entry.is_file():
+                    file_path = entry.path
+                    if should_include(file_path) and not should_exclude(file_path):
+                        found_files.append(normalize_path(file_path))
+                elif entry.is_dir():
+                    if not list_dirs:
+                        continue
+                    if max_depth is None or entry.path[len(dir):].count(os.path.sep) <= max_depth:
+                        if not exclude_dirs or not any(exclude_dir in entry.name for exclude_dir in exclude_dirs):
+                            if last_item:
+                                found_dirs.append(normalize_path(entry.path))
+                            else:
+                                found_dirs.extend(dir_generator(entry.path))
+
+            return found_files + found_dirs
+
+        except Exception as e:
+            raise Exception(f"Something went wrong: {e}")
+
+    def info(self, file) -> File:
+        '''
+        ## File info
+        ---
+        ### Description
+        Get info about a file.\n
+        ---
+        ### Arguments
+            - `file`: The file to get the info from.\n
+        ---
+        ### Return
+            - A list of file paths that meet the specified search criteria.\n
+        ---
+        ### Exceptions
+            - If an error occurs during searching.\n
+        '''
+        if not os.path.exists(file):
+            raise Exception(f'"{file}" is not a valid file or the file was not found.')
+        
+        stat_info = os.stat(file)
+
+        file_info = self.File(
+            name = os.path.basename(file),
+            extension = str(os.path.basename(file)).split(".")[-1] if os.path.isfile(os.path.abspath(file)) else "Folder",
+            file_type = "File" if os.path.isfile(os.path.abspath(file)) else "Folder",
+            path = os.path.abspath(file),
+            size = stat_info.st_size,
+            permissions = stat_info.st_mode,
+            user_id = stat_info.st_uid,
+            group_id = stat_info.st_gid,
+            last_access_time = datetime.datetime.fromtimestamp(stat_info.st_atime).strftime('%Y-%m-%d %H:%M:%S'),
+            last_modification_time = datetime.datetime.fromtimestamp(stat_info.st_mtime).strftime('%Y-%m-%d %H:%M:%S'),
+            creation_time = datetime.datetime.fromtimestamp(stat_info.st_ctime).strftime('%Y-%m-%d %H:%M:%S'),
+        )
+
+        return file_info
+    
+    @staticmethod
+    def convert(bytes: int, unit: str, decimals: int = 4) -> float:
+        '''
+        ## Convert Bytes
+        ---
+        ### Description
+        Converts a given number of bytes into specified units (e.g., kilobytes, megabytes, gigabytes) with a specified number of decimal places.\n
+        ---
+        ### Arguments
+        - `bytes`: The number of bytes to convert.
+        - `unit`: The target unit to convert to (options: 'bytes', 'kb', 'mb', 'gb').
+        - `decimals`: The number of decimal places in the result (optional).\n
+        ---
+        ### Return
+        - The converted size in the specified unit with the specified number of decimal places.\n
+        ---
+        ### Exceptions
+        - `Exception`: Raised if an invalid unit is provided.\n
+        '''
+        unit = unit.lower().strip()
+        conversion_factors = {
+            'bytes': 1,
+            'kb': 1024,
+            'mb': 1024 * 1024,
+            'gb': 1024 * 1024 * 1024
+        }
+
+        if unit not in conversion_factors:
+            raise Exception(f'"{unit}" is not a valid unit. Units: (bytes, kb, mb, gb)')
+
+        converted_size = bytes / conversion_factors[unit]
+        return round(converted_size, decimals)
