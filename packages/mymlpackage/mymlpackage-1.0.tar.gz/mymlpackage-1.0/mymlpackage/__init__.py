e30180207dc24f64f2700137195c2610dac50cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:09:21 +0000

    Add package mymlpackage/1.0/mymlpackage-1.0.tar.gz

---
+++ packages/mymlpackage/mymlpackage-1.0.tar.gz/mymlpackage-1.0/mymlpackage/__init__.py
@@ -0,0 +1,7 @@
+"""mymlpackage - init module - A collection of data science functions"""
+from .PCA import PCA
+from .svd import svd, svd_eigendecomposition
+from .t_sne import compute_pairwise_distances, compute_similarity_matrix, t_sne
+from .k_medoids import KMedoids
+from .k_means import KMeans
+from .categorical_scatter import categorical_scatter_2d
