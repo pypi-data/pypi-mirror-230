9e75e4f9d623a55c8c82c6e3fade4ddf2f181a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:07:34 +0000

    Add package pylyzer/0.0.47/pylyzer-0.0.47-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/pylyzer/pylyzer-0.0.47-cp38-cp38-macosx_11_0_arm64.whl/.erg/lib/std/_erg_operators.py
@@ -0,0 +1,30 @@
+def __add__(x, y):
+    return x + y
+def __sub__(x, y):
+    return x - y
+def __mul__(x, y):
+    return x * y
+def __div__(x, y):
+    return x / y
+def __floordiv__(x, y):
+    return x // y
+def __mod__(x, y):
+    return x % y
+def __pow__(x, y):
+    return x ** y
+def __lshift__(x, y):
+    return x << y
+def __rshift__(x, y):
+    return x >> y
+def __and__(x, y):
+    return x & y
+def __or__(x, y):
+    return x | y
+def __xor__(x, y):
+    return x ^ y
+def __pos__(x):
+    return +x
+def __neg__(x):
+    return -x
+def __invert__(x):
+    return ~x
