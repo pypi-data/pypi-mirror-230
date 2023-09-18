f6a159df099165c0aa2c0146e909872fa91a1705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:07:33 +0000

    Add package engineerai/2.0/engineerai-2.0-py3-none-any.whl

---
+++ packages/engineerai/engineerai-2.0-py3-none-any.whl/EngineerAi/EngineerAi.py
@@ -0,0 +1,26 @@
+from .api import text_to_speech, transcribe_audio, ask_question, generate_sound, text_to_frequencies, text_to_sound, frequencies_to_text
+
+class EngineerAi():
+	def __init__(self):
+		pass
+	def transcribe_audio(self, lang, audio_file):
+		return transcribe_audio(lang, audio_file)
+	def text_to_speech(self, text, lang, filename):
+		text_to_speech(text, lang, filename)
+		return True
+	def chatai(self, key, question):
+		return ask_question(key, question)
+	def generate_sound(self, frequencies, duration_ms, output_file):
+		generate_sound(frequencies, duration_ms, output_file)
+		return True
+	def text_to_frequencies(self, text, filename):
+		text_to_frequencies(text, filename)
+		return True
+	def text_to_frequencies(self, text):
+		return text_to_frequencies(text)
+	def text_to_sound(self, text, duration_ms, output_file):
+		text_to_sound(text, duration_ms, output_file)
+		return True
+	def frequencies_to_text(self, frequencies):
+		return frequencies_to_text(frequencies)
+	
