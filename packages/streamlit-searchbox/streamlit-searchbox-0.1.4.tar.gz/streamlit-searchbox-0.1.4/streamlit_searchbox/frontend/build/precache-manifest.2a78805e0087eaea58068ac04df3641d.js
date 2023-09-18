9c748b1ca82921f745cde825b95e897113b8f4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:06:25 +0000

    Add package streamlit-searchbox/0.1.4/streamlit-searchbox-0.1.4.tar.gz

---
+++ packages/streamlit-searchbox/streamlit-searchbox-0.1.4.tar.gz/streamlit-searchbox-0.1.4/streamlit_searchbox/frontend/build/precache-manifest.2a78805e0087eaea58068ac04df3641d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de464506a8fd1319e3204dc6e714d9ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "896ea15d566adc5aaeb9",
+    "url": "./static/js/2.1e75748d.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.1e75748d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6a1b76ee6b824fb356",
+    "url": "./static/js/main.3d532e07.chunk.js"
+  },
+  {
+    "revision": "6a4557bc8e92ab112127",
+    "url": "./static/js/runtime-main.1401e97d.js"
+  },
+  {
+    "revision": "6dbfe07fce4016ff25788901d8ba665d",
+    "url": "./static/media/st-arrow.6dbfe07f.svg"
+  },
+  {
+    "revision": "53d62d3c3945ab1a4ca800d49a13038f",
+    "url": "./static/media/st-clear.53d62d3c.svg"
+  }
+]);
