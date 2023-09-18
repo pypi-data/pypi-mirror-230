650a0d1a656e9426e207c8bea2d84cf75dec0fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:10:56 +0000

    Add package lcblp/0.0.3/lcblp-0.0.3.tar.gz

---
+++ packages/lcblp/lcblp-0.0.3.tar.gz/lcblp-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lcblp", ## 소문자 영단어
+    version="0.0.3", ##
+    author="Lee Changbok", ## ex) Sunkyeong Lee
+    author_email="lcvrjqnr@gmail.com", ##
+    description="LAON PEOPLE", ##
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/halfTaim/lcblp.git", ##
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
