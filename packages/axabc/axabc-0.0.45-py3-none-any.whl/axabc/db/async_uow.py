3c6a0ffd56cf6d627098e1b36e90f8b6bd64e17f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:09:45 +0000

    Add package axabc/0.0.45/axabc-0.0.45-py3-none-any.whl

---
+++ packages/axabc/axabc-0.0.45-py3-none-any.whl/axabc/db/async_uow.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from typing import Type
+
+from .abstract_uow import AbstractUOW
+from .repo_collector import BaseRepoCollector
+
+
+class AbstractUOWFactory(ABC):
+    def __init__(self, session_maker, repo: Type[BaseRepoCollector]) -> None:
+        self.session_maker = session_maker
+        self.repo = repo
+
+    @abstractmethod
+    def __call__(self) -> AbstractUOW:
+        raise NotImplementedError
+
