32537b2d195926072007ac3b14f095e622b14344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:09:24 +0000

    Add package sentencex/0.3.0/sentencex-0.3.0-py3-none-any.whl

---
+++ packages/sentencex/sentencex-0.3.0-py3-none-any.whl/sentencex/languages/__init__.py
@@ -0,0 +1,56 @@
+from .ar import Arabic
+from .bg import Bulgarian
+from .bn import Bengali
+from .ca import Catalan
+from .da import Danish
+from .de import Deutsch
+from .el import Greek
+from .en import English
+from .fr import French
+from .gu import Gujarati
+from .hi import Hindi
+from .hy import Armenian
+from .it import Italian
+from .kk import Kazakh
+from .kn import Kannada
+from .ml import Malayalam
+from .mr import Marathi
+from .my import Burmese
+from .nl import Dutch
+from .or_ import Odia
+from .pa import Punjabi
+from .pl import Polish
+from .ru import Russian
+from .sk import Slovak
+from .ta import Tamil
+from .te import Telugu
+
+__all__ = [
+    "Arabic",
+    "Armenian",
+    "Bengali",
+    "Bulgarian",
+    "Burmese",
+    "Catalan",
+    "Danish",
+    "Deutsch",
+    "Dutch",
+    "English",
+    "French",
+    "Gujarati",
+    "Greek",
+    "Hindi",
+    "Italian",
+    "Kannada",
+    "Kazakh",
+    "Malayalam",
+    "Marathi",
+    "Odia",
+    "Punjabi",
+    "Polish",
+    "Russian",
+    "Slovak",
+    "Spanish",
+    "Tamil",
+    "Telugu",
+]
