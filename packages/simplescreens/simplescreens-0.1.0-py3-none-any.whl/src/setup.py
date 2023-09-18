3a2ac89454826eb546aaea620a894b962f3c67b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 14:11:01 +0000

    Add package simplescreens/0.1.0/simplescreens-0.1.0-py3-none-any.whl

---
+++ packages/simplescreens/simplescreens-0.1.0-py3-none-any.whl/src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='simplescreens',
+    version='0.1.0',
+    description='Simple text-based UI (text screens) used for tree-like data browsing',
+    author='Arkadiusz Bryndza',
+    author_email='arbryn@gmail.com',
+    packages=['simplescreens']
+)
