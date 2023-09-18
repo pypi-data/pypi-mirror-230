ee7f1a0723ada860567d26accddfb83a5e177bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 14:09:51 +0000

    Add package pygaze/1.2.1/pygaze-1.2.1-py3-none-any.whl

---
+++ packages/pygaze/pygaze-1.2.1-py3-none-any.whl/pygaze/__init__.py
@@ -0,0 +1,2 @@
+from .pygaze import PyGaze, PyGazeRenderer
+from .gaze_estimator import GazeEstimator
---
+++ packages/pygaze/pygaze-1.2.1-py3-none-any.whl/pygaze/utils.py
@@ -0,0 +1,17 @@
+import bz2
+import logging
+import operator
+import pathlib
+import tempfile
+from loguru import logger
+import cv2
+import torch.hub
+import yaml
+from omegaconf import DictConfig
+
+from .common.face_model import FaceModel
+from .common.face_model_mediapipe import FaceModelMediaPipe
+
+
+def get_3d_face_model(config: DictConfig) -> FaceModel:
+	return FaceModelMediaPipe()
