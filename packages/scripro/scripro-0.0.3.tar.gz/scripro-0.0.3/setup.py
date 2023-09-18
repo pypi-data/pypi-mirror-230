9eccadb87494687e118c7114257c5af0b39961a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:06:44 +0000

    Add package scripro/0.0.3/scripro-0.0.3.tar.gz

---
+++ packages/scripro/scripro-0.0.3.tar.gz/scripro-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import glob
+import os
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="scripro",
+    version="0.0.3",
+    python_requires=">=3.8",
+    keywords="Single-Cell Gene Regulatory Network Inference using ChIP-seq for Multi-omics",
+    url="https://github.com/xuyunfan9991/SCRIPro",
+    license="GPL-3.0+",
+    packages=['scripro'],
+    install_requires=requirements,
+    include_package_data=True,
+    data_files=[("", ["requirements.txt", "hg38.genome", "mm10.genome"])],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+    
+)
