32984cd9bd9d6eb79428b97c38fe1dbee099ff82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:07:50 +0000

    Add package baseweb-plugin-template/0.1.0/baseweb-plugin-template-0.1.0.tar.gz

---
+++ packages/baseweb-plugin-template/baseweb-plugin-template-0.1.0.tar.gz/baseweb-plugin-template-0.1.0/setup.py
@@ -0,0 +1,64 @@
+import os
+import re
+import setuptools
+
+NAME             = "baseweb-plugin-template"
+AUTHOR           = "Christophe VG"
+AUTHOR_EMAIL     = "contact@christophe.vg"
+DESCRIPTION      = "an example baseweb plugin"
+LICENSE          = "MIT"
+KEYWORDS         = "example baseweb plugin"
+URL              = "https://github.com/christophevg/" + NAME
+README           = "README.md"
+CLASSIFIERS      = [
+  "Programming Language :: Python",
+  "Intended Audience :: Developers",
+  "Development Status :: 4 - Beta",
+  "License :: OSI Approved :: MIT License",
+  "Environment :: Plugins",
+  "Environment :: Web Environment",
+  "Programming Language :: Python :: 3",
+  
+]
+INSTALL_REQUIRES = [
+  "baseweb",
+  
+]
+ENTRY_POINTS = {
+  
+}
+SCRIPTS = [
+  
+]
+
+HERE = os.path.dirname(__file__)
+
+def read(file):
+  with open(os.path.join(HERE, file), "r") as fh:
+    return fh.read()
+
+VERSION = re.search(
+  r'__version__ = [\'"]([^\'"]*)[\'"]',
+  read(NAME.replace("-", "_") + "/__init__.py")
+).group(1)
+
+LONG_DESCRIPTION = read(README)
+
+if __name__ == "__main__":
+  setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    packages=setuptools.find_packages(),
+    author=AUTHOR,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license=LICENSE,
+    keywords=KEYWORDS,
+    url=URL,
+    classifiers=CLASSIFIERS,
+    install_requires=INSTALL_REQUIRES,
+    entry_points=ENTRY_POINTS,
+    scripts=SCRIPTS,
+    include_package_data=True    
+  )
