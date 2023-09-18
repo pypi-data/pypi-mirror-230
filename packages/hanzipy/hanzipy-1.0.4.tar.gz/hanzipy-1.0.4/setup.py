d4b75204d39dd737bdfaa9ad9cf33ed915cee790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:06:54 +0000

    Add package hanzipy/1.0.4/hanzipy-1.0.4.tar.gz

---
+++ packages/hanzipy/hanzipy-1.0.4.tar.gz/hanzipy-1.0.4/setup.py
@@ -0,0 +1,46 @@
+from setuptools import find_packages, setup
+
+from hanzipy import __version__ as version  # noqa  # isort:skip
+
+DESCRIPTION = "Hanzi decomposition and dictionary"
+EXTRAS_DEV_TEST = [
+    "coverage",
+    "pytest>=3.10",
+]
+
+# Setting up
+setup(
+    name="hanzipy",
+    version=version,
+    author="昆汀",
+    author_email="synkx@hotmail.fr",
+    description=DESCRIPTION,
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Synkied/hanzipy",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    extras_require={
+        "dev": EXTRAS_DEV_TEST,
+        "dev-test": EXTRAS_DEV_TEST,
+    },
+    download_url=("https://github.com/Synkied/hanzipy/archive/%s.tar.gz" % version),
+    keywords=[
+        "python",
+        "hanzi",
+        "hanzipy",
+        "decomposition",
+        "cjk",
+        "dictionary",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+    ],
+)
