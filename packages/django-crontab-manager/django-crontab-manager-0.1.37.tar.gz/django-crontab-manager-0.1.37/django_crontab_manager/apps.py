7c9a109b0d71486f68ff6434b7e518638f964244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:09:30 +0000

    Add package django-crontab-manager/0.1.37/django-crontab-manager-0.1.37.tar.gz

---
+++ packages/django-crontab-manager/django-crontab-manager-0.1.37.tar.gz/django-crontab-manager-0.1.37/django_crontab_manager/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+class DjangoCrontabManagerConfig(AppConfig):
+    name = 'django_crontab_manager'
+    verbose_name = _("Django Crontab Manager")
+
+    def ready(self):
+        from django_data_import_management.models import register_django_data_import_workflow
+        from .models import ScheduleImportWorkflow
+
+        register_django_data_import_workflow(_("Import Schedules"), ScheduleImportWorkflow)
