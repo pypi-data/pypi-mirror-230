535b2e91190722c5d7aeb20b12994df7ee01c002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:06:22 +0000

    Add package runtimepy/2.9.2/runtimepy-2.9.2-py3-none-any.whl

---
+++ packages/runtimepy/runtimepy-2.9.2-py3-none-any.whl/runtimepy/__init__.py
@@ -0,0 +1,16 @@
+# =====================================
+# generator=datazen
+# version=3.1.3
+# hash=654842c7a2fb5a8197ba11ccd211f613
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "2.9.2"
+
+# runtimepy-specific content.
+METRICS_NAME = "metrics"
