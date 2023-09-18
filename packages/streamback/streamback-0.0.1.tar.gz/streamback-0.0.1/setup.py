49493a551efbe043bec4130087bcf93b002a6445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 14:11:10 +0000

    Add package streamback/0.0.1/streamback-0.0.1.tar.gz

---
+++ packages/streamback/streamback-0.0.1.tar.gz/streamback-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="streamback",
+    version="0.0.1",
+    author='Stefanos Liakis',
+    author_email='stliakis@gmail.com',
+    description="2-way streams for your microservices",
+    packages=find_packages(),
+    install_requires=[
+        "redis>=2.10.6", "confluent-kafka>=1.6.0"
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
