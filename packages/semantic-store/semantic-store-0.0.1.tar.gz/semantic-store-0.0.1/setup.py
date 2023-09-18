9007327c5bc9c14607da3c8f985b02c04d7b24d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:06:26 +0000

    Add package semantic-store/0.0.1/semantic-store-0.0.1.tar.gz

---
+++ packages/semantic-store/semantic-store-0.0.1.tar.gz/semantic-store-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import os
+import pkg_resources
+from setuptools import setup, find_packages
+
+requirements_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
+
+if os.path.isfile(requirements_path):
+    with open(requirements_path) as f:
+        requirements = [str(r) for r in pkg_resources.parse_requirements(f)]
+else:
+    requirements = []
+
+setup(
+    name="semantic-store",
+    version="0.0.1",
+    description="An in-memory vector store for semantic data storage and retrieval",
+    author="Pragnesh Barik",
+    packages=find_packages(exclude=["tests*"]),
+    keywords=['in-memory', 'vector', 'database', 'semantic', 'search'],
+    install_requires=requirements,
+    package_dir={'': 'src'},
+    include_package_data=True,
+
+)
