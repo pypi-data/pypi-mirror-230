cd91773355cceaa1b77a7852515e5a4480a47e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 03:06:48 +0000

    Add package hgtk/0.2.1/hgtk-0.2.1-py2.py3-none-any.whl

---
+++ packages/hgtk/hgtk-0.2.1-py2.py3-none-any.whl/hgtk/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import unicode_literals
+
+from . import checker
+from . import josa
+from . import letter
+from . import text
