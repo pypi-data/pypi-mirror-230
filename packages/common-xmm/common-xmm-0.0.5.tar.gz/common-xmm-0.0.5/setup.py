29775ddf0c6fae018b05b4874f67d97e1d33d621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-18 15:10:06 +0000

    Add package common-xmm/0.0.5/common-xmm-0.0.5.tar.gz

---
+++ packages/common-xmm/common-xmm-0.0.5.tar.gz/common-xmm-0.0.5/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="common-xmm",
+    version="0.0.5",
+    author="剑爆是十条街",
+    author_email="author@example.com",
+    description="A small example package",
+    packages=["io"],
+    clean={"all": True}
+)
