0a34832520d40d8305c27b40d80ead5e31341bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:52:42 +0000

    Add package rec-model-pipeline/0.0.1/rec-model-pipeline-0.0.1.tar.gz

---
+++ packages/rec-model-pipeline/rec-model-pipeline-0.0.1.tar.gz/rec-model-pipeline-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='rec-model-pipeline',
+                 version='0.0.1',
+                 description='common_rec_pipeline_keras',
+                 url='https://git.woa.com/gcd/game_indust_v3/tree/master/rcmd_models/common_rec_pipeline_keras',
+                 author='hayzheng',
+                 author_email='zhenghangcx@163.com',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
